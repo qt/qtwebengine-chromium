0e730e9dbe8d0efbae8b4b66f269f1a933cc6666
Jocelyn Turcotte <jocelyn.turcotte@digia.com>
2013-06-11 15:44:26 +0200

    Add WebEngine classes as RunLoop/MessageLoop friends.
    Change-Id: I2bb2600637ac5a3473273789cd61c257d682b702
    Reviewed-by: Andras Becsi <andras.becsi@theqtcompany.com>
    Reviewed-by: Jocelyn Turcotte (Woboq GmbH) <jturcotte@woboq.com>
    Reviewed-by: Michael Brüning <michael.bruning@theqtcompany.com>

--- chromium/base/task/sequence_manager/sequence_manager_impl.h
+++ chromium/base/task/sequence_manager/sequence_manager_impl.h
@@ -40,6 +40,11 @@
 #include "base/values.h"
 #include "build/build_config.h"
 
+namespace QtWebEngineCore {
+class MessageLoopForUIQt;
+class WebContentsAdapter;
+}
+
 namespace base {
 
 namespace trace_event {
@@ -206,6 +211,8 @@ class BASE_EXPORT SequenceManagerImpl
 
   friend class internal::TaskQueueImpl;
   friend class ::base::sequence_manager::SequenceManagerForTest;
+  friend class QtWebEngineCore::MessagePumpForUIQt;
+  friend class QtWebEngineCore::WebContentsAdapter;
 
  private:
   class NativeWorkHandleImpl;
