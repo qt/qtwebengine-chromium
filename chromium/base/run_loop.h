7445a1091151113dc93dd73329855093b4c73c27
Jocelyn Turcotte <jocelyn.turcotte@digia.com>
2013-06-11 15:44:26 +0200

    <chromium> Add WebEngineContext to RunLoop's friends.
    Change-Id: I2bb2600637ac5a3473273789cd61c257d682b702
    Reviewed-by: Andras Becsi <andras.becsi@theqtcompany.com>
    Reviewed-by: Jocelyn Turcotte (Woboq GmbH) <jturcotte@woboq.com>
    Reviewed-by: Michael Brüning <michael.bruning@theqtcompany.com>

--- chromium/base/run_loop.h
+++ chromium/base/run_loop.h
@@ -12,6 +12,10 @@
 #include "base/message_loop/message_loop.h"
 #include "build/build_config.h"
 
+namespace QtWebEngineCore {
+class WebEngineContext;
+}
+
 namespace base {
 #if defined(OS_ANDROID)
 class MessagePumpForUI;
@@ -67,6 +71,7 @@ class BASE_EXPORT RunLoop {
 
  private:
   friend class MessageLoop;
+  friend class QtWebEngineCore::WebEngineContext;
 #if defined(OS_ANDROID)
   // Android doesn't support the blocking MessageLoop::Run, so it calls
   // BeforeRun and AfterRun directly.
