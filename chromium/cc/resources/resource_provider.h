df91af4f1c415604129323cf54bd78ed4704ee3e
Jocelyn Turcotte <jocelyn.turcotte@digia.com>
2013-09-06 17:08:45 +0200

    <chromium> Solve conflicts when including both QtOpenGL headers and resource_provider.h
    If resource_provider.h is going to be used to integrate delegated frame
    to external compositors, it should avoid conflicting with the system's gl.h.
    
    Change-Id: Ie37d6cde263157e6717cc006a0896f0b9ebdaa5d

--- chromium/cc/resources/resource_provider.h
+++ chromium/cc/resources/resource_provider.h
@@ -25,8 +25,6 @@
 #include "cc/resources/single_release_callback.h"
 #include "cc/resources/texture_mailbox.h"
 #include "cc/resources/transferable_resource.h"
-#include "third_party/khronos/GLES2/gl2.h"
-#include "third_party/khronos/GLES2/gl2ext.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "ui/gfx/size.h"
@@ -36,6 +34,9 @@ namespace gles {
 class GLES2Interface;
 }
 }
+// A correct fix would be not to use GL types in this interal API file.
+typedef unsigned int     GLenum;
+typedef int              GLint;
 
 namespace gfx {
 class Rect;
@@ -523,36 +524,6 @@ inline unsigned BitsPerPixel(ResourceFormat format) {
     4    // ETC1
   };
   return format_bits_per_pixel[format];
-}
-
-inline GLenum GLDataType(ResourceFormat format) {
-  DCHECK_LE(format, RESOURCE_FORMAT_MAX);
-  static const unsigned format_gl_data_type[RESOURCE_FORMAT_MAX + 1] = {
-    GL_UNSIGNED_BYTE,           // RGBA_8888
-    GL_UNSIGNED_SHORT_4_4_4_4,  // RGBA_4444
-    GL_UNSIGNED_BYTE,           // BGRA_8888
-    GL_UNSIGNED_BYTE,           // LUMINANCE_8
-    GL_UNSIGNED_SHORT_5_6_5,    // RGB_565,
-    GL_UNSIGNED_BYTE            // ETC1
-  };
-  return format_gl_data_type[format];
-}
-
-inline GLenum GLDataFormat(ResourceFormat format) {
-  DCHECK_LE(format, RESOURCE_FORMAT_MAX);
-  static const unsigned format_gl_data_format[RESOURCE_FORMAT_MAX + 1] = {
-    GL_RGBA,           // RGBA_8888
-    GL_RGBA,           // RGBA_4444
-    GL_BGRA_EXT,       // BGRA_8888
-    GL_LUMINANCE,      // LUMINANCE_8
-    GL_RGB,            // RGB_565
-    GL_ETC1_RGB8_OES   // ETC1
-  };
-  return format_gl_data_format[format];
-}
-
-inline GLenum GLInternalFormat(ResourceFormat format) {
-  return GLDataFormat(format);
 }
 
 }  // namespace cc
