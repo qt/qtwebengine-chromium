fae4995743c6f775fa142d8f709df425178ac39e
Jocelyn Turcotte <jocelyn.turcotte@digia.com>
2013-09-20 15:00:47 +0200

    <chromium> Add accessors for the Qt delegated renderer integration.
    This is needed to fetch the MessageLoop, the MailboxManager and the
    SyncPointManager of the GPU in-process host.
    
    Change-Id: I7f38e32b2df11da5b046f16643841d34260c11fb
    Reviewed-by: Andras Becsi <andras.becsi@digia.com>

--- chromium/content/gpu/gpu_child_thread.cc
+++ chromium/content/gpu/gpu_child_thread.cc
@@ -45,6 +45,8 @@ bool GpuProcessLogMessageHandler(int severity,
 
 }  // namespace
 
+GpuChildThread* GpuChildThread::instance_ = 0;
+
 GpuChildThread::GpuChildThread(GpuWatchdogThread* watchdog_thread,
                                bool dead_on_arrival,
                                const gpu::GPUInfo& gpu_info,
@@ -58,6 +60,8 @@ GpuChildThread::GpuChildThread(GpuWatchdogThread* watchdog_thread,
   target_services_ = NULL;
 #endif
   g_thread_safe_sender.Get() = thread_safe_sender();
+
+  instance_ = this;
 }
 
 GpuChildThread::GpuChildThread(const std::string& channel_id)
@@ -79,6 +83,8 @@ GpuChildThread::GpuChildThread(const std::string& channel_id)
     VLOG(1) << "gfx::GLSurface::InitializeOneOff failed";
 #endif
   g_thread_safe_sender.Get() = thread_safe_sender();
+
+  instance_ = this;
 }
 
 GpuChildThread::~GpuChildThread() {
