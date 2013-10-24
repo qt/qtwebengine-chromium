e1d08e6bd74dea9b9b18a74f36357cf42dcc35d8
Jocelyn Turcotte <jocelyn.turcotte@digia.com>
2013-10-24 18:26:59 +0200

    <chromium> Add seams to setup GL contexts sharing with QtQuick.
    This will allow us to know right before the first GL context is
    instantiated by Chromium so that we can install those contexts to
    be shared with QtQuick GL contexts as well.
    
    Change-Id: Id55337a7b42d25ac084698a23dbfab06cb273505
    Reviewed-by: Andras Becsi <andras.becsi@digia.com>

--- chromium/content/common/gpu/gpu_channel_manager.cc
+++ chromium/content/common/gpu/gpu_channel_manager.cc
@@ -11,6 +11,7 @@
 #include "content/common/gpu/gpu_messages.h"
 #include "content/common/gpu/sync_point_manager.h"
 #include "content/common/message_router.h"
+#include "content/public/browser/content_browser_client.h"
 #include "gpu/command_buffer/service/feature_info.h"
 #include "gpu/command_buffer/service/gpu_switches.h"
 #include "gpu/command_buffer/service/mailbox_manager.h"
@@ -131,7 +132,12 @@ void GpuChannelManager::OnEstablishChannel(int client_id, bool share_context) {
       DCHECK(!mailbox_manager_.get());
       mailbox_manager_ = new gpu::gles2::MailboxManager;
     }
-    share_group = share_group_.get();
+    // Qt: Ask the browser client at the top to manage the context sharing.
+    // This can only work with --in-process-gpu or --single-process.
+    if (GetContentClient()->browser() && GetContentClient()->browser()->GetInProcessGpuShareGroup())
+      share_group = GetContentClient()->browser()->GetInProcessGpuShareGroup();
+    else
+      share_group = share_group_.get();
     mailbox_manager = mailbox_manager_.get();
   }
 
