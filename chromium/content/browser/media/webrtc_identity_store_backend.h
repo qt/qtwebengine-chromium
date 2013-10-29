9dd610d7e4307655036ce03514ea8e611cd1e2fa
Zeno Albisser <zeno.albisser@digia.com>
2013-10-29 11:30:28 +0100

    <chromium> Do not forward declare UIResourceRequest, Identity and IdentityKey.
    Clang / libc++ does not support incomplete types in templates.
    See: http://clang.llvm.org/compatibility.html#undep_incomplete
    
    Change-Id: If160c458dfee1b0702bf119ebcf75adf73022b2f

--- chromium/content/browser/media/webrtc_identity_store_backend.h
+++ chromium/content/browser/media/webrtc_identity_store_backend.h
@@ -11,8 +11,7 @@
 #include "base/time/time.h"
 #include "sql/connection.h"
 #include "sql/meta_table.h"
-
-class GURL;
+#include "url/gurl.h"
 
 namespace base {
 class FilePath;
@@ -94,7 +93,44 @@ class WebRTCIdentityStoreBackend
   };
   struct PendingFindRequest;
   struct IdentityKey;
-  struct Identity;
+
+  struct IdentityKey {
+    IdentityKey(const GURL& origin, const std::string& identity_name)
+        : origin(origin), identity_name(identity_name) {}
+
+    bool operator<(const IdentityKey& other) const {
+      return origin < other.origin ||
+             (origin == other.origin && identity_name < other.identity_name);
+    }
+
+    GURL origin;
+    std::string identity_name;
+  };
+
+  struct Identity {
+    Identity(const std::string& common_name,
+             const std::string& certificate,
+             const std::string& private_key)
+        : common_name(common_name),
+          certificate(certificate),
+          private_key(private_key),
+          creation_time(base::Time::Now().ToInternalValue()) {}
+
+    Identity(const std::string& common_name,
+             const std::string& certificate,
+             const std::string& private_key,
+             int64 creation_time)
+        : common_name(common_name),
+          certificate(certificate),
+          private_key(private_key),
+          creation_time(creation_time) {}
+
+    std::string common_name;
+    std::string certificate;
+    std::string private_key;
+    int64 creation_time;
+  };
+
   typedef std::map<IdentityKey, Identity> IdentityMap;
 
   ~WebRTCIdentityStoreBackend();
