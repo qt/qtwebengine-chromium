d24b0bf5f5505774b0dfabfa0dc6cd3c1c26be86
boliu@chromium.org <boliu@chromium.org>
2014-01-15 12:54:58 +0000

    [Backport] cc: Add RendererCapabilitiesImpl
    Separate RendererCapabilities and RendererCapabilitiesImpl classes to
    separate out capabilities that are not needed on main thread to be
    not copied to main thread. This is to prepare for the future when main
    and impl RendererCapabilities copies can temporarily get out of sync,
    and there are less values to worry about.
    
    BUG=332616
    
    Review URL: https://codereview.chromium.org/133063003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@244870 0039d316-1c4b-4281-b951-d872f2087c98
    
    Conflicts:
    	cc/trees/layer_tree_impl.h
    
    Change-Id: I0580b6268d414e9ec46c7a154a4a3f659015dce7

--- chromium/cc/output/gl_renderer.cc
+++ chromium/cc/output/gl_renderer.cc
@@ -223,7 +223,7 @@ GLRenderer::~GLRenderer() {
   CleanupSharedObjects();
 }
 
-const RendererCapabilities& GLRenderer::Capabilities() const {
+const RendererCapabilitiesImpl& GLRenderer::Capabilities() const {
   return capabilities_;
 }
 
