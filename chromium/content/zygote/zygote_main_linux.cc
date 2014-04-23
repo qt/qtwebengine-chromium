073b4a6d14d1c00564429797baa952fa05f48170
Zeno Albisser <zeno.albisser@digia.com>
2014-04-23 16:54:47 +0200

    <chromium> Do not directly override libc symbols from the library.
    Instead we export _override symbols and add the symbol
    renaming directly into the QtWebEngineProcess source.
    
    Reviewed-by: Andras Becsi <andras.becsi@digia.com>
    Change-Id: I15049609dd77be6de2a2398f25a37fff1b6f5519

--- chromium/content/zygote/zygote_main_linux.cc
+++ chromium/content/zygote/zygote_main_linux.cc
@@ -181,8 +181,6 @@ static void InitLibcLocaltimeFunctions() {
 // references to localtime() will resolve to this function. Notice that we need
 // to set visibility attribute to "default" to export the symbol, as it is set
 // to "hidden" by default in chrome per build/common.gypi.
-__attribute__ ((__visibility__("default")))
-struct tm* localtime_override(const time_t* timep) __asm__ ("localtime");
 
 __attribute__ ((__visibility__("default")))
 struct tm* localtime_override(const time_t* timep) {
@@ -198,10 +196,6 @@ struct tm* localtime_override(const time_t* timep) {
     return g_libc_localtime(timep);
   }
 }
-
-// Use same trick to override localtime64(), localtime_r() and localtime64_r().
-__attribute__ ((__visibility__("default")))
-struct tm* localtime64_override(const time_t* timep) __asm__ ("localtime64");
 
 __attribute__ ((__visibility__("default")))
 struct tm* localtime64_override(const time_t* timep) {
@@ -219,10 +213,6 @@ struct tm* localtime64_override(const time_t* timep) {
 }
 
 __attribute__ ((__visibility__("default")))
-struct tm* localtime_r_override(const time_t* timep,
-                                struct tm* result) __asm__ ("localtime_r");
-
-__attribute__ ((__visibility__("default")))
 struct tm* localtime_r_override(const time_t* timep, struct tm* result) {
   if (g_am_zygote_or_renderer) {
     ProxyLocaltimeCallToBrowser(*timep, result, NULL, 0);
@@ -233,10 +223,6 @@ struct tm* localtime_r_override(const time_t* timep, struct tm* result) {
     return g_libc_localtime_r(timep, result);
   }
 }
-
-__attribute__ ((__visibility__("default")))
-struct tm* localtime64_r_override(const time_t* timep,
-                                  struct tm* result) __asm__ ("localtime64_r");
 
 __attribute__ ((__visibility__("default")))
 struct tm* localtime64_r_override(const time_t* timep, struct tm* result) {
