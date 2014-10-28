af9ec4cc373ea8eb88ad675c7846a7348619273d
Zeno Albisser <zeno.albisser@digia.com>
2014-10-28 17:04:20 +0100

    <third_party/WebKit> Fix building with MacOSX10.10.sdk.
    ScrollAnimatorMac.mm updated as done in WebKit:
    git-svn-id: http://svn.webkit.org/repository/webkit/trunk@164767
    268f45cc-cd09-0410-ab3c-d52691b4dbfc
    
    Change-Id: I6d9f864427a32192049792bb7395ae0abe1a07d6
    Reviewed-by: Andras Becsi <andras.becsi@theqtcompany.com>
    Reviewed-by: Michael Brüning <michael.bruning@theqtcompany.com>

--- chromium/third_party/WebKit/Source/platform/mac/ScrollAnimatorMac.mm
+++ chromium/third_party/WebKit/Source/platform/mac/ScrollAnimatorMac.mm
@@ -699,7 +699,7 @@ ScrollAnimatorMac::ScrollAnimatorMac(ScrollableArea* scrollableArea)
     if (ScrollbarThemeMacCommon::isOverlayAPIAvailable()) {
         m_scrollbarPainterControllerDelegate.adoptNS([[BlinkScrollbarPainterControllerDelegate alloc] initWithScrollableArea:scrollableArea]);
         m_scrollbarPainterController = [[[NSClassFromString(@"NSScrollerImpPair") alloc] init] autorelease];
-        [m_scrollbarPainterController.get() performSelector:@selector(setDelegate:) withObject:m_scrollbarPainterControllerDelegate.get()];
+        [m_scrollbarPainterController.get() performSelector:@selector(setDelegate:) withObject:(id)m_scrollbarPainterControllerDelegate.get()];
         [m_scrollbarPainterController.get() setScrollerStyle:ScrollbarThemeMacCommon::recommendedScrollerStyle()];
     }
 }
@@ -940,7 +940,7 @@ void ScrollAnimatorMac::didAddVerticalScrollbar(Scrollbar& scrollbar)
     ASSERT(!m_verticalScrollbarPainterDelegate);
     m_verticalScrollbarPainterDelegate.adoptNS([[BlinkScrollbarPainterDelegate alloc] initWithScrollbar:&scrollbar]);
 
-    [painter setDelegate:m_verticalScrollbarPainterDelegate.get()];
+    [painter setDelegate:(id)m_verticalScrollbarPainterDelegate.get()];
     [m_scrollbarPainterController.get() setVerticalScrollerImp:painter];
     if (scrollableArea()->inLiveResize())
         [painter setKnobAlpha:1];
@@ -975,7 +975,7 @@ void ScrollAnimatorMac::didAddHorizontalScrollbar(Scrollbar& scrollbar)
     ASSERT(!m_horizontalScrollbarPainterDelegate);
     m_horizontalScrollbarPainterDelegate.adoptNS([[BlinkScrollbarPainterDelegate alloc] initWithScrollbar:&scrollbar]);
 
-    [painter setDelegate:m_horizontalScrollbarPainterDelegate.get()];
+    [painter setDelegate:(id)m_horizontalScrollbarPainterDelegate.get()];
     [m_scrollbarPainterController.get() setHorizontalScrollerImp:painter];
     if (scrollableArea()->inLiveResize())
         [painter setKnobAlpha:1];
