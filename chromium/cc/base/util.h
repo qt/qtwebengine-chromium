ff829dc711fe934cb7db0511c257b0860f6d3432
Jocelyn Turcotte <jocelyn.turcotte@digia.com>
2014-11-05 18:55:49 +0100

    <chromium> Solve conflicts when including both QtOpenGL headers and resource_provider.h
    If resource_provider.h is going to be used to integrate delegated frame
    to external compositors, it should avoid conflicting with the system's
    gl.h.
    
    Change-Id: Ie37d6cde263157e6717cc006a0896f0b9ebdaa5d
    Reviewed-by: Andras Becsi <andras.becsi@theqtcompany.com>

--- chromium/cc/base/util.h
+++ chromium/cc/base/util.h
@@ -8,6 +8,9 @@
 #include <limits>
 
 #include "base/basictypes.h"
+#include "cc/resources/resource_provider.h"
+#include "third_party/khronos/GLES2/gl2.h"
+#include "third_party/khronos/GLES2/gl2ext.h"
 
 namespace cc {
 
@@ -24,6 +27,40 @@ template <typename T> T RoundDown(T n, T mul) {
   return (n > 0) ? (n / mul) * mul
                  : (n == 0) ? 0
                  : ((n - mul + 1) / mul) * mul;
+}
+
+inline GLenum GLDataType(ResourceFormat format) {
+  DCHECK_LE(format, RESOURCE_FORMAT_MAX);
+  static const unsigned format_gl_data_type[RESOURCE_FORMAT_MAX + 1] = {
+    GL_UNSIGNED_BYTE,           // RGBA_8888
+    GL_UNSIGNED_SHORT_4_4_4_4,  // RGBA_4444
+    GL_UNSIGNED_BYTE,           // BGRA_8888
+    GL_UNSIGNED_BYTE,           // ALPHA_8
+    GL_UNSIGNED_BYTE,           // LUMINANCE_8
+    GL_UNSIGNED_SHORT_5_6_5,    // RGB_565,
+    GL_UNSIGNED_BYTE,           // ETC1
+    GL_UNSIGNED_BYTE            // RED_8
+  };
+  return format_gl_data_type[format];
+}
+
+inline GLenum GLDataFormat(ResourceFormat format) {
+  DCHECK_LE(format, RESOURCE_FORMAT_MAX);
+  static const unsigned format_gl_data_format[RESOURCE_FORMAT_MAX + 1] = {
+    GL_RGBA,           // RGBA_8888
+    GL_RGBA,           // RGBA_4444
+    GL_BGRA_EXT,       // BGRA_8888
+    GL_ALPHA,          // ALPHA_8
+    GL_LUMINANCE,      // LUMINANCE_8
+    GL_RGB,            // RGB_565
+    GL_ETC1_RGB8_OES,  // ETC1
+    GL_RED_EXT         // RED_8
+  };
+  return format_gl_data_format[format];
+}
+
+inline GLenum GLInternalFormat(ResourceFormat format) {
+  return GLDataFormat(format);
 }
 
 }  // namespace cc
