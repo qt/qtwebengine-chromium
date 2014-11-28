bd51017bc45f1c35fc3b6c10b7fd0de451e8241e
Zeno Albisser <zeno.albisser@theqtcompany.com>
2014-11-28 17:53:02 +0100

    <chromium> Leave a chance to all location providers to get a fix
    In our case, the NetworkLocationProvider will always fail
    for lack of a valid API token, and we don't want that to
    take precedence over the QtPositioning-based backend.
    
    Change-Id: Ic175bd3fb527a76a578ef3568f7ac7ed07c4ccad
    Reviewed-by: Andras Becsi <andras.becsi@theqtcompany.com>

--- chromium/content/browser/geolocation/location_arbitrator_impl.cc
+++ chromium/content/browser/geolocation/location_arbitrator_impl.cc
@@ -126,12 +126,15 @@ void LocationArbitratorImpl::OnLocationUpdate(const LocationProvider* provider,
                                               const Geoposition& new_position) {
   DCHECK(new_position.Validate() ||
          new_position.error_code != Geoposition::ERROR_CODE_NONE);
-  if (!IsNewPositionBetter(position_, new_position,
-                           provider == position_provider_))
-    return;
-  position_provider_ = provider;
-  position_ = new_position;
-  arbitrator_update_callback_.Run(position_);
+  providers_polled_.insert(provider);
+  if (IsNewPositionBetter(position_, new_position,
+                          provider == position_provider_)) {
+      position_provider_ = provider;
+      position_ = new_position;
+  }
+  // Don't fail until all providers had their say.
+  if (position_.Validate() || (providers_polled_.size() == providers_.size()))
+      arbitrator_update_callback_.Run(position_);
 }
 
 AccessTokenStore* LocationArbitratorImpl::NewAccessTokenStore() {
