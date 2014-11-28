e8b2f595bf0de16e6efac79ce693486eea82f3e7
Zeno Albisser <zeno.albisser@theqtcompany.com>
2014-11-28 17:53:02 +0100

    Leave a chance to all location providers to get a fix
    In our case, the NetworkLocationProvider will always fail
    for lack of a valid API token, and we don't want that to
    take precedence over the QtPositioning-based backend.
    
    And disable location requests to the geolocation google API.
    If we don't have a valid token, it will fail anyway.
    And for now we don't provide a way to use an existing API token.
    
    Change-Id: Ic175bd3fb527a76a578ef3568f7ac7ed07c4ccad
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- chromium/services/device/geolocation/network_location_provider.cc
+++ chromium/services/device/geolocation/network_location_provider.cc
@@ -199,6 +199,16 @@ void NetworkLocationProvider::StartProvider(bool high_accuracy) {
   }
   start_time_ = base::TimeTicks::Now();
 
+  // No point in sending requests without an API key.
+  if (request_->api_key().empty()) {
+      device::mojom::GeopositionResultPtr result =
+          device::mojom::GeopositionResult::NewError(
+              mojom::GeopositionError::New(
+                  mojom::GeopositionErrorCode::kPositionUnavailable, "", ""));
+      location_provider_update_callback_.Run(this, std::move(result));
+      return;
+  }
+
   // Registers a callback with the data provider.
   // Releasing the handle will automatically unregister the callback.
   wifi_data_provider_handle_ =
