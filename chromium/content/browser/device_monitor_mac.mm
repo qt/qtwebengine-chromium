b5236ac740738d2dd37f0b95dfbf5cda4f2fceeb
Jocelyn Turcotte <jocelyn.turcotte@digia.com>
2015-01-22 16:48:22 +0100

    <chromium> Remove the runtime dependency to QTKit
    This always forces AVFoundation for QtWebEngine since we don't support
    OSX < 10.7 and the QTKit dependency prevents submitting applications
    using QtWebEngine to the Mac App Store.
    
    Task-number: QTBUG-43219
    Change-Id: I835b594b2d2c0f6db1cf88072a7830ace215daf0
    Reviewed-by: Michael Brüning <michael.bruning@theqtcompany.com>
    Reviewed-by: Jocelyn Turcotte (Woboq GmbH) <jturcotte@woboq.com>
    Reviewed-by: Andras Becsi <andras.becsi@theqtcompany.com>

--- chromium/content/browser/device_monitor_mac.mm
+++ chromium/content/browser/device_monitor_mac.mm
@@ -4,7 +4,9 @@
 
 #include "content/browser/device_monitor_mac.h"
 
+#ifndef TOOLKIT_QT
 #import <QTKit/QTKit.h>
+#endif
 
 #include <set>
 
@@ -134,6 +136,7 @@ void DeviceMonitorMacImpl::ConsolidateDevicesListAndNotify(
     monitor_->NotifyDeviceChanged(base::SystemMonitor::DEVTYPE_AUDIO_CAPTURE);
 }
 
+#ifndef TOOLKIT_QT
 class QTKitMonitorImpl : public DeviceMonitorMacImpl {
  public:
   explicit QTKitMonitorImpl(content::DeviceMonitorMac* monitor);
@@ -213,6 +216,7 @@ void QTKitMonitorImpl::OnDeviceChanged() {
   }
   ConsolidateDevicesListAndNotify(snapshot_devices);
 }
+#endif
 
 // Forward declaration for use by CrAVFoundationDeviceObserver.
 class SuspendObserverDelegate;
@@ -540,6 +544,7 @@ void DeviceMonitorMac::StartMonitoring(
     DVLOG(1) << "Monitoring via AVFoundation";
     device_monitor_impl_.reset(new AVFoundationMonitorImpl(this,
                                                            device_task_runner));
+#ifndef TOOLKIT_QT
   } else {
     // TODO(erikchen): Remove ScopedTracker below once http://crbug.com/458404
     // is fixed.
@@ -548,6 +553,7 @@ void DeviceMonitorMac::StartMonitoring(
             "458404 DeviceMonitorMac::StartMonitoring::QTKit"));
     DVLOG(1) << "Monitoring via QTKit";
     device_monitor_impl_.reset(new QTKitMonitorImpl(this));
+#endif
   }
 }
 
