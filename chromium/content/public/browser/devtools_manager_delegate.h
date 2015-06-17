5f71b00c8d7afc3c1d10f1fc547a00eda13a8236
Jocelyn Turcotte <jocelyn.turcotte@digia.com>
2015-06-17 15:30:50 +0200

    Expose a status callback for the DevToolsHttpHandler
    This allows us to give our own feedback on the console about the
    outcome of the server socket binding, outside of the Chromium
    logging.
    
    Change-Id: Ib16dd97b6f5e07ad2e889d3c4520b2fda1eb532b
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/content/public/browser/devtools_manager_delegate.h
+++ chromium/content/public/browser/devtools_manager_delegate.h
@@ -14,6 +14,10 @@
 #include "content/public/browser/devtools_agent_host.h"
 #include "url/gurl.h"
 
+namespace net {
+class IPEndPoint;
+}
+
 namespace content {
 
 class DevToolsAgentHostClientChannel;
@@ -87,6 +91,8 @@ class CONTENT_EXPORT DevToolsManagerDelegate {
   // the Lab testing). If you want to return true here, please get security
   // clearance from the devtools owners.
   virtual bool IsBrowserTargetDiscoverable();
+
+  virtual void Initialized(const net::IPEndPoint* ) { }
 
   virtual ~DevToolsManagerDelegate();
 };
