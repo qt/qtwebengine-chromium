a792e55302892ea941d79d30e0d350284d6286d4
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2015-07-10 11:17:35 +0200

    Optionally link with system ffmpeg
    Merge unbundle version of ffmpeg.gyp and fix a few building issues, we
    must use the system FFMPEG configured defines and can not access the
    avformat internal struct.
    
    Change-Id: I0a8923e9ba72f2da6d5e2d1834f63c2a7c740b37
    Reviewed-by: Andras Becsi <andras.becsi@theqtcompany.com>
    Reviewed-by: Michael Brüning <michael.bruning@theqtcompany.com>

--- chromium/media/filters/ffmpeg_demuxer.cc
+++ chromium/media/filters/ffmpeg_demuxer.cc
@@ -1092,6 +1092,7 @@ void FFmpegDemuxer::OnFindStreamInfoDone(const PipelineStatusCB& status_cb,
   // If no estimate is found, the stream entry will be kInfiniteDuration().
   std::vector<base::TimeDelta> start_time_estimates(format_context->nb_streams,
                                                     kInfiniteDuration());
+#if !defined(USE_SYSTEM_FFMPEG)
   const AVFormatInternal* internal = format_context->internal;
   if (internal && internal->packet_buffer &&
       format_context->start_time != static_cast<int64_t>(AV_NOPTS_VALUE)) {
@@ -1110,6 +1111,7 @@ void FFmpegDemuxer::OnFindStreamInfoDone(const PipelineStatusCB& status_cb,
       packet_buffer = packet_buffer->next;
     }
   }
+#endif
 
   scoped_ptr<MediaTracks> media_tracks(new MediaTracks());
   AVStream* audio_stream = NULL;
