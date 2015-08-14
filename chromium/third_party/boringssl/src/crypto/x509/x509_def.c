636e27caf18f64626946240070b69d89a11b8af1
Andras Becsi <andras.becsi@theqtcompany.com>
2015-08-14 15:12:24 +0200

    Only define OPENSSLDIR if it is not already defined
    For embedded we define our OPENSSLDIR which might be different from
    the default.
    
    Change-Id: Ied87502b76a79b47c5add3a763a9b88fed57318a
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
    Reviewed-by: Michael Brüning <michael.bruning@theqtcompany.com>

--- chromium/third_party/boringssl/src/crypto/x509/x509_def.c
+++ chromium/third_party/boringssl/src/crypto/x509/x509_def.c
@@ -59,10 +59,12 @@
 
 /* TODO(fork): cleanup */
 
+#ifndef OPENSSLDIR
 #if defined(OPENSSL_FUCHSIA)
 #define OPENSSLDIR "/config/ssl"
 #else
 #define OPENSSLDIR "/etc/ssl"
+#endif
 #endif
 
 #define X509_CERT_AREA          OPENSSLDIR
