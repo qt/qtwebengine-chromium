95f716c67f9e140d1230f84e025f223ae3b843ae
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2015-08-31 17:39:26 +0200

    Forward cleared selections
    The UI layer needs to have the selection updated also when the selection
    is not valid because it has been cleared.
    
    Change-Id: I86dac087a8ea94fca4a91a2667658395b65e6046
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/content/renderer/render_frame_impl.cc
+++ chromium/content/renderer/render_frame_impl.cc
@@ -2086,7 +2086,7 @@ void RenderFrameImpl::PepperSelectionChanged(
     PepperPluginInstanceImpl* instance) {
   if (instance != focused_pepper_plugin_)
     return;
-  SyncSelectionIfRequired();
+  SyncSelectionIfRequired(false);
 }
 
 RenderWidgetFullscreenPepper* RenderFrameImpl::CreatePepperFullscreenContainer(
@@ -5091,8 +5091,6 @@ void RenderFrameImpl::DidChangeSelection(bool is_empty_selection) {
       !handling_select_range_)
     return;
 
-  if (is_empty_selection)
-    selection_text_.clear();
 
   // UpdateTextInputState should be called before SyncSelectionIfRequired.
   // UpdateTextInputState may send TextInputStateChanged to notify the focus
@@ -5100,7 +5098,7 @@ void RenderFrameImpl::DidChangeSelection(bool is_empty_selection) {
   // to notify the selection was changed.  Focus change should be notified
   // before selection change.
   GetLocalRootRenderWidget()->UpdateTextInputState();
-  SyncSelectionIfRequired();
+  SyncSelectionIfRequired(is_empty_selection);
 }
 
 bool RenderFrameImpl::HandleCurrentKeyboardEvent() {
@@ -6762,9 +6760,9 @@ void RenderFrameImpl::UpdateEncoding(WebFrame* frame,
     GetFrameHost()->UpdateEncoding(encoding_name);
 }
 
-void RenderFrameImpl::SyncSelectionIfRequired() {
+void RenderFrameImpl::SyncSelectionIfRequired(bool is_empty_selection) {
   base::string16 text;
-  size_t offset;
+  size_t offset = 0;
   gfx::Range range;
 #if BUILDFLAG(ENABLE_PLUGINS)
   if (focused_pepper_plugin_) {
@@ -6773,7 +6771,7 @@ void RenderFrameImpl::SyncSelectionIfRequired() {
     // TODO(kinaba): cut as needed.
   } else
 #endif
-  {
+  if (!is_empty_selection) {
     WebRange selection =
         frame_->GetInputMethodController()->GetSelectionOffsets();
     if (selection.IsNull())
