a717223b1c3a5e784f6caa17b99513afaa1ff992
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2015-08-31 17:39:26 +0200

    Forward cleared selections
    The UI layer needs to have the selection updated also when the selection
    is not valid because it has been cleared.
    
    Change-Id: I86dac087a8ea94fca4a91a2667658395b65e6046
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/content/renderer/render_frame_impl.cc
+++ chromium/content/renderer/render_frame_impl.cc
@@ -1304,7 +1304,7 @@ void RenderFrameImpl::PepperSelectionChanged(
     PepperPluginInstanceImpl* instance) {
   if (instance != focused_pepper_plugin_)
     return;
-  SyncSelectionIfRequired();
+  SyncSelectionIfRequired(false);
 }
 
 RenderWidgetFullscreenPepper* RenderFrameImpl::CreatePepperFullscreenContainer(
@@ -1911,7 +1911,7 @@ void RenderFrameImpl::OnReplace(const base::string16& text) {
     frame_->selectWordAroundCaret();
 
   frame_->replaceSelection(text);
-  SyncSelectionIfRequired();
+  SyncSelectionIfRequired(false);
 }
 
 void RenderFrameImpl::OnReplaceMisspelling(const base::string16& text) {
@@ -3857,8 +3857,6 @@ void RenderFrameImpl::didChangeSelection(bool is_empty_selection) {
       !handling_select_range_)
     return;
 
-  if (is_empty_selection)
-    selection_text_.clear();
 
   // UpdateTextInputState should be called before SyncSelectionIfRequired.
   // UpdateTextInputState may send TextInputStateChanged to notify the focus
@@ -3867,7 +3865,7 @@ void RenderFrameImpl::didChangeSelection(bool is_empty_selection) {
   // before selection change.
   GetRenderWidget()->UpdateTextInputState(ShowIme::HIDE_IME,
                                           ChangeSource::FROM_NON_IME);
-  SyncSelectionIfRequired();
+  SyncSelectionIfRequired(is_empty_selection);
 }
 
 bool RenderFrameImpl::handleCurrentKeyboardEvent() {
@@ -5817,9 +5815,9 @@ void RenderFrameImpl::UpdateEncoding(WebFrame* frame,
     Send(new FrameHostMsg_UpdateEncoding(routing_id_, encoding_name));
 }
 
-void RenderFrameImpl::SyncSelectionIfRequired() {
+void RenderFrameImpl::SyncSelectionIfRequired(bool is_empty_selection) {
   base::string16 text;
-  size_t offset;
+  size_t offset = 0;
   gfx::Range range;
 #if defined(ENABLE_PLUGINS)
   if (focused_pepper_plugin_) {
@@ -5828,7 +5826,7 @@ void RenderFrameImpl::SyncSelectionIfRequired() {
     // TODO(kinaba): cut as needed.
   } else
 #endif
-  {
+  if (!is_empty_selection) {
     WebRange selection =
         GetRenderWidget()->GetWebWidget()->caretOrSelectionRange();
     if (selection.isNull())
