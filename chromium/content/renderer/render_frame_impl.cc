72d07669eb54a00bf8a11cda03c1c36b62c17829
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2015-08-31 17:39:26 +0200

    Forward cleared selections
    The UI layer needs to have the selection updated also when the selection
    is not valid because it has been cleared.
    
    Change-Id: I86dac087a8ea94fca4a91a2667658395b65e6046
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/content/renderer/render_frame_impl.cc
+++ chromium/content/renderer/render_frame_impl.cc
@@ -1997,7 +1997,7 @@ void RenderFrameImpl::PepperSelectionChanged(
   // We have no reason to believe the locally cached last synced selection is
   // invalid so we do not need to force the update if it matches our last synced
   // value.
-  SyncSelectionIfRequired(blink::SyncCondition::kNotForced);
+  SyncSelectionIfRequired(blink::SyncCondition::kNotForced, false);
 }
 
 #endif  // BUILDFLAG(ENABLE_PPAPI)
@@ -4108,8 +4108,6 @@ void RenderFrameImpl::DidChangeSelection(bool is_empty_selection,
       !GetLocalRootWebFrameWidget()->HandlingSelectRange())
     return;
 
-  if (is_empty_selection)
-    selection_text_.clear();
 
   // UpdateTextInputState should be called before SyncSelectionIfRequired.
   // UpdateTextInputState may send TextInputStateChanged to notify the focus
@@ -4117,7 +4115,7 @@ void RenderFrameImpl::DidChangeSelection(bool is_empty_selection,
   // to notify the selection was changed.  Focus change should be notified
   // before selection change.
   GetLocalRootWebFrameWidget()->UpdateTextInputState();
-  SyncSelectionIfRequired(force_sync);
+  SyncSelectionIfRequired(force_sync, is_empty_selection);
 }
 
 void RenderFrameImpl::OnMainFrameIntersectionChanged(
@@ -5511,9 +5509,9 @@ void RenderFrameImpl::UpdateEncoding(WebFrame* frame,
   }
 }
 
-void RenderFrameImpl::SyncSelectionIfRequired(blink::SyncCondition force_sync) {
+void RenderFrameImpl::SyncSelectionIfRequired(blink::SyncCondition force_sync, bool is_empty_selection) {
   std::u16string text;
-  size_t offset;
+  size_t offset = 0;
   gfx::Range range;
 #if BUILDFLAG(ENABLE_PPAPI)
   if (focused_pepper_plugin_) {
@@ -5522,7 +5520,7 @@ void RenderFrameImpl::SyncSelectionIfRequired(blink::SyncCondition force_sync) {
     // TODO(kinaba): cut as needed.
   } else
 #endif
-  {
+  if (!is_empty_selection) {
     WebRange selection =
         frame_->GetInputMethodController()->GetSelectionOffsets();
     if (selection.IsNull())
