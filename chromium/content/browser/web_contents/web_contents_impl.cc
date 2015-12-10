1ef20b2b10c1650b25d61ab55bc88641f69b66c8
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2015-12-10 17:47:24 +0100

    Lookup browser accessibility manager before notifying observers
    If the observer causes navigation, the render_frame_host object might
    have been deleted and accessing causes use-after-free.
    
    Change-Id: I1a4d6d57befa54d5908ee38b5caf70ac73a18ed3
    Task-number: QTBUG-48031
    Reviewed-by: Joerg Bornemann <joerg.bornemann@theqtcompany.com>

--- chromium/content/browser/web_contents/web_contents_impl.cc
+++ chromium/content/browser/web_contents/web_contents_impl.cc
@@ -2754,6 +2754,9 @@ void WebContentsImpl::DidFailProvisionalLoadWithError(
     RenderFrameHostImpl* render_frame_host,
     const FrameHostMsg_DidFailProvisionalLoadWithError_Params& params) {
   GURL validated_url(params.url);
+  FrameTreeNode* ftn = render_frame_host->frame_tree_node();
+  BrowserAccessibilityManager* manager =
+      ftn->current_frame_host()->browser_accessibility_manager();
   FOR_EACH_OBSERVER(WebContentsObserver,
                     observers_,
                     DidFailProvisionalLoad(render_frame_host,
@@ -2762,9 +2765,6 @@ void WebContentsImpl::DidFailProvisionalLoadWithError(
                                            params.error_description,
                                            params.was_ignored_by_handler));
 
-  FrameTreeNode* ftn = render_frame_host->frame_tree_node();
-  BrowserAccessibilityManager* manager =
-      ftn->current_frame_host()->browser_accessibility_manager();
   if (manager)
     manager->NavigationFailed();
 }
