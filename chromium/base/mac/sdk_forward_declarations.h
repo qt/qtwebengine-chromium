fd25230593ca6929defb166f79a06d92f4dd03f6
Peter Varga <pvarga@inf.u-szeged.hu>
2016-02-28 19:34:33 +0100

    Fix build on OS X Mavericks (10.9)
    Define unsupported NSEventModifierFlags type.
    
    Change-Id: I96f66c4c0f91bb0c7259909bce788fafbc9fd4a6
    Task-number: QTBUG-51350
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>

--- chromium/base/mac/sdk_forward_declarations.h
+++ chromium/base/mac/sdk_forward_declarations.h
@@ -195,6 +195,8 @@ BASE_EXPORT extern NSString* const NSAppearanceNameVibrantLight;
 - (void)viewDidLoad;
 @end
 
+typedef NSUInteger NSEventModifierFlags;
+
 @interface NSWindow (YosemiteSDK)
 - (void)setTitlebarAppearsTransparent:(BOOL)flag;
 @end
