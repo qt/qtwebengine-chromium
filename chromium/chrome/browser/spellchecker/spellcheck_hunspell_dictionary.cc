3265472d7c3311925960935064a766df0725fc72
Michal Klocek <michal.klocek@theqtcompany.com>
2016-04-28 19:47:46 +0200

    Update handling of loading the dictionary
    Add notifications if dictionary can not be loaded,
    do not delete corrupted files, simplify naming
    of dictionaries - do not use hardcoded region
    language mapping.
    
    Change-Id: Ifcc7952e5815cadf7aeb64577988749c0d1a604d
    Reviewed-by: Kai Koehne <kai.koehne@qt.io>
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/chrome/browser/spellchecker/spellcheck_hunspell_dictionary.cc
+++ chromium/chrome/browser/spellchecker/spellcheck_hunspell_dictionary.cc
@@ -379,7 +379,9 @@ SpellcheckHunspellDictionary::OpenDictionaryFile(base::TaskRunner* task_runner,
     dictionary.file.Initialize(dictionary.path,
                                base::File::FLAG_READ | base::File::FLAG_OPEN);
   } else {
+#ifndef TOOLKIT_QT
     base::DeleteFile(dictionary.path);
+#endif
   }
 
   return dictionary;
@@ -410,7 +412,7 @@ void SpellcheckHunspellDictionary::InitializeDictionaryLocationComplete(
     DictionaryFile file) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
   dictionary_file_ = std::move(file);
-
+#ifndef TOOLKIT_QT
   if (!dictionary_file_.file.IsValid()) {
     // Notify browser tests that this dictionary is corrupted. Skip downloading
     // the dictionary in browser tests.
@@ -429,6 +431,13 @@ void SpellcheckHunspellDictionary::InitializeDictionaryLocationComplete(
   }
 
   InformListenersOfInitialization();
+#else
+  if (!dictionary_file_.file.IsValid())
+      // We never download, so safe to reuse this handler
+      InformListenersOfDownloadFailure();
+  else
+      InformListenersOfInitialization();
+#endif
 }
 #endif  // !BUILDFLAG(IS_ANDROID)
 
