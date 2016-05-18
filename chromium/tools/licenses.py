a0344aa5c583ea64e11b833cb53afbf511145cd9
Kai Koehne <kai.koehne@qt.io>
2016-05-18 16:35:21 +0200

    Adapt licenses.py for qdoc needs
    * Mask '*/' in licenses.py output, so qdoc is not confused
    * Add 'name-sanitized', 'license-type' placeholders
    
    (cherry picked from commit f8554350d7477b4d3a8ca4596be054ba95c7f091)
    
    Change-Id: I080ed7894464a39a4c33ef15e6a1b56b421e2c9f
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/tools/licenses.py
+++ chromium/tools/licenses.py
@@ -552,6 +552,7 @@ def GenerateCredits(
         for key, val in env.items():
             if escape:
                 val = cgi.escape(val)
+                val = val.replace("*/", "* /")
             template = template.replace('{{%s}}' % key, val)
         return template
 
@@ -594,8 +595,10 @@ def GenerateCredits(
                 continue
         env = {
             'name': metadata['Name'],
+            'name-sanitized': metadata['Name'].replace(' ', '-'),
             'url': metadata['URL'],
             'license': open(metadata['License File'], 'rb').read(),
+            'license-type': metadata['License']
         }
         entry = {
             'name': metadata['Name'],
