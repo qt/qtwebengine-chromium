88da3f0615b9ec3fa83c26f74570e59450f0db25
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2016-05-18 16:11:39 +0200

    Fix build with PNG_LIBPNG_VER >= 10500
    Upstream broke building against newer libpng versions.
    
    Change-Id: Ice1979e28b1bcd68751fba23c2321cf3c4a34120
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- chromium/third_party/WebKit/Source/platform/image-decoders/png/PNGImageDecoder.cpp
+++ chromium/third_party/WebKit/Source/platform/image-decoders/png/PNGImageDecoder.cpp
@@ -238,7 +238,7 @@ void PNGImageDecoder::headerAvailable()
 #endif
             png_uint_32 profileLength = 0;
             if (png_get_iCCP(png, info, &profileName, &compressionType, &profile, &profileLength)) {
-                setColorProfileAndTransform(reinterpret_cast<char*>(profile), profileLength, imageHasAlpha, false /* useSRGB */);
+                setColorProfileAndTransform(reinterpret_cast<const char*>(profile), profileLength, imageHasAlpha, false /* useSRGB */);
             }
         }
 #endif // PNG_iCCP_SUPPORTED
