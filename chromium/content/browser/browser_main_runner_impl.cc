0d07b3d01d14b9e94f3dec7e2da7d9d3450e9958
Joerg Bornemann <joerg.bornemann@qt.io>
2016-06-22 17:01:46 +0200

    Do not call Ole(Un)Initialize
    The Windows platform plugin already calls OleInitialize. When
    ScopedOleInitializer detects that OleInitialize was already called it
    will yield an error message. This is not much of a problem but unsettles
    users.
    
    Task-number: QTBUG-53746
    Change-Id: If5e978c6ef022a7ec0637a2e171fc0f9b3e92a41
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- chromium/content/browser/browser_main_runner_impl.cc
+++ chromium/content/browser/browser_main_runner_impl.cc
@@ -87,10 +87,12 @@ int BrowserMainRunnerImpl::Initialize(const MainFunctionParams& parameters) {
     notification_service_.reset(new NotificationServiceImpl);
 
 #if defined(OS_WIN)
+#if !defined(TOOLKIT_QT)
     // Ole must be initialized before starting message pump, so that TSF
     // (Text Services Framework) module can interact with the message pump
     // on Windows 8 Metro mode.
     ole_initializer_.reset(new ui::ScopedOleInitializer);
+#endif
 #endif  // OS_WIN
 
     gfx::InitializeFonts();
@@ -178,7 +180,7 @@ void BrowserMainRunnerImpl::Shutdown() {
     main_loop_->ShutdownThreadsAndCleanUp();
 
     ui::ShutdownInputMethod();
-#if defined(OS_WIN)
+#if defined(OS_WIN) && !defined(TOOLKIT_QT)
     ole_initializer_.reset(NULL);
 #endif
 #if defined(OS_ANDROID)
