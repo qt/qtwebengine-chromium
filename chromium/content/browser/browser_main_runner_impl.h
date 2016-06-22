14e0f4207d0f27e3528fa7a5bfe1e16be48162a5
Joerg Bornemann <joerg.bornemann@qt.io>
2016-06-22 17:01:46 +0200

    Do not call Ole(Un)Initialize
    The Windows platform plugin already calls OleInitialize. When
    ScopedOleInitializer detects that OleInitialize was already called it
    will yield an error message. This is not much of a problem but unsettles
    users.
    
    Task-number: QTBUG-53746
    Change-Id: If5e978c6ef022a7ec0637a2e171fc0f9b3e92a41
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- chromium/content/browser/browser_main_runner_impl.h
+++ chromium/content/browser/browser_main_runner_impl.h
@@ -56,7 +56,7 @@ class BrowserMainRunnerImpl : public BrowserMainRunner {
 
   std::unique_ptr<NotificationServiceImpl> notification_service_;
   std::unique_ptr<BrowserMainLoop> main_loop_;
-#if BUILDFLAG(IS_WIN)
+#if BUILDFLAG(IS_WIN) && !defined(TOOLKIT_QT)
   std::unique_ptr<ui::ScopedOleInitializer> ole_initializer_;
 #endif
 };
