7366110654eec46f21b6824f302356426f48cd74
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2016-07-01 12:20:27 +0200

    BASELINE: Update Chromium to 51.0.2704.106
    Also add a few extra files we might need for future features.
    
    Change-Id: I517c35e43221c610976d347c966d070ad44d4c2b
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

---
+++ chromium/extensions/browser/value_store/testing_value_store.cc
@@ -0,0 +1,130 @@
+// Copyright 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "extensions/browser/value_store/testing_value_store.h"
+
+#include <utility>
+
+#include "base/logging.h"
+
+namespace {
+
+const char kGenericErrorMessage[] = "TestingValueStore configured to error";
+
+}  // namespace
+
+TestingValueStore::TestingValueStore() : read_count_(0), write_count_(0) {}
+
+TestingValueStore::~TestingValueStore() {}
+
+void TestingValueStore::set_status_code(StatusCode status_code) {
+  status_ = ValueStore::Status(status_code, kGenericErrorMessage);
+}
+
+size_t TestingValueStore::GetBytesInUse(const std::string& key) {
+  // Let SettingsStorageQuotaEnforcer implement this.
+  NOTREACHED() << "Not implemented";
+  return 0;
+}
+
+size_t TestingValueStore::GetBytesInUse(
+    const std::vector<std::string>& keys) {
+  // Let SettingsStorageQuotaEnforcer implement this.
+  NOTREACHED() << "Not implemented";
+  return 0;
+}
+
+size_t TestingValueStore::GetBytesInUse() {
+  // Let SettingsStorageQuotaEnforcer implement this.
+  NOTREACHED() << "Not implemented";
+  return 0;
+}
+
+ValueStore::ReadResult TestingValueStore::Get(const std::string& key) {
+  return Get(std::vector<std::string>(1, key));
+}
+
+ValueStore::ReadResult TestingValueStore::Get(
+    const std::vector<std::string>& keys) {
+  read_count_++;
+  if (!status_.ok())
+    return MakeReadResult(status_);
+
+  base::DictionaryValue* settings = new base::DictionaryValue();
+  for (std::vector<std::string>::const_iterator it = keys.begin();
+      it != keys.end(); ++it) {
+    base::Value* value = NULL;
+    if (storage_.GetWithoutPathExpansion(*it, &value)) {
+      settings->SetWithoutPathExpansion(*it, value->DeepCopy());
+    }
+  }
+  return MakeReadResult(make_scoped_ptr(settings), status_);
+}
+
+ValueStore::ReadResult TestingValueStore::Get() {
+  read_count_++;
+  if (!status_.ok())
+    return MakeReadResult(status_);
+  return MakeReadResult(make_scoped_ptr(storage_.DeepCopy()), status_);
+}
+
+ValueStore::WriteResult TestingValueStore::Set(
+    WriteOptions options, const std::string& key, const base::Value& value) {
+  base::DictionaryValue settings;
+  settings.SetWithoutPathExpansion(key, value.DeepCopy());
+  return Set(options, settings);
+}
+
+ValueStore::WriteResult TestingValueStore::Set(
+    WriteOptions options, const base::DictionaryValue& settings) {
+  write_count_++;
+  if (!status_.ok())
+    return MakeWriteResult(status_);
+
+  scoped_ptr<ValueStoreChangeList> changes(new ValueStoreChangeList());
+  for (base::DictionaryValue::Iterator it(settings);
+       !it.IsAtEnd(); it.Advance()) {
+    base::Value* old_value = NULL;
+    if (!storage_.GetWithoutPathExpansion(it.key(), &old_value) ||
+        !old_value->Equals(&it.value())) {
+      changes->push_back(
+          ValueStoreChange(
+              it.key(),
+              old_value ? old_value->DeepCopy() : old_value,
+              it.value().DeepCopy()));
+      storage_.SetWithoutPathExpansion(it.key(), it.value().DeepCopy());
+    }
+  }
+  return MakeWriteResult(std::move(changes), status_);
+}
+
+ValueStore::WriteResult TestingValueStore::Remove(const std::string& key) {
+  return Remove(std::vector<std::string>(1, key));
+}
+
+ValueStore::WriteResult TestingValueStore::Remove(
+    const std::vector<std::string>& keys) {
+  write_count_++;
+  if (!status_.ok())
+    return MakeWriteResult(status_);
+
+  scoped_ptr<ValueStoreChangeList> changes(new ValueStoreChangeList());
+  for (std::vector<std::string>::const_iterator it = keys.begin();
+      it != keys.end(); ++it) {
+    scoped_ptr<base::Value> old_value;
+    if (storage_.RemoveWithoutPathExpansion(*it, &old_value)) {
+      changes->push_back(ValueStoreChange(*it, old_value.release(), NULL));
+    }
+  }
+  return MakeWriteResult(std::move(changes), status_);
+}
+
+ValueStore::WriteResult TestingValueStore::Clear() {
+  std::vector<std::string> keys;
+  for (base::DictionaryValue::Iterator it(storage_);
+       !it.IsAtEnd(); it.Advance()) {
+    keys.push_back(it.key());
+  }
+  return Remove(keys);
+}
