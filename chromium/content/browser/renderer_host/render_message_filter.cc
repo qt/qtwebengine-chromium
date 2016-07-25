c32be72ccdc626cc6f583f2faa0ea0ecdf6e8f13
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2016-07-25 11:39:45 +0200

    [Backport] Ignore certain messages in plugin broker process if they are not sent by the browser.
    BUG=610600
    CQ_INCLUDE_TRYBOTS=tryserver.chromium.linux:linux_site_isolation
    
    Review-Url: https://codereview.chromium.org/2069853002
    Cr-Commit-Position: refs/heads/master@{#400495}
    (cherry picked from commit 92cad45212731b59e74c8f2a2913691db3e9d770)
    
    Review URL: https://codereview.chromium.org/2122313004 .
    (CVE-2016-1706)
    
    Change-Id: I3caa9f4432b1da7f1c1ed31ae95e222019f5439a
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/content/browser/renderer_host/render_message_filter.cc
+++ chromium/content/browser/renderer_host/render_message_filter.cc
@@ -164,6 +164,9 @@ class OpenChannelToPpapiPluginCallback
 
   void GetPpapiChannelInfo(base::ProcessHandle* renderer_handle,
                            int* renderer_id) override {
+    // base::kNullProcessHandle indicates that the channel will be used by the
+    // browser itself. Make sure we never output that value here.
+    CHECK_NE(base::kNullProcessHandle, filter()->PeerHandle());
     *renderer_handle = filter()->PeerHandle();
     *renderer_id = filter()->render_process_id();
   }
@@ -197,6 +200,9 @@ class OpenChannelToPpapiBrokerCallback
 
   void GetPpapiChannelInfo(base::ProcessHandle* renderer_handle,
                            int* renderer_id) override {
+    // base::kNullProcessHandle indicates that the channel will be used by the
+    // browser itself. Make sure we never output that value here.
+    CHECK_NE(base::kNullProcessHandle, filter_->PeerHandle());
     *renderer_handle = filter_->PeerHandle();
     *renderer_id = filter_->render_process_id();
   }
