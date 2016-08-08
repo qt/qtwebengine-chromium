f3fc1c83d1a17e44147dbf462b9b9ce7bf837476
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2016-08-08 10:50:37 +0200

    Fixup select elements.
    We use RenderWidgetHostViewQt, not RWHVMac, even on macOS.
    Since some platform decisions seem to have transpired all the way to
    the content layer, we should simply use the web ui for select elements
    on mac for the time being.
    
    Change-Id: Ib430f8686862cf0958ccdbfefb8bfa36af590e97
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/content/browser/browser_plugin/browser_plugin_guest.h
+++ chromium/content/browser/browser_plugin/browser_plugin_guest.h
@@ -23,6 +23,7 @@
 #include "base/memory/raw_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "build/build_config.h"
+#include "content/common/buildflags.h"
 #include "content/public/browser/browser_plugin_guest_delegate.h"
 #include "content/public/browser/guest_host.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -93,7 +94,7 @@ class BrowserPluginGuest : public GuestHost, public WebContentsObserver {
 
   void PrimaryMainFrameRenderProcessGone(
       base::TerminationStatus status) override;
-#if defined(OS_MAC)
+#if defined(OS_MAC) && BUILDFLAG(USE_EXTERNAL_POPUP_MENU)
   // On MacOS X popups are painted by the browser process. We handle them here
   // so that they are positioned correctly.
   void ShowPopupMenu(
