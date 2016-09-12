5aafca86e4747aee83f42bd42133a90fe247dd59
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2016-09-12 18:26:06 +0200

    [profile] A small Profile class
    Implements a small subset of Chromium's Profile class.
    
    Change-Id: I44967861c10aa7921e4a31b2a5873b54bb71c6ec

---
+++ chromium/chrome/browser/profiles/profile.h
@@ -0,0 +1,55 @@
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// This a QtWebEngine specific stripped down replacement of Chromiums's Profile
+// class, because it is used many places just for preference access.
+
+#ifndef CHROME_BROWSER_PROFILES_PROFILE_H_
+#define CHROME_BROWSER_PROFILES_PROFILE_H_
+
+#include "content/public/browser/browser_context.h"
+
+class PrefService;
+
+namespace content {
+class WebUI;
+}
+
+class Profile : public content::BrowserContext {
+ public:
+  // Returns the profile corresponding to the given browser context.
+  static Profile* FromBrowserContext(content::BrowserContext* browser_context);
+
+  // Returns the profile corresponding to the given WebUI.
+  static Profile* FromWebUI(content::WebUI* web_ui);
+
+  // Retrieves a pointer to the PrefService that manages the
+  // preferences for this user profile.
+  virtual PrefService* GetPrefs() = 0;
+  virtual const PrefService* GetPrefs() const = 0;
+
+  Profile *GetOriginalProfile();
+  const Profile *GetOriginalProfile() const;
+
+  // Returns whether the profile is new.  A profile is new if the browser has
+  // not been shut down since the profile was created.
+  virtual bool IsNewProfile() const = 0;
+
+  // Returns whether it's a regular profile.
+  bool IsRegularProfile() const;
+
+  // Returns whether it is an Incognito profile. An Incognito profile is an
+  // off-the-record profile that is used for incognito mode.
+  bool IsIncognitoProfile() const;
+
+  // Returns whether it is a system profile.
+  bool IsSystemProfile() const;
+
+  // Returns whether it is a Guest session. This covers both regular and
+  // off-the-record profiles of a Guest session.
+  virtual bool IsGuestSession() const;
+
+};
+
+#endif  // CHROME_BROWSER_PROFILES_PROFILE_H_
