610710a8e38e75b8dd0f4355a0e830b59a1586a8
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2016-09-13 14:52:02 +0200

    Forward declare newer EGL typedefs
    We need these if the system EGL is not new enough because the
    EGL autobindings are using them.
    
    Task-number: QTBUG-66038
    Change-Id: I373152283ff094886ba2d8c263ae840a5feb3555
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/ui/gl/gl_bindings.h
+++ chromium/ui/gl/gl_bindings.h
@@ -469,6 +469,27 @@
 
 // Forward declare EGL types.
 typedef uint64_t EGLuint64CHROMIUM;
+#ifndef EGL_VERSION_1_5
+typedef intptr_t EGLAttrib;
+#endif
+#ifndef EGL_KHR_stream
+typedef void *EGLStreamKHR;
+typedef uint64_t EGLuint64KHR;
+#endif
+#ifndef EGL_ANDROID_presentation_time
+typedef khronos_stime_nanoseconds_t EGLnsecsANDROID;
+#endif
+#ifndef EGL_KHR_debug
+typedef void* EGLObjectKHR;
+typedef void* EGLLabelKHR;
+typedef void (APIENTRY *EGLDEBUGPROCKHR)(
+             EGLenum error,
+             const char *command,
+             EGLint messageType,
+             EGLLabelKHR threadLabel,
+             EGLLabelKHR objectLabel,
+             const char* message);
+#endif
 
 #include "gl_bindings_autogen_gl.h"
 
