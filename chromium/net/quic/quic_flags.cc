43f905816b95a4a1e09e4eaaff830e354b74d196
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2016-10-04 15:06:26 +0200

    [Backport] Disables QUIC flag that shifts cubic epoch on quiescence, since shifting code is buggy and should not be used.
    R=rch@chromium.org
    BUG=646495
    
    Review URL: https://codereview.chromium.org/2341513002 .
    
    Change-Id: Icee12d47b67b067428d0be37119bb0959da34561
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/net/quic/quic_flags.cc
+++ chromium/net/quic/quic_flags.cc
@@ -63,7 +63,7 @@ bool FLAGS_quic_limit_max_cwnd = true;
 bool FLAGS_quic_require_handshake_confirmation = false;
 
 // If true, Cubic's epoch is shifted when the sender is application-limited.
-bool FLAGS_shift_quic_cubic_epoch_when_app_limited = true;
+bool FLAGS_shift_quic_cubic_epoch_when_app_limited = false;
 
 // If true, QUIC will measure head of line (HOL) blocking due between
 // streams due to packet losses on the headers stream.  The
