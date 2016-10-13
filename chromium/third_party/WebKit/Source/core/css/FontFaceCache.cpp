20350980c2fe7828752ed2bc8f63466d2ff83b2f
Allan Sandfeld Jensen <allan.jensen@theqtcompany.com>
2016-10-13 13:06:22 +0200

    Fix renderer crash on null family strings
    The hash map will try to call methods on the String implementation and
    crash if it is null.
    
    Change-Id: Ib8301b9c2d0f5efdb4493e99c234af2fee504b46
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- chromium/third_party/WebKit/Source/core/css/FontFaceCache.cpp
+++ chromium/third_party/WebKit/Source/core/css/FontFaceCache.cpp
@@ -127,6 +127,9 @@ void FontFaceCache::clearAll()
 
 CSSSegmentedFontFace* FontFaceCache::get(const FontDescription& fontDescription, const AtomicString& family)
 {
+    if (family.isEmpty())
+        return nullptr;
+
     TraitsMap* familyFontFaces = m_fontFaces.get(family);
     if (!familyFontFaces || familyFontFaces->isEmpty())
         return nullptr;
