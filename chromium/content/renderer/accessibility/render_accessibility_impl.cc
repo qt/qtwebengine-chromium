dfe301325b721ee091ed7395949bf331d9cfcfaf
Alexandru Croitor <alexandru.croitor@qt.io>
2016-10-13 15:59:34 +0200

    Stop the flood of accessibility messages
    Change-Id: Idaeffa4045f631171e6a99d63a7aef7caa80e782
    Task-number: QTBUG-56261
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/content/renderer/accessibility/render_accessibility_impl.cc
+++ chromium/content/renderer/accessibility/render_accessibility_impl.cc
@@ -953,7 +953,7 @@ bool RenderAccessibilityImpl::SerializeUpdatesAndEvents(
 
     updates.push_back(update);
 
-    VLOG(1) << "Accessibility tree update:\n" << update.ToString();
+    DVLOG(1) << "Accessibility tree update:\n" << update.ToString();
   }
 
   if (had_end_of_test_event) {
