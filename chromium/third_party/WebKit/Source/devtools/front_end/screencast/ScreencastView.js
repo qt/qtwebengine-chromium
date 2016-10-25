2ff5d1668ee5d1ccb57b3eb33a021d8af4f35a7e
Dmitry Gozman <dgozman@chromium.org>
2016-10-25 11:53:53 -0700

    [Backport] Merge to 2883 "[DevTools] Move sanitize url to devtools_ui.cc."
    > [DevTools] Move sanitize url to devtools_ui.cc.
    > > Compatibility script is not reliable enough.
    > > BUG=653134
    > > Review-Url: https://codereview.chromium.org/2403633002
    > Cr-Commit-Position: refs/heads/master@{#425814} (cherry picked from commit f865c2dfddb1d95af3a2467587c62566e3f7dfe4) TBR=pfeldman
    
    Review URL: https://codereview.chromium.org/2444423002 .
    
    Cr-Commit-Position: refs/branch-heads/2883@{#289}
    Cr-Branched-From: 614d31daee2f61b0180df403a8ad43f20b9f6dd7-refs/heads/master@{#423768}
    (CVE-2016-5212)
    
    Change-Id: I0d8ffd085ca15754fcc58a118d65e7501df901ae
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- chromium/third_party/WebKit/Source/devtools/front_end/screencast/ScreencastView.js
+++ chromium/third_party/WebKit/Source/devtools/front_end/screencast/ScreencastView.js
@@ -706,8 +706,6 @@ WebInspector.ScreencastView.prototype = {
     _createNavigationBar: function()
     {
         this._navigationBar = this.element.createChild("div", "toolbar-background screencast-navigation");
-        if (Runtime.queryParam("hideNavigation"))
-            this._navigationBar.classList.add("hidden");
 
         this._navigationBack = this._navigationBar.createChild("button", "back");
         this._navigationBack.disabled = true;
