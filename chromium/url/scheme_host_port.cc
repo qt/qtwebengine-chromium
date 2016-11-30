00499c0419ba225e88f28daa319d988dcf20c2c1
Alexandru Croitor <alexandru.croitor@qt.io>
2016-11-30 15:34:34 +0100

    Fix loading of blobs which have a qrc:// origin
    After applying the security patch in
    I6f07598c50d8f48f0791793cc7dc569792ce0aa4
    loading of clipboard data, blobs, and files that have a qrc origin,
    was broken due to additional security checks in
    BlobDispatcherHost::OnRegisterPublicBlobURL. In the end it boils down
    to a check in SchemeHostPort -> IsValidInput() where qrc is not
    considered a standard scheme, and thus a qrc URL is treated as
    an invalid.
    
    Fix consists in treating the qrc scheme just like the file:// scheme.
    
    Change-Id: I2d8c1dc87d01dc07c3e7bb4e6b02f4682eff173a
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/url/scheme_host_port.cc
+++ chromium/url/scheme_host_port.cc
@@ -23,6 +23,8 @@ namespace url {
 
 namespace {
 
+extern const char kQrcScheme[] = "qrc";
+
 bool IsCanonicalHost(const base::StringPiece& host) {
   std::string canon_host;
 
@@ -56,8 +58,11 @@ bool IsValidInput(const base::StringPiece& scheme,
       scheme.data(),
       Component(0, base::checked_cast<int>(scheme.length())),
       &scheme_type);
-  if (!is_standard)
+  if (!is_standard && scheme != kQrcScheme)
     return false;
+
+  if (scheme == kQrcScheme)
+      scheme_type = SCHEME_WITHOUT_PORT;
 
   switch (scheme_type) {
     case SCHEME_WITH_PORT:
