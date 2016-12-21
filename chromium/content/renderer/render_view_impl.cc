a2974666e4cb414fb78cda1b6176d78500fa21ec
Alexandru Croitor <alexandru.croitor@qt.io>
2016-12-21 11:38:23 +0100

    Fix macOS build when plugins are disabled
    A method was not defined but still used when building with plugins
    disabled. Checking original chromium history, the ifdef block seems
    like an oversight and is safe to remove. The method contents does not
    depend on any plugin code.
    
    Change-Id: I1aff3abc352faf545fb82d5ea0139a911c0722be
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/content/renderer/render_view_impl.cc
+++ chromium/content/renderer/render_view_impl.cc
@@ -1155,8 +1155,6 @@ blink::WebView* RenderViewImpl::webview() const {
   return webview_;
 }
 
-#if BUILDFLAG(ENABLE_PLUGINS)
-
 #if defined(OS_MACOSX)
 void RenderViewImpl::OnGetRenderedText() {
   if (!webview())
@@ -1181,8 +1179,6 @@ void RenderViewImpl::OnGetRenderedText() {
   Send(new ViewMsg_GetRenderedTextCompleted(GetRoutingID(), text));
 }
 #endif  // defined(OS_MACOSX)
-
-#endif  // ENABLE_PLUGINS
 
 void RenderViewImpl::TransferActiveWheelFlingAnimation(
     const blink::WebActiveWheelFlingParameters& params) {
