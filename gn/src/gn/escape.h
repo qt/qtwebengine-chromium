99a003a70d8c998b8e7cbe7074538b90339c4464
Michal Klocek <michal.klocek@qt.io>
2017-01-09 13:28:49 +0100

    [gn] Add rsp target writer
    Rsp writer is able to dump link data in form of
    set of response files which have objects, archives,
    defines, linker flags or libs.
    
    Task-number: QTBUG-95590
    Task-number: QTBUG-105053
    Task-number: QTBUG-111225
    Task-number: QTBUG-126156
    Change-Id: Id153effb43fb7b70e38d9eca881e12018cbed729
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- gn/src/gn/escape.h
+++ gn/src/gn/escape.h
@@ -7,6 +7,7 @@
 
 #include <iosfwd>
 #include <string_view>
+#include <string>
 
 enum EscapingMode {
   // No escaping.
@@ -36,6 +37,9 @@ enum EscapingMode {
   // Parameters use shell quoting and shell escaping of quotes, with ‘"’ and ‘\’
   // being the only special characters.
   ESCAPE_COMPILATION_DATABASE,
+
+  // Like ESCAPE_NINJA_COMMAND but without ninja string escaping
+  ESCAPE_COMMAND,
 };
 
 enum EscapingPlatform {
