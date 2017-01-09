109a0bcdfcaa5a1f6301377e59a018b8541de5c6
Michal Klocek <michal.klocek@qt.io>
2017-01-09 13:28:49 +0100

    Add support for creating pri link information
    Add variable create_pri_file for binary tagets, which
    generates <target_name>.pri. Instead of calling linker
    add timestamp file, this will be a dependency for
    qmake linking.
    
    Also adds sources for Qt Creator indexes.
    
    Change-Id: I91a41e5cab0a2db99a32e4d799d9664467b9cb18
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/tools/gn/target.cc
+++ chromium/tools/gn/target.cc
@@ -282,6 +282,7 @@ Target::Target(const Settings* settings, const Label& label)
       check_includes_(true),
       complete_static_lib_(false),
       testonly_(false),
+      create_pri_file_(false),
       toolchain_(nullptr) {}
 
 Target::~Target() {
@@ -685,6 +686,9 @@ void Target::FillOutputFiles() {
       } else {
         SubstitutionWriter::ApplyListToLinkerAsOutputFile(
             this, tool, tool->runtime_outputs(), &runtime_outputs_);
+      }
+      if (create_pri_file()) {
+          dependency_output_file_ = OutputFile(label().name() + ".stamp");
       }
       break;
     case UNKNOWN:
