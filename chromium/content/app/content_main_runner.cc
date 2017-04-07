1c3527461b6b70263e671dd02fd25c78cd65d4db
Florian Bruhin <git@the-compiler.org>
2017-04-07 21:22:09 +0200

    FIXUP: <chromium> Do not overwrite signal handlers in the browser process.
    While related changes to chromium/content/public/app/content_main.h are still
    there, this got lost (probably due to a bad rebase with 55/56-based?), which
    caused the bug to regress.
    
    Task-number: QTBUG-42747
    Change-Id: Ic4c47cf81e343f77b23a8d9f4ccec45f1284cf3f
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/content/app/content_main_runner.cc
+++ chromium/content/app/content_main_runner.cc
@@ -478,7 +478,10 @@ class ContentMainRunnerImpl : public ContentMainRunner {
     // in correct encoding.
     setlocale(LC_ALL, "");
 
-    SetupSignalHandlers();
+    if (params.setup_signal_handlers) {
+      SetupSignalHandlers();
+    }
+
     g_fds->Set(kMojoIPCChannel,
                kMojoIPCChannel + base::GlobalDescriptors::kBaseDescriptor);
 #endif  // !OS_ANDROID
