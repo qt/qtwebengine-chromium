4beafc32ebd7452f0459cfadd59e1f2266f7f8e8
Peter Varga <pvarga@inf.u-szeged.hu>
2017-04-27 11:23:55 +0200

    Update TextSelection for non-user initiated events
    This makes Chromium Content API to be able to notify about text
    selection changes triggered by non-user events (eg. JavaScript, IME,
    autofill).
    
    Based on:
    https://codereview.chromium.org/2903833002
    
    Corresponding Chromium bug report:
    https://bugs.chromium.org/p/chromium/issues/detail?id=671986
    
    Change-Id: I86a7f203d789853199469b301facd06fe5ba54bd
    Task-number: QTBUG-53134
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

--- chromium/content/renderer/render_widget.cc
+++ chromium/content/renderer/render_widget.cc
@@ -1775,6 +1775,7 @@ void RenderWidget::OnImeSetComposition(
   }
 #endif
   ImeEventGuard guard(this);
+  input_handler_->set_ime_composition_replacement(replacement_range.IsValid());
   blink::WebInputMethodController* controller = GetInputMethodController();
   if (!controller ||
       !controller->SetComposition(
@@ -1791,6 +1792,7 @@ void RenderWidget::OnImeSetComposition(
       host->ImeCancelComposition();
     }
   }
+  input_handler_->set_ime_composition_replacement(false);
   UpdateCompositionInfo(false /* not an immediate request */);
 }
 
