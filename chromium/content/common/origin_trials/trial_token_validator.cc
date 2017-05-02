45119155e59d60cbc2ace75834355f7c6891fbf2
Viktor Engelmann <viktor.engelmann@qt.io>
2017-05-02 15:48:46 +0200

    Allow Service Workers without HTTP headers (again)
    Service Workers may be loaded over different schemes than HTTP(S)
    (chromium itself provides interfaces to register schemes for this!).
    Despite this, chromium assumes here, that Service Workers
    were always loaded over HTTP(S). The code for registering Service
    Workers blindly dereferences the requests "header" pointer
    without checking it for NULL beforehand.
    
    Task-number: QTBUG-56169
    Change-Id: If7ba87fbf0cc032cc3b3c34d9d4e0d1041c93178
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/content/common/origin_trials/trial_token_validator.cc
+++ chromium/content/common/origin_trials/trial_token_validator.cc
@@ -98,11 +98,13 @@ TrialTokenValidator::GetValidTokensFromHeaders(
 
   size_t iter = 0;
   std::string token;
-  while (headers->EnumerateHeader(&iter, "Origin-Trial", &token)) {
-    std::string token_feature;
-    if (TrialTokenValidator::ValidateToken(token, origin, &token_feature) ==
-        blink::WebOriginTrialTokenStatus::Success) {
-      (*tokens)[token_feature].push_back(token);
+  if (headers) {
+    while (headers->EnumerateHeader(&iter, "Origin-Trial", &token)) {
+      std::string token_feature;
+      if (TrialTokenValidator::ValidateToken(token, origin, &token_feature) ==
+          blink::WebOriginTrialTokenStatus::Success) {
+        (*tokens)[token_feature].push_back(token);
+      }
     }
   }
   return tokens;
