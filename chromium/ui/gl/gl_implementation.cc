7581d79e73307c15862a278033eac484a57916c0
Alexandru Croitor <alexandru.croitor@qt.io>
2017-05-17 16:53:20 +0200

    Add switch option for using OpenGL Core Profile in Chromium
    By default the Core Profile is only used on macOS if no --use-gl
    command line argument is passed. Depending on this behavior is a bit
    risky, thus a new switch option called "opengl_core_profile" is
    introduced to allow explicitly using the core profile, for example
    when a default QSurfaceFormat is set.
    
    Task-number: QTBUG-60605
    Change-Id: Ie927432ab555f0cf4b35c74326c53d0232f98f6a
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/ui/gl/gl_implementation.cc
+++ chromium/ui/gl/gl_implementation.cc
@@ -31,6 +31,7 @@ const struct {
   GLImplementation implementation;
 } kGLImplementationNamePairs[] = {
     {kGLImplementationDesktopName, kGLImplementationDesktopGL},
+    {kGLImplementationCoreProfileName, kGLImplementationDesktopGLCoreProfile},
     {kGLImplementationOSMesaName, kGLImplementationOSMesaGL},
     {kGLImplementationSwiftShaderName, kGLImplementationSwiftShaderGL},
 #if defined(OS_MACOSX)
