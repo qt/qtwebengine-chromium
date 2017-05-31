800c6b0b3f4280415f6246904a98569d2ab66ad1
Allan Sandfeld Jensen <allan.jensen@qt.io>
2017-05-31 16:15:03 +0200

    Silence gcc -Waddress warnings in logging.h
    We were getting too many of these, poisoning our compile output.
    
    Change-Id: Ic7e41e2bb538bf69a873e32217f87fd369317a20
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/base/logging.h
+++ chromium/base/logging.h
@@ -648,6 +648,11 @@ class CheckOpResult {
 
 #endif  // !(OFFICIAL_BUILD && NDEBUG)
 
+#ifdef __GNUC__
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Waddress"
+#endif // __GNUC__
+
 // This formats a value for a failing CHECK_XX statement.  Ordinarily,
 // it uses the definition for operator<<, with a few special cases below.
 template <typename T>
@@ -682,6 +687,10 @@ inline typename std::enable_if<
 MakeCheckOpValueString(std::ostream* os, const T& v) {
   (*os) << static_cast<typename std::underlying_type<T>::type>(v);
 }
+
+#ifdef __GNUC__
+#pragma GCC diagnostic pop
+#endif
 
 // We need an explicit overload for std::nullptr_t.
 BASE_EXPORT void MakeCheckOpValueString(std::ostream* os, std::nullptr_t p);
