16d6f847afde13dd42b2ae03d585ded6af44ac9d
Alexandru Croitor <alexandru.croitor@qt.io>
2017-08-04 15:18:10 +0200

    [Windows] Disable warnings about mf.dll
    Currently when building a debug configuration of QtWebEngine with
    proprietary codecs enabled, running an application will display a
    "mf.dll is required for hardware video decoding" warning.
    This is due to the fact that the
    "DXVAVideoDecodeAccelerator::PreSandboxInitialization" method
    which loads the respective shared library is not called at all.
    
    The only call site is in ContentSandboxHelper::PreSandboxStartup,
    which is not called by Qt code, because we do not use a separate
    GPU process, and the in-process-gpu-thread code lacks the sandbox
    initialization.
    
    In Qt's case, adding the missing code to the in-process-gpu thread
    does not make sense due to the following reasons:
    1) The DXVA accelarator depends on DirectX API being available via
       ANGLE / EGL extensions. When running in Desktop OpenGL mode,
       ANGLE is not available, and thus initialization of DXVA fails.
    2) When running in ANGLE mode, Qt disables GPU rendering completely,
       because ANGLE does not support multi-threaded access to OpenGL
       contexts. Because GPU rendering is disabled, so is DXVA support.
    
    Thus this change simply removes the warning output, so as to not
    confuse users that something is misconfigured with their QtWebEngine
    build.
    
    And disabling the warning message "mf.dll is required for encoding"
    upon application startup.
    
    Change-Id: Ib91f6e4753aa811d366c741cf17220d2892cef2d
    Reviewed-by: Kai Koehne <kai.koehne@qt.io>
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- chromium/media/gpu/windows/dxva_video_decode_accelerator_win.cc
+++ chromium/media/gpu/windows/dxva_video_decode_accelerator_win.cc
@@ -1229,7 +1229,9 @@ DXVAVideoDecodeAccelerator::GetSupportedProfiles(
     if (!::GetModuleHandle(mfdll)) {
       // Windows N is missing the media foundation DLLs unless the media
       // feature pack is installed.
+#if !defined(TOOLKIT_QT)
       DVLOG(ERROR) << mfdll << " is required for hardware video decoding";
+#endif
       return profiles;
     }
   }
