e284f716a43f88358e6883840900bdb907d1132d
Allan Sandfeld Jensen <allan.jensen@qt.io>
2017-08-08 15:09:20 +0200

    [Backport] Fix for CVE-2017-5095
    M60: Fix a buffer overflow in FPDFPage_Flatten().
    
    BUG=chromium:732661
    TBR=dsinclair@chromium.org
    
    Change-Id: I19d4929570b9e7795457ffef673f6ac629662547
    Reviewed-on: https://pdfium-review.googlesource.com/6555
    Reviewed-on: https://pdfium-review.googlesource.com/7231
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/third_party/pdfium/fpdfsdk/fpdf_flatten.cpp
+++ chromium/third_party/pdfium/fpdfsdk/fpdf_flatten.cpp
@@ -304,15 +304,18 @@ DLLEXPORT int STDCALL FPDFPage_Flatten(FPDF_PAGE page, int nFlag) {
     pRes->SetFor("XObject", pPageXObject);
   }
 
-  CFX_ByteString key = "";
+  CFX_ByteString key;
   int nStreams = pdfium::CollectionSize<int>(ObjectArray);
   if (nStreams > 0) {
-    for (int iKey = 0; /*iKey < 100*/; iKey++) {
-      char sExtend[5] = {};
-      FXSYS_itoa(iKey, sExtend, 10);
-      key = CFX_ByteString("FFT") + CFX_ByteString(sExtend);
-      if (!pPageXObject->KeyExist(key))
+    CFX_ByteString sKey;
+    int i = 0;
+    while (i < INT_MAX) {
+      sKey.Format("FFT%d", i);
+      if (!pPageXObject->KeyExist(sKey)) {
+        key = sKey;
         break;
+      }
+      ++i;
     }
   }
 
