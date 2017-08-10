be826db4caa4accd1962a05d003d972d2e276a9d
Peter Varga <pvarga@inf.u-szeged.hu>
2017-08-10 11:37:05 +0200

    Fix Renderer and GPU threads on windows
    These threads are using the UI message loop on Windows per default. This
    won't work since the UI message loop is handled by Qt and the Renderer
    and GPU threads won't work with it properly.
    Force these threads for using the default message loop as they use it on
    Linux platform.
    
    Change-Id: I8b04ba9070912378cf1d13173ddff48e98f9a49b
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- chromium/content/browser/renderer_host/render_process_host_impl.cc
+++ chromium/content/browser/renderer_host/render_process_host_impl.cc
@@ -1826,7 +1826,7 @@ bool RenderProcessHostImpl::Init() {
             &mojo_invitation_, child_connection_->service_token())));
 
     base::Thread::Options options;
-#if defined(OS_WIN) && !defined(OS_MACOSX)
+#if defined(OS_WIN) && !defined(OS_MACOSX) && !defined(TOOLKIT_QT)
     // In-process plugins require this to be a UI message loop.
     options.message_loop_type = base::MessageLoop::TYPE_UI;
 #else
