c0ca7c4dac3422a8a146cad2e58d5b664ede2969
Peter Varga <pvarga@inf.u-szeged.hu>
2017-08-10 11:37:05 +0200

    Fix Renderer and GPU threads on windows
    These threads are using the UI message loop on Windows per default. This
    won't work since the UI message loop is handled by Qt and the Renderer
    and GPU threads won't work with it properly.
    Force these threads for using the default message loop as they use it on
    Linux platform.
    
    Change-Id: I8b04ba9070912378cf1d13173ddff48e98f9a49b
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- chromium/content/browser/gpu/gpu_process_host.cc
+++ chromium/content/browser/gpu/gpu_process_host.cc
@@ -597,7 +597,7 @@ bool GpuProcessHost::Init() {
             process_->child_connection()->service_token()),
         gpu_preferences));
     base::Thread::Options options;
-#if defined(OS_WIN)
+#if defined(OS_WIN) && !defined(TOOLKIT_QT)
     // WGL needs to create its own window and pump messages on it.
     options.message_loop_type = base::MessageLoop::TYPE_UI;
 #endif
--- chromium/content/browser/renderer_host/render_process_host_impl.cc
+++ chromium/content/browser/renderer_host/render_process_host_impl.cc
@@ -1515,7 +1515,7 @@ bool RenderProcessHostImpl::Init() {
             child_connection_->service_token())));
 
     base::Thread::Options options;
-#if defined(OS_WIN) && !defined(OS_MACOSX)
+#if defined(OS_WIN) && !defined(OS_MACOSX) && !defined(TOOLKIT_QT)
     // In-process plugins require this to be a UI message loop.
     options.message_loop_type = base::MessageLoop::TYPE_UI;
 #else
