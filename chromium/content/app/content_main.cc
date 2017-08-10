45350f1395bcb751b8565b3abde7de1580f344ff
Allan Sandfeld Jensen <allan.jensen@qt.io>
2017-08-10 12:29:07 +0200

    Disable termination on out of memory.
    By default chromium installs a new_handler that calls exit() when
    new/malloc fail to allocate memory. While this is a reasonable thing
    to do for a browser, a library should leave the decision to the user.
    
    Task-number: QTBUG-45459
    Change-Id: Ice5ff8a3347a9df836fb298fed1e80bf21e6b7dc
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- chromium/content/app/content_main.cc
+++ chromium/content/app/content_main.cc
@@ -282,7 +282,9 @@ RunContentProcess(ContentMainParams params,
 #if BUILDFLAG(IS_MAC) && BUILDFLAG(USE_ALLOCATOR_SHIM)
     base::allocator::InitializeAllocatorShim();
 #endif
+#if !defined(TOOLKIT_QT)
     base::EnableTerminationOnOutOfMemory();
+#endif
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
     // The various desktop environments set this environment variable that
