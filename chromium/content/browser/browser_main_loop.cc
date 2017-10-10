0173ddec6bc8eca9ba02a42ed1c3d69b551dbbe6
Allan Sandfeld Jensen <allan.jensen@qt.io>
2017-10-10 15:11:42 +0200

    Fix Chromium built with X11 running without
    Handles cases where gfx::GetXDisplay() returns null at least for Qt.
    
    Task-number: QTBUG-55384
    Change-Id: Ie57c57d7189c279cb5c702325b450f9fb964ac60
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/content/browser/browser_main_loop.cc
+++ chromium/content/browser/browser_main_loop.cc
@@ -444,7 +444,7 @@ class GpuDataManagerVisualProxy : public GpuDataManagerObserver {
   }
 
   void OnGpuInfoUpdate() override {
-    if (base::CommandLine::ForCurrentProcess()->HasSwitch(switches::kHeadless))
+    if (base::CommandLine::ForCurrentProcess()->HasSwitch(switches::kHeadless) || !gfx::GetXDisplay())
       return;
     gpu::GPUInfo gpu_info = gpu_data_manager_->GetGPUInfo();
     if (!ui::XVisualManager::GetInstance()->OnGPUInfoChanged(
@@ -605,7 +605,7 @@ int BrowserMainLoop::EarlyInitialization() {
   DCHECK(SandboxHostLinux::GetInstance()->IsInitialized());
 #endif
 
-#if defined(USE_X11)
+#if defined(USE_X11) && !defined(TOOLKIT_QT)
   if (UsingInProcessGpu()) {
     if (!gfx::InitializeThreadedX11()) {
       LOG(ERROR) << "Failed to put Xlib into threaded mode.";
@@ -1506,7 +1506,7 @@ bool BrowserMainLoop::InitializeToolkit() {
 
 #if defined(USE_AURA)
 
-#if defined(USE_X11)
+#if defined(USE_X11) && !defined(TOOLKIT_QT)
   if (!parsed_command_line_.HasSwitch(switches::kHeadless) &&
       !gfx::GetXDisplay()) {
     LOG(ERROR) << "Unable to open X display.";
