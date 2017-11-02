2ed76568020b457e78753c1758f0e2437e81c846
Allan Sandfeld Jensen <allan.jensen@qt.io>
2017-11-02 14:14:03 +0100

    [Backport] Fix for CVE-2017-5125 [2/2]
    Fix poor handling of nullptr for new blur code
    
    BUG=chromium:749147
    
    Change-Id: Iab051bd7b3b257d10d2a45c709dc7bde802bc524
    Reviewed-on: https://skia-review.googlesource.com/37121
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/third_party/skia/src/effects/SkBlurMask.cpp
+++ chromium/third_party/skia/src/effects/SkBlurMask.cpp
@@ -591,6 +591,10 @@ bool SkBlurMask::BoxBlur(SkMask* dst, const SkMask& src,
 #else
     SkMaskBlurFilter blurFilter{sigma, sigma};
     border = blurFilter.blur(src, dst);
+    // If src.fImage is null, then this call is only to calculate the border.
+    if (src.fImage != nullptr && dst->fImage == nullptr) {
+        return false;
+    }
 #endif  // SK_SUPPORT_LEGACY_MASK_BLUR
 
     if (src.fImage != nullptr) {
