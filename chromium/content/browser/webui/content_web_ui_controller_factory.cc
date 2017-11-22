484214209b7fdab43fdb0539fd99e468b4bbfc87
Allan Sandfeld Jensen <allan.jensen@qt.io>
2017-11-22 16:28:21 +0100

    Do not bundle the tracing UI resources
    We do not support loading the chrome://tracing WebUI and therefore
    we shouldn't bundle those resources in our library.
    
    Change-Id: If3aa4c676b22a55e772f19487e21b4cd48babe65
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/content/browser/webui/content_web_ui_controller_factory.cc
+++ chromium/content/browser/webui/content_web_ui_controller_factory.cc
@@ -33,7 +33,7 @@ WebUI::TypeID ContentWebUIControllerFactory::GetWebUIType(
     return WebUI::kNoWebUI;
 
   if (url.host_piece() == kChromeUIWebRTCInternalsHost ||
-#if !defined(OS_ANDROID)
+#if !defined(OS_ANDROID) && !defined(TOOLKIT_QT)
       url.host_piece() == kChromeUITracingHost ||
 #endif
       url.host_piece() == kChromeUIGpuHost ||
@@ -81,7 +81,7 @@ ContentWebUIControllerFactory::CreateWebUIControllerForURL(WebUI* web_ui,
     return std::make_unique<ServiceWorkerInternalsUI>(web_ui);
   if (url.host_piece() == kChromeUINetworkErrorsListingHost)
     return std::make_unique<NetworkErrorsListingUI>(web_ui);
-#if !defined(OS_ANDROID)
+#if !defined(OS_ANDROID)&& !defined(TOOLKIT_QT)
   if (url.host_piece() == kChromeUITracingHost)
     return std::make_unique<TracingUI>(web_ui);
 #endif
