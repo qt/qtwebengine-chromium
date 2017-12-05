7751b8cb0a03771678d6cb12e3cebf5494fcb505
Michael Brüning <michael.bruning@qt.io>
2017-12-05 14:45:04 +0100

    Add extensions to Qt build and fix it up for building
    Jumbo patch that includes all the patches needed to enable the
    extension system for the Qt build. Currently used for conveniently
    moving the code around, but may become the actual change to get it
    merged upstream.
    
    Change-Id: Ie1920b31f9bbf40c8753cada2716d7f9fcb39280
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/extensions/browser/extension_web_contents_observer.cc
+++ chromium/extensions/browser/extension_web_contents_observer.cc
@@ -65,6 +65,7 @@ ExtensionWebContentsObserver::CreateExtensionFrameHost(
 
 void ExtensionWebContentsObserver::ListenToWindowIdChangesFrom(
     sessions::SessionTabHelper* helper) {
+#if !defined(TOOLKIT_QT)
   if (!window_id_subscription_) {
     // We use an unretained receiver here: the callback is inside the
     // subscription, which is a member of |this|, so it can't be run after the
@@ -73,6 +74,7 @@ void ExtensionWebContentsObserver::ListenToWindowIdChangesFrom(
         base::BindRepeating(&ExtensionWebContentsObserver::OnWindowIdChanged,
                             base::Unretained(this)));
   }
+#endif //  !defined(TOOLKIT_QT)
 }
 
 void ExtensionWebContentsObserver::Initialize() {
@@ -93,12 +95,14 @@ void ExtensionWebContentsObserver::Initialize() {
       },
       this));
 
+#if !defined(TOOLKIT_QT)
   // It would be ideal if SessionTabHelper was created before this object,
   // because then we could start observing it here instead of needing to be
   // externally notified when it is created, but it isn't. If that ordering ever
   // changes, this code can be restructured and ListenToWindowIdChangesFrom()
   // can become private.
   DCHECK(!sessions::SessionTabHelper::FromWebContents(web_contents()));
+#endif //  !defined(TOOLKIT_QT)
 }
 
 ExtensionWebContentsObserver::ExtensionWebContentsObserver(
@@ -223,13 +227,17 @@ void ExtensionWebContentsObserver::ReadyToCommitNavigation(
       parent_or_outerdoc ? parent_or_outerdoc->GetOutermostMainFrame()
                          : navigation_handle->GetRenderFrameHost();
 
+#ifndef TOOLKIT_QT
   const Extension* const extension =
       GetExtensionFromFrame(outermost_main_rfh, false);
   KioskDelegate* const kiosk_delegate =
       ExtensionsBrowserClient::Get()->GetKioskDelegate();
   DCHECK(kiosk_delegate);
   bool is_kiosk =
-      extension && kiosk_delegate->IsAutoLaunchedKioskApp(extension->id());
+      extension && kiosk_delegate && kiosk_delegate->IsAutoLaunchedKioskApp(extension->id());
+#else
+  bool is_kiosk = false;
+#endif
 
   // If the top most frame is an extension, packaged app, hosted app, etc. then
   // the main frame and all iframes should be able to autoplay without
