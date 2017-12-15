182b8da88adc47766c11cf8547524429e6b0ba5b
Alexandru Croitor <alexandru.croitor@qt.io>
2017-12-15 19:14:18 +0100

    Fix hanging of process when application is closed too fast
    What essentially happens is that the main thread is waiting for all
    other threads end, whereas an IO thread tries to recreate the
    in-process GPU thread, fails, tries again, fails and so on.
    
    The fix is to move the condition that checks that the application main
    loop has not finished, earlier.
    
    Task-number: QTBUG-65239
    Change-Id: I5800d19b986657a249b1f672c95297d01f8dc11b
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/content/browser/gpu/gpu_process_host.cc
+++ chromium/content/browser/gpu/gpu_process_host.cc
@@ -556,6 +556,12 @@ GpuProcessHost* GpuProcessHost::Get(GpuProcessKind kind, bool force_create) {
                           ? BrowserThread::UI
                           : BrowserThread::IO);
 
+  // Do not create a new process if browser is shutting down.
+  if (BrowserMainRunner::ExitedMainMessageLoop()) {
+    DLOG(ERROR) << "BrowserMainRunner::ExitedMainMessageLoop()";
+    return nullptr;
+  }
+
   // Do not launch the unsandboxed GPU info collection process if GPU is
   // disabled
   if (kind == GPU_PROCESS_KIND_INFO_COLLECTION) {
@@ -571,12 +577,6 @@ GpuProcessHost* GpuProcessHost::Get(GpuProcessKind kind, bool force_create) {
 
   if (!force_create)
     return nullptr;
-
-  // Do not create a new process if browser is shutting down.
-  if (BrowserMainRunner::ExitedMainMessageLoop()) {
-    DLOG(ERROR) << "BrowserMainRunner::ExitedMainMessageLoop()";
-    return nullptr;
-  }
 
   static int last_host_id = 0;
   int host_id;
