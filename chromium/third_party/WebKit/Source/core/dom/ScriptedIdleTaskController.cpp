17e4aafb6fe894047b46abeb3c3b8290de4094cf
Allan Sandfeld Jensen <allan.jensen@qt.io>
2018-01-15 11:13:11 +0100

    [Backport] Clamp performance.now() to 100us.
    This patch reduces the resolution of performance.now() from 5us to 100us
    and adds pseudorandom jitter on top.
    
    TBR=skyostil@chromium.org
    
    (cherry picked from commit a77687fd89adc1bc2ce91921456e0b9b59388120)
    
    Authors: Ross McIlroy <rmcilroy@chromium.org>, Sami Kyostila <skyostil@chromium.org>
    Bug: 798964
    Reviewed-on: https://chromium-review.googlesource.com/849993
    Commit-Queue: Sami Kyöstilä <skyostil@chromium.org>
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#527008}
    Reviewed-on: https://chromium-review.googlesource.com/853505
    Reviewed-by: Sami Kyöstilä <skyostil@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3282@{#439}
    Cr-Branched-From: 5fdc0fab22ce7efd32532ee989b223fa12f8171e-refs/heads/master@{#520840}
    
    Change-Id: Ia7e1171e1505ddc73cb5356fcc0aac2466f49e08
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/third_party/WebKit/Source/core/dom/ScriptedIdleTaskController.cpp
+++ chromium/third_party/WebKit/Source/core/dom/ScriptedIdleTaskController.cpp
@@ -35,7 +35,6 @@ class IdleRequestCallbackWrapper
   static void IdleTaskFired(
       PassRefPtr<IdleRequestCallbackWrapper> callback_wrapper,
       double deadline_seconds) {
-    // TODO(rmcilroy): Implement clamping of deadline in some form.
     if (ScriptedIdleTaskController* controller =
             callback_wrapper->Controller()) {
       // If we are going to yield immediately, reschedule the callback for
