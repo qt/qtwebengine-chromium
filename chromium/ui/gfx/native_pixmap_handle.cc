05c869e2a275e0ad487bc1110026c17df7ac4cec
Allan Sandfeld Jensen <allan.jensen@qt.io>
2018-02-15 17:57:59 +0100

    Avoid using libdrm
    Change-Id: I62f1048995935995a4f9c84bf636624c6d5ec14c
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- chromium/ui/gfx/native_pixmap_handle.cc
+++ chromium/ui/gfx/native_pixmap_handle.cc
@@ -4,14 +4,14 @@
 
 #include "ui/gfx/native_pixmap_handle.h"
 
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) && !defined(TOOLKIT_QT)
 #include <drm_fourcc.h>
 #include "base/posix/eintr_wrapper.h"
 #endif
 
 namespace gfx {
 
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) && !defined(TOOLKIT_QT)
 static_assert(NativePixmapPlane::kNoModifier == DRM_FORMAT_MOD_INVALID,
               "gfx::NativePixmapPlane::kNoModifier should be an alias for"
               "DRM_FORMAT_MOD_INVALID");
@@ -36,7 +36,7 @@ NativePixmapHandle::NativePixmapHandle(const NativePixmapHandle& other) =
 
 NativePixmapHandle::~NativePixmapHandle() {}
 
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) && !defined(TOOLKIT_QT)
 NativePixmapHandle CloneHandleForIPC(const NativePixmapHandle& handle) {
   NativePixmapHandle clone;
   std::vector<base::ScopedFD> scoped_fds;
