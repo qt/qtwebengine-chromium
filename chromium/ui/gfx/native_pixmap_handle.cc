615780860e9cdd15e332292b36b245c3232371cf
Allan Sandfeld Jensen <allan.jensen@qt.io>
2018-02-15 17:57:59 +0100

    Avoid using libdrm
    Change-Id: I62f1048995935995a4f9c84bf636624c6d5ec14c
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- chromium/ui/gfx/native_pixmap_handle.cc
+++ chromium/ui/gfx/native_pixmap_handle.cc
@@ -10,7 +10,9 @@
 #include "build/build_config.h"
 
 #if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#if !defined(TOOLKIT_QT)
 #include <drm_fourcc.h>
+#endif
 #include <unistd.h>
 
 #include "base/posix/eintr_wrapper.h"
@@ -23,7 +25,7 @@
 
 namespace gfx {
 
-#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+#if (BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)) && !defined(TOOLKIT_QT)
 static_assert(NativePixmapHandle::kNoModifier == DRM_FORMAT_MOD_INVALID,
               "gfx::NativePixmapHandle::kNoModifier should be an alias for"
               "DRM_FORMAT_MOD_INVALID");
