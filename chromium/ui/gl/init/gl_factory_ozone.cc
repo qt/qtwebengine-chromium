6b8844832dd6f83e9a933cb092f67b700b38abc5
Michal Klocek <michal.klocek@qt.io>
2018-02-19 12:54:27 +0100

    Switch to use glx over ozone
    Use ozone glx instead of x11 glx.
    
    Task-number: QTBUG-65682
    Change-Id: I612ff7993812607a170a42c4fe72abd9913ef0c5
    Reviewed-by: Kai Koehne <kai.koehne@qt.io>
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/ui/gl/init/gl_factory_ozone.cc
+++ chromium/ui/gl/init/gl_factory_ozone.cc
@@ -57,7 +57,6 @@ bool GetGLWindowSystemBindingInfo(const GLVersionInfo& gl_info,
   return false;
 }
 
-#if !defined(TOOLKIT_QT)
 scoped_refptr<GLContext> CreateGLContext(GLShareGroup* share_group,
                                          GLSurface* compatible_surface,
                                          const GLContextAttribs& attribs) {
@@ -165,7 +164,6 @@ scoped_refptr<GLSurface> CreateOffscreenGLSurfaceWithFormat(
 
   return nullptr;
 }
-#endif
 
 void SetDisabledExtensionsPlatform(const std::string& disabled_extensions) {
 #if defined(USE_X11)
