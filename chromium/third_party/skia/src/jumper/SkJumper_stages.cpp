91bd0b098805de154642190a759c71904069de88
Allan Sandfeld Jensen <allan.jensen@qt.io>
2018-02-20 12:53:16 +0100

    Fix building on aarch64 without fp16
    Change-Id: I51bd51966cff7aa7a79370b6c7321acfd746754a
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/third_party/skia/src/jumper/SkJumper_stages.cpp
+++ chromium/third_party/skia/src/jumper/SkJumper_stages.cpp
@@ -666,8 +666,8 @@ SI F approx_powf(F x, F y) {
 }
 
 SI F from_half(U16 h) {
-#if defined(__aarch64__) && !defined(SK_BUILD_FOR_GOOGLE3)  // Temporary workaround for some Google3 builds.
-    return vcvt_f32_f16(h);
+#if defined(__ARM_FP16_FORMAT_IEEE)
+    return float(reinterpret_cast<__fp16>(h));
 
 #elif defined(JUMPER_IS_HSW) || defined(JUMPER_IS_AVX512)
     return _mm256_cvtph_ps(h);
@@ -686,8 +686,8 @@ SI F from_half(U16 h) {
 }
 
 SI U16 to_half(F f) {
-#if defined(__aarch64__) && !defined(SK_BUILD_FOR_GOOGLE3)  // Temporary workaround for some Google3 builds.
-    return vcvt_f16_f32(f);
+#if defined(__ARM_FP16_FORMAT_IEEE)
+    return reinterpret_cast<U16>(__fp16(f));
 
 #elif defined(JUMPER_IS_HSW) || defined(JUMPER_IS_AVX512)
     return _mm256_cvtps_ph(f, _MM_FROUND_CUR_DIRECTION);
