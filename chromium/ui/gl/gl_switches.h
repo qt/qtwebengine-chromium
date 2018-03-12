2d8455ff3851fee7d16b288ae3f66e4e86c144ae
Michal Klocek <michal.klocek@qt.io>
2018-03-12 15:38:02 +0100

    Fix freezes of glx context
    Add back support for compatibility profile.
    
    Change-Id: I746b53c4622614f847540481cdb43da416ba03b1
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/ui/gl/gl_switches.h
+++ chromium/ui/gl/gl_switches.h
@@ -75,7 +75,7 @@ GL_EXPORT extern const char kOverrideUseSoftwareGLForTests[];
 
 GL_EXPORT extern const char* const kGLSwitchesCopiedFromGpuProcessHost[];
 GL_EXPORT extern const int kGLSwitchesCopiedFromGpuProcessHostNumSwitches;
-
+GL_EXPORT extern const char kCreateDefaultGLContext[];
 }  // namespace switches
 
 namespace features {
