ff5f8b73a15920ff133b6d397ed74a6bc1786c7c
Michal Klocek <michal.klocek@qt.io>
2018-03-13 12:54:35 +0100

    [Backport] Security Bug 789764
    [parser] Fix func numbering inside for in.
    
    BUG=chromium:789764
    
    Change-Id: I6a466660159721683c4979af32019d740094151b
    Reviewed-on: https://chromium-review.googlesource.com/803217
    Reviewed-by: Ross McIlroy <rmcilroy@chromium.org>
    Reviewed-by: Jochen Eisinger <jochen@chromium.org>
    Commit-Queue: Marja Hölttä <marja@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#49795}
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/v8/src/ast/ast-traversal-visitor.h
+++ chromium/v8/src/ast/ast-traversal-visitor.h
@@ -245,6 +245,7 @@ void AstTraversalVisitor<Subclass>::VisitForStatement(ForStatement* stmt) {
 template <class Subclass>
 void AstTraversalVisitor<Subclass>::VisitForInStatement(ForInStatement* stmt) {
   PROCESS_NODE(stmt);
+  RECURSE(Visit(stmt->each()));
   RECURSE(Visit(stmt->enumerable()));
   RECURSE(Visit(stmt->body()));
 }
