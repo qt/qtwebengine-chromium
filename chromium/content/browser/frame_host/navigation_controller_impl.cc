fd5e46c3747d72da53b5427d980b3debfd322243
Allan Sandfeld Jensen <allan.jensen@qt.io>
2018-04-17 16:24:21 +0200

    Pass virtual url of data url request back to the UI
    Otherwise we lose information about interpage navigation.
    This fixes a regression after Chromium switched to browser-side
    navigation
    
    Task-number: QTBUG-67701
    Change-Id: I10659699f1acc033948e297e3f7319ee44d6423f
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

--- chromium/content/browser/frame_host/navigation_controller_impl.cc
+++ chromium/content/browser/frame_host/navigation_controller_impl.cc
@@ -1474,6 +1474,8 @@ void NavigationControllerImpl::RendererDidNavigateToNewPage(
   new_entry->SetURL(params.url);
   if (update_virtual_url)
     UpdateVirtualURLToURL(new_entry.get(), params.url);
+  else if (!params.virtual_url.is_empty())
+    new_entry->SetVirtualURL(params.virtual_url);
   new_entry->SetReferrer(params.referrer);
   new_entry->SetTransitionType(params.transition);
   new_entry->set_site_instance(
@@ -1671,6 +1673,8 @@ void NavigationControllerImpl::RendererDidNavigateToExistingPage(
   entry->SetReferrer(params.referrer);
   if (entry->update_virtual_url_with_url())
     UpdateVirtualURLToURL(entry, params.url);
+  else if (!params.virtual_url.is_empty())
+    entry->SetVirtualURL(params.virtual_url);
 
   entry->SetOriginalRequestURL(params.original_request_url);
 
@@ -1736,6 +1740,8 @@ void NavigationControllerImpl::RendererDidNavigateToSamePage(
                                                           : PAGE_TYPE_NORMAL);
   if (existing_entry->update_virtual_url_with_url())
     UpdateVirtualURLToURL(existing_entry, params.url);
+  else if (!params.virtual_url.is_empty())
+    existing_entry->SetVirtualURL(params.virtual_url);
   existing_entry->SetURL(params.url);
 
   // If a user presses enter in the omnibox and the server redirects, the URL
