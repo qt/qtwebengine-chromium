5c09e88abac9b3c67e45ab3a80d66f52bb87a5f3
Jüri Valdmann <juri.valdmann@qt.io>
2018-04-19 12:14:44 +0200

    Extend url library for WebEngine custom schemes
    Adds (another) parallel scheme registry in url/url_util_qt, which is
    then used
    in Chromium and Blink to specialize URL handling for WebEngine custom
    schemes.
    
    The registry is transmitted from the main process to subprocesses in a
    new
    command line flag (--webengine-schemes), since the scheme lists in
    url/url_util
    are locked before IPC is initialized.
    
    Task-number: QTBUG-62536
    Change-Id: Id26811a18d4c740cc4d281d2da5720304a235a41
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/third_party/blink/renderer/platform/weborigin/scheme_registry.cc
+++ chromium/third_party/blink/renderer/platform/weborigin/scheme_registry.cc
@@ -33,6 +33,7 @@
 #include "third_party/blink/renderer/platform/wtf/thread_specific.h"
 #include "third_party/blink/renderer/platform/wtf/threading.h"
 #include "url/url_util.h"
+#include "url/url_util_qt.h"
 
 namespace blink {
 
@@ -76,6 +77,15 @@ class URLSchemesRegistry final {
     }
     for (auto& scheme : url::GetEmptyDocumentSchemes())
       empty_document_schemes.insert(scheme.c_str());
+
+    // NOTE(juvaldma)(Chromium 67.0.3396.47)
+    //
+    // Non-blink Chromium has it's own version of this list (see
+    // content::RegisterContentSchemes).
+    for (auto& cs : url::CustomScheme::GetSchemes()) {
+      if (cs.flags & url::CustomScheme::ServiceWorkersAllowed)
+        service_worker_schemes.insert(String(cs.name.c_str()));
+    }
   }
   ~URLSchemesRegistry() = default;
 
--- chromium/third_party/blink/renderer/platform/weborigin/security_origin.h
+++ chromium/third_party/blink/renderer/platform/weborigin/security_origin.h
@@ -105,6 +105,7 @@ class PLATFORM_EXPORT SecurityOrigin : public RefCounted<SecurityOrigin> {
 
   static scoped_refptr<SecurityOrigin> CreateFromUrlOrigin(const url::Origin&);
   url::Origin ToUrlOrigin() const;
+  bool IsBroken() const;
 
   SecurityOrigin(const SecurityOrigin&) = delete;
   SecurityOrigin& operator=(const SecurityOrigin&) = delete;
@@ -446,7 +447,7 @@ class PLATFORM_EXPORT SecurityOrigin : public RefCounted<SecurityOrigin> {
   const String protocol_ = g_empty_string;
   const String host_ = g_empty_string;
   String domain_ = g_empty_string;
-  const uint16_t port_ = 0;
+  uint16_t port_ = 0;
   const std::optional<url::Origin::Nonce> nonce_if_opaque_;
   bool universal_access_ = false;
   bool domain_was_set_in_dom_ = false;
