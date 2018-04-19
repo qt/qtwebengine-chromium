adb836fcdfcb777da5be55728b9e618c44af2402
Allan Sandfeld Jensen <allan.jensen@qt.io>
2018-04-19 13:39:18 +0200

    [Backport] Oilpan: Disable prompty free for weak collection backings
    In anticipation for incremental marking, disable promptly free for weak collection backings. This is required to avoid callbacks to objects that can be destroyed during the mutator between incremental marking.
    
    This CL:
    - Disable prompty free for weak hash table backings
    - Adds CheckObjectNotInCallbackStacks which checks that an object doesn't have any callback registered
    
    Bug: 757440
    Change-Id: I4364a46f7d532df65422fbc341a3986ec57eef1a
    Reviewed-on: https://chromium-review.googlesource.com/836299
    (CVE-2018-6094) 2/3
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/third_party/WebKit/Source/platform/heap/ThreadState.h
+++ chromium/third_party/WebKit/Source/platform/heap/ThreadState.h
@@ -269,8 +269,7 @@ class PLATFORM_EXPORT ThreadState {
   void IncrementalMarkingFinalize();
 
   bool IsIncrementalMarkingInProgress() const {
-    return GcState() == kIncrementalMarkingStartScheduled ||
-           GcState() == kIncrementalMarkingStepScheduled ||
+    return GcState() == kIncrementalMarkingStepScheduled ||
            GcState() == kIncrementalMarkingFinalizeScheduled;
   }
 
@@ -340,6 +339,8 @@ class PLATFORM_EXPORT ThreadState {
 
   bool IsIncrementalMarking() const { return incremental_marking_; }
   void SetIncrementalMarking(bool value) { incremental_marking_ = value; }
+
+  void CheckObjectNotInCallbackStacks(const void*);
 
   class MainThreadGCForbiddenScope final {
     STACK_ALLOCATED();
