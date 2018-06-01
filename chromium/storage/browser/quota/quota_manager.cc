e1bbcc5262bd6dc83ab335faf86eeaa265842f40
Jüri Valdmann <juri.valdmann@qt.io>
2018-06-01 15:11:38 +0200

    Revert "Allow using quota management API for non-standard URLs"
    This reverts commit c6693cf3bf1457aa1a0607e3a52990ee77831657.
    
    Task-number: QTBUG-62536
    Change-Id: I807a937b2e490cdeb30bc1cfe996fef20ec764ed
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/storage/browser/quota/quota_manager.cc
+++ chromium/storage/browser/quota/quota_manager.cc
@@ -874,6 +874,7 @@ void QuotaManager::GetUsageAndQuotaWithBreakdown(
     const GURL& origin,
     StorageType type,
     UsageAndQuotaWithBreakdownCallback callback) {
+  DCHECK(origin == origin.GetOrigin());
   if (!IsSupportedType(type) ||
       (is_incognito_ && !IsSupportedIncognitoType(type))) {
     std::move(callback).Run(blink::mojom::QuotaStatusCode::kErrorNotSupported,
