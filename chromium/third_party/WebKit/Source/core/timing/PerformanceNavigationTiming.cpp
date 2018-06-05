9b92e684f7dfce1cf50b0acb9fdbd636457a0fd5
Michal Klocek <michal.klocek@qt.io>
2018-06-05 15:12:21 +0200

    [Backport] CVE-2018-6134
    Fix the |name| of PerformanceNavigationTiming
    
    Previously, the |name| of a PerformanceNavigationTiming entry was the initial
    URL (the request URL). After this CL, it is the response URL, so for example
    a url of the form 'redirect?location=newLoc' will have 'newLoc' as the |name|.
    
    Bug: 797465
    
    Reviewed-on: https://chromium-review.googlesource.com/996579
    Change-Id: I7bfbeabe1663d8f12c7b76bda1afd92ba648d193
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/third_party/WebKit/Source/core/timing/PerformanceNavigationTiming.cpp
+++ chromium/third_party/WebKit/Source/core/timing/PerformanceNavigationTiming.cpp
@@ -21,10 +21,11 @@ PerformanceNavigationTiming::PerformanceNavigationTiming(
     ResourceTimingInfo* info,
     double time_origin,
     const WebVector<WebServerTimingInfo>& server_timing)
-    : PerformanceResourceTiming(info ? info->InitialURL().GetString() : "",
-                                "navigation",
-                                time_origin,
-                                server_timing),
+    : PerformanceResourceTiming(
+          info ? info->FinalResponse().Url().GetString() : "",
+          "navigation",
+          time_origin,
+          server_timing),
       ContextClient(frame),
       resource_timing_info_(info) {
   DCHECK(frame);
