b9807813c346dec8f6404109d85cc6e8def9011a
Alexandru Croitor <alexandru.croitor@qt.io>
2018-07-16 18:47:07 +0200

    Fix --single-process --disable-gpu combination not to hit asserts
    Chromium always gives permission to create an in-process GPU thread
    when using --single-process mode, despite having passed --disable-gpu
    switch (for example in ANGLE case).
    
    Remove the condition that gave special treatment to --single-process
    mode in disable-gpu mode.
    
    Task-number: QTBUG-69491
    Change-Id: I8c7edfe38b9c1716cd226b1e0aac8cc5b99d9c78
    Reviewed-by: Joerg Bornemann <joerg.bornemann@qt.io>

--- chromium/content/browser/renderer_host/render_process_host_impl.cc
+++ chromium/content/browser/renderer_host/render_process_host_impl.cc
@@ -1830,7 +1830,10 @@ bool RenderProcessHostImpl::Init() {
   is_dead_ = false;
   sent_render_process_ready_ = false;
 
-  if (gpu_client_)
+  // Don't grant further access to GPU if it is not allowed.
+  GpuDataManagerImpl* gpu_data_manager = GpuDataManagerImpl::GetInstance();
+  DCHECK(gpu_data_manager);
+  if (gpu_client_ && gpu_data_manager->GpuAccessAllowed(nullptr))
     gpu_client_->PreEstablishGpuChannel();
 
   // We may reach Init() during process death notification (e.g.
