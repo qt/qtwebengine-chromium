8fb92d090ddfd84f6b7fd1a098158eb1836d32f3
Allan Sandfeld Jensen <allan.jensen@qt.io>
2018-08-30 17:16:53 +0200

    [Revert] Remove base::internal::PassingTraits
    This simplification doesn't work on MSVC 2017 due to some MSVC bug,
    so stick with the old version for now.
    
    Reverting https://chromium-review.googlesource.com/1118061
    
    Change-Id: I3dff6075f9cc37dcc39c26b4f24de0501c8a362f
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/base/callback.h
+++ chromium/base/callback.h
@@ -60,7 +60,7 @@ class OnceCallback<R(Args...)> : public internal::CallbackBase {
   using ResultType = R;
   using RunType = R(Args...);
   using PolymorphicInvoke = R (*)(internal::BindStateBase*,
-                                  internal::PassingType<Args>...);
+                                  internal::PassingTraitsType<Args>...);
 
   constexpr OnceCallback() = default;
   OnceCallback(std::nullptr_t) = delete;
@@ -107,7 +107,7 @@ class RepeatingCallback<R(Args...)> : public internal::CallbackBaseCopyable {
   using ResultType = R;
   using RunType = R(Args...);
   using PolymorphicInvoke = R (*)(internal::BindStateBase*,
-                                  internal::PassingType<Args>...);
+                                  internal::PassingTraitsType<Args>...);
 
   constexpr RepeatingCallback() = default;
   RepeatingCallback(std::nullptr_t) = delete;
