f923d9032b32773ddcc738fa410e4a6113ec6164
Allan Sandfeld Jensen <allan.jensen@qt.io>
2018-09-18 13:17:51 +0200

    Fix non-desktop Linux builds
    Note this disables configuration of password manage stores for networkservice,
    neither of which we currently use, but we might in the future.
    
    Change-Id: I315d42145726799398dad1abb86220c6030b5a2a
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/services/network/network_service.cc
+++ chromium/services/network/network_service.cc
@@ -672,7 +672,7 @@ void NetworkService::OnCertDBChanged() {
 
 #if defined(OS_LINUX) && !defined(OS_CHROMEOS)
 void NetworkService::SetCryptConfig(mojom::CryptConfigPtr crypt_config) {
-#if !BUILDFLAG(IS_CHROMECAST)
+#if !BUILDFLAG(IS_CHROMECAST) && !defined(TOOLKIT_QT)
   DCHECK(!os_crypt_config_set_);
   auto config = std::make_unique<os_crypt::Config>();
   config->store = crypt_config->store;
