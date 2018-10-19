c0b92372fb04547fbc2cc25da4f92dbc4974736f
Allan Sandfeld Jensen <allan.jensen@qt.io>
2018-10-19 14:56:23 +0200

    Silence most warnings
    Suppress and fix many clang, GCC and MSVC warnings
    
    "cause the night is crap and full of warnings"
    
    Change-Id: I09882f850bac97c04a3d772e5f6386ce896c57b9
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- chromium/base/bind_internal.h
+++ chromium/base/bind_internal.h
@@ -732,6 +732,7 @@ bool QueryCancellationTraitsImpl(BindStateBase::CancellationQueryMode mode,
           functor, std::get<indices>(bound_args)...);
   }
   NOTREACHED();
+  return false;
 }
 
 // Relays |base| to corresponding CallbackCancellationTraits<>::Run(). Returns
