658c0f9a10d4fa0edd66e259fbd32554de0b5c81
Allan Sandfeld Jensen <allan.jensen@qt.io>
2018-10-29 12:45:10 +0100

    Fix selection paste on Linux
    Change-Id: Ib269a80077834c1aff73be75e16e2338c52e23a9
    Fixes: QTBUG-71457
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

--- chromium/ui/base/mojom/clipboard_blink_mojom_traits.h
+++ chromium/ui/base/mojom/clipboard_blink_mojom_traits.h
@@ -27,7 +27,8 @@ struct EnumTraits<blink::mojom::ClipboardBuffer, ui::ClipboardType> {
         *out = ui::ClipboardType::kCopyPaste;
         return true;
       case blink::mojom::ClipboardBuffer::kSelection:
-#if defined(USE_X11)
+#if !defined(OS_WIN) && !defined(OS_MACOSX) && !defined(OS_CHROMEOS)
+        // mirroring ui/base/clipboard/clipboard.h
         *out = ui::ClipboardType::kSelection;
         return true;
 #else
