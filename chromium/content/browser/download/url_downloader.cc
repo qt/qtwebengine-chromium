f59df0d577301e27da4ee72dc5f42ee7b43fd5b9
Xing Liu <xingliu@chromium.org>
2018-11-06 23:13:19 +0000

    [Backport] Dependency of fix for CVE-2019-13758 (2/3)
    Manual backport of patch originally reviewed on
    https://chromium-review.googlesource.com/c/1318840 :
    Download retry: Add security checks for download without renderer.
    
    This CL starts to add security check for download without renderer in
    non network service code path.
    
    The security hook is in content, so we may need to introduce some
    content dependency on network service code path as well. The in memory
    download backend of background download may have the same issue. These
    scenarios will be addressed in following up CLs.
    
    Bug: 883387
    Change-Id: I02001fe97a54960903f26d0e5376f2a8b46e2db6
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/content/browser/download/url_downloader.cc
+++ chromium/content/browser/download/url_downloader.cc
@@ -16,7 +16,9 @@
 #include "components/download/public/common/url_download_request_handle.h"
 #include "content/browser/byte_stream.h"
 #include "content/browser/download/byte_stream_input_stream.h"
+#include "content/browser/download/download_utils.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/common/child_process_host.h"
 #include "net/base/io_buffer.h"
 #include "net/base/load_flags.h"
 #include "net/base/net_errors.h"
@@ -39,6 +41,7 @@ std::unique_ptr<UrlDownloader> UrlDownloader::BeginDownload(
       Referrer::SanitizeForRequest(request->url(), referrer);
   Referrer::SetReferrerForRequest(request.get(), sanitized_referrer);
 
+  // TODO(xingliu): Figure out if we can support blob scheme.
   if (request->url().SchemeIs(url::kBlobScheme))
     return nullptr;
 
@@ -83,9 +86,13 @@ void UrlDownloader::Start() {
 void UrlDownloader::OnReceivedRedirect(net::URLRequest* request,
                                        const net::RedirectInfo& redirect_info,
                                        bool* defer_redirect) {
-  DVLOG(1) << "OnReceivedRedirect: " << request_->url().spec();
-  if (follow_cross_origin_redirects_)
+  DVLOG(1) << __func__ << " , request url: " << request_->url().spec()
+           << " ,redirect url:" << redirect_info.new_url;
+  if (follow_cross_origin_redirects_ &&
+      CanRequestURL(content::ChildProcessHost::kInvalidUniqueID,
+                    redirect_info.new_url)) {
     return;
+  }
 
   // Block redirects since there is no security policy being applied here.
   core_.OnWillAbort(download::DOWNLOAD_INTERRUPT_REASON_SERVER_UNREACHABLE);
@@ -99,6 +106,14 @@ void UrlDownloader::OnResponseStarted(net::URLRequest* request, int net_error) {
 
   if (net_error != net::OK) {
     ResponseCompleted(net_error);
+    return;
+  }
+
+  if (!CanRequestURL(content::ChildProcessHost::kInvalidUniqueID,
+                     request_->url())) {
+    core_.OnWillAbort(
+        download::DOWNLOAD_INTERRUPT_REASON_NETWORK_INVALID_REQUEST);
+    request_->CancelWithError(net::ERR_DISALLOWED_URL_SCHEME);
     return;
   }
 
