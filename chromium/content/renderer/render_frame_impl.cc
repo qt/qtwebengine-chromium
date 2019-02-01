c350fc2e32587acf01028c989b21a9b31ea7face
Allan Sandfeld Jensen <allan.jensen@qt.io>
2019-02-01 15:57:16 +0100

    [Backport] Fix for CVE-2019-5759
    Merge "Fix crashes in RenderFrameImpl::OnSelectPopupMenuItem(s)" to M72 branch
    
    ExternalPopupMenu::DidSelectItem(s) can delete the RenderFrameImpl.
    We need to reset external_popup_menu_ before calling it.
    
    Bug: 912211
    Change-Id: Ia9a628e144464a2ebb14ab77d3a693fd5cead6fc
    Reviewed-on: https://chromium-review.googlesource.com/c/1381325
    Commit-Queue: Kent Tamura <tkent@chromium.org>
    Reviewed-by: Avi Drissman <avi@chromium.org>
    Cr-Original-Commit-Position: refs/heads/master@{#618026}(cherry picked from commit 5405341d5cc268a0b2ff0678bd78ddda0892e7ea)
    Reviewed-on: https://chromium-review.googlesource.com/c/1390879
    Reviewed-by: Kent Tamura <tkent@chromium.org>
    Cr-Commit-Position: refs/branch-heads/3626@{#519}
    Cr-Branched-From: d897fb137fbaaa9355c0c93124cc048824eb1e65-refs/heads/master@{#612437}
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/content/renderer/render_frame_impl.cc
+++ chromium/content/renderer/render_frame_impl.cc
@@ -6501,8 +6501,12 @@ void RenderFrameImpl::OnSelectPopupMenuItem(int selected_index) {
     return;
 
   blink::WebScopedUserGesture gesture(frame_);
-  external_popup_menu_->DidSelectItem(selected_index);
-  external_popup_menu_.reset();
+  // We need to reset |external_popup_menu_| before calling DidSelectItem(),
+  // which might delete |this|.
+  // See ExternalPopupMenuRemoveTest.RemoveFrameOnChange
+  std::unique_ptr<ExternalPopupMenu> popup;
+  popup.swap(external_popup_menu_);
+  popup->DidSelectItem(selected_index);
 }
 #else
 void RenderFrameImpl::OnSelectPopupMenuItems(
@@ -6516,8 +6520,12 @@ void RenderFrameImpl::OnSelectPopupMenuItems(
     return;
 
   blink::WebScopedUserGesture gesture(frame_);
-  external_popup_menu_->DidSelectItems(canceled, selected_indices);
-  external_popup_menu_.reset();
+  // We need to reset |external_popup_menu_| before calling DidSelectItems(),
+  // which might delete |this|.
+  // See ExternalPopupMenuRemoveTest.RemoveFrameOnChange
+  std::unique_ptr<ExternalPopupMenu> popup;
+  popup.swap(external_popup_menu_);
+  popup->DidSelectItems(canceled, selected_indices);
 }
 #endif
 #endif
