4394e071f23afdbe1d532fc3ac7107e097f0b340
Allan Sandfeld Jensen <allan.jensen@qt.io>
2019-02-20 10:41:49 +0100

    Disable responsiveness watcher
    It uses implementation specific details that doesn't apply to us.
    
    Change-Id: Ic84f8046605cad4f84cf691f4bda0a51b7d620c9
    Reviewed-by: Jüri Valdmann <juri.valdmann@qt.io>

--- chromium/content/browser/browser_main_loop.cc
+++ chromium/content/browser/browser_main_loop.cc
@@ -1445,8 +1445,10 @@ int BrowserMainLoop::BrowserThreadsStarted() {
   ThemeHelperMac::GetInstance();
 #endif  // defined(OS_MACOSX)
 
+#if !defined(TOOLKIT_QT)
   responsiveness_watcher_ = new responsiveness::Watcher;
   responsiveness_watcher_->SetUp();
+#endif
 
 #if defined(OS_ANDROID)
   media::SetMediaDrmBridgeClient(GetContentClient()->GetMediaDrmBridgeClient());
