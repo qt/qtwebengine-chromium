66327ddf79ab17e9bc22d508873be66d6da8a942
Allan Sandfeld Jensen <allan.jensen@qt.io>
2019-02-20 10:41:49 +0100

    Disable responsiveness watcher
    It uses implementation specific details that doesn't apply to us.
    
    Change-Id: Ic84f8046605cad4f84cf691f4bda0a51b7d620c9
    Reviewed-by: Jüri Valdmann <juri.valdmann@qt.io>

--- chromium/content/browser/browser_main_loop.cc
+++ chromium/content/browser/browser_main_loop.cc
@@ -1010,8 +1010,10 @@ int BrowserMainLoop::PreMainMessageLoopRun() {
   // responsiveness::Watcher to catch jank induced by any blocking tasks not
   // instrumented with ScopedBlockingCall's assert.
   base::DisallowUnresponsiveTasks();
+#if !defined(TOOLKIT_QT)
   responsiveness_watcher_ = new responsiveness::Watcher;
   responsiveness_watcher_->SetUp();
+#endif
   return result_code_;
 }
 
