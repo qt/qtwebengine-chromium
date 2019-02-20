5e8834070740eb9a1fc155abca9bee7a972eec72
Allan Sandfeld Jensen <allan.jensen@qt.io>
2019-02-20 10:41:49 +0100

    Disable responsiveness watcher
    It uses implementation specific details that doesn't apply to us.
    
    Change-Id: Ic84f8046605cad4f84cf691f4bda0a51b7d620c9
    Reviewed-by: Jüri Valdmann <juri.valdmann@qt.io>

--- chromium/content/browser/browser_main_loop.cc
+++ chromium/content/browser/browser_main_loop.cc
@@ -1034,7 +1034,9 @@ int BrowserMainLoop::PreMainMessageLoopRun() {
                       self->parts_->OnFirstIdle();
                     }
 
-                    self->responsiveness_watcher_->OnFirstIdle();
+                    if (self->responsiveness_watcher_) {
+                      self->responsiveness_watcher_->OnFirstIdle();
+                    }
 
                     // Enable MessagePumpPhases metrics/tracing on-first-idle,
                     // not before as queuing time is not relevant before first
@@ -1058,8 +1060,10 @@ int BrowserMainLoop::PreMainMessageLoopRun() {
   // responsiveness::Watcher to catch jank induced by any unintentionally
   // blocking tasks.
   base::DisallowUnresponsiveTasks();
+#if !defined(TOOLKIT_QT)
   responsiveness_watcher_ = new responsiveness::Watcher;
   responsiveness_watcher_->SetUp();
+#endif
   return result_code_;
 }
 
