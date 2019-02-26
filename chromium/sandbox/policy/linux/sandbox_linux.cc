d3d29f65111d197f8515fe50c3767185747d5354
Michal Klocek <michal.klocek@qt.io>
2019-02-26 13:47:00 +0100

    [sandbox] Soften check for single thread only if layer1 or layer2 sandbox
    Currently qemu emulation for arm spawns some threads before
    entering level 1 of sandbox. Chromium code is written to make check
    even before trying to check support for layer1 sandbox (even
    with --no-sandbox flag)
    
    Make minimal code change to simply avoid the check if level1 or level2
    sandbox is not supported or unwanted.
    
    Task-number: QTBUG-63346
    Change-Id: Ia017f97281b7e41fbf2294e58a6ea77c838f6bd6
    Reviewed-by: Jüri Valdmann <juri.valdmann@qt.io>

--- chromium/sandbox/policy/linux/sandbox_linux.cc
+++ chromium/sandbox/policy/linux/sandbox_linux.cc
@@ -187,7 +187,7 @@ SandboxLinux* SandboxLinux::GetInstance() {
   return instance;
 }
 
-void SandboxLinux::PreinitializeSandbox() {
+bool SandboxLinux::PreinitializeSandbox() {
   CHECK(!pre_initialized_);
   seccomp_bpf_supported_ = false;
 #if BUILDFLAG(USING_SANITIZER)
@@ -222,6 +222,7 @@ void SandboxLinux::PreinitializeSandbox() {
   yama_is_enforcing_ = (yama_status & Yama::STATUS_PRESENT) &&
                        (yama_status & Yama::STATUS_ENFORCING);
   pre_initialized_ = true;
+  return seccomp_bpf_supported_;
 }
 
 void SandboxLinux::EngageNamespaceSandbox(bool from_zygote) {
