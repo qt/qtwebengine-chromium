9ccd70b1edbe5226ef6a7f911c560a85d52a3bea
Charlie Harrison <csharrison@chromium.org>
2019-04-05 13:30:53 +0000

    [Backport] CVE-2019-5839
    Make path URL parsing more lax
    
    Parsing the path component of a non-special URL like javascript or data
    should not fail for invalid URL characters like \uFFFF. See this bit
    in the spec:
    https://url.spec.whatwg.org/#cannot-be-a-base-url-path-state
    
    Note: some failing WPTs are added which are because url parsing
    replaces invalid characters (e.g. \uFFFF) with the replacement char
    \uFFFD, when that isn't in the spec.
    
    Bug: 925614
    Change-Id: Iad9ef7456ddb4d86b1d8d995e2d48fee9483864e
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/url/url_canon_unittest.cc
+++ chromium/url/url_canon_unittest.cc
@@ -1808,9 +1808,13 @@ TEST(URLCanonTest, CanonicalizePathURL) {
     const char* input;
     const char* expected;
   } path_cases[] = {
-    {"javascript:", "javascript:"},
-    {"JavaScript:Foo", "javascript:Foo"},
-    {"Foo:\":This /is interesting;?#", "foo:\":This /is interesting;?#"},
+      {"javascript:", "javascript:"},
+      {"JavaScript:Foo", "javascript:Foo"},
+      {"Foo:\":This /is interesting;?#", "foo:\":This /is interesting;?#"},
+
+      // Validation errors should not cause failure. See
+      // https://crbug.com/925614.
+      {"javascript:\uFFFF", "javascript:%EF%BF%BD"},
   };
 
   for (size_t i = 0; i < arraysize(path_cases); i++) {
