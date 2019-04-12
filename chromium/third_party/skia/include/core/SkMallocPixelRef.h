07267def1cd6f1e8dbaeba48920c480fae7fec50
Mike Klein <mtklein@google.com>
2019-04-12 11:50:39 -0500

    [Backport] Security bug 934161
    always zero SkMallocPixelRefs
    
    I'm getting tired of trying to figure out where
    clients screw up and forget to clear these buffers,
    and I'd like a safer safety net for our own screw ups.
    
    Bug: chromium:934161, many more
    Change-Id: I339d61b97a4911e778061d2e58389a7327fc3bef
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/third_party/skia/include/core/SkMallocPixelRef.h
+++ chromium/third_party/skia/include/core/SkMallocPixelRef.h
@@ -42,11 +42,6 @@ public:
     static sk_sp<SkPixelRef> MakeAllocate(const SkImageInfo&, size_t rowBytes);
 
     /**
-     *  Identical to MakeAllocate, except all pixel bytes are zeroed.
-     */
-    static sk_sp<SkPixelRef> MakeZeroed(const SkImageInfo&, size_t rowBytes);
-
-    /**
      *  Return a new SkMallocPixelRef with the provided pixel storage,
      *  rowBytes, and optional colortable. On destruction, ReleaseProc
      *  will be called.
@@ -76,11 +71,6 @@ protected:
     ~SkMallocPixelRef() override;
 
 private:
-    // Uses alloc to implement NewAllocate or NewZeroed.
-    static sk_sp<SkPixelRef> MakeUsing(void*(*alloc)(size_t),
-                                       const SkImageInfo&,
-                                       size_t rowBytes);
-
     ReleaseProc fReleaseProc;
     void*       fReleaseProcContext;
 
