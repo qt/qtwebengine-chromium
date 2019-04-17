de381abe2ff5d5b148d2520b8b8e31a40ef27768
Henrik Boström <hbos@chromium.org>
2019-04-17 12:40:19 +0000

    [Backport] Dependency for CVE-2020-6534 (2/4)
    Manual backport of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/1570172:
    Interpret getStats(callback, nonsense) as getStats(callback).
    
    This fixes the regression described in the referenced bug.
    
    The rtc_peer_connection.idl used to rely on
    [LegacyInterfaceTypeChecking] which would make invalid second arguments,
    such as the common misuse of an "errorCallback", be accepted by ignoring
    the second argument. When this was removed, this caused regressions in
    popular apps that incorrectly assumed the second argument was also a
    callback.
    
    In order to not break apps, this CL updates the custom bindings logic
    to allow nonsense second arguments by ignoring them instead of throwing
    TypeError. This continues not to inform misuses of the legacy API, but
    making this API more type-friendly would require more use counters and
    deprecation warnings.
    
    Deprecation warnings can wait until we are ready to remove the legacy
    API altogether.
    
    Bug: 953512
    Change-Id: Ie211cf8a4921812e5d243a513fbd95b224885440
    Commit-Queue: Henrik Boström <hbos@chromium.org>
    Reviewed-by: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#651688}
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/third_party/blink/renderer/modules/peerconnection/rtc_peer_connection.h
+++ chromium/third_party/blink/renderer/modules/peerconnection/rtc_peer_connection.h
@@ -162,15 +162,14 @@ class MODULES_EXPORT RTCPeerConnection final
 
   String id(ScriptState*) const;
 
-  // Calls one of the below versions (or rejects with an exception) depending on
-  // type, see RTCPeerConnection.idl.
-  ScriptPromise getStats(ScriptState*, blink::ScriptValue callback_or_selector);
-  // Calls LegacyCallbackBasedGetStats().
-  ScriptPromise getStats(ScriptState*,
-                         V8RTCStatsCallback* success_callback,
-                         MediaStreamTrack* selector = nullptr);
-  // Calls PromiseBasedGetStats().
-  ScriptPromise getStats(ScriptState*, MediaStreamTrack* selector = nullptr);
+  // Calls LegacyCallbackBasedGetStats() or PromiseBasedGetStats() (or rejects
+  // with an exception) depending on type, see rtc_peer_connection.idl.
+  ScriptPromise getStats(ScriptState* script_state);
+  ScriptPromise getStats(ScriptState* script_state,
+                         ScriptValue callback_or_selector);
+  ScriptPromise getStats(ScriptState* script_state,
+                         ScriptValue callback_or_selector,
+                         ScriptValue legacy_selector);
   ScriptPromise LegacyCallbackBasedGetStats(
       ScriptState*,
       V8RTCStatsCallback* success_callback,
