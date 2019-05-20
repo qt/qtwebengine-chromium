e770193b2ac5611415c06af731b23e52af8788d6
Min Qin <qinmin@chromium.org>
2019-05-20 17:04:38 +0000

    [Backport] CVE-2019-5829
    Early return if a download Id is already used when creating a download
    
    This is protect against download Id overflow and use-after-free
    issue.
    
    BUG=958533
    
    Change-Id: I85a97e06d4ff84680871702e9e7df7990b813f55
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/content/browser/download/download_manager_impl.cc
+++ chromium/content/browser/download/download_manager_impl.cc
@@ -362,7 +362,10 @@ download::DownloadItemImpl* DownloadManagerImpl::CreateActiveItem(
     uint32_t id,
     const download::DownloadCreateInfo& info) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
-  DCHECK(!base::ContainsKey(downloads_, id));
+
+  if (base::ContainsKey(downloads_, id))
+    return nullptr;
+
   download::DownloadItemImpl* download =
       item_factory_->CreateActiveItem(this, id, info);
 
@@ -628,11 +631,13 @@ void DownloadManagerImpl::CreateNewDownloadItemToStart(
   download::DownloadItemImpl* download = CreateActiveItem(id, *info);
   std::move(callback).Run(std::move(info), download,
                           should_persist_new_download_);
-  // For new downloads, we notify here, rather than earlier, so that
-  // the download_file is bound to download and all the usual
-  // setters (e.g. Cancel) work.
-  for (auto& observer : observers_)
-    observer.OnDownloadCreated(this, download);
+  if (download) {
+      // For new downloads, we notify here, rather than earlier, so that
+      // the download_file is bound to download and all the usual
+      // setters (e.g. Cancel) work.
+      for (auto& observer : observers_)
+          observer.OnDownloadCreated(this, download);
+  }
   OnDownloadStarted(download, on_started);
 }
 
