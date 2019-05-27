ac20335298599c68a8e3f5c30929e9d074db603e
Allan Sandfeld Jensen <allan.jensen@qt.io>
2019-05-27 15:17:52 +0200

    Allow non-Chrome projects to run
    Change-Id: Iddb00ae4d7362da4b90b7f185acb6914c1b4ff4b
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

--- chromium/content/browser/child_process_launcher_helper_posix.cc
+++ chromium/content/browser/child_process_launcher_helper_posix.cc
@@ -84,8 +84,8 @@ std::unique_ptr<PosixFileDescriptorInfo> CreateDefaultPosixFilesToMap(
 // Mac shared memory doesn't use file descriptors.
 #if !defined(OS_MACOSX)
   int fd = base::FieldTrialList::GetFieldTrialDescriptor();
-  DCHECK_NE(fd, -1);
-  files_to_register->Share(service_manager::kFieldTrialDescriptor, fd);
+  if (fd != -1)
+    files_to_register->Share(service_manager::kFieldTrialDescriptor, fd);
 
   DCHECK(mojo_channel_remote_endpoint.is_valid());
   files_to_register->Share(
