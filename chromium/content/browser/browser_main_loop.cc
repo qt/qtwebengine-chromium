862c0be382be4ff5809872b45272a435326f55ad
Allan Sandfeld Jensen <allan.jensen@qt.io>
2019-05-27 15:17:52 +0200

    Allow projects without field trials to run
    Change-Id: Iddb00ae4d7362da4b90b7f185acb6914c1b4ff4b
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

--- chromium/content/browser/browser_main_loop.cc
+++ chromium/content/browser/browser_main_loop.cc
@@ -1308,7 +1308,9 @@ void BrowserMainLoop::PostCreateThreadsImpl() {
 
   HistogramSynchronizer::GetInstance();
 
+#if !BUILDFLAG(IS_QTWEBENGINE)
   FieldTrialSynchronizer::CreateInstance();
+#endif
 
   // cc assumes a single client name for metrics in a process, which is
   // is inconsistent with single process mode where both the renderer and
