77de851faabc6d95ceee093946c47f968df90109
Michal Klocek <michal.klocek@qt.io>
2019-06-06 13:26:01 +0200

    [Backport] Fix for CVE-2019-5825
    [TurboFan] Array.prototype.map wrong ElementsKind for output array.
    
    Bug: chromium:941743
    Change-Id: I0437fee99203a154e1fcd1c7b172684211af3dfa
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/v8/src/compiler/js-call-reducer.cc
+++ chromium/v8/src/compiler/js-call-reducer.cc
@@ -1531,6 +1531,13 @@ Reduction JSCallReducer::ReduceArrayMap(Node* node,
       simplified()->LoadField(AccessBuilder::ForJSArrayLength(kind)), receiver,
       effect, control);
 
+  // If the array length >= kMaxFastArrayLength, then CreateArray
+  // will create a dictionary. We should deopt in this case, and make sure
+  // not to attempt inlining again.
+  original_length = effect = graph()->NewNode(
+      simplified()->CheckBounds(p.feedback()), original_length,
+      jsgraph()->Constant(JSArray::kMaxFastArrayLength), effect, control);
+
   // This array should be HOLEY_SMI_ELEMENTS because of the non-zero length.
   // Even though {JSCreateArray} is not marked as {kNoThrow}, we can elide the
   // exceptional projections because it cannot throw with the given parameters.
