7956dfb8d2051a1d473826e6d78de5e6f53b939f
Szabolcs David <davidsz@inf.u-szeged.hu>
2019-09-10 17:32:04 +0200

    Support qrc protocol in UrlPattern
    We use UrlPattern to parse @match rules from the metadata headers
    of user scripts.
    
    Task-number: QTBUG-76173
    Change-Id: Ibd1b73508e3229bcc5babe26e0048da069da6dec
    Reviewed-by: Jüri Valdmann <juri.valdmann@qt.io>

--- chromium/url/url_constants.cc
+++ chromium/url/url_constants.cc
@@ -27,6 +27,8 @@ const char kMailToScheme[] = "mailto";
 const char kWsScheme[] = "ws";
 const char kWssScheme[] = "wss";
 
+const char kQrcScheme[] = "qrc";
+
 const char kStandardSchemeSeparator[] = "://";
 
 const size_t kMaxURLChars = 2 * 1024 * 1024;
--- chromium/url/url_constants.h
+++ chromium/url/url_constants.h
@@ -33,6 +33,8 @@ COMPONENT_EXPORT(URL) extern const char kMailToScheme[];
 COMPONENT_EXPORT(URL) extern const char kWsScheme[];
 COMPONENT_EXPORT(URL) extern const char kWssScheme[];
 
+COMPONENT_EXPORT(URL) extern const char kQrcScheme[];
+
 // Used to separate a standard scheme and the hostname: "://".
 COMPONENT_EXPORT(URL) extern const char kStandardSchemeSeparator[];
 
