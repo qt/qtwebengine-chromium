b5d06a4582837e4fa6e021688fccdefa7417af16
Allan Sandfeld Jensen <allan.jensen@qt.io>
2019-10-15 12:29:03 +0200

    [Backport] Security issue 946978 [1/2]
    Fail decoder / SharedContextState creation if context is lost
    
    When the context is lost during creation, on some drivers we may have had bogus values
    during initialization for the static queries, which means we might have constructed a
    ContextState that's inconsistent with other ContextStates for the same real context,
    which causes problems when doing virtualized context switches.
    So check reset state after initialization and fail if the context is lost.
    
    Bug: 946978
    Change-Id: I38fb73d819dfcedf0565e6b4345c3700c49fbb07
    Commit-Queue: Antoine Labour <piman@chromium.org>
    Reviewed-by: Zhenyao Mo <zmo@chromium.org>
    Auto-Submit: Antoine Labour <piman@chromium.org>
    Cr-Commit-Position: refs/heads/master@{#670301}
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/gpu/command_buffer/service/raster_decoder_unittest_base.cc
+++ chromium/gpu/command_buffer/service/raster_decoder_unittest_base.cc
@@ -240,6 +240,11 @@ void RasterDecoderTestBase::InitDecoder(const InitState& init) {
   SetupInitCapabilitiesExpectations(group_->feature_info()->IsES3Capable());
   SetupInitStateExpectations(group_->feature_info()->IsES3Capable());
 
+  if (context_->WasAllocatedUsingRobustnessExtension()) {
+    EXPECT_CALL(*gl_, GetGraphicsResetStatusARB())
+        .WillOnce(Return(GL_NO_ERROR));
+  }
+
   shared_context_state_ = base::MakeRefCounted<SharedContextState>(
       new gl::GLShareGroup(), surface_, context_,
       feature_info->workarounds().use_virtualized_gl_contexts,
