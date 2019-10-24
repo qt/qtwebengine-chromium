cd59d41b510b98d87cc22873bac3af57a8d65714
Allan Sandfeld Jensen <allan.jensen@qt.io>
2019-10-24 16:48:33 +0200

    Fix QRC cookies for tests with network-service
    Change-Id: Ic77cde753103681242c5df31f00ce7cfed9a7d51
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/net/cookies/cookie_monster.cc
+++ chromium/net/cookies/cookie_monster.cc
@@ -521,7 +521,7 @@ bool CookieMonster::IsCookieableScheme(const std::string& scheme) {
 }
 
 const char* const CookieMonster::kDefaultCookieableSchemes[] = {"http", "https",
-                                                                "ws", "wss"};
+                                                                "ws", "wss", "qrc"};
 const int CookieMonster::kDefaultCookieableSchemesCount =
     base::size(kDefaultCookieableSchemes);
 
