b951227e561ea48b9eb3c09b502c884619748d70
Allan Sandfeld Jensen <allan.jensen@qt.io>
2019-12-18 14:56:56 +0100

    Disable dumb code
    This dumb code has already been defanged in Chromium as it broke their
    own builds, but it is still causing trouble for us as it still
    fundamentally wrong.
    
    We can only reinstate if we stop tracking ico dependencies or fix rc.py
    to work on anything other than Google's setup.
    
    Change-Id: I089ed9997b6eafb1b927eb99854f0f38b87798c0
    Reviewed-by: Kirill Burtsev <kirill.burtsev@qt.io>

--- gn/src/gn/binary_target_generator.cc
+++ gn/src/gn/binary_target_generator.cc
@@ -93,6 +93,7 @@ bool BinaryTargetGenerator::FillSources() {
   bool ret = TargetGenerator::FillSources();
   for (std::size_t i = 0; i < target_->sources().size(); ++i) {
     const auto& source = target_->sources()[i];
+#if 0
     switch (source.type()) {
       case SourceFile::SOURCE_CPP:
       case SourceFile::SOURCE_MODULEMAP:
@@ -120,6 +121,7 @@ bool BinaryTargetGenerator::FillSources() {
                     Target::GetStringForOutputType(target_->output_type()) +
                     ". " + source.value() + " is not one of the valid types.");
     }
+#endif
 
     target_->source_types_used().Set(source.type());
   }
