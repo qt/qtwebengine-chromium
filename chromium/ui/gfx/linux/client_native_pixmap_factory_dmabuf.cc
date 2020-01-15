8417e8352600298a901aeb0360eba25361b2343d
Michael Brüning <michael.bruning@qt.io>
2020-01-15 16:48:35 +0100

    [Backport] Security bug 974375
    Manual backport of patch:
    Implement NativePixmapHandle validation
    
    Now ClientNativePixmapDmaBuf and ScenicClientNativePixmapFactory
    validate layout of the NativePixmapHandle to ensure that the buffer
    fits the image.
    
    Bug: 957314, 974375
    Change-Id: Ifc0c0deae2c833e7a74ae96f84a41ae4a0657890
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/ui/gfx/linux/client_native_pixmap_factory_dmabuf.cc
+++ chromium/ui/gfx/linux/client_native_pixmap_factory_dmabuf.cc
@@ -60,7 +60,7 @@ class ClientNativePixmapFactoryDmabuf : public ClientNativePixmapFactory {
       case gfx::BufferUsage::SCANOUT_CAMERA_READ_WRITE:
       case gfx::BufferUsage::CAMERA_AND_CPU_READ_WRITE:
         return ClientNativePixmapDmaBuf::ImportFromDmabuf(std::move(handle),
-                                                          size);
+                                                          size, format);
       case gfx::BufferUsage::GPU_READ:
       case gfx::BufferUsage::SCANOUT:
       case gfx::BufferUsage::SCANOUT_VDA_WRITE:
