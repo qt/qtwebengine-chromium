1c04bfe71006c8df36998466eff0700bddc37939
Jüri Valdmann <juri.valdmann@qt.io>
2020-01-24 14:42:17 +0100

    Expose StoragePartitionImpl::InitNetworkContext
    Allows recreating the NetworkContext used inside a storage partition so as to
    change network settings dynamically.
    
    Task-number: QTBUG-81558
    Change-Id: I7b4b0a7d203eed976e13fdf3dbd4375acdae6265
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

--- chromium/content/browser/storage_partition_impl.h
+++ chromium/content/browser/storage_partition_impl.h
@@ -347,6 +347,10 @@ class CONTENT_EXPORT StoragePartitionImpl
 
   std::vector<std::string> GetCorsExemptHeaderList();
 
+#ifdef TOOLKIT_QT
+  void ResetNetworkContext() { InitNetworkContext(); }
+#endif
+
  private:
   class DataDeletionHelper;
   class QuotaManagedDataDeletionHelper;
