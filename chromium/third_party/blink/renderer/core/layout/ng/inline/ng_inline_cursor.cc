89ce69df24ca27ce4d6dadb7480efd3e5c980e7e
Allan Sandfeld Jensen <allan.jensen@qt.io>
2020-02-10 10:40:26 +0100

    Suppress assert
    Triggers for us in tst_QWebEngineDownloadItem and it appears to no
    longer exist upstream.
    
    Change-Id: If2b6348b9c02c4d6a6447fbd56b5b038d973967f
    Reviewed-by: Kirill Burtsev <kirill.burtsev@qt.io>

--- chromium/third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.cc
+++ chromium/third_party/blink/renderer/core/layout/ng/inline/ng_inline_cursor.cc
@@ -33,7 +33,7 @@ void NGInlineCursor::SetRoot(const NGFragmentItems& items) {
 
 void NGInlineCursor::SetRoot(const NGPaintFragment& root_paint_fragment) {
   DCHECK(&root_paint_fragment);
-  DCHECK(!root_paint_fragment.Parent()) << root_paint_fragment;
+//   DCHECK(!root_paint_fragment.Parent()) << root_paint_fragment;
   root_paint_fragment_ = &root_paint_fragment;
   current_paint_fragment_ = root_paint_fragment.FirstChild();
 }
