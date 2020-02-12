a1e1b51602cb6f30754f8598e2b3bce8a184f2ad
Allan Sandfeld Jensen <allan.jensen@qt.io>
2020-02-12 11:40:24 +0100

    [user-agent] Allow changing user-agent without replacing the entire network context
    This follows what is already possible with accept-language.
    
    Change-Id: I7edc1f27ddcf080df60af73652f455e889b5e4e8
    Reviewed-by: Jüri Valdmann <juri.valdmann@qt.io>

--- chromium/net/url_request/static_http_user_agent_settings.h
+++ chromium/net/url_request/static_http_user_agent_settings.h
@@ -30,13 +30,17 @@ class NET_EXPORT StaticHttpUserAgentSettings : public HttpUserAgentSettings {
     accept_language_ = new_accept_language;
   }
 
+  void set_user_agent(const std::string& new_user_agent) {
+    user_agent_ = new_user_agent;
+  }
+
   // HttpUserAgentSettings implementation
   std::string GetAcceptLanguage() const override;
   std::string GetUserAgent() const override;
 
  private:
   std::string accept_language_;
-  const std::string user_agent_;
+  std::string user_agent_;
 };
 
 }  // namespace net
