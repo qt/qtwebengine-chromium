9233ac86f079dbe69870ba14bcc2fd11efcb6ebe
Allan Sandfeld Jensen <allan.jensen@qt.io>
2020-02-12 11:40:24 +0100

    Allow changing user-agent without replacing the entire network context
    This follows what is already possible with accept-language.
    
    Change-Id: I7edc1f27ddcf080df60af73652f455e889b5e4e8
    Reviewed-by: Jüri Valdmann <juri.valdmann@qt.io>

--- chromium/services/network/network_context.h
+++ chromium/services/network/network_context.h
@@ -300,6 +300,7 @@ class COMPONENT_EXPORT(NETWORK_SERVICE) NetworkContext
   void SetNetworkConditions(const base::UnguessableToken& throttling_profile_id,
                             mojom::NetworkConditionsPtr conditions) override;
   void SetAcceptLanguage(const std::string& new_accept_language) override;
+  void SetUserAgent(const std::string& new_user_agent) override;
   void SetEnableReferrers(bool enable_referrers) override;
 #if BUILDFLAG(IS_CHROMEOS)
   void UpdateAdditionalCertificates(
