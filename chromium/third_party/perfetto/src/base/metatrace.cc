c3737fb3824b8bc4e1dfd84573d413bce0a68bdd
Allan Sandfeld Jensen <allan.jensen@qt.io>
2020-02-18 15:01:55 +0100

    [Backport] metatrace: remove memset and trivial-ctor assumption
    Turns out that on MSVC std::atomic<int> is not trivially constructible
    (although I think is still a plain old int, it just fails the check).
    Fall back on resetting each element individually.
    Thankfully the compiler can see through and eventually figures out
    it can do a memset: https://godbolt.org/z/wMre8O
    
    Bug: chromium:1010616
    Fixes: QTBUG-82272
    Change-Id: I971ff888306d6bdbaf6e6b886f9ca506ddc1b30a
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/third_party/perfetto/src/base/metatrace.cc
+++ chromium/third_party/perfetto/src/base/metatrace.cc
@@ -84,11 +84,9 @@ void Disable() {
 
 // static
 void RingBuffer::Reset() {
-  static_assert(PERFETTO_IS_TRIVIALLY_CONSTRUCTIBLE(Record) &&
-                    std::is_trivially_destructible<Record>::value,
-                "Record must be trivial");
-  memset(&records_[0], 0, sizeof(records_));
-  memset(&bankruptcy_record_, 0, sizeof(bankruptcy_record_));
+  bankruptcy_record_.clear();
+  for (Record& record : records_)
+    record.clear();
   wr_index_ = 0;
   rd_index_ = 0;
   has_overruns_ = false;
