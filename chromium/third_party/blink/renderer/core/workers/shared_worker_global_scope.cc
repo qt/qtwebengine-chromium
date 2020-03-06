915899195a3120502354d381984accd4d93cb04a
Eriko Kurimoto <elkurin@google.com>
2020-03-06 09:30:39 +0000

    [Backport] Security Bug 1056161
    Backport of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/2082855
    SharedWorker: Inherit outside settings CSP when top-level script is data url
    
    This CL lets SharedWorkerGlobalScope inherit outside settings CSP when
    response url's scheme is a local scheme.
    This behavior is defined here:
    
    If response’s url’s scheme is a local scheme, or if global is a
    DedicatedWorkerGlobalScope: ...
    - Insert a copy of policy into global’s CSP list.
    https://w3c.github.io/webappsec-csp/#initialize-global-object-csp
    
    Bug: 1056161
    Change-Id: I614c9ad6cd192f20d8aa556be0308671e84c498e
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/third_party/blink/renderer/core/workers/shared_worker_global_scope.cc
+++ chromium/third_party/blink/renderer/core/workers/shared_worker_global_scope.cc
@@ -94,9 +94,21 @@ void SharedWorkerGlobalScope::Initialize(
 
   // Step 12.6. "Execute the Initialize a global object's CSP list algorithm
   // on worker global scope and response. [CSP]"
+  // SharedWorkerGlobalScope inherits the outside's CSP instead of the response
+  // CSP headers when the response's url's scheme is a local scheme. Otherwise,
+  // use the response CSP headers. Here a local scheme is defined as follows:
+  // "A local scheme is a scheme that is "about", "blob", or "data"."
+  // https://fetch.spec.whatwg.org/#local-scheme
+  //
+  // https://w3c.github.io/webappsec-csp/#initialize-global-object-cs
   // These should be called after SetAddressSpace() to correctly override the
   // address space by the "treat-as-public-address" CSP directive.
-  InitContentSecurityPolicyFromVector(response_csp_headers);
+  Vector<CSPHeaderAndType> csp_headers =
+      response_url.ProtocolIsAbout() || response_url.ProtocolIsData() ||
+              response_url.ProtocolIs("blob")
+          ? OutsideContentSecurityPolicyHeaders()
+          : response_csp_headers;
+  InitContentSecurityPolicyFromVector(csp_headers);
   BindContentSecurityPolicyToExecutionContext();
 
   OriginTrialContext::AddTokens(this, response_origin_trial_tokens);
