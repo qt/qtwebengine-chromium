qt/qtwebengine-chromium

commit b88a10e7a666792cc8a2d9a9310748a79b1f032b

[Backport] CVE-2020-6401 (2/3)
Manual backport of patch originally reviewed on
https://chromium-review.googlesource.com/c/chromium/src/+/1881344:
Restrict Latin Small Letter Eth (U+00F0) to Icelandic domains

crrev.com/c/1879992 restricted Latin Small Letter Thorn to Icelandic
domains. This CL does the same for Eth (ð) as it can be confused with
the characters "o" and "d" in some fonts.

This change affects less than 10 real world domains with limited popularity.

Bug: 1017707, 929711
Change-Id: I8f0394d4c1531eb2051d38c78afe00f550d3da73
Reviewed-by: Jüri Valdmann <juri.valdmann@qt.io>

chromium/components/url_formatter/spoof_checks/idn_spoof_checker.cc
@@ -167,6 +167,14 @@ IDNSpoofChecker::IDNSpoofChecker() {
       status);
   lgc_letters_n_ascii_.freeze();
 
+  // Latin small letter thorn ("þ", U+00FE) can be used to spoof both b and p.
+  // It's used in modern Icelandic orthography, so allow it for the Icelandic
+  // ccTLD (.is) but block in any other TLD. Also block Latin small letter eth
+  // ("ð", U+00F0) which can be used to spoof the letter o.
+  icelandic_characters_ =
+      icu::UnicodeSet(UNICODE_STRING_SIMPLE("[\\u00fe\\u00f0]"), status);
+  icelandic_characters_.freeze();
+
   // Used for diacritics-removal before the skeleton calculation. Add
   // "ł > l; ø > o; đ > d" that are not handled by "NFD; Nonspacing mark
   // removal; NFC".
@@ -275,13 +283,9 @@ bool IDNSpoofChecker::SafeToDisplayAsUnicode(
   if (deviation_characters_.containsSome(label_string))
     return false;
 
-  // Latin small letter thorn (U+00FE) can be used to spoof both b and p. It's
-  // used in modern Icelandic orthography, so allow it for the Icelandic ccTLD
-  // (.is) but block in any other TLD.
-  if (label_string.length() > 1 && label_string.indexOf("þ") != -1 &&
-      top_level_domain != ".is") {
-    return false;
-  }
+  // Disallow Icelandic confusables for domains outside Iceland's ccTLD (.is).
+  if (label_string.length() > 1 && top_level_domain != ".is" &&
+      icelandic_characters_.containsSome(label_string))
 
   // If there's no script mixing, the input is regarded as safe without any
   // extra check unless it falls into one of three categories:

chromium/components/url_formatter/spoof_checks/idn_spoof_checker.h
@@ -95,6 +95,7 @@ class IDNSpoofChecker {
   icu::UnicodeSet cyrillic_letters_;
   icu::UnicodeSet cyrillic_letters_latin_alike_;
   icu::UnicodeSet lgc_letters_n_ascii_;
+  icu::UnicodeSet icelandic_characters_;
   std::unique_ptr<icu::Transliterator> diacritic_remover_;
   std::unique_ptr<icu::Transliterator> extra_confusable_mapper_;
 

commit f7ffd2f7dffd911e37f6a638bb410bd71da23491

Fixup for [Backport] CVE-2020-6401 (2/3)
b88a10e7a666792cc8a2d9a9310748a79b1f032b accidentally removed a
return statement. Add it again.

Change-Id: I358f7929b42779e2bea64dcb306884d484cb2743
Reviewed-by: Jüri Valdmann <juri.valdmann@qt.io>

chromium/components/url_formatter/spoof_checks/idn_spoof_checker.cc
@@ -316,6 +316,7 @@ bool IDNSpoofChecker::SafeToDisplayAsUnicode(
   // Disallow Icelandic confusables for domains outside Iceland's ccTLD (.is).
   if (label_string.length() > 1 && top_level_domain != "is" &&
       icelandic_characters_.containsSome(label_string))
+    return false;
 
   // Disallow Latin Schwa (U+0259) for domains outside Azerbaijan's ccTLD (.az).
   if (label_string.length() > 1 && top_level_domain != "az" &&