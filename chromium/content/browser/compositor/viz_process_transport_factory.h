b82fe8749d691f836f9677311205f3a519147e5b
Michal Klocek <michal.klocek@qt.io>
2020-03-24 15:21:28 +0100

    Fix viz crash when cleanup
    When we cleanup viz, OnConnectionLost will delete frame_sink_manger.
    However we can still have messages to create sinks and to invalidate them
    later when destroying compositor. Also, the destructor of EmbeddedFrameSinkImpl
    may call UnregisterFrameSinkHierarchy on shutdown.
    
    Do not create new frame sink, and do not invalidate them later.
    
    Make possible to bail out in viz process transport factory as
    compositor was already deleted.
    
    Task-number: QTBUG-83040
    Task-number: QTBUG-79864
    Change-Id: Ibf0606368dfd81bc376a06721ada8d13cd8ea417
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/content/browser/compositor/viz_process_transport_factory.h
+++ chromium/content/browser/compositor/viz_process_transport_factory.h
@@ -63,6 +63,7 @@ class VizProcessTransportFactory : public ui::ContextFactory,
 
   // Connects HostFrameSinkManager to FrameSinkManagerImpl in viz process.
   void ConnectHostFrameSinkManager();
+  void PrepareForShutDown();
 
   // ui::ContextFactory implementation.
   void CreateLayerTreeFrameSink(
@@ -151,6 +152,7 @@ class VizProcessTransportFactory : public ui::ContextFactory,
   bool is_gpu_compositing_disabled_ = false;
 
   base::WeakPtrFactory<VizProcessTransportFactory> weak_ptr_factory_{this};
+  bool shutdown_ = false;
 };
 
 }  // namespace content
