c8b517eb447e52eba75eb2ec4148456609eb9025
Aaron Leventhal <aleventhal@google.com>
2020-04-02 17:14:57 +0000

    [Backport] Security bug 1025740 2/2
    Lifecycle change safety check
    
    Lifecycle changes should not occur during a11y serialization.
    This adds a safety check that triggers a DCHECK when that occurs.
    
    Bug: 1025740
    Change-Id: I858b7e437eeb97e3d0284192bda39128c16b6b7e
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/content/renderer/accessibility/blink_ax_tree_source.cc
+++ chromium/content/renderer/accessibility/blink_ax_tree_source.cc
@@ -28,6 +28,7 @@
 #include "third_party/blink/public/platform/web_vector.h"
 #include "third_party/blink/public/web/web_ax_enums.h"
 #include "third_party/blink/public/web/web_ax_object.h"
+#include "third_party/blink/public/web/web_disallow_transition_scope.h"
 #include "third_party/blink/public/web/web_document.h"
 #include "third_party/blink/public/web/web_element.h"
 #include "third_party/blink/public/web/web_form_control_element.h"
@@ -436,6 +437,13 @@ WebAXObject BlinkAXTreeSource::GetNull() const {
 
 void BlinkAXTreeSource::SerializeNode(WebAXObject src,
                                       AXContentNodeData* dst) const {
+#if DCHECK_IS_ON()
+  // Never causes a document lifecycle change during serialization,
+  // because the assumption is that layout is in a safe, stable state.
+  WebDocument document = GetMainDocument();
+  blink::WebDisallowTransitionScope disallow(&document);
+#endif
+
   dst->role = AXRoleFromBlink(src.Role());
   AXStateFromBlink(src, dst);
   dst->id = src.AxID();
