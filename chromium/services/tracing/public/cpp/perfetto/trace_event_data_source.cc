6b151acc9f31574c7b739433c538b90b989c9a23
Kirill Burtsev <kirill.burtsev@qt.io>
2020-04-03 15:09:33 +0200

    FIXUP: Fix clang and embedded gcc builds
    Work around an issues causing a dependency on libatomic
    because clang doesn't have internal atomic handling, which
    lead to runtime error: 'undefined symbol: __atomic_is_lock_free'
    
    Change-Id: I29bb222242a5bfce790980753b26b86ebed3c6b3
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/services/tracing/public/cpp/perfetto/trace_event_data_source.cc
+++ chromium/services/tracing/public/cpp/perfetto/trace_event_data_source.cc
@@ -441,9 +441,11 @@ TraceEventDataSource::TraceEventDataSource()
     : DataSourceBase(mojom::kTraceEventDataSourceName),
       disable_interning_(base::CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kPerfettoDisableInterning)) {
+#ifndef TOOLKIT_QT
   DCHECK(session_flags_.is_lock_free())
       << "SessionFlags are not atomic! We rely on efficient lock-free look-up "
          "of the session flags when emitting a trace event.";
+#endif
   g_trace_event_data_source_for_testing = this;
   DETACH_FROM_SEQUENCE(perfetto_sequence_checker_);
 }
