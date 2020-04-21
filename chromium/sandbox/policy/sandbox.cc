a461ccfe13288229f48f2e785eb11dbc40457542
Peter Varga <pvarga@inf.u-szeged.hu>
2020-04-21 16:57:13 +0200

    Disable alternate window station of Windows sandbox
    Initialization of Windows sandbox creates an alternate desktop. It also
    creates an alternate window station then assigns the browser process to
    it for creating the new desktop. Windows QPA has already registered for
    WM_CLIPBOARDUPDATE message by AddClipboardFormatListener() when sandbox
    initialization starts.
    
    Assigning to a new window station by SetProcessWindowStation() seems to
    clear the registration for the messages. Switching back to the original
    window station won't bring back the already registered listeners but
    new ones can be registered.
    
    Since, Windows QPA can't be accessed to re-register clipboard format
    listener, disable alternate window station when creating alternate
    desktop for the sandboxed renderer process.
    
    Change-Id: I236186774ebe89a138c17eae2bfb9271a18f3fd8
    Fixes: QTBUG-83300
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/sandbox/policy/sandbox.cc
+++ chromium/sandbox/policy/sandbox.cc
@@ -58,8 +58,14 @@ bool Sandbox::Initialize(sandbox::mojom::Sandbox sandbox_type,
       // process because it will initialize the sandbox broker, which requires
       // the process to swap its window station. During this time all the UI
       // will be broken. This has to run before threads and windows are created.
+#ifdef TOOLKIT_QT
+      // Disable alternate window station due to QTBUG-83300
+      ResultCode result = broker_services->CreateAlternateDesktop(
+          Desktop::kAlternateDesktop);
+#else
       ResultCode result = broker_services->CreateAlternateDesktop(
           Desktop::kAlternateWinstation);
+#endif
       // This failure is usually caused by third-party software or by the host
       // system exhausting its desktop heap.
       CHECK(result == SBOX_ALL_OK);
