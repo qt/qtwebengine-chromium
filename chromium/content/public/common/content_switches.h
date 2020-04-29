8f9ac99289799527258c2753f86e5cfb0c3c7465
Michal Klocek <michal.klocek@qt.io>
2020-04-29 09:06:01 +0200

    [expose] Fix initialize of cdm for sandbox from widevine-path
    Pass widevine-path when doing zygote on Linux, so cdm
    library can be preloaded before entering sandbox.
    
    Fixes: QTBUG-78709
    Change-Id: I8bbdba1a94e843a96bfea85db5699289c226c08c
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/content/public/common/content_switches.h
+++ chromium/content/public/common/content_switches.h
@@ -285,6 +285,7 @@ extern const char kIpcFuzzerTestcase[];
 
 #if BUILDFLAG(IS_QTWEBENGINE)
 extern const char kApplicationName[];
+extern const char kCdmWidevinePath[];
 #endif
 
 // DON'T ADD RANDOM STUFF HERE. Put it in the main section above in
