7c34012060da2ed33d67f8cfe8aa14a3f89ff811
Allan Sandfeld Jensen <allan.jensen@qt.io>
2020-05-06 14:39:13 +0200

    [Backport] Fix for CVE-2020-6464
    Fix customized built-in element constructor behavior
    
    This CL implements two changes:
     1. It fixes the implementation to better match the spec for the
        "create an element for the token" [1] algorithm. Prior to this CL,
        step 7 of that algorithm was skipping directly to step 6 of the
        "create an element" [2] algorithm, skipping over step 5 for
        customized built-in elements. This is now fixed. This case is
        illustrated by the issue and example at [3] and [4]. This becomes
        the first test in customized-built-in-constructor-exceptions.html.
    
     2. It updates the comments to match the new behavior discussed in [3]
        and the [5] spec PR, which changes the return value in the case
        that a customized built-in element constructor throws an exception.
        With the change above, that is actually already the behavior. So
        this is just a comment change. Two new tests are added to
        customized-built-in-constructor-exceptions.html.
    
    [1] https://html.spec.whatwg.org/multipage/parsing.html#create-an-element-for-the-token
    [2] https://dom.spec.whatwg.org/#concept-create-element
    [3] https://github.com/whatwg/html/issues/5084
    [4] https://crbug.com/1024866
    [5] https://github.com/whatwg/dom/pull/797
    
    TBR=masonfreed@chromium.org
    
    (cherry picked from commit 7101418f85a0f17e4f9a35dfe3a9acff76340a93)
    
    Bug: 1071059, 1024866
    Change-Id: I814c81991eb5e83501304bcb3d2da476743aef52
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/third_party/blink/renderer/core/html/custom/custom_element.cc
+++ chromium/third_party/blink/renderer/core/html/custom/custom_element.cc
@@ -163,7 +163,8 @@ Element* CustomElement::CreateUncustomizedOrUndefinedElement(
 
 HTMLElement* CustomElement::CreateFailedElement(Document& document,
                                                 const QualifiedName& tag_name) {
-  DCHECK(ShouldCreateCustomElement(tag_name));
+  CHECK(ShouldCreateCustomElement(tag_name))
+      << "HTMLUnknownElement with built-in tag name: " << tag_name;
 
   // "create an element for a token":
   // https://html.spec.whatwg.org/multipage/syntax.html#create-an-element-for-the-token
