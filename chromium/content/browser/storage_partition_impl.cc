5c1ff9950f79a725196a70477302d7652317cd39
Allan Sandfeld Jensen <allan.jensen@qt.io>
2020-08-03 13:45:55 +0200

    Decouple payment from content
    Change-Id: I824859ff291b66016ec00d4d44172caeec2db34c
    Reviewed-by: Jüri Valdmann <juri.valdmann@qt.io>

--- chromium/content/browser/storage_partition_impl.cc
+++ chromium/content/browser/storage_partition_impl.cc
@@ -1451,8 +1451,10 @@ void StoragePartitionImpl::Initialize(
   background_sync_context_->Init(service_worker_context_,
                                  *devtools_background_services_context_.get());
 
+#if !defined(TOOLKIT_QT)
   payment_app_context_ = new PaymentAppContextImpl();
   payment_app_context_->Init(service_worker_context_);
+#endif
 
   broadcast_channel_service_ = std::make_unique<BroadcastChannelService>();
 
@@ -1830,7 +1832,11 @@ BackgroundFetchContext* StoragePartitionImpl::GetBackgroundFetchContext() {
 
 PaymentAppContextImpl* StoragePartitionImpl::GetPaymentAppContext() {
   DCHECK(initialized_);
+#if !defined(TOOLKIT_QT)
   return payment_app_context_.get();
+#else
+  return nullptr;
+#endif
 }
 
 BroadcastChannelService* StoragePartitionImpl::GetBroadcastChannelService() {
