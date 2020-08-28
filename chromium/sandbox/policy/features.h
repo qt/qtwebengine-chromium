f0fd6825513521b5b7376ce1c3e688fa6109bd83
Peter Varga <pvarga@inf.u-szeged.hu>
2020-08-28 14:06:37 +0200

    Disable NetworkServiceSandbox on macOS by default
    It is also disabled for webengine on other platforms.
    
    It is necessary to make cookie database working on a custom persistent
    storage path after:
    https://chromium-review.googlesource.com/c/chromium/src/+/2085112
    
    Change-Id: I3f1fc338e6b05430307e9eef1c2b6fbbea7dd3c9
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/sandbox/policy/features.h
+++ chromium/sandbox/policy/features.h
@@ -17,7 +17,7 @@ namespace sandbox {
 namespace policy {
 namespace features {
 
-#if !defined(OS_MAC)
+#if defined(TOOLKIT_QT) || !defined(OS_MAC)
 SANDBOX_POLICY_EXPORT extern const base::Feature kNetworkServiceSandbox;
 #endif
 
