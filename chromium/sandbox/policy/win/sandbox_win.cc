7974e32748cbb6a39a2838506a62599c36fa0828
Peter Varga <pvarga@inf.u-szeged.hu>
2020-10-02 13:54:07 +0200

    Fix assert when checking if RendererAppContainer feature is enabled
    It asserts with MSVC 32bit but it might fail with other configurations
    too because the RendererAppContainer feature will have more than
    one instance in the memory and it can break pointer-based identity
    lookup (see documentation of base::Feature in
    chromium/base/feature_list.h).
    
    Make the Feature instance static to ensure it has the same address for
    each of the IsAppContainerEnabled() calls.
    
    Change-Id: I937cbd7c4dd46df2d4b48c4af239db1e6e8feb6f
    Fixes: QTBUG-86945
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/sandbox/policy/win/sandbox_win.cc
+++ chromium/sandbox/policy/win/sandbox_win.cc
@@ -582,8 +582,11 @@ bool IsAppContainerEnabled() {
   if (base::win::GetVersion() < base::win::Version::WIN8)
     return false;
 
-  return base::FeatureList::IsEnabled(
-      {"RendererAppContainer", base::FEATURE_DISABLED_BY_DEFAULT});
+  static const base::Feature kRendererAppContainer {
+     "RendererAppContainer", base::FEATURE_DISABLED_BY_DEFAULT
+  };
+
+  return base::FeatureList::IsEnabled(kRendererAppContainer);
 }
 
 ResultCode SetJobMemoryLimit(const base::CommandLine& cmd_line,
