1da1fcb8264a22f9afd6f19314fe8e999796c025
Peter Varga <pvarga@inf.u-szeged.hu>
2020-10-29 10:01:00 +0100

    Add minimal webrtcdesktopcapture api for hangout services extension
    The hangout services extension is required for meet.google.com.
    
    The extension is only able to capture the primary screen without audio
    with this fix.
    
    Change-Id: Ic5563ebe4e0b8f1e8bfa83ca0830f180281a1f8c
    Fixes: QTBUG-85731
    Task-number: QTBUG-51185
    Task-number: QTBUG-61676
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    
    Enable webrtcLoggingPrivate extension API for hangout extension
    
    Change-Id: I8be102e18bba8fca21b76eaec8f0eb5f0aedabde
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/chrome/browser/media/webrtc/webrtc_event_log_uploader.cc
+++ chromium/chrome/browser/media/webrtc/webrtc_event_log_uploader.cc
@@ -12,7 +12,9 @@
 #include "base/task/sequenced_task_runner.h"
 #include "build/build_config.h"
 #include "build/chromeos_buildflags.h"
+#if !defined(TOOLKIT_QT)
 #include "chrome/browser/browser_process.h"
+#endif
 #include "chrome/browser/media/webrtc/webrtc_log_uploader.h"
 #include "components/version_info/version_info.h"
 #include "content/public/browser/browser_task_traits.h"
@@ -95,11 +97,15 @@ std::string MimeContentType() {
 void BindURLLoaderFactoryReceiver(
     mojo::PendingReceiver<network::mojom::URLLoaderFactory>
         url_loader_factory_receiver) {
+#if !defined(TOOLKIT_QT)
   DCHECK_CURRENTLY_ON(content::BrowserThread::UI);
   scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory =
       g_browser_process->shared_url_loader_factory();
   DCHECK(shared_url_loader_factory);
   shared_url_loader_factory->Clone(std::move(url_loader_factory_receiver));
+#else
+  NOTREACHED();
+#endif // !defined(TOOLKIT_QT)
 }
 
 void OnURLLoadUploadProgress(uint64_t current, uint64_t total) {
