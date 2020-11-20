751850da19dd17af3b8947f32f03b6cd4fa45369
Allan Sandfeld Jensen <allan.jensen@qt.io>
2020-11-20 17:15:12 +0100

    Refix quota for non-host based urls
    We might consider dropping this for Qt6
    
    Change-Id: I08259168f5fcc5c0346253ff24d89f4dedb6fcd2
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/content/browser/quota/quota_manager_host.cc
+++ chromium/content/browser/quota/quota_manager_host.cc
@@ -22,6 +22,7 @@
 #include "content/public/browser/storage_partition.h"
 #include "content/public/common/content_client.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
+#include "net/base/url_util.h"
 #include "storage/browser/quota/quota_manager.h"
 #include "third_party/blink/public/mojom/quota/quota_types.mojom.h"
 #include "url/origin.h"
@@ -168,7 +169,7 @@ void QuotaManagerHost::DidGetPermissionResponse(
 
   // Otherwise, return the new quota.
   quota_manager_->SetPersistentHostQuota(
-      origin_.host(), requested_quota,
+      net::GetHostOrSpecFromURL(origin_.GetURL()), requested_quota,
       base::BindOnce(&QuotaManagerHost::DidSetHostQuota,
                      weak_factory_.GetWeakPtr(), current_usage,
                      std::move(callback)));
