3235c9e4ec4dbb59e1592a319deaba2de970af90
Allan Sandfeld Jensen <allan.jensen@qt.io>
2020-11-26 12:08:50 +0100

    [Revert] ui/gl: Remove WGL support on Windows
    We still use it
    
    Change-Id: I4fbe6a55fcecdcd875c2a948170423d0c4380f5d
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/ui/gl/gl_bindings.cc
+++ chromium/ui/gl/gl_bindings.cc
@@ -15,12 +15,26 @@
 #include "ui/gfx/x/glx.h"
 #endif
 
+#if defined(OS_WIN)
+#include "ui/gl/gl_surface_wgl.h"
+#endif
+
 #if defined(USE_EGL)
 #include "ui/gl/gl_display.h"
 #include "ui/gl/gl_surface_egl.h"
 #endif
 
 namespace gl {
+
+#if defined(OS_WIN)
+std::string DriverWGL::GetPlatformExtensions() {
+  const char* str = nullptr;
+  str = wglGetExtensionsStringARB(GLSurfaceWGL::GetDisplayDC());
+  if (str)
+    return str;
+  return wglGetExtensionsStringEXT();
+}
+#endif
 
 #if defined(USE_EGL)
 void DisplayExtensionsEGL::UpdateConditionalExtensionSettings(
--- chromium/ui/gl/init/gl_factory_win.cc
+++ chromium/ui/gl/init/gl_factory_win.cc
@@ -10,12 +10,15 @@
 #include "ui/gl/gl_context.h"
 #include "ui/gl/gl_context_egl.h"
 #include "ui/gl/gl_context_stub.h"
+#include "ui/gl/gl_context_wgl.h"
 #include "ui/gl/gl_egl_api_implementation.h"
 #include "ui/gl/gl_implementation.h"
 #include "ui/gl/gl_share_group.h"
 #include "ui/gl/gl_surface.h"
 #include "ui/gl/gl_surface_egl.h"
 #include "ui/gl/gl_surface_stub.h"
+#include "ui/gl/gl_surface_wgl.h"
+#include "ui/gl/gl_wgl_api_implementation.h"
 #include "ui/gl/vsync_provider_win.h"
 
 namespace gl {
@@ -30,6 +33,8 @@ std::vector<GLImplementationParts> GetAllowedGLImplementations() {
 bool GetGLWindowSystemBindingInfo(const GLVersionInfo& gl_info,
                                   GLWindowSystemBindingInfo* info) {
   switch (GetGLImplementation()) {
+    case kGLImplementationDesktopGL:
+      return GetGLWindowSystemBindingInfoWGL(info);
     case kGLImplementationEGLANGLE:
       return GetGLWindowSystemBindingInfoEGL(info);
     default:
@@ -44,6 +49,9 @@ scoped_refptr<GLContext> CreateGLContext(GLShareGroup* share_group,
   switch (GetGLImplementation()) {
     case kGLImplementationEGLANGLE:
       return InitializeGLContext(new GLContextEGL(share_group),
+                                 compatible_surface, attribs);
+    case kGLImplementationDesktopGL:
+      return InitializeGLContext(new GLContextWGL(share_group),
                                  compatible_surface, attribs);
     case kGLImplementationMockGL:
       return new GLContextStub(share_group);
@@ -69,6 +77,9 @@ scoped_refptr<GLSurface> CreateViewGLSurface(GLDisplay* display,
           display->GetAs<gl::GLDisplayEGL>(), window,
           std::make_unique<VSyncProviderWin>(window)));
     }
+    case kGLImplementationDesktopGL:
+      return InitializeGLSurface(
+          base::MakeRefCounted<NativeViewGLSurfaceWGL>(window));
     case kGLImplementationMockGL:
     case kGLImplementationStubGL:
       return new GLSurfaceStub;
@@ -95,6 +106,9 @@ scoped_refptr<GLSurface> CreateOffscreenGLSurfaceWithFormat(
             new PbufferGLSurfaceEGL(display_egl, size), format);
       }
     }
+    case kGLImplementationDesktopGL:
+      return InitializeGLSurfaceWithFormat(
+          new PbufferGLSurfaceWGL(size), format);
     case kGLImplementationMockGL:
     case kGLImplementationStubGL:
       return new GLSurfaceStub;
@@ -109,6 +123,9 @@ void SetDisabledExtensionsPlatform(const std::string& disabled_extensions) {
   GLImplementation implementation = GetGLImplementation();
   DCHECK_NE(kGLImplementationNone, implementation);
   switch (implementation) {
+    case kGLImplementationDesktopGL:
+      SetDisabledExtensionsWGL(disabled_extensions);
+      break;
     case kGLImplementationEGLANGLE:
       SetDisabledExtensionsEGL(disabled_extensions);
       break;
@@ -124,6 +141,8 @@ bool InitializeExtensionSettingsOneOffPlatform(GLDisplay* display) {
   GLImplementation implementation = GetGLImplementation();
   DCHECK_NE(kGLImplementationNone, implementation);
   switch (implementation) {
+    case kGLImplementationDesktopGL:
+      return InitializeExtensionSettingsOneOffWGL();
     case kGLImplementationEGLANGLE:
       return InitializeExtensionSettingsOneOffEGL(
           display->GetAs<GLDisplayEGL>());
