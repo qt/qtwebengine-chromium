58d15f61b897e95cc60299f819936dbb539b0373
Allan Sandfeld Jensen <allan.jensen@qt.io>
2020-12-09 10:12:38 +0100

    Reinstate jumbo-builds
    Reverts four patches disabling and removing jumbo builds.
    
    It makes too big of a difference for us to drop.
    
    Change-Id: Ib7dd65996f9e985480f49fab2d1781a5d08bf6fd
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/components/performance_manager/execution_context_priority/frame_capturing_media_stream_voter.cc
+++ chromium/components/performance_manager/execution_context_priority/frame_capturing_media_stream_voter.cc
@@ -13,14 +13,14 @@ namespace performance_manager::execution_context_priority {
 
 namespace {
 
-const execution_context::ExecutionContext* GetExecutionContext(
+const execution_context::ExecutionContext* GetExecutionContext_FCVSV(
     const FrameNode* frame_node) {
   return execution_context::ExecutionContext::From(frame_node);
 }
 
 // Returns a vote with the appropriate priority depending on if the frame is
 // capturing media.
-Vote GetVote(bool is_capturing_media_stream) {
+Vote GetVote_FCVSV(bool is_capturing_media_stream) {
   base::TaskPriority priority = is_capturing_media_stream
                                     ? base::TaskPriority::USER_BLOCKING
                                     : base::TaskPriority::LOWEST;
@@ -50,19 +50,19 @@ void FrameCapturingMediaStreamVoter::TearDownOnGraph(Graph* graph) {
 
 void FrameCapturingMediaStreamVoter::OnFrameNodeInitializing(
     const FrameNode* frame_node) {
-  const Vote vote = GetVote(frame_node->IsCapturingMediaStream());
-  voting_channel_.SubmitVote(GetExecutionContext(frame_node), vote);
+  const Vote vote = GetVote_FCVSV(frame_node->IsCapturingMediaStream());
+  voting_channel_.SubmitVote(GetExecutionContext_FCVSV(frame_node), vote);
 }
 
 void FrameCapturingMediaStreamVoter::OnFrameNodeTearingDown(
     const FrameNode* frame_node) {
-  voting_channel_.InvalidateVote(GetExecutionContext(frame_node));
+  voting_channel_.InvalidateVote(GetExecutionContext_FCVSV(frame_node));
 }
 
 void FrameCapturingMediaStreamVoter::OnIsCapturingMediaStreamChanged(
     const FrameNode* frame_node) {
-  const Vote new_vote = GetVote(frame_node->IsCapturingMediaStream());
-  voting_channel_.ChangeVote(GetExecutionContext(frame_node), new_vote);
+  const Vote new_vote = GetVote_FCVSV(frame_node->IsCapturingMediaStream());
+  voting_channel_.ChangeVote(GetExecutionContext_FCVSV(frame_node), new_vote);
 }
 
 }  // namespace performance_manager::execution_context_priority
--- chromium/docs/linux/build_instructions.md
+++ chromium/docs/linux/build_instructions.md
@@ -230,6 +230,15 @@ That's it. Remember to always use `autoninja` for building Chromium as described
 below, which handles the startup and shutdown of the reproxy daemon process
 that's required during the build, instead of directly invoking `ninja`.
 
+#### Jumbo/Unity builds
+
+Jumbo builds merge many translation units ("source files") and compile them
+together. Since a large portion of Chromium's code is in shared header files,
+this dramatically reduces the total amount of work needed. Check out the
+[Jumbo / Unity builds](jumbo.md) for more information.
+
+Enable jumbo builds by setting the GN arg `use_jumbo_build=true`.
+
 #### Disable NaCl
 
 By default, the build includes support for
