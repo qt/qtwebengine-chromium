10ecb52ad9f5ba232baf7702e86909e132cb4134
Allan Sandfeld Jensen <allan.jensen@qt.io>
2021-01-15 13:16:25 +0100

    Keep the close button when "undocked", as we can dock differently
    Change-Id: I6e3c3092941e0211faba2ba0d1ad99d6e25c50d3
    Reviewed-by: Kirill Burtsev <kirill.burtsev@qt.io>

--- chromium/third_party/devtools-frontend/src/front_end/ui/legacy/DockController.ts
+++ chromium/third_party/devtools-frontend/src/front_end/ui/legacy/DockController.ts
@@ -90,7 +90,7 @@ export class DockController extends Common.ObjectWrapper.ObjectWrapper {
 
     if (!canDock) {
       this._dockSide = State.Undocked;
-      this._closeButton.setVisible(false);
+//       this._closeButton.setVisible(false);
       return;
     }
 
