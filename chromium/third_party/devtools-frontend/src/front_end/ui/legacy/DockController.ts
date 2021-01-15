43bbe37a17f3b76639cabd065e7f1da89193a841
Allan Sandfeld Jensen <allan.jensen@qt.io>
2021-01-15 13:16:25 +0100

    Keep the close button when "undocked", as we can dock differently
    Change-Id: I6e3c3092941e0211faba2ba0d1ad99d6e25c50d3
    Reviewed-by: Kirill Burtsev <kirill.burtsev@qt.io>

--- chromium/third_party/devtools-frontend/src/front_end/ui/legacy/DockController.ts
+++ chromium/third_party/devtools-frontend/src/front_end/ui/legacy/DockController.ts
@@ -88,7 +88,7 @@ export class DockController extends Common.ObjectWrapper.ObjectWrapper<EventType
 
     if (!canDock) {
       this.dockSideInternal = DockState.UNDOCKED;
-      this.closeButton.setVisible(false);
+//       this.closeButton.setVisible(false);
       return;
     }
 
@@ -170,7 +170,7 @@ export class DockController extends Common.ObjectWrapper.ObjectWrapper<EventType
     this.currentDockStateSetting.set(dockSide);
     Host.InspectorFrontendHost.InspectorFrontendHostInstance.setIsDocked(
         dockSide !== DockState.UNDOCKED, this.setIsDockedResponse.bind(this, eventData));
-    this.closeButton.setVisible(this.dockSideInternal !== DockState.UNDOCKED);
+//     this.closeButton.setVisible(this.dockSideInternal !== DockState.UNDOCKED);
     this.dispatchEventToListeners(Events.DockSideChanged, eventData);
   }
 
