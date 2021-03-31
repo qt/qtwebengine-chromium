07af1bb4559d63ffe80a15603622dc2b75792da7
Balazs Engedy <engedy@chromium.org>
2021-03-31 07:47:19 +0000

    [Backport] CVE-2021-21201: Use after free in permissions
    Partial backport of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/2791431:
    Use IDType for permission change subscriptions.
    
    Bug: 1025683
    Change-Id: I3b44ba7833138e8a657a4192e1a36c978695db32
    Reviewed-by: Richard Coles <torne@chromium.org>
    Reviewed-by: Yuchen Liu <yucliu@chromium.org>
    Reviewed-by: Nasko Oskov <nasko@chromium.org>
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Reviewed-by: Fabrice de Gans-Riberi <fdegans@chromium.org>
    Reviewed-by: Arthur Sonzogni <arthursonzogni@chromium.org>
    Reviewed-by: Illia Klimov <elklm@google.com>
    Auto-Submit: Balazs Engedy <engedy@chromium.org>
    Commit-Queue: Balazs Engedy <engedy@chromium.org>
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/content/browser/renderer_host/media/media_stream_manager.cc
+++ chromium/content/browser/renderer_host/media/media_stream_manager.cc
@@ -675,9 +675,9 @@ class MediaStreamManager::DeviceRequest {
 
   std::string tab_capture_device_id;
 
-  int audio_subscription_id = PermissionControllerImpl::kNoPendingOperation;
+  PermissionController::SubscriptionId audio_subscription_id;
 
-  int video_subscription_id = PermissionControllerImpl::kNoPendingOperation;
+  PermissionController::SubscriptionId video_subscription_id;
 
  private:
   std::vector<MediaRequestState> state_;
@@ -2740,8 +2740,8 @@ void MediaStreamManager::SubscribeToPermissionControllerOnUIThread(
   if (!controller)
     return;
 
-  int audio_subscription_id = PermissionControllerImpl::kNoPendingOperation;
-  int video_subscription_id = PermissionControllerImpl::kNoPendingOperation;
+  PermissionController::SubscriptionId audio_subscription_id;
+  PermissionController::SubscriptionId video_subscription_id;
 
   if (is_audio_request) {
     // It is safe to bind base::Unretained(this) because MediaStreamManager is
@@ -2783,8 +2783,8 @@ void MediaStreamManager::SetPermissionSubscriptionIDs(
     const std::string& label,
     int requesting_process_id,
     int requesting_frame_id,
-    int audio_subscription_id,
-    int video_subscription_id) {
+    PermissionController::SubscriptionId audio_subscription_id,
+    PermissionController::SubscriptionId video_subscription_id) {
   DCHECK_CURRENTLY_ON(BrowserThread::IO);
 
   DeviceRequest* const request = FindRequest(label);
@@ -2811,8 +2811,8 @@ void MediaStreamManager::SetPermissionSubscriptionIDs(
 void MediaStreamManager::UnsubscribeFromPermissionControllerOnUIThread(
     int requesting_process_id,
     int requesting_frame_id,
-    int audio_subscription_id,
-    int video_subscription_id) {
+    PermissionController::SubscriptionId audio_subscription_id,
+    PermissionController::SubscriptionId video_subscription_id) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
 
   PermissionControllerImpl* controller =
