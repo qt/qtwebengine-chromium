6a98644e201b8557b47e5ceb2d4505d8fc41ea48
Michal Klocek <michal.klocek@qt.io>
2021-04-30 19:30:50 +0200

    Fix crashes on getUserMediaRequest
    If the build is compiled with no webrtc, the platform
    abstraction will return no VideoCaptureImplManager and this
    is not handled well within LocalVideoCapturerSource.
    After a short call stack analysis, it seems the most suitable
    place to bailout is in the user media processor, report no
    hardware as in unit tests.
    
    Change-Id: I75c92e4fe8869d6f3fb6831e8bf24c982d0b347c
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/third_party/blink/renderer/modules/mediastream/user_media_processor.cc
+++ chromium/third_party/blink/renderer/modules/mediastream/user_media_processor.cc
@@ -973,6 +973,11 @@ void UserMediaProcessor::OnStreamGenerated(
     return;
   }
 
+#if !BUILDFLAG(ENABLE_WEBRTC)
+  OnStreamGenerationFailed(request_id, MediaStreamRequestResult::NO_HARDWARE);
+  return;
+#endif
+
   if (!IsCurrentRequestInfo(request_id)) {
     // This can happen if the request is canceled or the frame reloads while
     // MediaStreamDispatcherHost is processing the request.
