2e3194bc9afbc8428dfadc490bfc22abeffd9a0b
Peter Varga <pvarga@inf.u-szeged.hu>
2021-05-20 16:04:20 +0200

    Workaround revoked certificate check on Linux
    Comparing std::string with base::BasicStringPiece<std::string> doesn't
    seem to work properly in std::binary_search().
    
    Task-number: QTBUG-91467
    Change-Id: I535faa358e3ce20ddb87a0830aa97fa1953d5994
    Reviewed-by: Kirill Burtsev <kirill.burtsev@qt.io>
    (cherry picked from commit 5b2293cf1f238916da5cb40c4746dd987c071abd)
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/net/cert/crl_set.cc
+++ chromium/net/cert/crl_set.cc
@@ -298,7 +298,7 @@ bool CRLSet::Parse(base::StringPiece data, scoped_refptr<CRLSet>* out_crl_set) {
 
 CRLSet::Result CRLSet::CheckSPKI(base::StringPiece spki_hash) const {
   if (std::binary_search(blocked_spkis_.begin(), blocked_spkis_.end(),
-                         spki_hash))
+                         spki_hash.data()))
     return REVOKED;
   return GOOD;
 }
