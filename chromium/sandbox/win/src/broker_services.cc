1baa6e7f5c8bcc45c2f36d38dcd39e366864b82b
Allan Sandfeld Jensen <allan.jensen@qt.io>
2021-06-15 17:07:44 +0200

    Reintroduce SANDBOX_EXPORTS, and fix it for heterogeneous executables
    Fixes some bit-rot with the SANDBOX_EXPORTS define, and adds a configure
    flag to enable it.
    
    This makes it possible for us to enable Windows sandboxing with a separate
    helper process executable, as long as it statically links in the sandboxing
    code.
    
    Task-number: QTBUG-51170
    Change-Id: I2159cd178651ea94566839bec59ca0c681b916fb
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

--- chromium/sandbox/win/src/broker_services.cc
+++ chromium/sandbox/win/src/broker_services.cc
@@ -369,6 +369,7 @@ ResultCode BrokerServicesBase::SpawnTarget(const wchar_t* exe_path,
   if (!exe_path)
     return SBOX_ERROR_BAD_PARAMS;
 
+#if !defined(SANDBOX_EXPORTS)
   // This code should only be called from the exe, ensure that this is always
   // the case.
   HMODULE exe_module = nullptr;
@@ -377,7 +378,7 @@ ResultCode BrokerServicesBase::SpawnTarget(const wchar_t* exe_path,
       &exe_module));
   if (CURRENT_MODULE() != exe_module)
     return SBOX_ERROR_INVALID_LINK_STATE;
-
+#endif
   if (!policy)
     return SBOX_ERROR_BAD_PARAMS;
 
