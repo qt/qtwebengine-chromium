bf7b0c04ad65c5e2c88c5fefde5c91eadb51e18c
Allan Sandfeld Jensen <allan.jensen@qt.io>
2021-06-15 17:07:44 +0200

    [Revert] [Windows]: Remove SANDBOX_EXPORTS support.
    Change-Id: I74f7ef9332a41171e1968cf294d70f3669238a61
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/sandbox/win/src/sandbox_types.h
+++ chromium/sandbox/win/src/sandbox_types.h
@@ -195,7 +195,11 @@ struct SandboxInterfaceInfo {
   raw_ptr<TargetServices> target_services;
 };
 
+#if SANDBOX_EXPORTS
+#define SANDBOX_INTERCEPT extern "C" __declspec(dllexport)
+#else
 #define SANDBOX_INTERCEPT extern "C"
+#endif
 
 enum InterceptionType {
   INTERCEPTION_INVALID = 0,
