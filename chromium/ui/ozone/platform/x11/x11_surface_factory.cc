8ac7034da77bbfa8f4110ed3f7e81434663370ce
Allan Sandfeld Jensen <allan.jensen@qt.io>
2021-07-05 15:40:14 +0200

    Remove dependency on libgbm
    We don't currently use it so avoid adding it as a dependency
    
    Change-Id: Id363ddfc4d6d95af8325dc73fb7e240a8d80c95e
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

--- chromium/ui/ozone/platform/x11/x11_surface_factory.cc
+++ chromium/ui/ozone/platform/x11/x11_surface_factory.cc
@@ -152,6 +152,7 @@ scoped_refptr<gfx::NativePixmap> X11SurfaceFactory::CreateNativePixmap(
     gfx::BufferUsage usage,
     absl::optional<gfx::Size> framebuffer_size) {
   scoped_refptr<gfx::NativePixmapDmaBuf> pixmap;
+#if !defined(TOOLKIT_QT)
   auto buffer = ui::GpuMemoryBufferSupportX11::GetInstance()->CreateBuffer(
       format, size, usage);
   if (buffer) {
@@ -159,7 +160,7 @@ scoped_refptr<gfx::NativePixmap> X11SurfaceFactory::CreateNativePixmap(
     pixmap = base::MakeRefCounted<gfx::NativePixmapDmaBuf>(size, format,
                                                            std::move(handle));
   }
-
+#endif
   // CreateNativePixmap is non-blocking operation. Thus, it is safe to call it
   // and return the result with the provided callback.
   return pixmap;
