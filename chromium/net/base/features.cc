5f7cf63037d8876b06f5fd4ff93f5f144c41fdd0
Allan Sandfeld Jensen <allan.jensen@qt.io>
2021-07-12 13:28:03 +0200

    Use Chrome HSTS
    We now enable it by default,but allow users to disable it with
    --disable-features=ChromeStaticPinning
    
    Fixes: QTBUG-88036
    Change-Id: I0fac896c16a8a060232419d9d03978cd0e2803d0
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/net/base/features.cc
+++ chromium/net/base/features.cc
@@ -23,6 +23,10 @@ BASE_FEATURE(kCapReferrerToOriginOnCrossOrigin,
              "CapReferrerToOriginOnCrossOrigin",
              base::FEATURE_DISABLED_BY_DEFAULT);
 
+BASE_FEATURE(kChromeStaticPinning,
+             "ChromeStaticPinning",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 BASE_FEATURE(kAsyncDns,
              "AsyncDns",
 #if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_MAC) || BUILDFLAG(IS_ANDROID) || \
