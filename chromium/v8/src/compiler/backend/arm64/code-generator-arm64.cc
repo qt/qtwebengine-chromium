adcb7c9d94a507286e02c6c2005498011eb5a554
Georg Neis <neis@chromium.org>
2021-08-23 19:37:54 +0200

    [Backport] Security bug 1239116
    Cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/v8/v8/+/3114128:
    Merged: Squashed multiple commits.
    
    Merged: [wasm][arm64] Fix zero-extention of i32.load8_s
    Revision: 748ae7cbf4d747906eb34bbe5def5433734fbf7d
    
    Merged: [wasm][arm64] Fix 16-bit to 32-bit sign extension
    Revision: 4d0730084da9ee790363979b51f93d1465708e9e
    
    BUG=chromium:1239116
    NOTRY=true
    NOPRESUBMIT=true
    NOTREECHECKS=true
    R=ahaas@chromium.org
    
    Change-Id: I1de24ec7c64fdbc50d9ccf0c2a9d136fec1a1e42
    Reviewed-by: Andreas Haas <ahaas@chromium.org>
    Commit-Queue: Georg Neis <neis@chromium.org>
    Cr-Commit-Position: refs/branch-heads/9.2@{#61}
    Cr-Branched-From: 51238348f95a1f5e0acc321efac7942d18a687a2-refs/heads/9.2.230@{#1}
    Cr-Branched-From: 587a04f02ab0487d194b55a7137dc2045e071597-refs/heads/master@{#74656}
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>
    (cherry picked from commit 49738bea0e67e07d8c183683d74fe84d40a4f734)

--- chromium/v8/src/compiler/backend/arm64/code-generator-arm64.cc
+++ chromium/v8/src/compiler/backend/arm64/code-generator-arm64.cc
@@ -101,7 +101,7 @@ class Arm64OperandConverter final : public InstructionOperandConverter {
 
   Register OutputRegister64() { return OutputRegister(); }
 
-  Register OutputRegister32() { return ToRegister(instr_->Output()).W(); }
+  Register OutputRegister32() { return OutputRegister().W(); }
 
   Register TempRegister32(size_t index) {
     return ToRegister(instr_->TempAt(index)).W();
@@ -1644,6 +1644,10 @@ CodeGenerator::CodeGenResult CodeGenerator::AssembleArchInstruction(
       __ Ldrsb(i.OutputRegister(), i.MemoryOperand());
       EmitWordLoadPoisoningIfNeeded(this, opcode, instr, i);
       break;
+    case kArm64LdrsbW:
+      EmitOOLTrapIfNeeded(zone(), this, opcode, instr, __ pc_offset());
+      __ Ldrsb(i.OutputRegister32(), i.MemoryOperand());
+      break;
     case kArm64Strb:
       __ Strb(i.InputOrZeroRegister64(0), i.MemoryOperand(1));
       break;
@@ -1654,6 +1658,10 @@ CodeGenerator::CodeGenResult CodeGenerator::AssembleArchInstruction(
     case kArm64Ldrsh:
       __ Ldrsh(i.OutputRegister(), i.MemoryOperand());
       EmitWordLoadPoisoningIfNeeded(this, opcode, instr, i);
+      break;
+    case kArm64LdrshW:
+      EmitOOLTrapIfNeeded(zone(), this, opcode, instr, __ pc_offset());
+      __ Ldrsh(i.OutputRegister32(), i.MemoryOperand());
       break;
     case kArm64Strh:
       __ Strh(i.InputOrZeroRegister64(0), i.MemoryOperand(1));
