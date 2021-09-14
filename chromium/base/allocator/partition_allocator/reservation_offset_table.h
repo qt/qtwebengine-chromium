d621ab3ee7186ec07edd2b68ec5498598696e495
Peter Varga <pvarga@inf.u-szeged.hu>
2021-09-14 15:25:32 +0200

    Fix QtWebEngine build with clang-cl
    Change-Id: Ia212712f07627f919c30de4d06bbe2d7f8e3ddea
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/base/allocator/partition_allocator/reservation_offset_table.h
+++ chromium/base/allocator/partition_allocator/reservation_offset_table.h
@@ -196,7 +196,7 @@ PA_ALWAYS_INLINE uintptr_t GetDirectMapReservationStart(uintptr_t address) {
   if (*offset_ptr == kOffsetTagNormalBuckets)
     return 0;
   uintptr_t reservation_start = ComputeReservationStart(address, offset_ptr);
-#if BUILDFLAG(PA_DCHECK_IS_ON)
+#if BUILDFLAG(PA_DCHECK_IS_ON) && !defined(COMPILER_MSVC)
   // MSVC workaround: the preprocessor seems to choke on an `#if` embedded
   // inside another macro (PA_DCHECK).
 #if !BUILDFLAG(HAS_64_BIT_POINTERS)
