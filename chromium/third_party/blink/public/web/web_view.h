d8763a05fe1d6645ab524b80e9e5337192f7c795
Allan Sandfeld Jensen <allan.jensen@qt.io>
2021-09-30 11:25:49 +0200

    [expose] Export a base background-color override from WebView again
    web_view_impl.h does not compile outside of blink, so we need
    the setter in WebView.
    
    Change-Id: Ibdd48b86cd026270a5c48c4d6ba7e3c251062438
    Reviewed-by: Jüri Valdmann <juri.valdmann@qt.io>

--- chromium/third_party/blink/public/web/web_view.h
+++ chromium/third_party/blink/public/web/web_view.h
@@ -365,6 +365,8 @@ class BLINK_EXPORT WebView {
 
   // Custom colors -------------------------------------------------------
 
+  virtual void SetBaseBackgroundColorOverrideForInspector(std::optional<SkColor>) {}
+
   virtual void SetDeviceColorSpaceForTesting(
       const gfx::ColorSpace& color_space) = 0;
 
