75462814a26dca951c9b18d077cd61ec0698a2eb
Allan Sandfeld Jensen <allan.jensen@qt.io>
2021-10-13 12:36:02 +0200

    Fix build with Win10 21H1 SDK and Win11 SDK
    Different parts are not fully defined
    
    Task-number: QTBUG-96533
    Change-Id: I4fcb795cea33c519ee082aa69a63660e581a2759
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

--- chromium/media/gpu/windows/d3d11_av1_accelerator.cc
+++ chromium/media/gpu/windows/d3d11_av1_accelerator.cc
@@ -16,6 +16,7 @@
 #include "media/gpu/codec_picture.h"
 #include "media/gpu/windows/d3d11_picture_buffer.h"
 
+#if !defined(NTDDI_WIN10_FE) // Windows 10.0.20348.0
 // These are from <dxva.h> in a newer SDK than the one Chrome ships with. They
 // should be deleted once Chrome switches to the updated SDK; they have been
 // copied from: https://www.microsoft.com/en-us/download/details.aspx?id=101577
@@ -280,6 +281,7 @@ typedef struct _DXVA_Tile_AV1 {
   UCHAR Reserved8Bits;
 } DXVA_Tile_AV1, *LPDXVA_Tile_AV1;
 #pragma pack(pop)
+#endif // !defined(NTDDI_WIN10_FE)
 
 namespace media {
 
