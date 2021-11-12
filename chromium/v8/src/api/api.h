d293c66f6ecb3473043ca9d33ae93c2a527b7a52
Igor Sheludko <ishell@chromium.org>
2021-11-12 21:31:44 +0100

    [Backport] CVE-2022-0470: Out of bounds memory access in V8
    Cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/v8/v8/+/3279680:
    Fix tracking of entered contexts
    
    The entered contexts stack must be in sync with the flags stack.
    
    Bug: chromium:1269225
    Change-Id: Ibb522286b47866d5f13aaec1a0a02914c13a5545
    Commit-Queue: Igor Sheludko <ishell@chromium.org>
    Commit-Queue: Shu-yu Guo <syg@chromium.org>
    Auto-Submit: Igor Sheludko <ishell@chromium.org>
    Reviewed-by: Shu-yu Guo <syg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#77882}
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/v8/src/api/api.h
+++ chromium/v8/src/api/api.h
@@ -459,6 +459,7 @@ bool HandleScopeImplementer::HasSavedContexts() {
 }
 
 void HandleScopeImplementer::EnterContext(Context context) {
+  DCHECK_EQ(entered_contexts_.capacity(), is_microtask_context_.capacity());
   DCHECK_EQ(entered_contexts_.size(), is_microtask_context_.size());
   entered_contexts_.push_back(context);
   is_microtask_context_.push_back(0);
@@ -466,6 +467,7 @@ void HandleScopeImplementer::EnterContext(Context context) {
 
 void HandleScopeImplementer::LeaveContext() {
   DCHECK(!entered_contexts_.empty());
+  DCHECK_EQ(entered_contexts_.capacity(), is_microtask_context_.capacity());
   DCHECK_EQ(entered_contexts_.size(), is_microtask_context_.size());
   entered_contexts_.pop_back();
   is_microtask_context_.pop_back();
@@ -476,6 +478,7 @@ bool HandleScopeImplementer::LastEnteredContextWas(Context context) {
 }
 
 void HandleScopeImplementer::EnterMicrotaskContext(Context context) {
+  DCHECK_EQ(entered_contexts_.capacity(), is_microtask_context_.capacity());
   DCHECK_EQ(entered_contexts_.size(), is_microtask_context_.size());
   entered_contexts_.push_back(context);
   is_microtask_context_.push_back(1);
