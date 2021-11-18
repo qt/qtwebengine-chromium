a3ceaefaa941693545f0281e48aa13fb142f2362
Peter Varga <pvarga@inf.u-szeged.hu>
2021-11-18 16:13:17 +0100

    Revert "Enable build of tracing UI"
    This reverts commit b441b8e512ff72cab0802d130b4999b6ff27dfa6.
    
    Change-Id: I7413f75852cfc20fd966e454ab4d0421c8af7385
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/third_party/catapult/common/py_vulcanize/py_vulcanize/parse_html_deps.py
+++ chromium/third_party/catapult/common/py_vulcanize/py_vulcanize/parse_html_deps.py
@@ -14,6 +14,7 @@ from py_vulcanize import html_generation_controller
 from py_vulcanize import js_utils
 from py_vulcanize import module
 from py_vulcanize import strip_js_comments
+import six
 
 
 def _AddToPathIfNeeded(path):
@@ -52,7 +53,6 @@ def _InitBeautifulSoup():
 
 _InitBeautifulSoup()
 import bs4
-import six
 
 class Script(object):
 
