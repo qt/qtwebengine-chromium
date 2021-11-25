d64ac86b356d7128ed4988aeacadae4820eb1f6d
Kirill Burtsev <kirill.burtsev@qt.io>
2021-11-25 15:38:04 +0100

    [gpu] Fix stack overflow on gpu channel recreate with an error
    On system resume new gpu channel is requested by viz. That will
    inevitably fail multiple time due to gpu driver peculiarities. In case
    of in-process gpu, callback with an error for channel recreate will be
    called in the same stack frame, so cover this case to not cause
    recursive calls and overflow.
    
    Task-number: QTBUG-71611
    Change-Id: I989d5666d391b6b1678a48d1c06a58d60a305d48
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit 0777b581381eca311b1eb93ed96e8c43f38c174c)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- chromium/content/browser/compositor/viz_process_transport_factory.cc
+++ chromium/content/browser/compositor/viz_process_transport_factory.cc
@@ -209,9 +209,10 @@ void VizProcessTransportFactory::CreateLayerTreeFrameSink(
   }
 #endif
 
-  gpu_channel_establish_factory_->EstablishGpuChannel(
-      base::BindOnce(&VizProcessTransportFactory::OnEstablishedGpuChannel,
-                     weak_ptr_factory_.GetWeakPtr(), compositor));
+  EstablishGpuChannel(std::move(compositor));
+  // gpu_channel_establish_factory_->EstablishGpuChannel(
+  //     base::BindOnce(&VizProcessTransportFactory::OnEstablishedGpuChannel,
+  //                    weak_ptr_factory_.GetWeakPtr(), compositor));
 }
 
 scoped_refptr<viz::RasterContextProvider>
@@ -349,6 +350,13 @@ void VizProcessTransportFactory::PrepareForShutDown() {
   shutdown_=true;
 }
 
+void VizProcessTransportFactory::EstablishGpuChannel(base::WeakPtr<ui::Compositor> compositor)
+{
+  gpu_channel_establish_factory_->EstablishGpuChannel(
+      base::BindOnce(&VizProcessTransportFactory::OnEstablishedGpuChannel,
+                     weak_ptr_factory_.GetWeakPtr(), std::move(compositor)));
+}
+
 void VizProcessTransportFactory::OnEstablishedGpuChannel(
     base::WeakPtr<ui::Compositor> compositor_weak_ptr,
     scoped_refptr<gpu::GpuChannelHost> gpu_channel_host) {
@@ -363,9 +371,13 @@ void VizProcessTransportFactory::OnEstablishedGpuChannel(
     auto context_result = TryCreateContextsForGpuCompositing(gpu_channel_host);
     if (context_result == gpu::ContextResult::kTransientFailure) {
       // Get a new GpuChannelHost and retry context creation.
-      gpu_channel_establish_factory_->EstablishGpuChannel(
-          base::BindOnce(&VizProcessTransportFactory::OnEstablishedGpuChannel,
-                         weak_ptr_factory_.GetWeakPtr(), compositor_weak_ptr));
+      // MEMO: do this async, since callback may be called down the stack
+      // (with in proc gpu), leading to recursive retries and stack overflow
+      GetUIThreadTaskRunner({})->PostDelayedTask(
+        FROM_HERE,
+        base::BindOnce(&VizProcessTransportFactory::EstablishGpuChannel,
+          weak_ptr_factory_.GetWeakPtr(), std::move(compositor_weak_ptr)),
+        base::Milliseconds(16));
       return;
     } else if (gpu::IsFatalOrSurfaceFailure(context_result)) {
       DisableGpuCompositing(compositor);
