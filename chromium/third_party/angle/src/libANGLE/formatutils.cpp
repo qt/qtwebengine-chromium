240779de2a7e14e5917350e146a58800e0e1b692
Jonah Ryan-Davis <jonahr@google.com>
2021-11-30 16:14:03 -0500

    [Backport] CVE-2021-4058: Heap buffer overflow in ANGLE (2/2)
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/angle/angle/+/3308825:
    Rework compressed texture pixel unpack state handling.
    
    Compressed images do not use the pixel unpack parameters. Instead of
    handling this in Context, move this to formatutils, where it's already
    handled for the 2D case. Also, update the test to generate an ASAN error
    if not ignored for the 2D case.
    
    Bug: chromium:1267496
    Change-Id: Ib93bae00a2b0b75eafd74c267f737da225afd993
    Commit-Queue: Jonah Ryan-Davis <jonahr@google.com>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Jamie Madill <jmadill@chromium.org>
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/third_party/angle/src/libANGLE/formatutils.cpp
+++ chromium/third_party/angle/src/libANGLE/formatutils.cpp
@@ -1321,7 +1321,7 @@ bool InternalFormat::computeRowPitch(GLenum formatType,
                                      GLint rowLength,
                                      GLuint *resultOut) const
 {
-    // Compressed images do not use pack/unpack parameters.
+    // Compressed images do not use pack/unpack parameters (rowLength).
     if (compressed)
     {
         ASSERT(rowLength == 0);
@@ -1342,8 +1342,10 @@ bool InternalFormat::computeDepthPitch(GLsizei height,
                                        GLuint rowPitch,
                                        GLuint *resultOut) const
 {
-    CheckedNumeric<GLuint> pixelsHeight(imageHeight > 0 ? static_cast<GLuint>(imageHeight)
-                                                        : static_cast<GLuint>(height));
+    // Compressed images do not use pack/unpack parameters (imageHeight).
+    CheckedNumeric<GLuint> pixelsHeight(!compressed && (imageHeight > 0)
+                                            ? static_cast<GLuint>(imageHeight)
+                                            : static_cast<GLuint>(height));
 
     CheckedNumeric<GLuint> rowCount;
     if (compressed)
