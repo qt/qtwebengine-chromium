6e3aab676e9e14ab58a716350ca0fbc821f9da1f
Clemens Backes <clemensb@chromium.org>
2022-02-24 14:53:01 +0100

    [Backport] Secuirity Bug 1296876
    Manual backport of patch originally reviewed on
    https://chromium-review.googlesource.com/c/v8/v8/+/3509394:
    [M96-LTS][liftoff][x64] Fix bug in i32.atomic.sub32
    
    {AtomicSub} on x64 first negates the {value} register, then does an
    atomic addition. For that reason, {value} should be a unique register.
    So far, we only checked that it's not used in the value stack, but we
    should also check for overlap with the destination address or the offset
    register.
    
    Drive-by: Remove unneeded handling of non-unique register index on arm,
    as that cannot happen (LiftoffCompiler ensures that the result register
    is unique).
    
    R=thibaudm@chromium.org
    
    (cherry picked from commit b5003a3c631328bfbe3357dfea7aaebf46316c09)
    
    Bug: chromium:1296876
    Change-Id: Ie6b97eec8e8dea07b0bcc644d261f47467cc5b8e
    Commit-Queue: Clemens Backes <clemensb@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#79265}
    Reviewed-by: Clemens Backes <clemensb@chromium.org>
    Commit-Queue: Roger Felipe Zanoni da Silva <rzanoni@google.com>
    Cr-Commit-Position: refs/branch-heads/9.6@{#52}
    Cr-Branched-From: 0b7bda016178bf438f09b3c93da572ae3663a1f7-refs/heads/9.6.180@{#1}
    Cr-Branched-From: 41a5a247d9430b953e38631e88d17790306f7a4c-refs/heads/main@{#77244}
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/v8/src/wasm/baseline/liftoff-compiler.cc
+++ chromium/v8/src/wasm/baseline/liftoff-compiler.cc
@@ -2155,6 +2155,7 @@ class LiftoffCompiler {
   void AlignmentCheckMem(FullDecoder* decoder, uint32_t access_size,
                          uint32_t offset, Register index,
                          LiftoffRegList pinned) {
+    DEBUG_CODE_COMMENT("alignment check");
     Label* trap_label = AddOutOfLineTrap(
         decoder->position(), WasmCode::kThrowWasmTrapUnalignedAccess, 0);
     Register address = __ GetUnusedRegister(kGpReg, pinned).gp();
@@ -3122,9 +3123,9 @@ class LiftoffCompiler {
     LiftoffRegister value = pinned.set(__ PopToRegister());
 #ifdef V8_TARGET_ARCH_IA32
     // We have to reuse the value register as the result register so that we
-    //  don't run out of registers on ia32. For this we use the value register
-    //  as the result register if it has no other uses. Otherwise  we allocate
-    //  a new register and let go of the value register to get spilled.
+    // don't run out of registers on ia32. For this we use the value register as
+    // the result register if it has no other uses. Otherwise we allocate a new
+    // register and let go of the value register to get spilled.
     LiftoffRegister result = value;
     if (__ cache_state()->is_used(value)) {
       result = pinned.set(__ GetUnusedRegister(value.reg_class(), pinned));
@@ -3143,6 +3144,7 @@ class LiftoffCompiler {
     }
     AlignmentCheckMem(decoder, type.size(), imm.offset, index, pinned);
 
+    DEBUG_CODE_COMMENT("atomic binop");
     uint32_t offset = imm.offset;
     index = AddMemoryMasking(index, &offset, &pinned);
     Register addr = pinned.set(__ GetUnusedRegister(kGpReg, pinned)).gp();
