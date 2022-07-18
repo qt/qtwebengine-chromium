23b231e6ced518daf63ac549f1618bf3d2397ce5
Shrek Shao <shrekshao@google.com>
2022-07-18 10:44:11 +0000

    [Backport] Security bug 1340654
    Cherry-pick of patch orignally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/3765222:
    Fix dawn write handle data update OOB check
    
    (cherry picked from commit 0ba6ae3d447de7bc599a191f6792a4e6676f10a3)
    
    Bug: chromium:1340654
    Change-Id: I9d87cb868eccc380f707ab6c3c6bdc26c386fbfc
    Commit-Queue: Shrek Shao <shrekshao@google.com>
    Cr-Original-Commit-Position: refs/heads/main@{#1021911}
    Reviewed-by: Simon Hangl <simonha@google.com>
    Owners-Override: Simon Hangl <simonha@google.com>
    Commit-Queue: Roger Felipe Zanoni da Silva <rzanoni@google.com>
    Cr-Commit-Position: refs/branch-heads/4664@{#1660}
    Cr-Branched-From: 24dc4ee75e01a29d390d43c9c264372a169273a7-refs/heads/main@{#929512}
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/gpu/command_buffer/service/webgpu_decoder_impl.cc
+++ chromium/gpu/command_buffer/service/webgpu_decoder_impl.cc
@@ -1026,7 +1026,6 @@ error::Error WebGPUDecoderImpl::HandleDawnCommands(
                "WebGPUDecoderImpl::HandleDawnCommands", "bytes", size);
 
   if (!wire_server_->HandleCommands(shm_commands, size)) {
-    NOTREACHED();
     return error::kLostContext;
   }
 
