36f07d88ec39bf73cf3c251205fee8a4d8da4947
Martin Negyokru <negyokru@inf.u-szeged.hu>
2022-09-20 12:51:49 +0200

    Adapt DevToolsFileHelper for WebEngine
    Task-number: QTBUG-85171
    Change-Id: Ie22d374f89c82633c9e9aa317050a3906eb67bff
    Reviewed-by: Szabolcs David <davidsz@inf.u-szeged.hu>
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/chrome/browser/devtools/devtools_ui_bindings.cc
+++ chromium/chrome/browser/devtools/devtools_ui_bindings.cc
@@ -2251,7 +2251,11 @@ void DevToolsUIBindings::ReadyToCommitNavigation(
 }
 
 void DevToolsUIBindings::DocumentOnLoadCompletedInPrimaryMainFrame() {
+  // Rely on LoadCompleted event dispatched by the frontend rather than the
+  // 'onload' DOM event.
+#if !defined(TOOLKIT_QT)
   FrontendLoaded();
+#endif  // !defined(TOOLKIT_QT)
 }
 
 void DevToolsUIBindings::PrimaryPageChanged() {
