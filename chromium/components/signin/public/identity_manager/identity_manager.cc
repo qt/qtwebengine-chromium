481d91afcf30a6491cdf27868025b2d2c242ef69
Michal Klocek <michal.klocek@qt.io>
2022-09-23 09:20:35 +0200

    Fix initalization for FCM
    We should not use account fetcher as it initalizes more
    stuff and expects image fetcher for GAIA accounts.
    
    Change-Id: I39cb08b118ea6cadc2d369049cf852515ebd7a0d
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/434519
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/components/signin/public/identity_manager/identity_manager.cc
+++ chromium/components/signin/public/identity_manager/identity_manager.cc
@@ -128,7 +128,10 @@ IdentityManager::IdentityManager(IdentityManager::InitParameters&& parameters)
                         std::move(parameters.device_accounts_synchronizer)),
       diagnostics_provider_(std::move(parameters.diagnostics_provider)),
       account_consistency_(parameters.account_consistency) {
+
+#if !defined(TOOLKIT_QT)
   DCHECK(account_fetcher_service_);
+#endif
   DCHECK(diagnostics_provider_);
 
   primary_account_manager_observation_.Observe(primary_account_manager_.get());
@@ -414,8 +417,12 @@ void IdentityManager::RemoveDiagnosticsObserver(DiagnosticsObserver* observer) {
 }
 
 void IdentityManager::OnNetworkInitialized() {
+#if !defined(TOOLKIT_QT)
   gaia_cookie_manager_service_->InitCookieListener();
   account_fetcher_service_->OnNetworkInitialized();
+#else
+  DCHECK(false);  // we should never call network initialized
+#endif
 }
 
 #if BUILDFLAG(IS_CHROMEOS_ASH)
