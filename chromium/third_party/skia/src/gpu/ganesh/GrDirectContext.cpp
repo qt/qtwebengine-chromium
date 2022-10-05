d5574f2787c7ce7a4579464b286870cf3fc69392
Greg Daniel <egdaniel@google.com>
2022-10-05 15:28:56 -0400

    [Backport] CVE-2022-3445: Use after free in Skia
    Cherry-pick of patch originally reviewed on
    https://skia-review.googlesource.com/c/skia/+/587879:
    Fix GrDirectContext::fClinetMappedBuffer access in abandoned callbacks.
    
    Bug: chromium:1364604
    Change-Id: I1ca44cab1c762e7f94ac94be94991ec94a7497be
    Commit-Queue: Greg Daniel <egdaniel@google.com>
    Reviewed-by: Brian Salomon <bsalomon@google.com>
    Auto-Submit: Greg Daniel <egdaniel@google.com>
    Commit-Queue: Brian Salomon <bsalomon@google.com>
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/third_party/skia/src/gpu/ganesh/GrDirectContext.cpp
+++ chromium/third_party/skia/src/gpu/ganesh/GrDirectContext.cpp
@@ -142,9 +142,6 @@ void GrDirectContext::abandonContext() {
 
     fGpu->disconnect(GrGpu::DisconnectType::kAbandon);
 
-    // Must be after GrResourceCache::abandonAll().
-    fMappedBufferManager.reset();
-
     if (fSmallPathAtlasMgr) {
         fSmallPathAtlasMgr->reset();
     }
