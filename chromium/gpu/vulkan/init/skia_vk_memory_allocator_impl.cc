3f9e2f397c6b6968c6416601cd6fca91eb485778
Peter Varga <pvarga@inf.u-szeged.hu>
2022-10-27 14:34:46 +0200

    Make GrVkImage external
    Task-number: QTBUG-107669
    Change-Id: I8ba6c8bab4f29c13b368a8e53f762d6aac1f009c
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/435455
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/442494
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/gpu/vulkan/init/skia_vk_memory_allocator_impl.cc
+++ chromium/gpu/vulkan/init/skia_vk_memory_allocator_impl.cc
@@ -50,6 +50,10 @@ class SkiaVulkanMemoryAllocator : public skgpu::VulkanMemoryAllocator {
         .pUserData = nullptr,
     };
 
+#if defined(TOOLKIT_QT)
+    info.flags |= VMA_ALLOCATION_CREATE_DEDICATED_MEMORY_BIT;
+#endif
+
     if (kDedicatedAllocation_AllocationPropertyFlag & flags) {
       info.flags |= VMA_ALLOCATION_CREATE_DEDICATED_MEMORY_BIT;
     }
