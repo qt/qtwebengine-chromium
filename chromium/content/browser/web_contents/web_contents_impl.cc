f18347f3e8487e6ba027610747f7252ddaf2cdcd
Szabolcs David <davidsz@inf.u-szeged.hu>
2022-12-12 18:13:26 +0100

    Do not stop navigation during saving the page if not necessary
    Calling Stop() interrupts printing, even if there is no page load
    in progress.
    
    Task-number: QTBUG-108154
    Change-Id: Ibb8e898c3c7e7ea1933c88aa32d8f96604a6527a
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/470895
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/content/browser/web_contents/web_contents_impl.cc
+++ chromium/content/browser/web_contents/web_contents_impl.cc
@@ -5652,7 +5652,8 @@ void WebContentsImpl::OnSavePage() {
     return;
   }
 
-  Stop();
+  if (IsLoading())
+    Stop();
 
   // Create the save package and possibly prompt the user for the name to save
   // the page as. The user prompt is an asynchronous operation that runs on
