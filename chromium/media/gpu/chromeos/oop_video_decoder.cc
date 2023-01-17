349fab546e7709c01aad0936e2274167009a19f5
Allan Sandfeld Jensen <allan.jensen@qt.io>
2023-01-17 17:20:04 +0100

    Fix vaapi support for QtWebEngine
    Make Linux GPU memory buffers compile time optional based on use_vaapi
    
    Task-number: QTBUG-91677
    Change-Id: I07880836a3e2944dd2079e982d9eaf3ac3fde97e
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/454907
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

--- chromium/media/gpu/chromeos/oop_video_decoder.cc
+++ chromium/media/gpu/chromeos/oop_video_decoder.cc
@@ -8,12 +8,15 @@
 #include "base/task/bind_post_task.h"
 #include "base/task/sequenced_task_runner.h"
 #include "build/chromeos_buildflags.h"
-#include "chromeos/components/cdm_factory_daemon/stable_cdm_context_impl.h"
 #include "media/gpu/macros.h"
 #include "media/mojo/common/mojo_decoder_buffer_converter.h"
 #include "mojo/public/cpp/bindings/pending_remote.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "mojo/public/cpp/bindings/self_owned_receiver.h"
+
+#if BUILDFLAG(IS_CHROMEOS)
+#include "chromeos/components/cdm_factory_daemon/stable_cdm_context_impl.h"
+#endif
 
 #if BUILDFLAG(USE_VAAPI)
 #include "media/gpu/vaapi/vaapi_wrapper.h"
