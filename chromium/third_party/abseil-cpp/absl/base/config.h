8b2b44128018e2df1c236be32d28dac36c90e165
Peter Varga <pvarga@inf.u-szeged.hu>
2023-01-23 10:28:34 +0100

    [Backport] Map the absl::is_trivially_* functions to their std impl
    There's no point redefining these functions if they are supported by the
    compiler and the version of libstdc++. Also, some of the builtins used
    by the absl implementation of these functions (e.g.
    __has_trivial_destructor) have been deprecated in Clang 15.
    
    Original commit:
    https://github.com/abseil/abseil-cpp/commit/cfe27e79cfcbefb2b4479e04f80cbb299bc46965
    
    Task-number: QTBUG-108240
    Change-Id: I7373b65d84909b2e1877b78ff9058446ab5720e4
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/455714
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/third_party/abseil-cpp/absl/base/config.h
+++ chromium/third_party/abseil-cpp/absl/base/config.h
@@ -208,6 +208,17 @@ static_assert(ABSL_INTERNAL_INLINE_NAMESPACE_STR[0] != 'h' ||
 #define ABSL_HAVE_STD_IS_TRIVIALLY_ASSIGNABLE 1
 #endif
 
+// ABSL_HAVE_STD_IS_TRIVIALLY_COPYABLE
+//
+// Checks whether `std::is_trivially_copyable<T>` is supported.
+//
+// Notes: Clang 15+ with libc++ supports these features, GCC hasn't been tested.
+#if defined(ABSL_HAVE_STD_IS_TRIVIALLY_COPYABLE)
+#error ABSL_HAVE_STD_IS_TRIVIALLY_COPYABLE cannot be directly set
+#elif defined(__clang__) && (__clang_major__ >= 15)
+#define ABSL_HAVE_STD_IS_TRIVIALLY_COPYABLE 1
+#endif
+
 // ABSL_HAVE_SOURCE_LOCATION_CURRENT
 //
 // Indicates whether `absl::SourceLocation::current()` will return useful
