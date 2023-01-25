2cfc5b82f506d10d4b566f11f8af920c34945214
Martin Negyokru <negyokru@inf.u-szeged.hu>
2023-01-25 14:48:11 +0100

    [override] Do not override getaddrinfo in libc_interceptor
    This code should live in an executable,
    but never in a library as it causes erratic behavior
    depending on the linking order.
    It is safe to guard it since it was only used for logging.
    Chromium change: https://chromium-review.googlesource.com/c/chromium/src/+/3936307
    
    Fixes: QTBUG-110287
    Change-Id: I511b24be3b3a89fa28d95f90293bdca9f38f70af
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/456327
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/sandbox/linux/services/libc_interceptor.cc
+++ chromium/sandbox/linux/services/libc_interceptor.cc
@@ -402,6 +402,7 @@ namespace {
 std::atomic<bool> g_getaddrinfo_discouraged{false};
 }  // namespace
 
+#if !BUILDFLAG(IS_QTWEBENGINE)
 extern "C" {
 __attribute__((visibility("default"), noinline)) int getaddrinfo(
     const char* node,
@@ -417,6 +418,7 @@ __attribute__((visibility("default"), noinline)) int getaddrinfo(
   return CALL_FUNC(getaddrinfo, node, service, hints, res);
 }
 }
+#endif  // !BUILDFLAG(IS_QTWEBENGINE)
 
 void DiscourageGetaddrinfo() {
   g_getaddrinfo_discouraged = true;
