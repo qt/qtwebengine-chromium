799b46219664a8b3f005cadeb02076590f6dbcc2
Jack Hsieh <chengweih@chromium.org>
2023-03-13 21:19:03 +0000

    [Backport] CVE-2023-2462: Inappropriate implementation in Prompts (8/10)
    Cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/4237626:
    usb: Reject using WebUSB API in an opaque origin
    
    Rejects renderer's request of using WebUSB API when the top-level
    document has an opaque origin.
    
    Bug: 1375133
    Change-Id: I1b449389e55ea8ead412ea9e87fc99971997b491
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4237626
    Reviewed-by: Hiroki Nakagawa <nhiroki@chromium.org>
    Commit-Queue: Jack Hsieh <chengweih@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Reilly Grant <reillyg@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116595}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/476782
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/content/browser/renderer_host/render_frame_host_impl.cc
+++ chromium/content/browser/renderer_host/render_frame_host_impl.cc
@@ -10521,6 +10521,12 @@ void RenderFrameHostImpl::CreateWebUsbService(
     mojo::ReportBadMessage("Permissions policy blocks access to USB.");
     return;
   }
+  if (GetOutermostMainFrame()->GetLastCommittedOrigin().opaque()) {
+    mojo::ReportBadMessage(
+        "WebUSB is not allowed when the top-level document has an opaque "
+        "origin.");
+    return;
+  }
   BackForwardCache::DisableForRenderFrameHost(
       this, BackForwardCacheDisable::DisabledReason(
                 BackForwardCacheDisable::DisabledReasonId::kWebUSB));
