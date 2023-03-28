65494811453b6b67adedba559a55959b8dc7719b
Michal Klocek <michal.klocek@qt.io>
2023-03-28 10:11:00 +0200

    Fix v8 context snapshot generator compilation
    Add missing overrides.
    Avoid to include GL headers on macOS.
    
    Change-Id: I5d5a8b6522eca9bb7511a6533abb4cdedcf1fd33
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

---
+++ chromium/tools/v8_context_snapshot/qt_overrides.cc
@@ -0,0 +1,66 @@
+﻿// Copyright (C) 2023 The Qt Company Ltd.
+// SPDX-License-Identifier: LicenseRef-Qt-Commercial OR LGPL-3.0-only OR
+// GPL-2.0-only OR GPL-3.0-only
+
+#include "build/build_config.h"
+#include "gpu/vulkan/buildflags.h"
+#include "ui/base/ozone_buildflags.h"
+
+#include "ui/base/dragdrop/os_exchange_data_provider_factory.h"
+#include "ui/base/pointer/pointer_device.h"
+#include "ui/base/resource/resource_bundle.h"
+
+#if BUILDFLAG(ENABLE_VULKAN)
+#include "gpu/vulkan/init/vulkan_factory.h"
+#endif
+
+#if BUILDFLAG(IS_LINUX) && BUILDFLAG(IS_OZONE_X11)
+void* GetQtXDisplay() {
+  return nullptr;
+}
+#endif  // if BUILDFLAG(IS_LINUX) && BUILDFLAG(IS_OZONE_X11)
+
+namespace ui {
+class OzonePlatform;
+OzonePlatform* CreateOzonePlatformQt() {
+  return nullptr;
+}
+std::unique_ptr<OSExchangeDataProvider>
+OSExchangeDataProviderFactory::CreateProvider() {
+  return nullptr;
+}
+bool ResourceBundle::LocaleDataPakExists(const std::string& locale) {
+  return false;
+}
+std::string ResourceBundle::LoadLocaleResources(const std::string& pref_locale,
+                                                bool crash_on_failure) {
+  return std::string();
+}
+void ResourceBundle::LoadCommonResources() {}
+int GetAvailablePointerTypes() {
+  return POINTER_TYPE_NONE;
+}
+int GetAvailableHoverTypes() {
+  return HOVER_TYPE_NONE;
+}
+gfx::Image& ResourceBundle::GetNativeImageNamed(int resource_id) {
+  return GetImageNamed(resource_id);
+}
+
+namespace gfx {
+class ClientNativePixmapFactory;
+}
+gfx::ClientNativePixmapFactory* CreateClientNativePixmapFactoryQt() {
+  return nullptr;
+}
+}  // namespace ui
+
+#if BUILDFLAG(ENABLE_VULKAN)
+namespace gpu {
+std::unique_ptr<VulkanImplementation> CreateVulkanImplementation(
+    bool use_swiftshader,
+    bool allow_protected_memory) {
+  return nullptr;
+}
+}
+#endif  // BUILDFLAG(ENABLE_VULKAN)
