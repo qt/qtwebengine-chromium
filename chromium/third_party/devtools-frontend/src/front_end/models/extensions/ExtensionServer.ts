fd7386da96e1170aebd911e81466d6583cab5cca
Danil Somsikov <dsv@chromium.org>
2023-03-31 09:02:16 +0200

    [Backport] Security bug 1428743
    Cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4386962:
    Do not allow devtools extensions to inspect the new tab page.
    
    Bug: 1428743
    Change-Id: I1f9aff0b249515ca40f37ab68aa12a6f29e5c1b9
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/4386962
    Reviewed-by: Andrey Kosyakov <caseq@chromium.org>
    Auto-Submit: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Danil Somsikov <dsv@chromium.org>
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/486077
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/third_party/devtools-frontend/src/front_end/models/extensions/ExtensionServer.ts
+++ chromium/third_party/devtools-frontend/src/front_end/models/extensions/ExtensionServer.ts
@@ -65,10 +65,7 @@ declare global {
   }
 }
 
-const kAllowedOrigins = [
-  'chrome://newtab',
-  'chrome://new-tab-page',
-].map(url => (new URL(url)).origin);
+const kAllowedOrigins = [].map(url => (new URL(url)).origin);
 
 let extensionServerInstance: ExtensionServer|null;
 
