fad30368aa72d0326e3103ad6d24ae4dec93a24a
Allan Sandfeld Jensen <allan.jensen@qt.io>
2023-04-20 12:11:48 +0200

    Collect extra gpu info on in-process-gpu too
    This gives us ANGLE feature data in chrome://gpu
    
    Change-Id: If4b1eae4697a297ae86f8d85059f531fe1924c8b
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/473467
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

--- chromium/gpu/ipc/service/gpu_init.cc
+++ chromium/gpu/ipc/service/gpu_init.cc
@@ -951,6 +951,12 @@ void GpuInit::InitializeInProcess(base::CommandLine* command_line,
 #endif  // BUILDFLAG(IS_MAC)
 
   if (!gl_disabled) {
+    if (!CollectGpuExtraInfo(&gpu_extra_info_, gpu_preferences)) {
+      VLOG(1) << "gpu::CollectGpuExtraInfo failed";
+    }
+  }
+
+  if (!gl_disabled) {
     if (!gpu_feature_info_.disabled_extensions.empty()) {
       gl::init::SetDisabledExtensionsPlatform(
           gpu_feature_info_.disabled_extensions);
