27281c33a6ccadfb74ce639677289fabfd60b170
Allan Sandfeld Jensen <allan.jensen@qt.io>
2023-05-30 15:25:07 +0200

    Fixup select elements
    Change-Id: I4a5701086ad7c69b4751060600d04afc536406b9
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/480619
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

--- chromium/content/browser/web_contents/web_contents_view_mac.h
+++ chromium/content/browser/web_contents/web_contents_view_mac.h
@@ -110,6 +110,7 @@ class WebContentsViewMac : public WebContentsView,
   void TakeFocus(bool reverse) override;
   void ShowContextMenu(RenderFrameHost& render_frame_host,
                        const ContextMenuParams& params) override;
+#if BUILDFLAG(USE_EXTERNAL_POPUP_MENU)
   void ShowPopupMenu(
       RenderFrameHost* render_frame_host,
       mojo::PendingRemote<blink::mojom::PopupMenuClient> popup_client,
@@ -120,6 +121,7 @@ class WebContentsViewMac : public WebContentsView,
       std::vector<blink::mojom::MenuItemPtr> menu_items,
       bool right_aligned,
       bool allow_multiple_selection) override;
+#endif
 
   // PopupMenuHelper::Delegate:
   void OnMenuClosed() override;
