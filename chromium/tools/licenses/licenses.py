75387cf7f09de5e833ba3f4ecf9480456e9836c7
Michal Klocek <michal.klocek@qt.io>
2023-08-01 15:36:29 +0200

    Pass python interpreter path to gn during license check
    The license check runs with interpreter found by cmake,
    unfortunately it is not set when script calls gn, which
    can end up in calling some incorrect executable.
    
    Set proper python interpreter path for gn.
    
    Fixes: QTBUG-115488
    Pick-to: 108-based
    Change-Id: Ic1c2b0933ddeb671843ce502da5fe00a893a59f8
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/493762
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/tools/licenses/licenses.py
+++ chromium/tools/licenses/licenses.py
@@ -906,7 +906,8 @@ def FindThirdPartyDeps(gn_binary : str,
     else:
        tmp_dir = gn_out_dir
     gn_deps = subprocess.check_output([
-        _GnBinary(), "desc",
+        gn_binary, "desc",
+        "--script-executable=%s" % sys.executable,
         "--root=%s" % _REPOSITORY_ROOT, tmp_dir, gn_target, "deps",
         "--as=buildfile", "--all"
     ],
