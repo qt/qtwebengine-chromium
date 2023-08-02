bfb686cc2df14a75bb3283e0e1c6729b1ccd9df3
Peter Varga <pvarga@inf.u-szeged.hu>
2023-08-02 09:28:40 +0200

    [profile] Make NetworkContext resettable
    Expose StoragePartitionImpl::InitNetworkContext() and add
    StoragePartition::NetworkContextCreatedObserver.
    
    The public StoragePartitionImpl::ResetNetworkContext() method allows
    recreating the NetworkContext used inside a storage partition so as to
    change network settings dynamically. The old network context is
    destroyed before creating the replacement.
    
    The observer notifies when NetworkContext is created and makes it
    possible to check if StoragePartitionImpl::ResetNetworkContext()
    is done with its asynchronous tasks.
    
    ===============================================================
    
    We posted the OnNetworkContextCreated call right after the
    construction of NetworkContext, however its client() is not
    guaranteed to exist at this point. Magically it worked for a long
    time, because typically the next call after construction is
    SetClient(), which (most of the time) can run before the posted
    lambda.
    
    The following change [1] introduced a new mojo call between the
    constructor and SetClient, which defers SetClient and so we miss
    the opportunity to notify client() in the lambda.
    
    Moving OnNetworkContextCreated() to SetClient() seems to be a good
    solution, because setting the client typically means that we
    finished the construction of a NetworkContext.
    
    This brings back clearHttpCacheCompleted signal to life and fixes
    tst_QWebEngineProfile::clearDataFromCache().
    
    [1] https://chromium-review.googlesource.com/c/chromium/src/+/5440136
    
    Task-number: QTBUG-81558
    Task-number: QTBUG-89670
    Task-number: QTBUG-111541
    Change-Id: I5f4ea50650b35e1f35ad67a009964e76038ea750
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/493884
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/content/browser/storage_partition_impl.h
+++ chromium/content/browser/storage_partition_impl.h
@@ -266,6 +266,8 @@ class CONTENT_EXPORT StoragePartitionImpl
   void ClearBluetoothAllowedDevicesMapForTesting() override;
   void AddObserver(DataRemovalObserver* observer) override;
   void RemoveObserver(DataRemovalObserver* observer) override;
+  void SetNetworkContextCreatedObserver(NetworkContextCreatedObserver *observer) override;
+  NetworkContextCreatedObserver *GetNetworkContextCreatedObserver() override;
   void FlushNetworkInterfaceForTesting() override;
   void FlushCertVerifierInterfaceForTesting() override;
   void WaitForDeletionTasksForTesting() override;
@@ -344,6 +346,7 @@ class CONTENT_EXPORT StoragePartitionImpl
       OnCanSendSCTAuditingReportCallback callback) override;
   void OnNewSCTAuditingReportSent() override;
 #endif
+  void OnNetworkContextCreated() override;
 
   // network::mojom::URLLoaderNetworkServiceObserver interface.
   void OnSSLCertificateError(const GURL& url,
@@ -483,6 +486,9 @@ class CONTENT_EXPORT StoragePartitionImpl
 
   std::vector<std::string> GetCorsExemptHeaderList();
 
+#if BUILDFLAG(IS_QTWEBENGINE)
+  void ResetNetworkContext() { InitNetworkContext(); }
+#endif
   void OpenLocalStorageForProcess(
       int process_id,
       const blink::StorageKey& storage_key,
@@ -929,6 +935,8 @@ class CONTENT_EXPORT StoragePartitionImpl
 #if DCHECK_IS_ON()
   bool on_browser_context_will_be_destroyed_called_ = false;
 #endif
+
+  NetworkContextCreatedObserver *network_context_created_observer_ = nullptr;
 
   // A copy of the network revocation nonces in `NetworkContext`. It is used for
   // restoring the network revocation states of fenced frames when there is a
