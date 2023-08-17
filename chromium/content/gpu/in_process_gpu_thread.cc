053835946c48a135b86e28b37f8ff127cbea1210
Peter Varga <pvarga@inf.u-szeged.hu>
2023-08-17 15:03:15 +0200

    Disable Windows IME for GPU thread
    Windows may send WM_IME_NOTIFY message to the GPU thread. This thread
    does not implement Windows message handler and will not handle it. The
    unhandled message causes the UI thread hang unless the IME is disabled
    for this thread.
    
    Fixes: QTBUG-73994
    Change-Id: I80809baa6cdad5b21c4c39e2e85ad842ca48b033
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/497434
    Reviewed-by: Martin Negyokru <negyokru@inf.u-szeged.hu>
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit 1f853a93a43ca3162ec1c6f85ea6224e380169b1)
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/500354

--- chromium/content/gpu/in_process_gpu_thread.cc
+++ chromium/content/gpu/in_process_gpu_thread.cc
@@ -24,6 +24,10 @@
 #include "base/android/jni_android.h"
 #endif
 
+#if BUILDFLAG(IS_WIN)
+#include <windows.h>
+#endif
+
 namespace content {
 
 InProcessGpuThread::InProcessGpuThread(
@@ -40,6 +44,10 @@ InProcessGpuThread::~InProcessGpuThread() {
 
 void InProcessGpuThread::Init() {
   base::ThreadPriority io_thread_priority = base::ThreadPriority::NORMAL;
+
+#if BUILDFLAG(IS_WIN)
+  ImmDisableIME(0);
+#endif
 
 #if defined(OS_ANDROID)
   // Call AttachCurrentThreadWithName, before any other AttachCurrentThread()
