10b47cf633096ed5aa48a22dad7e979c5534e053
Anu Aliyas <anu.aliyas@qt.io>
2023-08-24 17:41:35 +0200

    Fix for QtWebEngine crash on Windows
    - 112-based chromium code enforces that validating command decoder is not used on
    platforms where passthrough is fully launched. Refer https://chromium-review.googlesource.com/c/chromium/src/+/4212134
    for more details.
    - Allowed use of command decoder on Windows to avoid crash.
    
    Task-number: QTBUG-115703
    Change-Id: I0382bd9f95299bdd429fda42271c0951b8354bbe
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/499165
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ chromium/gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -2979,8 +2979,10 @@ GLES2Decoder* GLES2Decoder::Create(
                                            outputter, group);
   }
 
+// Fix for Qt WebEngine Crash when QSG_RHI_BACKEND=opengl
 // Allow linux to run fuzzers.
-#if BUILDFLAG(ENABLE_VALIDATING_COMMAND_DECODER) || BUILDFLAG(IS_LINUX)
+#if BUILDFLAG(ENABLE_VALIDATING_COMMAND_DECODER) || BUILDFLAG(IS_LINUX) || \
+    BUILDFLAG(IS_WIN)
   return new GLES2DecoderImpl(client, command_buffer_service, outputter, group);
 #else
   LOG(FATAL) << "Validating command decoder is not supported.";
