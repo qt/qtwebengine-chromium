96c7f547c282ae7e07a2ea552c183a8cca0b806b
Peter Varga <pvarga@inf.u-szeged.hu>
2023-09-08 09:50:36 +0200

    Add Browser.close DevTools protocol command
    Upstream implements it in Chrome. For QtWebEngine, use
    QWebEnginePage::windowCloseRequested() signal to notify when the browser
    is requested to be gracefully closed via DevTools.
    
    Task-number: QTBUG-82046
    Change-Id: If50fa7a625404198f19ff29fa731baec68592e22
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/502668
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/content/browser/devtools/protocol/browser_handler.h
+++ chromium/content/browser/devtools/protocol/browser_handler.h
@@ -91,6 +91,7 @@ class BrowserHandler : public DevToolsDomainHandler,
 
   Response Crash() override;
   Response CrashGpuProcess() override;
+  Response Close() override;
 
   // DownloadItem::Observer overrides
   void OnDownloadUpdated(download::DownloadItem* item) override;
