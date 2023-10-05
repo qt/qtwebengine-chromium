cf3833591a301c2fecf95548ee9edeee3b49baf4
Michal Klocek <michal.klocek@qt.io>
2023-10-05 16:00:18 +0200

    Use pkg-config on mac if found
    Our configure logic checks for pkg-config,
    runs it with cmake and set the system libs features
    accordingly. In case the user installs pkg-cofnig
    on mac this will however run into issues as
    pkg-config wrapper for gn assumes it works only on linux
    on the mac platform returns the success without any data.
    
    Assume if cmake was successful with pkg-config calls, gn
    pkg-config wrapper script would also be.
    
    Do not use sysroot in case of mac, as mac sdk
    does not provide pc files and should therefore not be used
    for that.
    
    Task-number: QTBUG-115357
    Change-Id: Ie222bd1840590110270af1191dfd37a661780fdf
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/509803
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/525671
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/build/config/linux/pkg-config.py
+++ chromium/build/config/linux/pkg-config.py
@@ -108,7 +108,7 @@ def main():
   # If this is run on non-Linux platforms, just return nothing and indicate
   # success. This allows us to "kind of emulate" a Linux build from other
   # platforms.
-  if "linux" not in sys.platform:
+  if 'linux' not in sys.platform and 'darwin' not in sys.platform:
     print("[[],[],[],[],[]]")
     return 0
 
@@ -135,7 +135,7 @@ def main():
     for regexp in options.strip_out:
       strip_out.append(re.compile(regexp))
 
-  if options.sysroot:
+  if options.sysroot and 'darwin' not in sys.platform:
     libdir = SetConfigPath(options)
     if options.debug:
       sys.stderr.write('PKG_CONFIG_LIBDIR=%s\n' % libdir)
@@ -206,7 +206,7 @@ def main():
 
 
   sysroot = options.sysroot
-  if not sysroot:
+  if not sysroot or 'darwin' in sys.platform:
     sysroot = ''
 
   includes = []
