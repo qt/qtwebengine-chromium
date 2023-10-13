6cd096da8a1c7445efd58a395ffe624b7d2f3301
Peter Varga <pvarga@inf.u-szeged.hu>
2023-10-13 14:40:31 +0200

    [Backport] Add Intel Raptorlake GPU series type
    This also enables supports_two_yuv_hardware_overlays on it.
    
    Bug: 1062334
    Change-Id: Ieba553f29d840768148f3437ce1abcb9969ad243
    Review-URL: https://chromium-review.googlesource.com/c/chromium/src/+/3969628
    Cr-Commit-Position: refs/heads/main@{#1063149}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/511749
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>
    (cherry picked from commit c45073961c27d7788532c7126e0fe3fdec651d60)
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/511910
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/gpu/config/gpu_info.h
+++ chromium/gpu/config/gpu_info.h
@@ -57,8 +57,8 @@ enum class IntelGpuSeriesType {
   kApollolake = 7,
   kSkylake = 8,
   kGeminilake = 9,
-  kAmberlake = 23,
   kKabylake = 10,
+  kAmberlake = 23,
   kCoffeelake = 11,
   kWhiskeylake = 12,
   kCometlake = 13,
@@ -74,8 +74,9 @@ enum class IntelGpuSeriesType {
   kDG1 = 25,
   kAlderlake = 22,
   kAlchemist = 26,
+  kRaptorlake = 27,
   // Please also update |gpu_series_map| in process_json.py.
-  kMaxValue = kAlchemist,
+  kMaxValue = kRaptorlake,
 };
 
 // Video profile.  This *must* match media::VideoCodecProfile.
