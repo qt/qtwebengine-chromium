3c710b74286c6f16d5b4d5833e33fd8fc917e18b
Michal Klocek <michal.klocek@qt.io>
2023-10-25 11:05:33 +0200

    [msvc] Add shorter include paths workaround
    The msvc cl compiler does not support longer than 260 paths.
    Unfortunately the generated union bindings embed class names into
    final header and source filenames and those can together with the
    longer build directory exceed the limitation.
    
    Add the poor man workaround to cover the case and generate
    shorter filenames. Cut four 'tokens' from union files if
    the file names exceeds 120 chars.
    
    Handle included files with SHORT_INCLUDE_FILE macro to keep
    original headers on other platforms, and short include files
    only on msvc. Note preprocessor macros can not handle file
    paths, as these are not valid tokens, therefore implemented
    macro has some rough edges.
    
    The short_includes.h file contains mapping between original headers
    and short ones. Currently it is hard-coded as it only affects few files,
    but it could be also generated during binding generation.
    Moreover 'generated_in_modules.gni' file keeps also hard-coded
    names so there is currently no point to do any auto generated mappings.
    
    Fixes: QTBUG-118410
    Change-Id: I6d0867798b0879da7c86ebf83bc959843d76094b
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/513994
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/525672
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
+++ chromium/third_party/blink/renderer/modules/canvas/canvas2d/canvas_rendering_context_2d.cc
@@ -60,7 +60,8 @@
 #include "third_party/blink/public/mojom/scroll/scroll_into_view_params.mojom-blink.h"
 #include "third_party/blink/public/platform/task_type.h"
 #include "third_party/blink/renderer/bindings/modules/v8/v8_typedefs.h"
-#include "third_party/blink/renderer/bindings/modules/v8/v8_union_canvasrenderingcontext2d_gpucanvascontext_imagebitmaprenderingcontext_webgl2renderingcontext_webglrenderingcontext.h"
+#include "third_party/blink/renderer/modules/shorter_includes.h"
+#include SHORT_INCLUDE_FILE(third_party/blink/renderer/bindings/modules/v8/v8_union,canvasrenderingcontext2d_gpucanvascontext_imagebitmaprenderingcontext_webgl2renderingcontext_webglrenderingcontext)
 #include "third_party/blink/renderer/core/accessibility/ax_object_cache.h"
 #include "third_party/blink/renderer/core/css/css_property_names.h"
 #include "third_party/blink/renderer/core/css/css_property_value_set.h"
