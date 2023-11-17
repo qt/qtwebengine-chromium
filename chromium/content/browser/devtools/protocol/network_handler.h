67322671b8ff2774eca96f04fc1157cb295d702a
Andrey Kosyakov <caseq@chromium.org>
2023-11-17 17:48:22 +0000

    [Backport] CVE-2024-0810: Insufficient policy enforcement in DevTools
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/5039174:
    Do not let chrome.debugger extensions invoke Network.getAllCookies
    
    Network.getAllCookies is deprecated in favor of Storage.getCookies
    and the latter is not allowed for extensions, so we shouldn't let
    extensions use the former either.
    
    Bug: 1496250
    Change-Id: I3e97e9249dbba61d1f7951ed22ef9b1bef9f2355
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5039174
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226203}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/537277
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/content/browser/devtools/protocol/network_handler.h
+++ chromium/content/browser/devtools/protocol/network_handler.h
@@ -72,7 +72,8 @@ class NetworkHandler : public DevToolsDomainHandler,
                  const base::UnguessableToken& devtools_token,
                  DevToolsIOContext* io_context,
                  base::RepeatingClosure update_loader_factories_callback,
-                 bool allow_file_access);
+                 bool allow_file_access,
+                 bool client_is_trusted);
 
   NetworkHandler(const NetworkHandler&) = delete;
   NetworkHandler& operator=(const NetworkHandler&) = delete;
@@ -335,6 +336,8 @@ class NetworkHandler : public DevToolsDomainHandler,
 
   const base::UnguessableToken devtools_token_;
   DevToolsIOContext* const io_context_;
+  const bool allow_file_access_;
+  const bool client_is_trusted_;
 
   std::unique_ptr<Network::Frontend> frontend_;
   BrowserContext* browser_context_;
@@ -356,7 +359,6 @@ class NetworkHandler : public DevToolsDomainHandler,
       loaders_;
   absl::optional<std::set<net::SourceStream::SourceType>>
       accepted_stream_types_;
-  const bool allow_file_access_;
   std::unordered_map<String, std::pair<String, bool>> received_body_data_;
   base::WeakPtrFactory<NetworkHandler> weak_factory_{this};
 };
