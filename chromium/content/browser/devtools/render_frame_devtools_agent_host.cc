c65a61370e3973386062d1db3b20c62f01a49954
Andrey Kosyakov <caseq@chromium.org>
2023-11-17 17:48:22 +0000

    [Backport] CVE-2024-0810: Insufficient policy enforcement in DevTools
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/5039174:
    Do not let chrome.debugger extensions invoke Network.getAllCookies
    
    Network.getAllCookies is deprecated in favor of Storage.getCookies
    and the latter is not allowed for extensions, so we shouldn't let
    extensions use the former either.
    
    Bug: 1496250
    Change-Id: I3e97e9249dbba61d1f7951ed22ef9b1bef9f2355
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5039174
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Andrey Kosyakov <caseq@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1226203}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/535521
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/537149

--- chromium/content/browser/devtools/render_frame_devtools_agent_host.cc
+++ chromium/content/browser/devtools/render_frame_devtools_agent_host.cc
@@ -354,7 +354,8 @@ bool RenderFrameDevToolsAgentHost::AttachSession(DevToolsSession* session,
       base::BindRepeating(
           &RenderFrameDevToolsAgentHost::UpdateResourceLoaderFactories,
           base::Unretained(this)),
-      session->GetClient()->MayReadLocalFiles());
+      session->GetClient()->MayReadLocalFiles(),
+      session->GetClient()->IsTrusted());
   session->CreateAndAddHandler<protocol::FetchHandler>(
       GetIOContext(), base::BindRepeating(
                           [](RenderFrameDevToolsAgentHost* self,
