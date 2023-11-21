e2caa382cd6bfa91b43c11d140469dbdf37f7c3c
Peter Varga <pvarga@inf.u-szeged.hu>
2023-11-21 14:55:59 +0100

    [Backport] Remove std::ranges usage.
    This is banned.
    
    Bug: none
    Change-Id: I592f49aa7f12111160d644d33a0dceef187c8cab
    Review-URL: https://chromium-review.googlesource.com/c/chromium/src/+/4879418
    Cr-Commit-Position: refs/heads/main@{#1199145}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/520225
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/chrome/browser/extensions/api/passwords_private/passwords_private_delegate_impl_unittest.cc
+++ chromium/chrome/browser/extensions/api/passwords_private/passwords_private_delegate_impl_unittest.cc
@@ -13,6 +13,7 @@
 #include "base/memory/ptr_util.h"
 #include "base/memory/raw_ptr.h"
 #include "base/rand_util.h"
+#include "base/ranges/algorithm.h"
 #include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/test/bind.h"
@@ -875,12 +876,9 @@ TEST_F(PasswordsPrivateDelegateImplTest,
   const PasswordsPrivateDelegate::UiEntries& credentials =
       GetCredentials(*delegate);
   EXPECT_EQ(credentials.size(), 2u);
-  const auto account_credential_it =
-      std::ranges::find_if(credentials, [](const auto& credential) {
-        return credential.stored_in ==
-               api::passwords_private::PasswordStoreSet::
-                   PASSWORD_STORE_SET_ACCOUNT;
-      });
+  const auto account_credential_it = base::ranges::find(
+      credentials, api::passwords_private::PASSWORD_STORE_SET_ACCOUNT,
+      &api::passwords_private::PasswordUiEntry::stored_in);
   ASSERT_NE(account_credential_it, credentials.end());
 
   api::passwords_private::PasswordUiEntry updated_credential =
