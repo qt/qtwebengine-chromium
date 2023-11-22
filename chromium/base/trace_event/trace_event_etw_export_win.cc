c6a45cd9e3be11d7e575d0c06092320f8d19096b
Peter Varga <pvarga@inf.u-szeged.hu>
2023-11-22 10:24:54 +0100

    Add conversion of ContiguousContainer iterators to pointer for MSVC
    libstdc++ and libc++ use pointer as an iterator thus iterator
    conversion to pointer is implicit. STL has its own iterators thus they
    need explicit conversion.
    
    Change-Id: I7c2af9381317e9de8da7fd5cc3d6f15803d750e1
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/520431
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/base/trace_event/trace_event_etw_export_win.cc
+++ chromium/base/trace_event/trace_event_etw_export_win.cc
@@ -484,8 +484,9 @@ uint64_t CategoryGroupToETWKeyword(std::string_view category_group_name) {
   // TODO(joel@microsoft.com) Explore better methods in future integration
   // with perfetto.
 
-  StringViewTokenizer category_group_tokens(category_group_name.begin(),
-                                            category_group_name.end(), ",");
+  StringViewTokenizer category_group_tokens(
+      category_group_name.data(),
+      category_group_name.data() + category_group_name.size(), ",");
   while (category_group_tokens.GetNext()) {
     std::string_view category_group_token = category_group_tokens.token_piece();
 
