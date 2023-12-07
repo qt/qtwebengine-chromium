539f0009628b925f12b6fbc174ecb2c36c58b9b0
Martin Negyokru <negyokru@inf.u-szeged.hu>
2023-12-07 15:37:53 +0100

    Fix building with system libxml2
    Libxml2 2.12.0 has changed the const-ness of the
    xmlError pointers, which results in a build error.
    
    Pick-to: 112-based 108-based 87-based
    Change-Id: Id86fcfbfe004f89109808b9bdb11c01133553884
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/523633
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/third_party/blink/renderer/core/xml/xslt_processor.h
+++ chromium/third_party/blink/renderer/core/xml/xslt_processor.h
@@ -77,7 +77,12 @@ class XSLTProcessor final : public ScriptWrappable {
 
   void reset();
 
+#if LIBXML_VERSION >= 21200
   static void ParseErrorFunc(void* user_data, const xmlError*);
+#else
+  static void ParseErrorFunc(void* user_data, xmlError*);
+#endif
+
   static void GenericErrorFunc(void* user_data, const char* msg, ...);
 
   // Only for libXSLT callbacks
