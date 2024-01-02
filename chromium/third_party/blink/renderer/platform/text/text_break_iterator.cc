ba162657be5cfbd233d89fbcf5dbcb2f7a118ac0
Martin Negyokru <negyokru@inf.u-szeged.hu>
2024-01-02 12:36:35 +0100

    FIXUP: Fix compilation with system ICU
    Fix build with ICU-74.
    
    Pick-to: 112-based 108-based 87-based
    Change-Id: I3d3e5b2b889d313a21696157f26944154bea92e4
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/528036
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/third_party/blink/renderer/platform/text/text_break_iterator.cc
+++ chromium/third_party/blink/renderer/platform/text/text_break_iterator.cc
@@ -161,7 +161,9 @@ static const unsigned char kAsciiLineBreakTable[][(kAsciiLineBreakTableLastChar 
 };
 // clang-format on
 
-#if U_ICU_VERSION_MAJOR_NUM >= 58
+#if U_ICU_VERSION_MAJOR_NUM >= 74
+#define BA_LB_COUNT (U_LB_COUNT - 8)
+#elif U_ICU_VERSION_MAJOR_NUM >= 58
 #define BA_LB_COUNT (U_LB_COUNT - 3)
 #else
 #define BA_LB_COUNT U_LB_COUNT
