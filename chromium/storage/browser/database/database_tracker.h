70c4ada4a8f27103cc3d856adb5991ad4d073ed4
Ayu Ishii <ayui@chromium.org>
2024-03-21 20:06:29 +0000

    [Backport] Security bug 323898565
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/5288855:
    WebSQL: Remove session data logic
    
    On Profile destruction, currently there is a race with
    the DatabaseTracker shutdown. DatabaseTracker shutdown
    will post task[1] on the database task runner because
    it needs to do file I/O. However it also has some logic
    to retrieve info for special storage policies from the
    Profile which may already be destroyed by that time.
    
    This change removes logic for special storage policy from
    DatabaseTracker. DatabaseTracker(WebSQL) is removed
    from all platforms as of M119 except for WebView, which does
    not utilize the special storage policy. All policy and
    deprecation support have ended with M123. Therefore cleaning
    up the code to avoid the race.
    
    [1]https://source.chromium.org/chromium/chromium/src/+/main:content/browser/storage_partition_impl.cc;l=1200;drc=be92f4cc2f137460213d52a926c9477275a456c5
    
    Bug: 323898565, 325476286
    Change-Id: Ie2ef898c558308439a8b0d3fdf67f7157440b20a
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5288855
    Commit-Queue: Ayu Ishii <ayui@chromium.org>
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Evan Stade <estade@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1276451}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/561328
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/storage/browser/database/database_tracker.h
+++ chromium/storage/browser/database/database_tracker.h
@@ -41,7 +41,6 @@ namespace storage {
 class DatabaseQuotaClient;
 class QuotaClientCallbackWrapper;
 class QuotaManagerProxy;
-class SpecialStoragePolicy;
 
 COMPONENT_EXPORT(STORAGE_BROWSER)
 extern const base::FilePath::CharType kDatabaseDirectoryName[];
@@ -99,13 +98,11 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) DatabaseTracker
   static scoped_refptr<DatabaseTracker> Create(
       const base::FilePath& profile_path,
       bool is_incognito,
-      scoped_refptr<SpecialStoragePolicy> special_storage_policy,
       scoped_refptr<QuotaManagerProxy> quota_manager_proxy);
 
   // Exposed for base::MakeRefCounted. Users should call Create().
   DatabaseTracker(const base::FilePath& profile_path,
                   bool is_incognito,
-                  scoped_refptr<SpecialStoragePolicy> special_storage_policy,
                   scoped_refptr<QuotaManagerProxy> quota_manager_proxy,
                   base::PassKey<DatabaseTracker>);
 
@@ -166,9 +163,6 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) DatabaseTracker
 
   // Deletes databases touched since `cutoff`.
   //
-  // Does not delete databases belonging to origins designated as protected by
-  // the SpecialStoragePolicy passed to the DatabaseTracker constructor.
-  //
   // `callback` must must be non-null, and is invoked upon completion with a
   // net::Error. The status will be net::OK on success, or net::FAILED if not
   // all databases could be deleted. `callback` may be called before this method
@@ -196,8 +190,6 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) DatabaseTracker
   // Shutdown the database tracker, deleting database files if the tracker is
   // used for an Incognito profile.
   void Shutdown();
-  // Disables the exit-time deletion of session-only data.
-  void SetForceKeepSessionState();
 
  protected:
   // Subclasses need PassKeys to call the constructor.
@@ -245,9 +237,6 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) DatabaseTracker
   // Deletes the directory that stores all DBs in Incognito mode, if it
   // exists.
   void DeleteIncognitoDBDirectory();
-
-  // Deletes session-only databases. Blocks databases from being created/opened.
-  void ClearSessionOnlyOrigins();
 
   bool DeleteClosedDatabase(const std::string& origin_identifier,
                             const std::u16string& database_name);
@@ -301,7 +290,6 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) DatabaseTracker
 
   bool is_initialized_ = false;
   const bool is_incognito_;
-  bool force_keep_session_state_ = false;
   bool shutting_down_ = false;
   const base::FilePath profile_path_;
 
@@ -321,9 +309,6 @@ class COMPONENT_EXPORT(STORAGE_BROWSER) DatabaseTracker
   DatabaseSet dbs_to_be_deleted_;
   std::vector<std::pair<net::CompletionOnceCallback, DatabaseSet>>
       deletion_callbacks_;
-
-  // Apps and Extensions can have special rights.
-  const scoped_refptr<SpecialStoragePolicy> special_storage_policy_;
 
   // Can be accessed from any thread via quota_manager_proxy().
   //
