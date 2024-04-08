301b572f66d5b2556ba272376aea45cc697d85ad
Shahbaz Youssefi <syoussefi@chromium.org>
2024-04-08 10:14:45 -0400

    [Backport] CVE-2024-4058: Type Confusion in ANGLE
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/angle/angle/+/5466390:
    SPIR-V: Fix const constructors with single scalar
    
    These constructors may be generated because of
    RemoveArrayLengthTraverser.
    
    Bug: chromium:332546345
    Change-Id: I2b2bf3728ef5bae148abc2a8518f8f3f42850025
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5462388
    (cherry picked from commit 0b776d32f69a932acb61963d9daad9e13f610944)
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5466390
    Commit-Queue: Zakhar Voit <voit@google.com>
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Geoff Lang <geofflang@chromium.org>
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/562698
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/third_party/angle/src/compiler/translator/Compiler.cpp
+++ chromium/third_party/angle/src/compiler/translator/Compiler.cpp
@@ -974,6 +974,11 @@ bool TCompiler::checkAndSimplifyAST(TIntermBlock *root,
     {
         return false;
     }
+    // Fold the expressions again, because |RemoveArrayLengthMethod| can introduce new constants.
+    if (!FoldExpressions(this, root, &mDiagnostics))
+    {
+        return false;
+    }
 
     if (!RemoveUnreferencedVariables(this, root, &mSymbolTable))
     {
