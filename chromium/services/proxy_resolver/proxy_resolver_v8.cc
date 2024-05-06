05c562fc14acb68c5e1f8f96be88fc541cb35186
David Benjamin <davidben@chromium.org>
2024-05-06 23:50:25 +0000

    [Backport] Security bug 338574384
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/5515743:
    Fix size calculations in V8StringToUTF8
    
    While I'm here, remove the unnecessary use of base::WriteInto, which is
    a remnant of C++03 copy-on-write strings. Also ask V8 not to write a
    NUL terminator because std::(u16)string already owns that byte.
    
    Bug: 338574384
    Change-Id: I5c6eaa99093925db799736f321eab92d35f5acbb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5515743
    Reviewed-by: mmenke <mmenke@chromium.org>
    Commit-Queue: David Benjamin <davidben@chromium.org>
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1297196}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/562704
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/580632

--- chromium/services/proxy_resolver/proxy_resolver_v8.cc
+++ chromium/services/proxy_resolver/proxy_resolver_v8.cc
@@ -14,6 +14,7 @@
 #include "base/debug/leak_annotations.h"
 #include "base/lazy_instance.h"
 #include "base/notreached.h"
+#include "base/numerics/safe_conversions.h"
 #include "base/stl_util.h"
 #include "base/strings/string_tokenizer.h"
 #include "base/strings/string_util.h"
@@ -139,25 +140,22 @@ const size_t kMaxStringBytesForCopy = 256;
 
 // Converts a V8 String to a UTF8 std::string.
 std::string V8StringToUTF8(v8::Isolate* isolate, v8::Local<v8::String> s) {
-  int len = s->Length();
-  std::string result;
-  if (len > 0)
-    s->WriteUtf8(isolate, base::WriteInto(&result, len + 1));
-  return result;
+  int len = s->Utf8Length(isolate);
+  std::string str(base::checked_cast<size_t>(len), '\0');
+  s->WriteUtf8(isolate, &str[0], len, /*nchars_ref=*/nullptr,
+               v8::String::NO_NULL_TERMINATION);
+  return str;
 }
 
 // Converts a V8 String to a UTF16 base::string16.
 base::string16 V8StringToUTF16(v8::Isolate* isolate, v8::Local<v8::String> s) {
   int len = s->Length();
-  base::string16 result;
-  // Note that the reinterpret cast is because on Windows string16 is an alias
-  // to wstring, and hence has character type wchar_t not uint16_t.
-  if (len > 0) {
-    s->Write(isolate,
-             reinterpret_cast<uint16_t*>(base::WriteInto(&result, len + 1)), 0,
-             len);
-  }
-  return result;
+  base::string16 str(base::checked_cast<size_t>(len), '\0');
+  // `char16_t` and `uint16_t` are not the same type, but we build with strict
+  // aliasing off. See https://crbug.com/42209752.
+  s->Write(isolate, reinterpret_cast<uint16_t*>(&str[0]), /*start=*/0, len,
+           v8::String::NO_NULL_TERMINATION);
+  return str;
 }
 
 // Converts an ASCII std::string to a V8 string.
