5f990ce3c233d5243ec8f0cade062d198e34f9a1
Philip Pfaffe <pfaffe@chromium.org>
2024-05-17 13:18:02 +0000

    [Backport] CVE-2024-5836: Inappropriate Implementation in DevTools (2/3)
    Cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5546062:
    Ensure inspectedWindow.reload reloads the correct page
    
    This prevents unintended reloads when racing with navigations.
    
    Drive-by: Check extension allowlist when loading page resources.
    
    Bug: 338248595
    Change-Id: Ibbac5bbd45b1db0d05e32fe8e384740933ee4639
    Reviewed-on: https://chromium-review.googlesource.com/c/devtools/devtools-frontend/+/5546062
    Reviewed-by: Danil Somsikov <dsv@chromium.org>
    Commit-Queue: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/574257
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/third_party/devtools-frontend/src/front_end/core/sdk/ResourceTreeModel.ts
+++ chromium/third_party/devtools-frontend/src/front_end/core/sdk/ResourceTreeModel.ts
@@ -419,6 +419,10 @@ export class ResourceTreeModel extends SDKModel<EventTypes> {
   }
 
   reloadPage(ignoreCache?: boolean, scriptToEvaluateOnLoad?: string): void {
+    const loaderId = this.mainFrame?.loaderId;
+    if (!loaderId) {
+      return;
+    }
     // Only dispatch PageReloadRequested upon first reload request to simplify client logic.
     if (!this.#pendingReloadOptions) {
       this.dispatchEventToListeners(Events.PageReloadRequested, this);
@@ -433,7 +437,7 @@ export class ResourceTreeModel extends SDKModel<EventTypes> {
       networkManager.clearRequests();
     }
     this.dispatchEventToListeners(Events.WillReloadPage);
-    void this.agent.invoke_reload({ignoreCache, scriptToEvaluateOnLoad});
+    void this.agent.invoke_reload({ignoreCache, scriptToEvaluateOnLoad, loaderId});
   }
 
   navigate(url: Platform.DevToolsPath.UrlString): Promise<Protocol.Page.NavigateResponse> {
@@ -661,7 +665,7 @@ export class ResourceTreeFrame {
   #sameTargetParentFrameInternal: ResourceTreeFrame|null;
   readonly #idInternal: Protocol.Page.FrameId;
   crossTargetParentFrameId: string|null;
-  #loaderIdInternal: string;
+  #loaderIdInternal: Protocol.Network.LoaderId;
   #nameInternal: string|null|undefined;
   #urlInternal: Platform.DevToolsPath.UrlString;
   #domainAndRegistryInternal: string;
@@ -694,7 +698,7 @@ export class ResourceTreeFrame {
     this.#idInternal = frameId;
     this.crossTargetParentFrameId = null;
 
-    this.#loaderIdInternal = payload?.loaderId || '';
+    this.#loaderIdInternal = payload?.loaderId ?? '' as Protocol.Network.LoaderId;
     this.#nameInternal = payload && payload.name;
     this.#urlInternal =
         payload && payload.url as Platform.DevToolsPath.UrlString || Platform.DevToolsPath.EmptyUrlString;
@@ -814,7 +818,7 @@ export class ResourceTreeFrame {
     return this.#unreachableUrlInternal;
   }
 
-  get loaderId(): string {
+  get loaderId(): Protocol.Network.LoaderId {
     return this.#loaderIdInternal;
   }
 
