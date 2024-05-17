9c68e689b72482cd5564c88b1d60e7544fcbfd99
Philip Pfaffe <pfaffe@chromium.org>
2024-05-17 10:12:02 +0000

    [Backport] CVE-2024-5836: Inappropriate Implementation in DevTools (1/3)
    Cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/5542082:
    Add loaderId argument to Page.reload
    
    By passing the loaderId, clients can prevent accidentally reloading
    unintended targets when Page.reload is racing with a navigiation.
    
    Bug: 338248595
    Change-Id: I68883658a2112bba2a4cb428b4c3c33314c5e894
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5542082
    Commit-Queue: Alex Rudenko <alexrudenko@chromium.org>
    Auto-Submit: Philip Pfaffe <pfaffe@chromium.org>
    Reviewed-by: Alex Rudenko <alexrudenko@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1302488}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/574256
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/content/browser/devtools/protocol/page_handler.cc
+++ chromium/content/browser/devtools/protocol/page_handler.cc
@@ -415,6 +415,7 @@ Response PageHandler::Close() {
 
 void PageHandler::Reload(Maybe<bool> bypassCache,
                          Maybe<std::string> script_to_evaluate_on_load,
+                         Maybe<std::string> loader_id,
                          std::unique_ptr<ReloadCallback> callback) {
   Response response = AssureTopLevelActiveFrame();
   if (response.IsError()) {
@@ -427,6 +428,16 @@ void PageHandler::Reload(Maybe<bool> bypassCache,
   // itself will fail.
   RenderFrameHostImpl* outermost_main_frame =
       host_->GetOutermostMainFrameOrEmbedder();
+
+  if (loader_id.has_value()) {
+    auto navigation_token = outermost_main_frame->GetDevToolsNavigationToken();
+    if (!navigation_token.has_value() ||
+        *loader_id != navigation_token->ToString()) {
+      callback->sendFailure(Response::InvalidParams(
+          "Reload was discarded because the page already navigated"));
+      return;
+    }
+  }
 
   // It is important to fallback before triggering reload, so that
   // renderer could prepare beforehand.
