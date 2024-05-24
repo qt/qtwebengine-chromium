1e0e25f451e029b03d3785b0d85000a32eca1f09
Tommy Steimel <steimel@chromium.org>
2024-05-24 13:57:53 +0000

    [Backport] CVE-2024-5496: Use after free in Media Session
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/5564794:
    MediaSession: Use a MediaSessionImpl WeakPtr in MediaSessionServiceImpl
    
    Currently, every time MediaSessionServiceImpl wants to talk to its
    associated MediaSessionImpl, it recalculates it from its
    RenderFrameHostId. This can lead to issues where a
    MediaSessionServiceImpl of a disconnected RenderFrameHost can no longer
    access the MediaSessionImpl to tell it that it is being deleted,
    leaving MediaSessionImpl with a dangling raw_ptr.
    
    Bug: 338929744
    Change-Id: I8f404c1a39510a24643c1f973a32bf6c0bbde123
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5564794
    Reviewed-by: Frank Liberato <liberato@chromium.org>
    Commit-Queue: Tommy Steimel <steimel@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1305678}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/567754
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/content/browser/media/session/media_session_impl.h
+++ chromium/content/browser/media/session/media_session_impl.h
@@ -17,6 +17,7 @@
 #include "base/containers/id_map.h"
 #include "base/memory/raw_ptr.h"
 #include "base/memory/raw_ptr_exclusion.h"
+#include "base/memory/weak_ptr.h"
 #include "base/timer/timer.h"
 #include "build/build_config.h"
 #include "content/browser/media/session/audio_focus_delegate.h"
@@ -346,6 +347,9 @@ class MediaSessionImpl : public MediaSession,
   // Returns the Audio Focus request ID associated with this media session.
   const base::UnguessableToken& GetRequestId() const;
 
+  // Returns a WeakPtr to `this`.
+  base::WeakPtr<MediaSessionImpl> GetWeakPtr();
+
   CONTENT_EXPORT bool HasImageCacheForTest(const GURL& image_url) const;
 
   // Make sure that all observers have received any pending callbacks from us,
@@ -640,6 +644,8 @@ class MediaSessionImpl : public MediaSession,
   std::optional<PlayerIdentifier> guarding_player_id_;
 
   media_session::mojom::RemotePlaybackMetadataPtr remote_playback_metadata_;
+
+  base::WeakPtrFactory<MediaSessionImpl> weak_factory_{this};
 
   WEB_CONTENTS_USER_DATA_KEY_DECL();
 };
