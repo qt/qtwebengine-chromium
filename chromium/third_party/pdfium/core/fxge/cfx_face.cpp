490efdc46dc4a31f9c6a2a24a8f2e7e7170d9fe2
Michal Klocek <michal.klocek@qt.io>
2024-05-30 12:59:01 +0200

    FIXUP: Fix build with older FreeType
    Change-Id: I5aeeae8245dc5648a5d95a04706882bb5f22d51b
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/564588
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>

--- chromium/third_party/pdfium/core/fxge/cfx_face.cpp
+++ chromium/third_party/pdfium/core/fxge/cfx_face.cpp
@@ -649,7 +649,8 @@ int CFX_Face::GetCharIndex(uint32_t code) {
 }
 
 int CFX_Face::GetNameIndex(const char* name) {
-  return FT_Get_Name_Index(GetRec(), name);
+  //TODO remove cast when free type 2.9 support droppped
+  return FT_Get_Name_Index(GetRec(), const_cast<char*>(name));
 }
 
 std::vector<CFX_Face::CharCodeAndIndex> CFX_Face::GetCharCodesAndIndices(
