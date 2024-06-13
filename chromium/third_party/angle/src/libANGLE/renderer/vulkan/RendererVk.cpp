f83463ac5bd093e91b85d10e094028eb5d57bdbb
Shahbaz Youssefi <syoussefi@chromium.org>
2024-06-13 16:54:03 -0400

    [Backport] CVE-2024-6291: Use after free in Swiftshader
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/angle/angle/+/5630161:
    Vulkan: Disable VK_EXT_shader_stencil_export on SwiftShader
    
    Bug: chromium:40942995
    Change-Id: I4c469108c420d3e68008a30f627989655a64c27c
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/5630161
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-by: Amirali Abdolrashidi <abdolrashidi@google.com>
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/580426
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/third_party/angle/src/libANGLE/renderer/vulkan/RendererVk.cpp
+++ chromium/third_party/angle/src/libANGLE/renderer/vulkan/RendererVk.cpp
@@ -1819,9 +1819,11 @@ void RendererVk::initFeatures(DisplayVk *displayVk, const ExtensionNameList &dev
     }
 #endif  // defined(ANGLE_PLATFORM_ANDROID)
 
+    // Disabled on SwiftShader due to http://crbug.com/40942995
     ANGLE_FEATURE_CONDITION(
         &mFeatures, supportsShaderStencilExport,
-        ExtensionFound(VK_EXT_SHADER_STENCIL_EXPORT_EXTENSION_NAME, deviceExtensionNames));
+        ExtensionFound(VK_EXT_SHADER_STENCIL_EXPORT_EXTENSION_NAME, deviceExtensionNames) &&
+            !isSwiftShader);
 
     ANGLE_FEATURE_CONDITION(&mFeatures, supportsTransformFeedbackExtension,
                             mTransformFeedbackFeatures.transformFeedback == VK_TRUE);
