de88aea7dee7ca609d92c0faf950f30bdf4b8b15
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2024-08-14 16:21:02 +0200

    FIXUP: "Shut GL down on process exit to silence Direct3D11 warnings"
    Speculative fix for CI failures on Ubuntu. The GL shutdown only
    matters on Windows (so far), so it's now placed inside #if guards.
    
    Change-Id: Iec8c8e5495026162421f209aa1fcd092ff7b9634
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/583052
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>
    (cherry picked from commit 9eb5f1cc1f3810f918cbea59c74e6da82c7b9610)
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/583077
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- chromium/gpu/ipc/service/gpu_init.cc
+++ chromium/gpu/ipc/service/gpu_init.cc
@@ -296,6 +296,7 @@ void SetupGLDisplayManagerEGL(const GPUInfo& gpu_info,
 GpuInit::GpuInit() = default;
 
 GpuInit::~GpuInit() {
+#if BUILDFLAG(IS_WIN)
   if (gl::GetGLImplementation() != gl::kGLImplementationDisabled) {
     auto *gl_display = gl::init::GetOrInitializeGLOneOffPlatformImplementation(
         false, false, false, gl::GpuPreference::kDefault);
@@ -303,6 +304,7 @@ GpuInit::~GpuInit() {
       gl::init::ShutdownGL(gl_display, false);
     }
   }
+#endif // BUILDFLAG(IS_WIN)
 
   StopForceDiscreteGPU();
 }
