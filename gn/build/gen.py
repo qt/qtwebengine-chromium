7b80a064240a1bf4a1632aedef347ebd8f1f8f19
Allan Sandfeld Jensen <allan.jensen@qt.io>
2024-09-03 15:19:44 +0200

    [gn][win-arm64] Support win-arm64 in gn
    Do not pick for 126-based, update GN instead
    
    Change-Id: I1278ce78d855f40a6ff3226c90a3281f72209b6c
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/588069
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- gn/build/gen.py
+++ gn/build/gen.py
@@ -656,6 +656,8 @@ def WriteGNNinja(path, platform, host, options, args_list):
     target_arch = windows_target_build_arch()
     if target_arch == 'x64':
         ldflags.extend(['/MACHINE:x64'])
+    elif target_arch == 'arm64':
+        ldflags.extend(['/MACHINE:arm64'])
     else:
         ldflags.extend(['/MACHINE:x86'])
 
@@ -1024,7 +1026,7 @@ def WriteGNNinja(path, platform, host, options, args_list):
 
 def windows_target_build_arch():
     target_arch = os.environ.get('Platform')
-    if target_arch in ['x64', 'x86']: return target_arch
+    if target_arch in ['x64', 'x86', 'arm64']: return target_arch
 
     if platform.machine().lower() in ['x86_64', 'amd64']: return 'x64'
     return 'x86'
