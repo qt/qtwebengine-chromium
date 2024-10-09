47016eee80e03363e30a4ebad980d7c632e70b09
Justin Lulejian <jlulejian@chromium.org>
2024-10-09 00:57:41 +0000

    [Backport] CVE-2024-10229: Inappropriate implementation in Extensions
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/5917013:
    [Extensions][ServiceWorker] Skip worker for isolated world module fetch
    
    Before this change, an isolated world (e.g. extension content script,
    but also others) could dynamically import a script from an accessible
    resource (for extensions this is possible with web accessible
    resources and a matching site). When this occurs a web service worker
    could intercept that request and respond with arbitrary content.
    
    After this change, isolated world module requests skip triggering the
    worker fetch handler. This includes extension content scripts, but also
    includes any other scripts that execute in the isolated world context.
    
    Bug: 371011220
    Change-Id: I37eda47324b6933a93d2a44792a06ff91399981f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/5917013
    Auto-Submit: Justin Lulejian <jlulejian@chromium.org>
    Reviewed-by: Hiroshige Hayashizaki <hiroshige@chromium.org>
    Commit-Queue: Justin Lulejian <jlulejian@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1365918}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/599397
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/third_party/blink/renderer/core/loader/modulescript/module_script_loader.cc
+++ chromium/third_party/blink/renderer/core/loader/modulescript/module_script_loader.cc
@@ -152,12 +152,19 @@ void ModuleScriptLoader::FetchInternal(
   url_ = module_request.Url();
 #endif
 
+  DOMWrapperWorld& request_world = modulator_->GetScriptState()->World();
+  // Prevents web service workers from intercepting isolated world dynamic
+  // script imports requests and responding with different contents.
+  // TODO(crbug.com/1296102): Link to documentation that describes the criteria
+  // where module imports are handled by service worker fetch handler.
+  resource_request.SetSkipServiceWorker(request_world.IsIsolatedWorld());
+
   // <spec step="9">Set request 's destination to the result of running the
   // fetch destination from module type steps given destination and
   // moduleType.</spec>
   SetFetchDestinationFromModuleType(resource_request, module_request);
 
-  ResourceLoaderOptions options(&modulator_->GetScriptState()->World());
+  ResourceLoaderOptions options(&request_world);
 
   // <spec step="12">Set up the module script request given request and
   // options.</spec>
