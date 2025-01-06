c5287f4d6a0e64d357e7405599ecb6c7d2d4e5fd
Yoshisato Yanagisawa <yyanagisawa@chromium.org>
2025-01-06 05:34:49 -0800

    [Backport] Security bug 379869752
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/6148168:
    Make AcceptLanguagesWatcher a weak persistent object
    
    DedicatedWorkerOrSharedWorkerFetchContext keeps on having a pointer
    to the AcceptLanguagesWatcher as a raw_ptr.  Even if the implementing
    object gets garbage-collected, the pointer is kept.  Therefore,
    DedicatedWorkerOrSharedWorkerFetchContext may use it after the GC.
    
    This CL revised the raw_ptr to AcceptLanguagesWatcher to use
    WeakPersistent so that the users of the object won't access it
    after it gets GCed.
    
    (cherry picked from commit 9e7b77da977401361ef3bc9ad0f43e7563f49657)
    
    Bug: 379869752
    Change-Id: Ic3997fcf17aa93f45507241e0fc18a486efc1932
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6038980
    Reviewed-by: Michael Lippautz <mlippautz@chromium.org>
    Auto-Submit: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Commit-Queue: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Original-Commit-Position: refs/heads/main@{#1390151}
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6148168
    Reviewed-by: Erhu Akpobaro <eakpobaro@google.com>
    Commit-Queue: Daniel Yip <danielyip@google.com>
    Auto-Submit: Daniel Yip <danielyip@google.com>
    Owners-Override: Daniel Yip <danielyip@google.com>
    Cr-Commit-Position: refs/branch-heads/6723@{#2761}
    Cr-Branched-From: 985f2961df230630f9cbd75bd6fe463009855a11-refs/heads/main@{#1356013}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/615719
    Reviewed-by: Anu Aliyas <anu.aliyas@qt.io>

--- chromium/third_party/blink/renderer/modules/service_worker/web_service_worker_fetch_context_impl.h
+++ chromium/third_party/blink/renderer/modules/service_worker/web_service_worker_fetch_context_impl.h
@@ -6,6 +6,7 @@
 #define THIRD_PARTY_BLINK_RENDERER_MODULES_SERVICE_WORKER_WEB_SERVICE_WORKER_FETCH_CONTEXT_IMPL_H_
 
 #include "base/memory/raw_ptr.h"
+#include "base/task/single_thread_task_runner.h"
 #include "mojo/public/cpp/bindings/pending_receiver.h"
 #include "mojo/public/cpp/bindings/receiver.h"
 #include "third_party/blink/public/common/renderer_preferences/renderer_preferences.h"
@@ -13,9 +14,9 @@
 #include "third_party/blink/public/mojom/service_worker/service_worker.mojom-forward.h"
 #include "third_party/blink/public/mojom/worker/subresource_loader_updater.mojom-blink.h"
 
-#include "base/task/single_thread_task_runner.h"
 #include "third_party/blink/public/platform/modules/service_worker/web_service_worker_fetch_context.h"
 #include "third_party/blink/public/platform/web_common.h"
+#include "third_party/blink/renderer/platform/heap/persistent.h"
 #include "third_party/blink/renderer/platform/weborigin/kurl.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 #include "third_party/blink/renderer/platform/wtf/vector.h"
@@ -136,8 +137,7 @@ class BLINK_EXPORT WebServiceWorkerFetchContextImpl final
   raw_ptr<base::WaitableEvent, ExperimentalRenderer>
       terminate_sync_load_event_ = nullptr;
 
-  raw_ptr<AcceptLanguagesWatcher, ExperimentalRenderer>
-      accept_languages_watcher_ = nullptr;
+  WeakPersistent<AcceptLanguagesWatcher> accept_languages_watcher_;
 
   Vector<String> cors_exempt_header_list_;
   bool is_offline_mode_ = false;
