f4d9070b93983d6e7fb4e51597478bdf488fdb24
Thibaud Michaud <thibaudm@chromium.org>
2025-02-25 15:02:30 +0100

    [Backport] Security bug 396460489 (2/2)
    Cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/v8/v8/+/6285400:
    Change scratch register from xmm0 to xmm7
    
    On ia32, xmm0 was not an allocatable register but was needed as a fixed
    register input for the pblendvb instruction when AVX is disabled.
    
    Supporting this is generally difficult. This caused issues in the
    register hinting mechanism which was fixed already, but I believe
    that this could also cause problems in the gap resolver for instance,
    because xmm0 is used as a scratch register and is therefore
    assumed not to alias with the move operands.
    
    In fact we already had a DCHECK to verify that fixed GP registers are
    allocatable, but this check was missing for FP registers.
    
    Instead of trying to support this case, change the scratch double
    register from xmm0 to xmm7 on ia32 to free xmm0, add the missing
    DCHECK, revert the register hint workaround, and explicitly use
    the list of allocatable registers in one place where it was hardcoded.
    
    R=dlehmann@chromium.org
    
    Bug: 396460489
    Change-Id: I78582383a4608408523a8efad79510487b981c27
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/6285400
    Commit-Queue: Thibaud Michaud <thibaudm@chromium.org>
    Reviewed-by: Jakob Kummerow <jkummerow@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#98927}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/637948
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/v8/src/compiler/backend/ia32/code-generator-ia32.cc
+++ chromium/v8/src/compiler/backend/ia32/code-generator-ia32.cc
@@ -30,8 +30,6 @@ namespace compiler {
 
 #define __ masm()->
 
-#define kScratchDoubleReg xmm0
-
 // Adds IA-32 specific methods for decoding operands.
 class IA32OperandConverter : public InstructionOperandConverter {
  public:
