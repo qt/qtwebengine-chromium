770e741db98d64f83e2ddb5c6a7c4ae8a005d543
Allan Sandfeld Jensen <allan.jensen@qt.io>
2025-03-08 11:49:39 +0100

    [backport] CVE-2025-24201 1/2
    Move WebGL primitive restart state setting to the GPU process
    
    ANGLE will validate and initialize this state and errors are generated
    when the WebGL client also initializes it on startup.
    
    Initialize it even in the passthrough command decoder temporarily so
    that ANGLE can roll without breaking WebGL tests.
    
    (cherry picked from commit 9dacf5694dfdb735c335805783840584a50bface)
    
    Bug: 401059730
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6330188
    Change-Id: I2133ad770c0e42d7925922811e395f95f8dfe52d
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/630855
    Reviewed-by: Anu Aliyas <anu.aliyas@qt.io>
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/639679

--- chromium/third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
+++ chromium/third_party/blink/renderer/modules/webgl/webgl_rendering_context_base.cc
@@ -1346,12 +1346,6 @@ void WebGLRenderingContextBase::InitializeNewContext() {
                             ->GetCapabilities()
                             .mesa_framebuffer_flip_y;
 
-  // If WebGL 2, the PRIMITIVE_RESTART_FIXED_INDEX should be always enabled.
-  // See the section <Primitive Restart is Always Enabled> in WebGL 2 spec:
-  // https://www.khronos.org/registry/webgl/specs/latest/2.0/#4.1.4
-  if (IsWebGL2OrHigher())
-    ContextGL()->Enable(GL_PRIMITIVE_RESTART_FIXED_INDEX);
-
   // This ensures that the context has a valid "lastFlushID" and won't be
   // mistakenly identified as the "least recently used" context.
   ContextGL()->Flush();
