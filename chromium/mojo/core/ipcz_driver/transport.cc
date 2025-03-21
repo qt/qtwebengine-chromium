0130d07aebeac2c1ffb86598a76341b8abf33d36
Alex Gough <ajgo@chromium.org>
2025-03-21 10:32:49 -0700

    [Backport] CVE-2025-2783: Incorrect handle provided in unspecified circumstances in Mojo on Windows
    Cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/6380193:
    Avoid receiving or sending sentinel handle values
    
    These values can be misinterpreted by OS functions, so
    avoid sending or receiving them over IPCZ.
    
    Bug: 405143032
    Change-Id: Ib578fb4727e78e2697c60c42005daa97e08695e9
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6380193
    Reviewed-by: Will Harris <wfh@chromium.org>
    Commit-Queue: Alex Gough <ajgo@chromium.org>
    Reviewed-by: Daniel Cheng <dcheng@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1436135}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/636300
    Reviewed-by: Anu Aliyas <anu.aliyas@qt.io>

--- chromium/mojo/core/ipcz_driver/transport.cc
+++ chromium/mojo/core/ipcz_driver/transport.cc
@@ -29,6 +29,7 @@
 #include "third_party/ipcz/include/ipcz/ipcz.h"
 
 #if BUILDFLAG(IS_WIN)
+#include "base/win/win_util.h"
 #include "mojo/public/cpp/platform/platform_handle_security_util_win.h"
 #endif
 
@@ -130,10 +131,12 @@ bool EncodeHandle(PlatformHandle& handle,
                   HandleOwner handle_owner,
                   HandleData& out_handle_data,
                   bool is_remote_process_untrusted) {
+  CHECK(handle.is_valid());
   // Duplicating INVALID_HANDLE_VALUE passes a process handle. If you intend to
   // do this, you must open a valid process handle, not pass the result of
-  // GetCurrentProcess(). e.g. https://crbug.com/243339.
-  CHECK(handle.is_valid());
+  // GetCurrentProcess() or GetCurrentThread(). e.g. https://crbug.com/243339.
+  CHECK(!handle.is_pseudo_handle());
+
   if (handle_owner == HandleOwner::kSender) {
     // Nothing to do when sending handles that belong to us. The recipient must
     // be sufficiently privileged and equipped to duplicate such handles to
@@ -173,6 +176,10 @@ PlatformHandle DecodeHandle(HandleData data,
                             HandleOwner handle_owner,
                             Transport& from_transport) {
   const HANDLE handle = DataToHandle(data);
+  // Do not decode sentinel values used by Windows (INVALID_HANDLE_VALUE &
+  // GetCurrentThread()).
+  CHECK(!base::win::IsPseudoHandle(handle));
+
   if (handle_owner == HandleOwner::kRecipient) {
     if (from_transport.destination_type() != Transport::kBroker &&
         !from_transport.is_peer_trusted() && !remote_process.is_current()) {
