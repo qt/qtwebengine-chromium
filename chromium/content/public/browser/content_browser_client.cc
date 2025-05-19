a3d9eeea1d1cd15eb1dcb00ef3bec3e341406020
rbpotter <rbpotter@chromium.org>
2025-05-19 14:50:25 -0700

    [backport] Internal debug UIs: Change implementation of pages behind pref, non-iOS
    Overriding CreateWebUIController() results in a mismatch in bindings
    when the user toggles the pref in chrome://chrome-urls and then uses
    forward/back navigation to return to a debug UI. This is because
    forward/back navigation requires state restoration for debug WebUI
    pages. The mismatch in bindings causes a crash.
    
    This CL instead has ChromeContentBrowserClient rewrite any debug UI
    URLs (still tracked via InternalWebUIConfig) to
    chrome://debug-webuis-disabled?host=hostname, where hostname is the host
    of the debug WebUI. The InternalDebugPagesDisabledUI is served from
    chrome://debug-webuis-disabled instead of being served from the
    debug UI URLs directly.
    
    A script is added to the internal debug pages disabled UI that
    redirects to the debug page if the disabled UI is served when the
    InternalDebugPagesEnabled pref is set to true. This is so that
    navigating back after enabling debug UIs in chrome://chrome-urls
    will appear to work correctly. Navigating back to a previously
    enabled UI after disabling the UIs in chrome://chrome-urls will still
    serve the original UI; a solution to this issue is TBD, but it is
    less likely to impact users.
    
    Note that iOS did not use the same implementation of this pref since
    WebUIConfig is not used on iOS. The iOS implementation is unaffected
    by this change.
    
    Bug: 404788318
    Change-Id: I6899e29687ec6426ccc3babb7dbdac40b4b9da22
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/660502
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/content/public/browser/content_browser_client.cc
+++ chromium/content/public/browser/content_browser_client.cc
@@ -1919,12 +1919,6 @@ bool ContentBrowserClient::ShouldDispatchPagehideDuringCommit(
   return true;
 }
 
-std::unique_ptr<WebUIController> ContentBrowserClient::OverrideForInternalWebUI(
-    WebUI* web_ui,
-    const GURL& url) {
-  return nullptr;
-}
-
 std::optional<network::CrossOriginEmbedderPolicy>
 ContentBrowserClient::MaybeOverrideLocalURLCrossOriginEmbedderPolicy(
     content::NavigationHandle* navigation_handle) {
