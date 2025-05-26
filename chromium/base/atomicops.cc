d37f50d612296b9cb57cb9fe699921d32b345234
Allan Sandfeld Jensen <allan.jensen@qt.io>
2025-05-26 17:25:49 +0200

    [macos] Sanitize RelaxedAtomicWriteMemcpy
    At least make it behave partially sane.
    
    And add a non implementation fallback for Apple clang
    
    Change-Id: I408b96e21a4791f051db67063761a55155e8d064
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/650986
    Reviewed-by: Peter Varga <pvarga@inf.u-szeged.hu>
    Reviewed-by: Martin Negyokru <negyokru@inf.u-szeged.hu>

--- chromium/base/atomicops.cc
+++ chromium/base/atomicops.cc
@@ -10,24 +10,52 @@
 
 namespace base::subtle {
 
+#if __cpp_lib_atomic_ref >= 201806L
+template<typename T>
+static bool RelaxedAtomicSubWrite(uint8_t*& dst_byte_ptr, const uint8_t*& src_byte_ptr, size_t &bytes)
+{
+  if (std::atomic_ref<T>::is_always_lock_free) {
+    if (bytes >= sizeof(T) && IsAligned(dst_byte_ptr, std::atomic_ref<T>::required_alignment)) {
+      std::atomic_ref<T>(*reinterpret_cast<T*>(dst_byte_ptr))
+          .store(*reinterpret_cast<const T*>(src_byte_ptr), std::memory_order_relaxed);
+      UNSAFE_BUFFERS(dst_byte_ptr+=sizeof(T));
+      UNSAFE_BUFFERS(src_byte_ptr+=sizeof(T));
+      bytes-=sizeof(T);
+      return true;
+    }
+  }
+  return false;
+}
+
+// Non-quite atomic memcpy, that at least writes as large chunks as we can atomically, so a non-atomic reader is almost guarenteed atomic field reads.
 void RelaxedAtomicWriteMemcpy(base::span<uint8_t> dst,
                               base::span<const uint8_t> src) {
   CHECK_EQ(dst.size(), src.size());
   size_t bytes = dst.size();
   uint8_t* dst_byte_ptr = dst.data();
   const uint8_t* src_byte_ptr = src.data();
-  // Make sure that we can at least copy byte by byte with atomics.
-  static_assert(std::atomic_ref<uint8_t>::required_alignment == 1);
-
-  // Alignment for uintmax_t atomics that we use in the happy case.
+  // Alignment for uintptr_t atomics that we use in the ideal case.
   constexpr size_t kDesiredAlignment =
-      std::atomic_ref<uintmax_t>::required_alignment;
+      std::atomic_ref<uintptr_t>::required_alignment;
+  // Assert this function even does anything useful
+  static_assert(std::atomic_ref<uintptr_t>::is_always_lock_free);
 
-  // Copy byte-by-byte until `dst_byte_ptr` is not properly aligned for
-  // the happy case.
+  // Copy up until `dst_byte_ptr` is properly aligned for the ideal case.
   while (bytes > 0 && !IsAligned(dst_byte_ptr, kDesiredAlignment)) {
-    std::atomic_ref<uint8_t>(*dst_byte_ptr)
-        .store(*src_byte_ptr, std::memory_order_relaxed);
+    if constexpr(!std::same_as<uintptr_t, uint32_t>) {
+      if (RelaxedAtomicSubWrite<uint32_t>(dst_byte_ptr, src_byte_ptr, bytes))
+        continue;
+    }
+    if (RelaxedAtomicSubWrite<uint16_t>(dst_byte_ptr, src_byte_ptr, bytes))
+        continue;
+
+    if constexpr(std::atomic_ref<uint8_t>::is_always_lock_free) {
+      std::atomic_ref<uint8_t>(*dst_byte_ptr)
+          .store(*src_byte_ptr, std::memory_order_relaxed);
+    } else {
+      // No atomic byte writes without locks, and since the readers do not use atomic and thus no locks; this is the best we can do.
+      *dst_byte_ptr = *src_byte_ptr;
+    }
     // SAFETY: We check above that `dst_byte_ptr` and `src_byte_ptr` point
     // to spans of sufficient size.
     UNSAFE_BUFFERS(++dst_byte_ptr);
@@ -35,25 +63,37 @@ void RelaxedAtomicWriteMemcpy(base::span<uint8_t> dst,
     --bytes;
   }
 
-  // Happy case where both `src_byte_ptr` and `dst_byte_ptr` are both properly
+  // Ideal case where both `src_byte_ptr` and `dst_byte_ptr` are both properly
   // aligned and the largest possible atomic is used for copying.
   if (IsAligned(src_byte_ptr, kDesiredAlignment)) {
-    while (bytes >= sizeof(uintmax_t)) {
-      std::atomic_ref<uintmax_t>(*reinterpret_cast<uintmax_t*>(dst_byte_ptr))
-          .store(*reinterpret_cast<const uintmax_t*>(src_byte_ptr),
-                 std::memory_order_relaxed);
+    while (bytes >= sizeof(uintptr_t)) {
+      std::atomic_ref<uintptr_t>(*reinterpret_cast<uintptr_t*>(dst_byte_ptr))
+          .store(*reinterpret_cast<const uintptr_t*>(src_byte_ptr),
+                std::memory_order_relaxed);
       // SAFETY: We check above that `dst_byte_ptr` and `src_byte_ptr` point
       // to spans of sufficient size.
-      UNSAFE_BUFFERS(dst_byte_ptr += sizeof(uintmax_t));
-      UNSAFE_BUFFERS(src_byte_ptr += sizeof(uintmax_t));
-      bytes -= sizeof(uintmax_t);
+      UNSAFE_BUFFERS(dst_byte_ptr += sizeof(uintptr_t));
+      UNSAFE_BUFFERS(src_byte_ptr += sizeof(uintptr_t));
+      bytes -= sizeof(uintptr_t);
     }
   }
 
   // Copy what's left after the happy-case byte-by-byte.
   while (bytes > 0) {
-    std::atomic_ref<uint8_t>(*dst_byte_ptr)
-        .store(*src_byte_ptr, std::memory_order_relaxed);
+    if constexpr(!std::same_as<uintptr_t, uint32_t>) {
+      if (RelaxedAtomicSubWrite<uint32_t>(dst_byte_ptr, src_byte_ptr, bytes))
+        continue;
+    }
+    if (RelaxedAtomicSubWrite<uint16_t>(dst_byte_ptr, src_byte_ptr, bytes))
+        continue;
+
+    if constexpr(!std::atomic_ref<uint8_t>::is_always_lock_free) {
+      // No atomic byte writes without locks, and since the readers do not use atomic and thus no locks; this is the best we can do.
+      *dst_byte_ptr = *src_byte_ptr;
+    } else {
+      std::atomic_ref<uint8_t>(*dst_byte_ptr)
+          .store(*src_byte_ptr, std::memory_order_relaxed);
+    }
     // SAFETY: We check above that `dst_byte_ptr` and `src_byte_ptr` point
     // to spans of sufficient size.
     UNSAFE_BUFFERS(++dst_byte_ptr);
@@ -61,5 +101,16 @@ void RelaxedAtomicWriteMemcpy(base::span<uint8_t> dst,
     --bytes;
   }
 }
+#else
+void RelaxedAtomicWriteMemcpy(base::span<uint8_t> dst,
+                              base::span<const uint8_t> src) {
+  CHECK_EQ(dst.size(), src.size());
+  size_t bytes = dst.size();
+  uint8_t* dst_byte_ptr = dst.data();
+  const uint8_t* src_byte_ptr = src.data();
+  // Type-punning atomic types is also UB, so stay with the old simple UB.
+  ::memcpy(dst_byte_ptr, src_byte_ptr, bytes);
+}
+#endif
 
 }  // namespace base::subtle
