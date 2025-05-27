175615a1f28ce312e43dee1eea6859937bdcc223
Michael Brüning <michael.bruning@qt.io>
2025-05-27 15:15:23 +0200

    [Backport] [CVE-2024-50602] Stop XML_ResumeParser from crashing #915 (2/2)
    Manual cherry-pick of patch originally reviewed on
    https://github.com/libexpat/libexpat/pull/915/commits/5fb89e7b3afa1c314b34834fe729cd063f65a4d4:
    lib: Be explicit about XML_PARSING in XML_StopParser
    
    Patch by Sebastin Pipping (https://github.com/hartwork)
    
    Change-Id: I8751fec1c92e9c7ec39b0c601f56777c583fce1e
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/648517
    Reviewed-by: Anu Aliyas <anu.aliyas@qt.io>

--- chromium/third_party/expat/src/expat/lib/xmlparse.c
+++ chromium/third_party/expat/src/expat/lib/xmlparse.c
@@ -2191,7 +2191,7 @@ XML_StopParser(XML_Parser parser, XML_Bool resumable) {
   case XML_FINISHED:
     parser->m_errorCode = XML_ERROR_FINISHED;
     return XML_STATUS_ERROR;
-  default:
+  case XML_PARSING:
     if (resumable) {
 #ifdef XML_DTD
       if (parser->m_isParamEntity) {
@@ -2202,6 +2202,9 @@ XML_StopParser(XML_Parser parser, XML_Bool resumable) {
       parser->m_parsingStatus.parsing = XML_SUSPENDED;
     } else
       parser->m_parsingStatus.parsing = XML_FINISHED;
+    break;
+  default:
+    assert(0);
   }
   return XML_STATUS_OK;
 }
