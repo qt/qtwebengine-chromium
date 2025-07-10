a0f8269f7f429771ef9b181639ccfc6cbc81a37a
Andrea Orru <andreaorru@chromium.org>
2025-07-10 11:31:11 -0700

    [backport] CVE-2025-9866
    Determine whether to bypass redirect checks per request
    
    This change introduces the BypassRedirectChecksPerRequest feature flag,
    which is enabled by default. This flag allows redirect checks to be
    bypassed only if the specific request was redirected by the extensions
    layer.
    
    Bug: 379337758
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6692491
    Change-Id: I191cb88c5ad84b03b6c72ad2e79014d063be43d9
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/673407
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/content/public/common/content_features.h
+++ chromium/content/public/common/content_features.h
@@ -60,6 +60,7 @@ CONTENT_EXPORT BASE_DECLARE_FEATURE(
 CONTENT_EXPORT BASE_DECLARE_FEATURE(
     kBrokerFileOperationsOnDiskCacheInNetworkService);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kBrowserUseCompositingThreadPriority);
+CONTENT_EXPORT BASE_DECLARE_FEATURE(kBypassRedirectChecksPerRequest);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kCacheControlNoStoreEnterBackForwardCache);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kCapturedSurfaceControlStickyPermissions);
 CONTENT_EXPORT BASE_DECLARE_FEATURE(kCapturedSurfaceControlKillswitch);
