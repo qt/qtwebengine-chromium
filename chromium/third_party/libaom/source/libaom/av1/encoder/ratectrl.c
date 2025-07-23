363d41dd45577409a6e32278970911ad8778ceb5
Marco Paniconi <marpan@google.com>
2025-07-22 20:15:32 -0700

    [Backport] CVE-2025-8879: Heap buffer overflow in libaom
    Cherry-pick of patch originally reviewed on
    https://aomedia-review.googlesource.com/c/aom/+/201661:
    rtc: Restrict usage of src_sad_blk_64x64 to single spatial layer
    
    For blk_sad usage: disallow for spatial layers.
    
    This is a possible fix for the crash in the issue below.
    
    The issue seems to be incorrect setting of svc->mi_cols/rows_full_resoln (used for the allocation of src_sad_blk_64x64 for spatial layers) for the case of switching up layers (2SL to 3SL) and then passing in lower resolution for the top layer after the switch.
    
    The performance effect of src_sad_blk_64x64 for spatial layered encoding is expected to be small/minimal, so ok to disable. Will follow-up with a unittest and maybe re-enable the feature with the proper fix.
    
    Bug b:433046392, b:432035817
    
    Change-Id: Id0b2b36771fffef75bc89f51cf62709d5b6ad606
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/669338
    Reviewed-by: Moss Heim <moss.heim@qt.io>

--- chromium/third_party/libaom/source/libaom/av1/encoder/ratectrl.c
+++ chromium/third_party/libaom/source/libaom/av1/encoder/ratectrl.c
@@ -3236,8 +3236,9 @@ static void rc_scene_detection_onepass_rt(AV1_COMP *cpi,
   // non-zero sad exists along bottom border even though source is static.
   const int border =
       rc->prev_frame_is_dropped || cpi->svc.number_temporal_layers > 1;
-  // Store blkwise SAD for later use
-  if (width == cm->render_width && height == cm->render_height) {
+  // Store blkwise SAD for later use. Disable for spatial layers for now.
+  if (width == cm->render_width && height == cm->render_height &&
+      cpi->svc.number_spatial_layers == 1) {
     if (cpi->src_sad_blk_64x64 == NULL) {
       CHECK_MEM_ERROR(cm, cpi->src_sad_blk_64x64,
                       (uint64_t *)aom_calloc(sb_cols * sb_rows,
