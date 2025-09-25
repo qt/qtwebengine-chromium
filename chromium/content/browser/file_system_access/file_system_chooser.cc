dee23d880b1972249c514b9139bb456e961396f9
Fergal Daly <fergal@chromium.org>
2025-09-24 21:56:58 -0700

    [Backport] CVE-2025-11216: Inappropriate implementation in Storage
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/6966639
    Close file picker dialog if the tab becomes invisible.
    
    A previous fix for a similar bug (https://crrev.com/c/659915) added code
    to not show the dialog if the tab is already invisible however there's
    lots of opportunity for races.
    
    This fixes that by making the FileSystemChooser a WebContentsObserver to
    catch later changes. The observer is created immediately after checking
    the visibility so there is no race. The old code is removed as it does
    the checking at a point many steps away from creating the chooser.
    
    The old unittest is removed rather than updated. The relevant code has
    moved and rather write a new unittest, I'm relying on coverage in
    content_browsertests and browser_tests.
    
    The changes to file_system_chooser_unittest.cc are basically a no-op but
    all of the tests need a WebContents instance now.
    
    Bug: 419721056,337356054
    Change-Id: I6d3fb55c4fc7c4468b6172cd505bef081060f722
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/6966639
    Reviewed-by: Joey Arhar <jarhar@chromium.org>
    Commit-Queue: Fergal Daly <fergal@chromium.org>
    Reviewed-by: Mingyu Lei <leimy@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1520251}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/682492
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/content/browser/file_system_access/file_system_chooser.cc
+++ chromium/content/browser/file_system_access/file_system_chooser.cc
@@ -14,6 +14,7 @@
 #include "content/browser/file_system_access/file_system_access_error.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/content_browser_client.h"
+#include "content/public/browser/visibility.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_client.h"
 #include "net/base/mime_util.h"
@@ -234,6 +235,17 @@ FileSystemChooser::ScopedObjects::ScopedObjects(
     : fullscreen_block(std::move(fullscreen_block)),
       pip_tucker(std::move(pip_tucker)) {}
 
+
+namespace {
+// Called when no file is selected due to being aborted.
+void AbortedCallback(FileSystemChooser::ResultCallback callback) {
+  std::move(callback).Run(
+      file_system_access_error::FromStatus(
+          blink::mojom::FileSystemAccessStatus::kOperationAborted),
+      {});
+}
+}  // namespace
+
 // static
 void FileSystemChooser::CreateAndShow(
     WebContents* web_contents,
@@ -241,9 +253,15 @@ void FileSystemChooser::CreateAndShow(
     ResultCallback callback,
     FileSystemChooser::ScopedObjects scoped_objects) {
   DCHECK_CURRENTLY_ON(BrowserThread::UI);
+  if (web_contents->GetVisibility() == Visibility::HIDDEN) {
+    AbortedCallback(std::move(callback));
+    return;
+  }
+
   // `listener` deletes itself.
-  auto* listener = new FileSystemChooser(options.type(), std::move(callback),
-                                         std::move(scoped_objects));
+ auto* listener =
+      new FileSystemChooser(options.type(), std::move(callback),
+                            std::move(scoped_objects), web_contents);
   listener->dialog_ = ui::SelectFileDialog::Create(
       listener,
       GetContentClient()->browser()->CreateSelectFilePolicy(web_contents));
@@ -306,15 +324,24 @@ bool FileSystemChooser::IsShellIntegratedExtension(
 FileSystemChooser::FileSystemChooser(
     ui::SelectFileDialog::Type type,
     ResultCallback callback,
-    FileSystemChooser::ScopedObjects scoped_objects)
-    : callback_(std::move(callback)),
+    FileSystemChooser::ScopedObjects scoped_objects,
+    WebContents* web_contents)
+    : WebContentsObserver(web_contents),
       type_(ValidateType(type)),
+      callback_(std::move(callback)),
       scoped_objects_(std::move(scoped_objects)) {}
 
 FileSystemChooser::~FileSystemChooser() {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
   if (dialog_)
     dialog_->ListenerDestroyed();
+}
+
+void FileSystemChooser::OnVisibilityChanged(Visibility visibility) {
+  DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
+  if (visibility == Visibility::HIDDEN) {
+    FileSelectionCanceled(nullptr);
+  }
 }
 
 void FileSystemChooser::FileSelected(const ui::SelectedFileInfo& file,
@@ -346,10 +373,7 @@ void FileSystemChooser::MultiFilesSelected(
 
 void FileSystemChooser::FileSelectionCanceled(void* params) {
   DCHECK_CALLED_ON_VALID_SEQUENCE(sequence_checker_);
-  std::move(callback_).Run(
-      file_system_access_error::FromStatus(
-          blink::mojom::FileSystemAccessStatus::kOperationAborted),
-      {});
+  AbortedCallback(std::move(callback_));
   delete this;
 }
 
