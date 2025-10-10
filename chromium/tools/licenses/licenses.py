1ce005b466749449d22c4969f9cc1903f6bf4476
Peter Varga <pvarga@inf.u-szeged.hu>
2025-10-10 12:53:03 +0200

    [tool] Adapt licenses.py for QtWebEngine
    - Mask '*/' in licenses.py output, so qdoc is not confused
    - Add 'name-sanitized', 'license-type' placeholders
    - Add --gn-binary argument to specify GN location in case of generating
      licenses using gn targets
    - Add --gn-generate argument to be able to skip project generation
      because it is not needed for QtWebEngine
    - Pass python interpreter path to gn during license check
    
    Task-number: QTBUG-88482
    Task-number: QTBUG-115488
    Change-Id: I7db32a0d7d2d1e31f6bcab0ff31a84675c2e411e
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/683630
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/tools/licenses/licenses.py
+++ chromium/tools/licenses/licenses.py
@@ -881,8 +881,10 @@ def GetThirdPartyDepsFromGNDepsOutput(
   return third_party_deps
 
 
-def FindThirdPartyDeps(gn_out_dir: str,
+def FindThirdPartyDeps(gn_binary : str,
+                       gn_out_dir: str,
                        gn_target: str,
+                       gn_generate: bool,
                        target_os: str,
                        extra_third_party_dirs: Optional[List[str]] = None,
                        extra_allowed_dirs: Optional[List[str]] = None):
@@ -897,12 +899,16 @@ def FindThirdPartyDeps(gn_out_dir: str,
   with tempfile.TemporaryDirectory(
       dir=os.path.join(gn_out_dir, '..')) as tmp_dir:
     shutil.copy(os.path.join(gn_out_dir, "args.gn"), tmp_dir)
-    # "gn desc" requires "build.ninja", but ok with empty "build.ninja".
-    # "gn gen" is slow and requires too much memory.
-    with open(os.path.join(tmp_dir, "build.ninja"), "w") as w:
-      pass
+    if not gn_generate:
+      # "gn desc" requires "build.ninja", but ok with empty "build.ninja".
+      # "gn gen" is slow and requires too much memory.
+      with open(os.path.join(tmp_dir, "build.ninja"), "w") as w:
+        pass
+    else:
+       tmp_dir = gn_out_dir
     gn_deps = subprocess.check_output([
-        _GnBinary(), "desc",
+        gn_binary, "desc",
+        "--script-executable=%s" % sys.executable,
         "--root=%s" % _REPOSITORY_ROOT, tmp_dir, gn_target, "deps",
         "--as=buildfile", "--all"
     ],
@@ -941,8 +947,10 @@ def GenerateCredits(file_template_file,
                     reciprocal_template_file,
                     output_file,
                     target_os,
+                    gn_binary,
                     gn_out_dir,
                     gn_target,
+                    gn_generate,
                     extra_third_party_dirs=None,
                     depfile=None,
                     enable_warnings=False):
@@ -953,6 +961,7 @@ def GenerateCredits(file_template_file,
     for key, val in env.items():
       if escape:
         val = html.escape(val)
+        val = val.replace("*/", "* /")
       template = template.replace('{{%s}}' % key, val)
     return template
 
@@ -965,8 +974,10 @@ def GenerateCredits(file_template_file,
 
     env = {
         'name': metadata['Name'],
+        'name-sanitized': metadata['Name'].replace(' ', '-'),
         'url': metadata['URL'],
         'license': license_content,
+        'license-type': metadata['License']
     }
 
     return {
@@ -976,7 +987,7 @@ def GenerateCredits(file_template_file,
     }
 
   if gn_target:
-    third_party_dirs = FindThirdPartyDeps(gn_out_dir, gn_target, target_os,
+    third_party_dirs = FindThirdPartyDeps(gn_binary, gn_out_dir, gn_target, gn_generate, target_os,
                                           extra_third_party_dirs)
 
     # Sanity-check to raise a build error if invalid gn_... settings are
@@ -1010,6 +1021,7 @@ def GenerateCredits(file_template_file,
       'Name': 'The Chromium Project',
       'URL': 'https://www.chromium.org',
       'Shipped': 'yes',
+      'License': 'BSD 3-clause "New" or "Revised" License',
       'License File': [os.path.join(_REPOSITORY_ROOT, 'LICENSE')],
   }
   entries.append(
@@ -1167,7 +1179,8 @@ def GenerateLicenseFile(args: argparse.Namespace, root_dir=_REPOSITORY_ROOT):
     ]
 
   if args.gn_target is not None:
-    third_party_dirs = FindThirdPartyDeps(args.gn_out_dir, args.gn_target,
+    third_party_dirs = FindThirdPartyDeps(args.gn_binary, args.gn_out_dir, args.gn_target,
+                                          args.gn_generate,
                                           args.target_os,
                                           extra_third_party_dirs,
                                           args.extra_allowed_dirs)
@@ -1340,6 +1353,7 @@ def GenerateLicenseFilePlainText(
 
     The LICENSE file contains licenses of both Chromium and third-party
     libraries which gn_target depends on. """
+
   # Start with Chromium's LICENSE file.
   content = [read_file(os.path.join(repo_root, 'LICENSE'))]
 
@@ -1443,6 +1457,8 @@ def main():
       '--spdx-doc-namespace',
       default='https://chromium.googlesource.com/chromium/src/tools/',
       help='Specify the document namespace for the SPDX doc')
+  parser.add_argument('--gn-binary', help="GN binary location.")
+  parser.add_argument('--gn-generate', action='store_false', help='Generates gn project.')
   parser.add_argument(
       '--enable-warnings',
       action='store_true',
@@ -1458,13 +1474,19 @@ def main():
   args.extra_allowed_dirs = action_helpers.parse_gn_list(
       args.extra_allowed_dirs)
 
+  if not args.gn_binary:
+    gn_binary = _GnBinary()
+  else:
+    gn_binary = args.gn_binary
+
   if args.command == 'scan':
     if not ScanThirdPartyDirs():
       return 1
   elif args.command == 'credits':
     if not GenerateCredits(
         args.file_template, args.entry_template, args.reciprocal_template,
-        args.output_file, args.target_os, args.gn_out_dir, args.gn_target,
+        args.output_file, args.target_os, gn_binary, args.gn_out_dir,
+        args.gn_target, args.gn_generate,
         args.extra_third_party_dirs, args.depfile, args.enable_warnings):
       return 1
   elif args.command == 'license_file':
