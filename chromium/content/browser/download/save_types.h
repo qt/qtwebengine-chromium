137dd516165e23c6f088647f24981a9fbb13c11b
Min Qin <qinmin@chromium.org>
2025-10-22 11:39:53 -0700

    [backport] CVE-2025-13634
    Run quarantine for all file save items
    
    Currently, quarantine is only run for files saved from the network.
    This CL make Chrome to also run quarantine for files that is
    saved from DOM since the DOM content is also from the web. This CL
    adds a quarantine callback to each SaveFile, so when SaveFile is
    finished, the quarantine will be invoked before completing the
    SavePackage.
    
    Bug: 429140219
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046052
    Change-Id: Ic26d9e7ef2ce8adb4a1cc212a3965c68b5f29310
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/702417
    Reviewed-by: Moss Heim <moss.heim@qt.io>
    (cherry picked from commit 0ed62af38eafaf7354d6b773b191122d8d523b86)
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/710995

--- chromium/content/browser/download/save_types.h
+++ chromium/content/browser/download/save_types.h
@@ -13,6 +13,7 @@
 #include <vector>
 
 #include "base/files/file_path.h"
+#include "base/functional/callback.h"
 #include "base/types/id_type.h"
 #include "url/gurl.h"
 
@@ -81,6 +82,8 @@ struct SaveFileCreateInfo {
   std::string content_disposition;
   // Source type of saved file.
   SaveFileSource save_source;
+  // Callback to run to quarantine the file;
+  base::OnceClosure quarantine_callback;
 };
 
 }  // namespace content
