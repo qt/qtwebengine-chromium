2b2c717ac5fc8c04221afa77f298ca7723b9da67
Alexander Cooper <alcooper@chromium.org>
2025-10-23 00:41:04 -0700

    [Backport] CVE-2025-12443: Out of bounds read in WebXR
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/7046576:
    Update detached matrix handling
    
    Updates a few places that handle an array representation of a matrix
    being detached by returning a 0-length array to simply recompute the
    array that should be present, based on the presence of other data. This
    more closely matches the spec, which for both of these cases essentially
    state that if the value is not null to check if it's detached, and if it
    is not detached to return the value. The steps following both of these
    checks then recompute the matrix, so technically our current impl is
    not spec-compliant to that.
    
    Further, we remove one unused array to transform conversion helper and
    update another to a CHECK from a DCHECK to match best practices for
    invariants.
    
    Fixed: 452071845
    Change-Id: Idf765fe5717d59ae63c71e8253784ff4473dea5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7046576
    Commit-Queue: Alexander Cooper <alcooper@chromium.org>
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Auto-Submit: Alexander Cooper <alcooper@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1530947}
    (cherry picked from commit 6c5963ad6b4541f5fa0812607fd36a979c48c0f1)
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/689567
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/third_party/blink/renderer/modules/xr/xr_view.cc
+++ chromium/third_party/blink/renderer/modules/xr/xr_view.cc
@@ -81,7 +81,8 @@ NotShared<DOMFloat32Array> XRView::projectionMatrix() const {
     // A page may take the projection matrix value and detach it so
     // projection_matrix_ is a detached array buffer.  This breaks the
     // inspector, so return null instead.
-    return NotShared<DOMFloat32Array>();
+    projection_matrix_ =
+        transformationMatrixToDOMFloat32Array(view_data_->ProjectionMatrix());
   }
 
   return projection_matrix_;
