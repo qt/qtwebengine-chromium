8e2d1e5c07a32ada8b63f41a6eafc61fa384f925
kylechar <kylechar@chromium.org>
2025-10-31 08:56:46 -0700

    [Backport] Dependency for security bug 495850837 (2/2)
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/7087007:
    Add SharedImageFormat::BytesPerPixel()
    
    It will replace the existing BitsPerPixel() which all callers just
    divide by 8 today.
    
    Bug: 450804509
    Change-Id: Ib3970ac730b96e80c3773300e59bdfc9804652b7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7087007
    Reviewed-by: Mingjing Zhang <mjzhang@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1538632}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/732370
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/737680
    Reviewed-by: Moss Heim <moss.heim@qt.io>

--- chromium/components/viz/common/resources/shared_image_format.h
+++ chromium/components/viz/common/resources/shared_image_format.h
@@ -186,6 +186,10 @@ class COMPONENT_EXPORT(VIZ_SHARED_IMAGE_FORMAT) SharedImageFormat final {
   // NOTE: Supported only for true single-plane formats.
   int BitsPerPixel() const;
 
+  // NOTE: Supported only for true single-plane formats that have a fixed number
+  // of bytes per pixel, eg. not ETC1.
+  int BytesPerPixel() const;
+
   bool operator==(const SharedImageFormat& o) const;
   std::weak_ordering operator<=>(const SharedImageFormat& o) const;
 
