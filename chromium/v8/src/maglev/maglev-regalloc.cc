fca212085dc3622b5f0ded5cc3110c4fa284ef10
Olivier Flückiger <olivf@chromium.org>
2025-11-05 14:11:51 +0100

    [backport] CVE-2025-13042
    Fix left over register allocations from regalloc
    
    The regalloc should clear the node allocations when it is done.
    Failing to do so can cause the codegen to use stale register state.
    In this concrete example the exception handler trampolines would not
    load from the spill slot due to the left over allocation.
    
    Bug: 457351015
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7119379
    Change-Id: Ia113c0b3373b5b11250e11d66d63b6a67b63b13f
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/707801
    Reviewed-by: Moss Heim <moss.heim@qt.io>

--- chromium/v8/src/maglev/maglev-regalloc.cc
+++ chromium/v8/src/maglev/maglev-regalloc.cc
@@ -631,6 +631,9 @@ void StraightForwardRegisterAllocator::AllocateRegisters() {
     AllocateControlNode(block->control_node(), block);
     ApplyPatches(block);
   }
+
+  // Clean up remaining register allocations at the end
+  ClearRegisters();
 }
 
 void StraightForwardRegisterAllocator::FreeRegistersUsedBy(ValueNode* node) {
@@ -1607,8 +1610,8 @@ void StraightForwardRegisterAllocator::SpillRegisters() {
   double_registers_.ForEachUsedRegister(spill);
 }
 
-template <typename RegisterT>
-void StraightForwardRegisterAllocator::SpillAndClearRegisters(
+template <typename RegisterT, bool spill>
+void StraightForwardRegisterAllocator::ClearRegisters(
     RegisterFrameState<RegisterT>& registers) {
   while (registers.used() != registers.empty()) {
     RegisterT reg = registers.used().first();
@@ -1617,7 +1620,9 @@ void StraightForwardRegisterAllocator::SpillAndClearRegisters(
       printing_visitor_->os()
           << "  clearing registers with " << PrintNodeLabel(node) << "\n";
     }
-    Spill(node);
+    if (spill) {
+      Spill(node);
+    }
     registers.FreeRegistersUsedBy(node);
     DCHECK(!registers.used().has(reg));
   }
@@ -1626,6 +1631,11 @@ void StraightForwardRegisterAllocator::SpillAndClearRegisters(
 void StraightForwardRegisterAllocator::SpillAndClearRegisters() {
   SpillAndClearRegisters(general_registers_);
   SpillAndClearRegisters(double_registers_);
+}
+
+void StraightForwardRegisterAllocator::ClearRegisters() {
+  ClearRegisters(general_registers_);
+  ClearRegisters(double_registers_);
 }
 
 void StraightForwardRegisterAllocator::SaveRegisterSnapshot(NodeBase* node) {
