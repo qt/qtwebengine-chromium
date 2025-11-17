cd6a5bc430aa90c27ffb194f98d80466eb328cc9
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2025-11-17 10:49:26 +0100

    [fixup]Fix QtWebEngine build on Windows
    Ensures the protoc wrapper batch script will call python, if
    a python3 doesn't exist in the PATH
    
    Change-Id: I91fb9ee22f0a51669e77bb2c1d662722ee2c6df0
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/691601
    Reviewed-by: Moss Heim <moss.heim@qt.io>
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/tools/protoc_wrapper/protoc-gen-ts_proto.bat
+++ chromium/tools/protoc_wrapper/protoc-gen-ts_proto.bat
@@ -4,4 +4,8 @@
 :: Use of this source code is governed by a BSD-style license that can be
 :: found in the LICENSE file.
 
-python3 "%~dp0protoc-gen-ts_proto.py" %*
+if exist python3 (
+    python3 "%~dp0protoc-gen-ts_proto.py" %*
+) else (
+    python "%~dp0protoc-gen-ts_proto.py" %*
+)
