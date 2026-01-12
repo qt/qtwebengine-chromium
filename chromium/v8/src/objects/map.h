869914bdd3c86f486c96832c4faa231703f3268f
Olivier Flückiger <olivf@chromium.org>
2026-01-12 15:41:30 +0100

    [backport] CVE-2026-1220
    Fix publishing of integrity-level transitions
    
    Integrity level transition target maps should not be published to the
    map tree before they are fully initialized. Otherwise concurrent access
    might pick up not fully updated target maps.
    
    Drive-By: Fix a dcheck in the map-updater to not fire when an indirectly
    reachable non-deprecatable map is deprecated due to the whole subtree
    being deprecated.
    
    Fixed: 473851441
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7450862
    Change-Id: I5d2c8a6397a84369453032c564d44a30e469826d
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/706075
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>
    (cherry picked from commit c971c6841dd6beab9f54aeaf9a53be413273039e)
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/711437
    Reviewed-by: Moss Heim <moss.heim@qt.io>

--- chromium/v8/src/objects/map.h
+++ chromium/v8/src/objects/map.h
@@ -1010,6 +1010,14 @@ class Map : public TorqueGeneratedMap<Map, HeapObject> {
   static Handle<Map> CopyAddDescriptor(Isolate* isolate, Handle<Map> map,
                                        Descriptor* descriptor,
                                        TransitionFlag flag);
+
+  template <typename InitMapCb>
+  static Handle<Map> CopyReplaceDescriptors(
+      Isolate* isolate, DirectHandle<Map> map,
+      DirectHandle<DescriptorArray> descriptors, TransitionFlag flag,
+      const InitMapCb& InitMap, MaybeDirectHandle<Name> maybe_name,
+      const char* reason, TransitionKindFlag transition_kind);
+
   static Handle<Map> CopyReplaceDescriptors(
       Isolate* isolate, Handle<Map> map,
       DirectHandle<DescriptorArray> descriptors, TransitionFlag flag,
@@ -1024,6 +1032,7 @@ class Map : public TorqueGeneratedMap<Map, HeapObject> {
                                     PropertyNormalizationMode mode);
 
   void DeprecateTransitionTree(Isolate* isolate);
+  void DeprecateTransitionTreeImpl(Isolate* isolate);
 
   void ReplaceDescriptors(Isolate* isolate,
                           Tagged<DescriptorArray> new_descriptors);
