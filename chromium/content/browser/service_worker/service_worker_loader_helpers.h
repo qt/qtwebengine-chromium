b4d8cf2b0b392a3efeed1cb091c6052f5b8dbf70
Shunya Shishido <sisidovski@chromium.org>
2026-01-13 00:18:35 -0800

    [Backport] Dependency for CVE-2026-7958
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/7396656:
    Skip unnecessary IPC for synthetic response registration
    
    This CL improves the performance of synthetic responses by creating the
    fake Service Worker registration directly in the browser process,
    avoiding an unnecessary Mojo call to ServiceWorkerStorage.
    
    Previously, the fake registration was delivered via the
    GetFakeRegistrationForClientUrl Mojo call to simulate the standard
    registration lookup flow. Since synthetic responses are part of an
    experimental feature and registration lookup optimizations are already
    allowing subsequent functions to be called on the same stack, the IPC
    overhead can be removed.
    
    This CL also resolves the TODO in ServiceWorkerRegistry by moving the
    insertion of the fake registration.
    
    The logic for creating the fake registration is moved to
    service_worker_loader_helpers.
    
    Change-Id: I509b8434cefd437382cab2a2a13f96965e863cbe
    Bug: 447039330, 352578800
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7396656
    Reviewed-by: Minoru Chikamune <chikamune@chromium.org>
    Commit-Queue: Shunya Shishido <sisidovski@chromium.org>
    Reviewed-by: Takashi Toyoshima <toyoshim@chromium.org>
    Reviewed-by: Yoshisato Yanagisawa <yyanagisawa@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1568276}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/746565
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/content/browser/service_worker/service_worker_loader_helpers.h
+++ chromium/content/browser/service_worker/service_worker_loader_helpers.h
@@ -100,6 +100,10 @@ CONTENT_EXPORT bool IsEligibleForSyntheticResponseForTesting(
     const GURL& client_url,
     const std::string& allowed_urls);
 
+storage::mojom::ServiceWorkerFindRegistrationResultPtr
+CreateSyntheticRegistration(const GURL& client_url, 
+                            const blink::StorageKey& key);
+
 }  // namespace service_worker_loader_helpers
 
 }  // namespace content
