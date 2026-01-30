d01003734ccf371ae75ecfb127437c4a3dadbb68
Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
2026-01-30 10:57:07 +0100

    [fixup] Fix QtWebEngine build on Windows
    Several functions in rule_invalidation_data_visitor.cc had
    changes in their definitions, but pur inlined copies weren't
    updated, leading to broken CSS selector behavior.
    
    Task-number: QTBUG-143720
    Change-Id: Ibb4454ac77c514d4b6b20c5dd68b6b87d760b5ff
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/708631
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/third_party/blink/renderer/core/css/invalidation/rule_invalidation_data_visitor.h
+++ chromium/third_party/blink/renderer/core/css/invalidation/rule_invalidation_data_visitor.h
@@ -589,16 +589,98 @@ class RuleInvalidationDataVisitor {
     if (selector.Match() == CSSSelector::kId) {
       if (type == InvalidationType::kInvalidateDescendants &&
           position == kSubject &&
-          InsertIntoSelfInvalidationBloomFilter(
-              selector.Value(), RuleInvalidationData::kIdSalt)) {
+          InsertIntoSelfInvalidationBloomFilter(selector.Value(),
+                                                RuleInvalidationData::kIdSalt)) {
         // Do not insert self-invalidation sets for IDs;
         // see comment on class_invalidation_sets_.
         return nullptr;
       }
       return EnsureIdInvalidationSet(selector.Value(), type, position,
-                                     in_nth_child);
+                                    in_nth_child);
     }
-
+    if (selector.Match() == CSSSelector::kPseudoClass) {
+      switch (selector.GetPseudoType()) {
+        case CSSSelector::kPseudoEmpty:
+        case CSSSelector::kPseudoFirstChild:
+        case CSSSelector::kPseudoLastChild:
+        case CSSSelector::kPseudoOnlyChild:
+        case CSSSelector::kPseudoLink:
+        case CSSSelector::kPseudoVisited:
+        case CSSSelector::kPseudoWebkitAnyLink:
+        case CSSSelector::kPseudoAnyLink:
+        case CSSSelector::kPseudoAutofill:
+        case CSSSelector::kPseudoWebKitAutofill:
+        case CSSSelector::kPseudoAutofillPreviewed:
+        case CSSSelector::kPseudoAutofillSelected:
+        case CSSSelector::kPseudoHover:
+        case CSSSelector::kPseudoDrag:
+        case CSSSelector::kPseudoFocus:
+        case CSSSelector::kPseudoFocusVisible:
+        case CSSSelector::kPseudoFocusWithin:
+        case CSSSelector::kPseudoActive:
+        case CSSSelector::kPseudoChecked:
+        case CSSSelector::kPseudoEnabled:
+        case CSSSelector::kPseudoDefault:
+        case CSSSelector::kPseudoDisabled:
+        case CSSSelector::kPseudoOptional:
+        case CSSSelector::kPseudoPlaceholderShown:
+        case CSSSelector::kPseudoRequired:
+        case CSSSelector::kPseudoReadOnly:
+        case CSSSelector::kPseudoReadWrite:
+        case CSSSelector::kPseudoState:
+        case CSSSelector::kPseudoUserInvalid:
+        case CSSSelector::kPseudoUserValid:
+        case CSSSelector::kPseudoValid:
+        case CSSSelector::kPseudoInvalid:
+        case CSSSelector::kPseudoIndeterminate:
+        case CSSSelector::kPseudoTarget:
+        case CSSSelector::kPseudoTargetCurrent:
+        case CSSSelector::kPseudoLang:
+        case CSSSelector::kPseudoDir:
+        case CSSSelector::kPseudoFullScreen:
+        case CSSSelector::kPseudoFullScreenAncestor:
+        case CSSSelector::kPseudoFullscreen:
+        case CSSSelector::kPseudoPaused:
+        case CSSSelector::kPseudoPermissionElementInvalidStyle:
+        case CSSSelector::kPseudoPermissionElementOccluded:
+        case CSSSelector::kPseudoPermissionGranted:
+        case CSSSelector::kPseudoPictureInPicture:
+        case CSSSelector::kPseudoPlaying:
+        case CSSSelector::kPseudoInRange:
+        case CSSSelector::kPseudoOutOfRange:
+        case CSSSelector::kPseudoDefined:
+        case CSSSelector::kPseudoOpen:
+        case CSSSelector::kPseudoPopoverOpen:
+        case CSSSelector::kPseudoVideoPersistent:
+        case CSSSelector::kPseudoVideoPersistentAncestor:
+        case CSSSelector::kPseudoXrOverlay:
+        case CSSSelector::kPseudoHasDatalist:
+        case CSSSelector::kPseudoMultiSelectFocus:
+        case CSSSelector::kPseudoModal:
+        case CSSSelector::kPseudoSelectorFragmentAnchor:
+        case CSSSelector::kPseudoActiveViewTransition:
+        case CSSSelector::kPseudoActiveViewTransitionType:
+        case CSSSelector::kPseudoHasSlotted:
+          return EnsurePseudoInvalidationSet(selector.GetPseudoType(), type,
+                                            position, in_nth_child);
+        case CSSSelector::kPseudoFirstOfType:
+        case CSSSelector::kPseudoLastOfType:
+        case CSSSelector::kPseudoOnlyOfType:
+        case CSSSelector::kPseudoNthChild:
+        case CSSSelector::kPseudoNthOfType:
+        case CSSSelector::kPseudoNthLastChild:
+        case CSSSelector::kPseudoNthLastOfType:
+          return EnsureNthInvalidationSet();
+        case CSSSelector::kPseudoHas:
+          return position == kAncestor
+                    ? EnsurePseudoInvalidationSet(selector.GetPseudoType(), type,
+                                                  position, in_nth_child)
+                    : nullptr;
+        case CSSSelector::kPseudoPart:
+        default:
+          break;
+      }
+    }
     return nullptr;
   }
   InvalidationSetType* EnsureClassInvalidationSet(
@@ -650,23 +732,22 @@ class RuleInvalidationDataVisitor {
       scoped_refptr<InvalidationSet>& invalidation_set =
           map.insert(key, nullptr).stored_value->value;
       return &EnsureMutableInvalidationSet(type, position, in_nth_child,
-                                           invalidation_set);
+                                          invalidation_set);
     } else {
       auto it = map.find(key);
       if (it != map.end()) {
         const InvalidationSet* invalidation_set = it->value.get();
         if (invalidation_set->GetType() == type) {
           return invalidation_set;
-        } else {
+        } else if (type == InvalidationType::kInvalidateDescendants) {
           // The caller wanted descendant and we found sibling+descendant.
-          CHECK(type == InvalidationType::kInvalidateDescendants);
           return To<SiblingInvalidationSet>(invalidation_set)->Descendants();
         }
       }
-      // It is possible for the Tracer not to find an InvalidationSet we expect
-      // to be there. One case where this can happen is when, at the time we run
-      // the Tracer, a rule has been added to a stylesheet but not yet indexed.
-      // In such a case, we'll pick up information about the new rule as it gets
+      // It is possible for the Tracer not to find an InvalidationSet we expect to
+      // be there. One case where this can happen is when, at the time we run the
+      // Tracer, a rule has been added to a stylesheet but not yet indexed. In
+      // such a case, we'll pick up information about the new rule as it gets
       // indexed on the next document lifecycle update.
       return nullptr;
     }
@@ -682,23 +763,22 @@ class RuleInvalidationDataVisitor {
       scoped_refptr<InvalidationSet>& invalidation_set =
           map.insert(key, nullptr).stored_value->value;
       return &EnsureMutableInvalidationSet(type, position, in_nth_child,
-                                           invalidation_set);
+                                          invalidation_set);
     } else {
       auto it = map.find(key);
       if (it != map.end()) {
         const InvalidationSet* invalidation_set = it->value.get();
         if (invalidation_set->GetType() == type) {
           return invalidation_set;
-        } else {
+        } else if (type == InvalidationType::kInvalidateDescendants) {
           // The caller wanted descendant and we found sibling+descendant.
-          CHECK(type == InvalidationType::kInvalidateDescendants);
           return To<SiblingInvalidationSet>(invalidation_set)->Descendants();
         }
       }
-      // It is possible for the Tracer not to find an InvalidationSet we expect
-      // to be there. One case where this can happen is when, at the time we run
-      // the Tracer, a rule has been added to a stylesheet but not yet indexed.
-      // In such a case, we'll pick up information about the new rule as it gets
+      // It is possible for the Tracer not to find an InvalidationSet we expect to
+      // be there. One case where this can happen is when, at the time we run the
+      // Tracer, a rule has been added to a stylesheet but not yet indexed. In
+      // such a case, we'll pick up information about the new rule as it gets
       // indexed on the next document lifecycle update.
       return nullptr;
     }
