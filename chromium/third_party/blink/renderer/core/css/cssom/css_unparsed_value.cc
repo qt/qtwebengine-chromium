00a690f8eb97d1d62b71bfb4f9b2e7a203b21bd2
Steinar H. Gunderson <sesse@chromium.org>
2026-02-19 05:43:25 -0800

    [backport] CVE-2026-3541
    Fix reading of invalid data in Typed OM.
    
    After seeing var(, we would not properly check that the next token
    was an ident, and would just call Value(), potentially reading
    uninitialized data. Our fix has a little defense in depth:
    
     1. For this specific case, we check the token type. (The spec is not
        exactly clear on exactly _what_ we should do, but right now,
        we abort the parsing. We should probably throw an exception,
        but getting an ExceptionState all the way down here is not trivial.)
     2. For CSSParserTokens without a value, we add a DCHECK if someone
        calls Value().
     3. In addition, we explicitly initialize data_length_ to zero,
        so that if someone calls Value() in a non-DCHECK build, we'll
        get an empty token instead of uninitialized data.
    
    Style perftest parsing performance is neutral.
    
    Fixed: 484811719
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7593255
    Change-Id: I3221cc17bd6625b6b6d2b3ee2e7f97e0947ec13a
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/719097
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
    (cherry picked from commit d2a2cb0d252715d3894460b1eac38cc9781e72fc)
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/719266

--- chromium/third_party/blink/renderer/core/css/cssom/css_unparsed_value.cc
+++ chromium/third_party/blink/renderer/core/css/cssom/css_unparsed_value.cc
@@ -20,7 +20,11 @@ namespace {
 
 String FindVariableName(CSSParserTokenStream& stream) {
   stream.ConsumeWhitespace();
-  return stream.Consume().Value().ToString();
+  if (stream.Peek().GetType() == CSSParserTokenType::kIdentToken) {
+    return stream.Consume().Value().ToString();
+  } else {
+    return {};
+  }
 }
 
 V8CSSUnparsedSegment* VariableReferenceValue(
@@ -36,6 +40,11 @@ V8CSSUnparsedSegment* VariableReferenceValue(
   CSSStyleVariableReferenceValue* variable_reference =
       CSSStyleVariableReferenceValue::Create(variable_name.ToString(),
                                              unparsed_value);
+  if (!variable_reference) {
+    // TODO(sesse): Plumb the ExceptionState here so that we can use
+    // the Create() variant that properly throws an exception.
+    return nullptr;
+  }
   return MakeGarbageCollected<V8CSSUnparsedSegment>(variable_reference);
 }
 
@@ -58,8 +67,12 @@ HeapVector<Member<V8CSSUnparsedSegment>> ParserTokenStreamToTokens(
       if (stream.Peek().GetType() == CSSParserTokenType::kCommaToken) {
         stream.Consume();
       }
-      tokens.push_back(VariableReferenceValue(
-          variable_name, ParserTokenStreamToTokens(stream)));
+      V8CSSUnparsedSegment* ref = VariableReferenceValue(
+          variable_name, ParserTokenStreamToTokens(stream));
+      if (!ref) {
+        break;
+      }
+      tokens.push_back(ref);
     } else {
       if (stream.Peek().GetBlockType() == CSSParserToken::kBlockStart) {
         ++nesting_level;
