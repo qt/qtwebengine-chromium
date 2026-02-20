4167a4949a3084357fef2aa9e0114558a094dcc0
Jakob Kummerow <jkummerow@chromium.org>
2026-02-20 15:12:18 +0100

    [backport] CVE-2026-3542
    Fix reset of heap_access_shift_position_
    
    It could get confused by nested shift expressions, leading to
    invalid Wasm modules being generated.
    
    Fixed: 485152421
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7594777
    Change-Id: I70220f6a63da8dddc2d36d4eacc4700c55ba72e8
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/719098
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/v8/src/asmjs/asm-parser.cc
+++ chromium/v8/src/asmjs/asm-parser.cc
@@ -1889,7 +1889,6 @@ AsmType* AsmJsParser::ShiftExpression() {
 #define HANDLE_CASE(op, opcode, name, result)                        \
   case TOK(op): {                                                    \
     EXPECT_TOKENn(TOK(op));                                          \
-    heap_access_shift_position_ = kNoHeapAccessShift;                \
     AsmType* b = nullptr;                                            \
     RECURSEn(b = AdditiveExpression());                              \
     if (!(a->IsA(AsmType::Intish()) && b->IsA(AsmType::Intish()))) { \
@@ -1897,6 +1896,8 @@ AsmType* AsmJsParser::ShiftExpression() {
     }                                                                \
     current_function_builder_->Emit(kExpr##opcode);                  \
     a = AsmType::result();                                           \
+    /* Must happen after the RECURSE call to unset its state! */     \
+    heap_access_shift_position_ = kNoHeapAccessShift;                \
     continue;                                                        \
   }
         HANDLE_CASE(SHL, I32Shl, "<<", Signed);
