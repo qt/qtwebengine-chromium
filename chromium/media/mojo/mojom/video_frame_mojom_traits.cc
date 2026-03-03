65847eea6746166a0e630be07842f6da00c026d5
Eugene Zemtsov <eugene@chromium.org>
2026-03-02 17:17:06 -0800

    [Backport] CVE-2026-5884: Insufficient validation of untrusted input in Media
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/7614704:
    media: Prevent passing of frames with absurdly large strides via Mojo
    
    - Make strides unsigned in mojo, as they are in VideoFrame
    - Validate that each plane's footprint (offset + stride * rows)
        fits in the buffer.
    - Remove support for interleaved planes. IMC4 pixel format.
    
    Bug: 484547633, 378046071
    Change-Id: I8e5dbebddc434041bd7c31c2b16c2b5963314061
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7614704
    Reviewed-by: Xiaohan Wang <xhwang@chromium.org>
    Reviewed-by: Dale Curtis <dalecurtis@chromium.org>
    Commit-Queue: Eugene Zemtsov <eugene@chromium.org>
    Reviewed-by: Matthew Denton <mpdenton@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1592880}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/729201
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/media/mojo/mojom/video_frame_mojom_traits.cc
+++ chromium/media/mojo/mojom/video_frame_mojom_traits.cc
@@ -37,7 +37,7 @@ namespace {
 
 base::ReadOnlySharedMemoryRegion CreateRegion(const media::VideoFrame& frame,
                                               std::vector<uint32_t>& offsets,
-                                              std::vector<int32_t>& strides) {
+                                              std::vector<uint32_t>& strides) {
   if (!media::IsYuvPlanar(frame.format()) || !media::IsOpaque(frame.format())) {
     DLOG(ERROR) << "format is not opaque YUV: "
                 << VideoPixelFormatToString(frame.format());
@@ -108,7 +108,7 @@ media::mojom::VideoFrameDataPtr MakeVideoFrameData(
       input->storage_type() == media::VideoFrame::STORAGE_UNOWNED_MEMORY ||
       input->storage_type() == media::VideoFrame::STORAGE_OWNED_MEMORY) {
     std::vector<uint32_t> offsets;
-    std::vector<int32_t> strides;
+    std::vector<uint32_t> strides;
     auto region = CreateRegion(*input, offsets, strides);
     if (!region.IsValid()) {
       DLOG(ERROR) << "Failed to create region from VideoFrame";
@@ -302,7 +302,7 @@ bool StructTraits<media::mojom::VideoFrameDataView,
     mojo::ArrayDataView<uint32_t> offsets;
     shared_memory_data.GetOffsetsDataView(&offsets);
 
-    mojo::ArrayDataView<int32_t> strides;
+    mojo::ArrayDataView<uint32_t> strides;
     shared_memory_data.GetStridesDataView(&strides);
 
     base::ReadOnlySharedMemoryMapping mapping = region.Map();
@@ -318,7 +318,6 @@ bool StructTraits<media::mojom::VideoFrameDataView,
     }
 
     auto mapped_region = mapping.GetMemoryAsSpan<uint8_t>();
-    std::array<base::span<const uint8_t>, 3> plane_data;
     std::vector<media::ColorPlaneLayout> planes(num_planes);
     for (size_t i = 0; i < num_planes; i++) {
       if (offsets[i] > mapped_region.size()) {
@@ -330,16 +329,9 @@ bool StructTraits<media::mojom::VideoFrameDataView,
 
       planes[i].stride = strides[i];
       planes[i].offset = base::strict_cast<size_t>(offsets[i]);
-      const size_t space_till_mapping_end = mapping.size() - offsets[i];
-      const size_t calculated_plane_size =
+      planes[i].size = 
           media::VideoFrame::Rows(i, format, coded_size.height()) * strides[i];
 
-      // TODO(crbug.com/378046071) For H.264 content Widevine outputs planes
-      // in IMC4 pixel format. Since Y and V planes in IMC4 overlap,
-      // the distance to the next plane can't be used to determent the size of
-      // the current plane.
-      planes[i].size = std::min(calculated_plane_size, space_till_mapping_end);
-      plane_data[i] = mapped_region.subspan(offsets[i], planes[i].size);
     }
 
     auto layout = media::VideoFrameLayout::CreateWithPlanes(format, coded_size,
@@ -347,6 +339,12 @@ bool StructTraits<media::mojom::VideoFrameDataView,
     if (!layout || !layout->FitsInContiguousBufferOfSize(mapping.size())) {
       DLOG(ERROR) << "Invalid layout";
       return false;
+    }
+
+    std::array<base::span<const uint8_t>, 3> plane_data;
+    for (size_t i = 0; i < num_planes; i++) {
+      plane_data[i] = mapped_region.subspan(layout->planes()[i].offset,
+                                            layout->planes()[i].size);
     }
 
     frame = media::VideoFrame::WrapExternalYuvDataWithLayout(
