2f670007b510bbe2d2b3a61d9793fb48c48b760f
Marco Paniconi <marpan@google.com>
2026-03-11 13:58:44 -0700

    [Backport] CVE-2026-4463: Heap buffer overflow in WebRTC (1/3)
    Manual cherry-pick of patch originally reviewed on
    https://aomedia-review.googlesource.com/c/aom/+/208221:
    rtc: Disable speed feature use_rtc_tf for spatial layers
    
    It was already disabled for resize and is_psnr_calc_enabled(),
    disable it always for now for spatial layers.
    
    Fixes the buffer overflow in issue below.
    
    Bug: 491358681
    Change-Id: If2a71249bbf07ff26e55da5905d959d6c1cdda84
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/723261
    Reviewed-by: Moss Heim <moss.heim@qt.io>

--- chromium/third_party/libaom/source/libaom/av1/encoder/speed_features.c
+++ chromium/third_party/libaom/source/libaom/av1/encoder/speed_features.c
@@ -1826,9 +1826,12 @@ static void set_rt_speed_feature_framesize_dependent(const AV1_COMP *const cpi,
   // temporal filtering which may change the input source during encoding:
   // this causes an issue on resized frames when psnr is calculated,
   // so disable it here for frames that are resized (encoding width/height
-  // different from configured width/height).
-  if (is_psnr_calc_enabled(cpi) && (cpi->oxcf.frm_dim_cfg.width != cm->width ||
-                                    cpi->oxcf.frm_dim_cfg.height != cm->height))
+  // different from configured width/height). Also disable for spatial layers.
+  // Bug: 491358681
+  if ((is_psnr_calc_enabled(cpi) &&
+       (cpi->oxcf.frm_dim_cfg.width != cm->width ||
+        cpi->oxcf.frm_dim_cfg.height != cm->height)) ||
+      cpi->svc.number_spatial_layers > 1)
     sf->rt_sf.use_rtc_tf = 0;
 }
 
