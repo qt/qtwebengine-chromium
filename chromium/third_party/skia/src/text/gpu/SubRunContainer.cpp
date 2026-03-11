14c4799b77532f183d30130ea24d136a617f2612
Greg Daniel <egdaniel@google.com>
2026-03-11 15:29:58 -0400

    [backport] CVE-2026-3909
    Make sure we are getting the correct atlas for glyph mask format.
    
    Bug: b/491421267
    Change-Id: I4eacd46599eca2df8c10a3fc894b9ce890fae1e2
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1184076
    Commit-Queue: Greg Daniel <egdaniel@google.com>
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>
    (cherry picked from commit 0cab3e4ee34b3bca6ba7df676639d73ffe4b2135)
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1184918
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/721822
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/third_party/skia/src/text/gpu/SubRunContainer.cpp
+++ chromium/third_party/skia/src/text/gpu/SubRunContainer.cpp
@@ -652,8 +652,9 @@ public:
 
     int glyphSrcPadding() const override { return 0; }
 
-    void testingOnly_packedGlyphIDToGlyph(StrikeCache* cache) const override {
-        fGlyphs.packedGlyphIDToGlyph(cache);
+    void testingOnly_packedGlyphIDToGlyph(StrikeCache* cache,
+                                          skgpu::MaskFormat maskFormat) const override {
+        fGlyphs.packedGlyphIDToGlyph(cache, maskFormat);
     }
 
     std::tuple<bool, SkRect> deviceRectAndNeedsTransform(
@@ -756,8 +757,9 @@ public:
 
     const AtlasSubRun* testingOnly_atlasSubRun() const override { return this; }
 
-    void testingOnly_packedGlyphIDToGlyph(StrikeCache *cache) const override {
-        fGlyphs.packedGlyphIDToGlyph(cache);
+    void testingOnly_packedGlyphIDToGlyph(StrikeCache *cache,
+                                          skgpu::MaskFormat maskFormat) const override {
+        fGlyphs.packedGlyphIDToGlyph(cache, maskFormat);
     }
 
     int glyphSrcPadding() const override { return 1; }
@@ -885,8 +887,9 @@ public:
 
     const AtlasSubRun* testingOnly_atlasSubRun() const override { return this; }
 
-    void testingOnly_packedGlyphIDToGlyph(StrikeCache *cache) const override {
-        fGlyphs.packedGlyphIDToGlyph(cache);
+    void testingOnly_packedGlyphIDToGlyph(StrikeCache *cache,
+                                          skgpu::MaskFormat maskFormat) const override {
+        fGlyphs.packedGlyphIDToGlyph(cache, maskFormat);
     }
 
     int glyphSrcPadding() const override { return SK_DistanceFieldInset; }
