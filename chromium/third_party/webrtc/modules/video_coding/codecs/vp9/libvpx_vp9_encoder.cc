9668b054a2666eb9783db3903dd6a09f02806d9e
Erik Språng <sprang@webrtc.org>
2026-03-13 10:56:43 +0100

    [backport] CVE-2026-5292 (4/4)
    Reject frames with inconsistent U/V strides.
    
    All of openh264, libvpx and libaom seems to have issues with this. For
    now, rejecting frames with this property. This should be seen as a
    temporary workaround to be removed when the implementations have been
    updated to support it.
    
    Bug: chromium:492213293
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/459721
    Change-Id: Ia300578b4e78f0538aeda475f3ddb02e96b4baca
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/727015
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/third_party/webrtc/modules/video_coding/codecs/vp9/libvpx_vp9_encoder.cc
+++ chromium/third_party/webrtc/modules/video_coding/codecs/vp9/libvpx_vp9_encoder.cc
@@ -1181,6 +1181,13 @@ int LibvpxVp9Encoder::Encode(const VideoFrame& input_image,
           i010_buffer = i010_copy.get();
         }
       }
+
+      // TODO: crbug.com/492213293 - Remove once the root cause is fixed.
+      if (i010_buffer->StrideU() != i010_buffer->StrideV()) {
+        RTC_LOG(LS_ERROR) << "Libvpx requires the U and V strides to be equal.";
+        return WEBRTC_VIDEO_CODEC_ERROR;
+      }
+
       raw_->planes[VPX_PLANE_Y] = const_cast<uint8_t*>(
           reinterpret_cast<const uint8_t*>(i010_buffer->DataY()));
       raw_->planes[VPX_PLANE_U] = const_cast<uint8_t*>(
@@ -2075,6 +2082,13 @@ rtc::scoped_refptr<VideoFrameBuffer> LibvpxVp9Encoder::PrepareBufferForProfile0(
       MaybeRewrapRawWithFormat(VPX_IMG_FMT_I420);
       const I420BufferInterface* i420_buffer = mapped_buffer->GetI420();
       RTC_DCHECK(i420_buffer);
+
+      // TODO: crbug.com/492213293 - Remove once the root cause is fixed.
+      if (i420_buffer->StrideU() != i420_buffer->StrideV()) {
+        RTC_LOG(LS_ERROR) << "Libvpx requires the U and V strides to be equal.";
+        return {};
+      }
+
       raw_->planes[VPX_PLANE_Y] = const_cast<uint8_t*>(i420_buffer->DataY());
       raw_->planes[VPX_PLANE_U] = const_cast<uint8_t*>(i420_buffer->DataU());
       raw_->planes[VPX_PLANE_V] = const_cast<uint8_t*>(i420_buffer->DataV());
