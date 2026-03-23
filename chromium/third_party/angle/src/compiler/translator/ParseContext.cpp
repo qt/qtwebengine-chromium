368130b67f65374c0cc795e533364a6fbce2f3ad
Shahbaz Youssefi <syoussefi@chromium.org>
2026-03-23 13:38:34 -0400

    [Backport] CVE-2026-9121: Out of bounds read in GPU
    Cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/angle/angle/+/7694532:
    Translator: Fix codegen of switch with empty last case
    
    ... which is not actually allowed, except may be introduced to the AST
    by transformations that remove dead code.  The parser in this change
    makes sure the switch ends in some branch just in case.
    
    To pass the unit tests, PruneEmptyCases is improved to consider cases
    whose only code is "break" as empty.
    
    Bug: angleproject:488064108
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7694532
    Change-Id: I728ee897b82a9a15d4c778839480969f3f743ace
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/743432
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/third_party/angle/src/compiler/translator/ParseContext.cpp
+++ chromium/third_party/angle/src/compiler/translator/ParseContext.cpp
@@ -6687,6 +6687,22 @@ TIntermSwitch *TParseContext::addSwitch(TIntermTyped *init,
     }
 
     ASSERT(statementList);
+
+    const size_t statementCount = statementList->getChildCount();
+
+    // In case the last statement isn't already a branch, add |break| automatically.  Some AST
+    // transformations may dead-code-eliminate the contents of the last |case| and leave the switch
+    // statements ending in a case (which is what the check above forbids).  Most generators do not
+    // handle this unexpected situation.
+    //
+    // Note that a branch may be present inside a nested block, but that's ok, the extra branch
+    // added here gets eliminated in |PruneNoOps|.
+    if (statementCount > 0 &&
+        statementList->getChildNode(statementCount - 1)->getAsBranchNode() == nullptr)
+    {
+        statementList->appendStatement(new TIntermBranch(EOpBreak, nullptr));
+    }
+
     if (!ValidateSwitchStatementList(switchType, mDiagnostics, statementList, loc))
     {
         ASSERT(mDiagnostics->numErrors() > 0);
