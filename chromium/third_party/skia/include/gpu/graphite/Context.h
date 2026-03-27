74cd92b6a15ae2128c1cb55f90d9ecb3b6cbd758
Kalvin Lee <kdlee@chromium.org>
2026-03-27 16:37:30 +0900

    [Backport] CVE-2026-6304: Use after free in Graphite
    Manual cherry-pick of patch originally reviewed on
    https://skia-review.googlesource.com/c/skia/+/1196676:
    Terracotta-Phase-1: Reorder member destruction
    
    This is a speculative patch. Please see the bug for details.
    
    Bug: b/496393742
    Change-Id: Ib574a0086f92abda83715b36a0d1e7a99e9edd67
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1196676
    Reviewed-by: Michael Ludwig <michaelludwig@google.com>
    Commit-Queue: Michael Ludwig <michaelludwig@google.com>
    Reviewed-by: Thomas Smith <thomsmit@google.com>
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/730288
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/734127

--- chromium/third_party/skia/include/gpu/graphite/Context.h
+++ chromium/third_party/skia/include/gpu/graphite/Context.h
@@ -355,8 +355,8 @@ private:
 
     sk_sp<SharedContext> fSharedContext;
     std::unique_ptr<ResourceProvider> fResourceProvider;
-    std::unique_ptr<QueueManager> fQueueManager;
     std::unique_ptr<ClientMappedBufferManager> fMappedBufferManager;
+    std::unique_ptr<QueueManager> fQueueManager;
 
     // In debug builds we guard against improper thread handling. This guard is passed to the
     // ResourceCache for the Context.
