68e45e30bef522cc53a46bb8d849a80ae7308721
mikt <mikt@google.com>
2026-03-27 00:56:47 -0700

    [Backport] CVE-2026-9126: Use after free in DOM (4/5)
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/7702091:
    Use index-based for in Element::CloneAttributesFrom
    
    We should not use range-based on if the vector can be updated in the
    loop.
    
    Bug: 496280532
    Change-Id: I0ff0b8d2039c184d7b40f4024dacb518834bd09f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7702091
    Reviewed-by: Kouhei Ueno <kouhei@chromium.org>
    Commit-Queue: Stephen Nusko <nuskos@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1606009}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/738829
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/third_party/blink/renderer/core/dom/element.cc
+++ chromium/third_party/blink/renderer/core/dom/element.cc
@@ -10663,7 +10663,8 @@ void Element::CloneAttributesFrom(const Element& other) {
     element_data_ = other.element_data_->MakeUniqueCopy();
   }
 
-  for (const Attribute& attr : element_data_->Attributes()) {
+  for (wtf_size_t i = 0; i < element_data_->Attributes().size(); ++i) {
+    const Attribute& attr = element_data_->Attributes().at(i);
     AttributeChanged(
         AttributeModificationParams(attr.GetName(), g_null_atom, attr.Value(),
                                     AttributeModificationReason::kByCloning));
