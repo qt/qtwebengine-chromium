f114ff57f953a7a492c15655c1520591db8a42b6
Corentin Wallez <cwallez@chromium.org>
2026-03-27 14:57:24 -0700

    [backport] CVE-2026-5284 (1/2)
    Reland "[dawn][wire] Put most of the wire in a source_set."
    
    This is a reland of commit a703d864e29f6765f7dfcea0a2531afea02bee70
    
    Instead of special-casing GetProc from ApiProcs_autogen.cpp to be
    exported, make that whole file part of the exported API (by making it a
    .inc that's inlined in WireClient.cpp).
    
    Original change's description:
    > [dawn][wire] Put most of the wire in a source_set.
    >
    > And change dawn_unittests to use the static version of dawn_wire, such
    > that in a follow-up CL it can test using its internals directly instead
    > of having to only rely on the public interface.
    >
    > Bug: 492139412
    > Change-Id: I43a26073382545d7502b8765c78525ea040812c3
    > Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/297036
    > Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    > Reviewed-by: Loko Kung <lokokung@google.com>
    
    Bug: 492139412
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/299995
    Change-Id: I470853434aa3ad2c4517eb322c9cd79c8915bb29
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/726506
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/third_party/dawn/generator/dawn_json_generator.py
+++ chromium/third_party/dawn/generator/dawn_json_generator.py
@@ -1516,7 +1516,7 @@ class MultiGeneratorFromDawnJSON(Generator):
                            wire_params))
             renders.append(
                 FileRender('dawn/wire/client/ApiProcs.cpp',
-                           'src/dawn/wire/client/ApiProcs_autogen.cpp',
+                           'src/dawn/wire/client/ApiProcs_autogen.cpp.inc',
                            wire_params))
             renders.append(
                 FileRender('dawn/wire/client/ClientBase.h',
