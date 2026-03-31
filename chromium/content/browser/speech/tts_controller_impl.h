c950a67309625f73306e69ff2a42ac1ff1cf5f2f
Evan Liu <evliu@google.com>
2026-03-31 12:39:40 -0700

    [Backport] CVE-2026-7935: Inappropriate implementation in Speech
    Cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/7705458:
    Fix Web Speech API audio persisting across page navigations
    
    Currently, speechSynthesis audio can continue playing after a page
    unloads or redirects to another origin. This CL fixes the issue by
    canceling the active TtsUtterance in the browser process when the
    WebContents' primary page changes.
    
    Fixed: 489624550
    Change-Id: I385ffc9856c572576a27e3b95be807b66bc3f8a5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7705458
    Reviewed-by: Katie D <katie@chromium.org>
    Commit-Queue: Evan Liu <evliu@google.com>
    Cr-Commit-Position: refs/heads/main@{#1608024}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/735008
    Reviewed-by: Moss Heim <moss.heim@qt.io>

--- chromium/content/browser/speech/tts_controller_impl.h
+++ chromium/content/browser/speech/tts_controller_impl.h
@@ -196,6 +196,7 @@ class CONTENT_EXPORT TtsControllerImpl
 
   // WebContentsObserver methods
   void WebContentsDestroyed() override;
+  void PrimaryPageChanged(Page& page) override;
   void OnVisibilityChanged(Visibility visibility) override;
 
   // net::NetworkChangeNotifier::NetworkChangeObserver
