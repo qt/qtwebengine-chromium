7473154e62d4e89541f2ed62f30754724db0dd67
Jonathan Ross <jonross@chromium.org>
2026-04-02 06:52:15 -0700

    [Backport] CVE-2026-8010: Insufficient validation of untrusted input in SiteIsolation (1/2)
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/7711382:
    FrameWidgetHost MojoJS Bindings
    
    MojoJS cannot use Associated Mojo bindings. Due to this
    RenderWidgetHostImpl::BindFrameWidgetInterfaces cannot be used for
    testing.
    
    Here we introduce a parallel binding point for mojo testing.
    
    Allows for JS testing with connection established by:
    ```
    const host = new FrameWidgetHostRemote();
    host.$.bindNewPipeAndPassReceiver().bindInBrowser();
    ```
    
    Bug: 496624084
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7711382
    Change-Id: Ic27b4af5aa3bf08ff2e21ae6308467f2700b5f35
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/742523
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/content/browser/browser_interface_binders.cc
+++ chromium/content/browser/browser_interface_binders.cc
@@ -1203,6 +1203,21 @@ void PopulateBinderMapWithContext(
       &EmptyBinderForFrame<blink::mojom::CredentialManager>));
   map->Add<blink::mojom::LCPCriticalPathPredictorHost>(base::BindRepeating(
       &EmptyBinderForFrame<blink::mojom::LCPCriticalPathPredictorHost>));
+
+  // Currently defined in content/shell/common/shell_switches.h which we cannot
+  // have a DEPS on.
+  constexpr char kExposeInternalsForTesting[] = "expose-internals-for-testing";
+  if (base::CommandLine::ForCurrentProcess()->HasSwitch(
+          kExposeInternalsForTesting)) {
+    map->Add<blink::mojom::FrameWidgetHost>(base::BindRepeating(
+        [](RenderFrameHost* host,
+           mojo::PendingReceiver<blink::mojom::FrameWidgetHost> receiver) {
+          static_cast<RenderFrameHostImpl*>(host)
+              ->GetRenderWidgetHost()
+              ->BindFrameWidgetHostReceiver(std::move(receiver));
+        }));
+  }
+
   if (base::FeatureList::IsEnabled(blink::features::kBrowsingTopics) &&
       base::FeatureList::IsEnabled(
           blink::features::kBrowsingTopicsDocumentAPI)) {
