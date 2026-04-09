ec9ca9e8593ca3593c1a4516f23be54906fef412
Jonathan Ross <jonross@chromium.org>
2026-04-09 06:52:47 -0700

    [Backport] CVE-2026-8010: Insufficient validation of untrusted input in SiteIsolation (2/2)
    Cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/7726682:
    Update RenderWidgetHost Zoom Bounds
    
    Add bounds validation to
    RenderWidgetHostImpl::ZoomToFindInPageRectInMainFrame and
    AnimateDoubleTapZoom to ensure the requested zoom rectangle is intersect
    the sender frame's bounds.
    
    It is possible that the target is an html element whose bounds exceeds
    that of its containing frame. It would be clipped/scrollable for users.
    So we do not restrict this solely to a contains check
    
    TEST=RenderWidgetHostTest.*Zoom*Bounds
    
    Bug: 496624084
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7726682
    Change-Id: I0b33105bd45267e8769ada90840e44486c42a1df
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/742524
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/content/browser/renderer_host/render_widget_host_impl.cc
+++ chromium/content/browser/renderer_host/render_widget_host_impl.cc
@@ -137,6 +137,7 @@
 #include "ui/events/event.h"
 #include "ui/events/keycodes/dom/dom_code.h"
 #include "ui/gfx/color_space.h"
+#include "ui/gfx/geometry/rect.h"
 #include "ui/gfx/geometry/size_conversions.h"
 #include "ui/gfx/geometry/vector2d_conversions.h"
 #include "ui/gfx/geometry/vector2d_f.h"
@@ -3845,6 +3846,13 @@ void RenderWidgetHostImpl::AnimateDoubleTapZoomInMainFrame(
     return;
   }
 
+  gfx::Rect view_local_bounds(view_->GetViewBounds().size());
+  if (!view_local_bounds.IsEmpty() &&
+      (!view_local_bounds.Contains(point) ||
+       !view_local_bounds.Intersects(rect_to_zoom))) {
+    return;
+  }
+
   auto* root_view = view_->GetRootView();
   gfx::Point transformed_point(point);
   gfx::Rect transformed_rect_to_zoom(rect_to_zoom);
@@ -3854,13 +3862,23 @@ void RenderWidgetHostImpl::AnimateDoubleTapZoomInMainFrame(
     return;
   }
 
-  auto* root_rvhi = RenderViewHostImpl::From(root_view->GetRenderWidgetHost());
-  root_rvhi->AnimateDoubleTapZoom(transformed_point, transformed_rect_to_zoom);
+  auto* root_rwhi =
+      RenderWidgetHostImpl::From(root_view->GetRenderWidgetHost());
+  if (root_rwhi && root_rwhi->owner_delegate()) {
+    root_rwhi->owner_delegate()->AnimateDoubleTapZoom(transformed_point,
+                                                      transformed_rect_to_zoom);
+  }
 }
 
 void RenderWidgetHostImpl::ZoomToFindInPageRectInMainFrame(
     const gfx::Rect& rect_to_zoom) {
   if (!view_) {
+    return;
+  }
+
+  gfx::Rect view_local_bounds(view_->GetViewBounds().size());
+  if (!view_local_bounds.IsEmpty() &&
+      !view_local_bounds.Intersects(rect_to_zoom)) {
     return;
   }
 
@@ -3871,8 +3889,11 @@ void RenderWidgetHostImpl::ZoomToFindInPageRectInMainFrame(
     return;
   }
 
-  auto* root_rvhi = RenderViewHostImpl::From(root_view->GetRenderWidgetHost());
-  root_rvhi->ZoomToFindInPageRect(transformed_rect_to_zoom);
+  auto* root_rwhi =
+      RenderWidgetHostImpl::From(root_view->GetRenderWidgetHost());
+  if (root_rwhi && root_rwhi->owner_delegate()) {
+    root_rwhi->owner_delegate()->ZoomToFindInPageRect(transformed_rect_to_zoom);
+  }
 }
 
 void RenderWidgetHostImpl::SetHasTouchEventConsumers(
