1ab92656b675b08580884d9895c4d02dfadd1e55
Jonathan Ross <jonross@chromium.org>
2026-04-17 19:27:49 +0000

    [Backport] CVE-2026-7360: Insufficient validation of untrusted input in Compositing (2/2)
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/7763688:
    viz: Add hierarchy validation to HitTestRegionList
    
    Enforce HitTestManager rejection when invalid flags are received. Viz
    will now disconnect the client at SubmitCompositorFrame.
    
    Bug: 495852034
    Change-Id: I7e04f54966514a723502a994a682afb20befeb5f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7763688
    Commit-Queue: Jonathan Ross <jonross@chromium.org>
    Reviewed-by: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1617837}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/732871
    Reviewed-by: Moss Heim <moss.heim@qt.io>

--- chromium/components/viz/service/frame_sinks/compositor_frame_sink_support.cc
+++ chromium/components/viz/service/frame_sinks/compositor_frame_sink_support.cc
@@ -11,6 +11,8 @@
 
 #include "base/check.h"
 #include "base/containers/contains.h"
+#include "base/features.h"
+#include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/functional/callback_helpers.h"
 #include "base/memory/raw_ptr.h"
@@ -57,6 +59,12 @@ bool HasElapsedCadenceInterval(
 }
 
 namespace viz {
+
+// TODO (crbug.com/495852034): Remove once M150 hits Stable.
+BASE_FEATURE(kDisconnectOnInvalidHitTestRegionList,
+             "DisconnectOnInvalidHitTestRegionList",
+             base::FEATURE_ENABLED_BY_DEFAULT);
+
 namespace {
 
 bool RecordShouldSendBeginFrame(const std::string& reason, bool should_send) {
@@ -920,8 +928,13 @@ SubmitResult CompositorFrameSinkSupport::MaybeSubmitCompositorFrame(
 
   // QueueFrame can fail in unit tests, so SubmitHitTestRegionList has to be
   // called before that.
-  frame_sink_manager()->SubmitHitTestRegionList(
-      last_created_surface_id_, frame_index, std::move(hit_test_region_list));
+  if (!frame_sink_manager()->SubmitHitTestRegionList(
+          last_created_surface_id_, frame_index,
+          std::move(hit_test_region_list))) {
+    if (base::FeatureList::IsEnabled(kDisconnectOnInvalidHitTestRegionList)) {
+      return SubmitResult::HIT_TEST_DATA_INVALID;
+    }
+  }
 
   Surface::QueueFrameResult result = current_surface->QueueFrame(
       std::move(frame), frame_index, std::move(frame_rejected_callback));
@@ -1360,6 +1373,8 @@ const char* CompositorFrameSinkSupport::GetSubmitResultAsString(
       return "LocalSurfaceId sequence numbers decreased";
     case SubmitResult::SURFACE_OWNED_BY_ANOTHER_CLIENT:
       return "Surface belongs to another client";
+    case SubmitResult::HIT_TEST_DATA_INVALID:
+      return "Invalid hit-test data";
   }
   NOTREACHED();
 }
