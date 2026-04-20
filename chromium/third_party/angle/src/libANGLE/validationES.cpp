b0ce1c2379ee659dcf64665c17411faa287da3a5
Geoff Lang <geofflang@chromium.org>
2026-04-20 17:01:04 -0400

    [Backport] CVE-2026-9925: Use after free in ANGLE
    Manual backport of patch originally reviewed on
    https://chromium-review.googlesource.com/c/angle/angle/+/7779747:
    Validate that no VAO buffers are mapped in WebGL.
    
    This validation was skipped for WebGL contexts because it's not possible
    to map buffers from the WebGL JS API. Mapping is still exposed from
    ANGLE however and buffer mapping is sometimes used to implement other
    features on top of WebGL.
    
    Fixed: chromium:500536458
    Change-Id: I214ca71d1b14d150c0fbd68624319ae654f2984e
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7779747
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/742571
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/third_party/angle/src/libANGLE/validationES.cpp
+++ chromium/third_party/angle/src/libANGLE/validationES.cpp
@@ -4156,13 +4156,10 @@ const char *ValidateDrawStates(const Context *context, GLenum *outErrorCode)
     const Extensions &extensions = context->getExtensions();
     const State &state           = context->getState();
 
-    // WebGL buffers cannot be mapped/unmapped because the MapBufferRange, FlushMappedBufferRange,
-    // and UnmapBuffer entry points are removed from the WebGL 2.0 API.
-    // https://www.khronos.org/registry/webgl/specs/latest/2.0/#5.14
     VertexArray *vertexArray = state.getVertexArray();
     ASSERT(vertexArray);
 
-    if (!extensions.webglCompatibilityANGLE && vertexArray->hasInvalidMappedArrayBuffer())
+    if (vertexArray->hasInvalidMappedArrayBuffer())
     {
         return kBufferMapped;
     }
