7ff19e793f88fc3da49719f11746ab14cb9313bc
Shahbaz Youssefi <syoussefi@chromium.org>
2026-04-23 09:38:04 -0400

    [Backport] CVE-2026-9935: Uninitialized Use in ANGLE
    Cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/angle/angle/+/7791058:
    Initialize textures during syncState
    
    Instead of marking the textures as initialized on syncState, actually
    initialize them.
    
    In most cases, this makes DIRTY_OBJECT_TEXTURES_INIT obsolete because
    textures needing initialization also get synced.  However, this is not
    true for some corner cases, such as glTexImage2D with nullptr resetting
    a texture that is already synced.
    
    Bug: chromium:501584689
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7791058
    Change-Id: I88ab23f91fce6f3077a25303a9f352317f07bbe9
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/746501
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/third_party/angle/src/libANGLE/Framebuffer.cpp
+++ chromium/third_party/angle/src/libANGLE/Framebuffer.cpp
@@ -2783,7 +2783,6 @@ bool Framebuffer::partialBufferClearNeedsInit(const Context *context,
     switch (bufferType)
     {
         case GL_COLOR:
-            ASSERT(drawBuffers.any());
             return partialClearNeedsInit(context, drawBuffers, false, false);
         case GL_DEPTH:
             return partialClearNeedsInit(context, {}, true, false);
