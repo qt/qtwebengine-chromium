99b58a028c0980367a17cae1da40975ddb73cf4d
Jordan Bayles <jophba@chromium.org>
2026-04-24 12:02:55 -0700

    [Backport] CVE-2026-10908: Use after free in FullScreen
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/7787660:
    Fix Use-After-Free in RenderFrameHostImpl::ExitFullscreen()
    
    The call to delegate_->ExitFullscreenMode() can synchronously destroy
    the RenderFrameHostImpl object (e.g., if the tab is closed).
    Subsequent calls in ExitFullscreen() then operate on the deleted object,
    causing a Use-After-Free.
    
    This CL adds a base::WeakPtr guard to detect if the object has been
    destroyed and returns early if so. This is a variant of the fix for
    EnterFullscreen in commit 91d5baaef742.
    
    Bug: 505045913
    Change-Id: Id2b34bea77bb80a3b6c11372cc8813a812e34a2e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7787660
    Reviewed-by: Alex Moshchuk <alexmos@chromium.org>
    Reviewed-by: Charlie Reis <creis@chromium.org>
    Commit-Queue: Jordan Bayles <jophba@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1620376}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/750963
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/content/browser/renderer_host/render_frame_host_impl.cc
+++ chromium/content/browser/renderer_host/render_frame_host_impl.cc
@@ -9020,7 +9020,13 @@ void RenderFrameHostImpl::EnterFullscreen(
 // process, use it to double-check that fullscreen can be entered here.
 void RenderFrameHostImpl::ExitFullscreen() {
   base::RecordAction(base::UserMetricsAction("ExitFullscreen_API"));
+
+  base::WeakPtr<RenderFrameHostImpl> weak_ptr = GetWeakPtr();
   delegate_->ExitFullscreenMode(/*will_cause_resize=*/true);
+
+  if (!weak_ptr) {
+    return;
+  }
 
   // The previous call might change the fullscreen state. We need to make sure
   // the renderer is aware of that, which is done via the resize message.
