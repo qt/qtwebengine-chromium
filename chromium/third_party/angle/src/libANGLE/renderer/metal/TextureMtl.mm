d156adcaa1244b901314b304ce87ee9133bce77d
Geoff Lang <geofflang@chromium.org>
2026-05-01 13:20:55 -0400

    [backport] CVE-2026-14385: Heap buffer overflow in ANGLE
    Metal: Treat glGenerateMipmap as an image redefinition.
    
    When TextureMtl::generateMipmap creates a native storage, it doesn't
    treat it as a texture redefinition and clear out old images. This leaves
    images with formats and sizes that do not match the storage.
    
    Fixed: chromium:499006005
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7806315
    Task-number: CVE-2026-14385
    Change-Id: I78cdbf77ccb75469fba3ca4654ea9118aa80edd5
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/753820
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/third_party/angle/src/libANGLE/renderer/metal/TextureMtl.mm
+++ chromium/third_party/angle/src/libANGLE/renderer/metal/TextureMtl.mm
@@ -956,10 +956,30 @@ void TextureMtl::deallocateNativeStorage(bool keepImages, bool keepSamplerStateA
     }
 }
 
-angle::Result TextureMtl::ensureNativeStorageCreated(const gl::Context *context)
+angle::Result TextureMtl::ensureNativeStorageCreated(const gl::Context *context, bool keepImages)
 {
+    auto clearImagesAssociatedWithStorage = [&]() {
+        ASSERT(mNativeTextureStorage);
+        GLuint mips      = mState.getMipmapMaxLevel() - mState.getEffectiveBaseLevel() + 1;
+        int numCubeFaces = static_cast<int>(mNativeTextureStorage->cubeFaces());
+        for (int face = 0; face < numCubeFaces; ++face)
+        {
+            for (mtl::MipmapNativeLevel actualMip = mtl::kZeroNativeMipLevel;
+                 actualMip.get() < mips; ++actualMip)
+            {
+                GLuint imageMipLevel = mNativeTextureStorage->getGLLevel(actualMip);
+                mTexImageDefs[face][imageMipLevel].image = nullptr;
+            }
+        }
+    };
+
     if (mNativeTextureStorage)
     {
+        // Storage exists, deallocate images associated with the storage.
+        if (!keepImages)
+        {
+            clearImagesAssociatedWithStorage();
+        }
         return angle::Result::Continue;
     }
 
@@ -1010,6 +1030,11 @@ angle::Result TextureMtl::ensureNativeStorageCreated(const gl::Context *context)
                 imageToTransfer = nullptr;
             }
         }
+    }
+
+    if (!keepImages)
+    {
+        clearImagesAssociatedWithStorage();
     }
 
     return angle::Result::Continue;
@@ -1686,7 +1711,7 @@ angle::Result TextureMtl::setImageExternal(const gl::Context *context,
 
 angle::Result TextureMtl::generateMipmap(const gl::Context *context)
 {
-    ANGLE_TRY(ensureNativeStorageCreated(context));
+    ANGLE_TRY(ensureNativeStorageCreated(context, false));
 
     ContextMtl *contextMtl = mtl::GetImpl(context);
     if (!mViewFromBaseToMaxLevel)
@@ -1864,7 +1889,7 @@ angle::Result TextureMtl::getAttachmentRenderTarget(const gl::Context *context,
                                                     GLsizei samples,
                                                     FramebufferAttachmentRenderTarget **rtOut)
 {
-    ANGLE_TRY(ensureNativeStorageCreated(context));
+    ANGLE_TRY(ensureNativeStorageCreated(context, true));
 
     ContextMtl *contextMtl = mtl::GetImpl(context);
     ANGLE_CHECK(contextMtl, mNativeTextureStorage, gl::err::kInternalError, GL_INVALID_OPERATION);
@@ -1925,7 +1950,7 @@ angle::Result TextureMtl::syncState(const gl::Context *context,
         }
     }
 
-    ANGLE_TRY(ensureNativeStorageCreated(context));
+    ANGLE_TRY(ensureNativeStorageCreated(context, true));
     ANGLE_TRY(ensureSamplerStateCreated(context));
 
     return angle::Result::Continue;
