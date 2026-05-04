a8273bcee58da6d0b83f9f3aaf79b1af3a7c04b5
Stephen Chenney <schenney@chromium.org>
2026-05-04 08:49:27 -0700

    [backport] CVE-2026-11182: Inappropriate implementation in SVG
    Improve tainting of SVG filters using colors
    
    SVG filters that use colors as input should taint if
    the color is `currentColor` because that may reference
    visited link colors. The existing code was not accounting
    for color functions that themselves used `currentColor`.
    Also verified that system colors used in SVG filters on
    canvas use the default system colors, not those from the
    user's theme.
    
    Bug: 502651014
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7808312
    Task-number: QTBUG-147437
    Change-Id: I5df1a8ef30013ef6f5e1ced97aa88c0cf6bc9d4d
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/749541
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/third_party/blink/renderer/core/svg/svg_fe_diffuse_lighting_element.cc
+++ chromium/third_party/blink/renderer/core/svg/svg_fe_diffuse_lighting_element.cc
@@ -151,7 +151,7 @@ bool SVGFEDiffuseLightingElement::TaintsOrigin() const {
   // TaintsOrigin() is only called after a successful call to Build()
   // (see above), so we should have a ComputedStyle here.
   DCHECK(style);
-  return style->LightingColor().IsCurrentColor();
+  return style->LightingColor().DependsOnCurrentColor();
 }
 
 SVGAnimatedPropertyBase* SVGFEDiffuseLightingElement::PropertyFromAttribute(
