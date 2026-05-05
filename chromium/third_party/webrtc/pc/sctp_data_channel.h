2b9ac4d69db4e37a202eb7bf1da4d04994d3ecfa
Danil Chapovalov <danilchap@webrtc.org>
2026-05-05 17:44:03 +0200

    [Backport] CVE-2026-9962: Use after free in WebRTC
    Manual cherry-pick of patch originally reviewed on
    https://webrtc-review.googlesource.com/c/src/+/469980:
    In SctpDataChannel use plain bool as safety flag.
    
    SctpDataChannel can be destroyed on various threads and thus safety flag
    couldn't be properly invalidated in the destructor. Instead of relying
    on it when posting async send, rely on reference counting of the full
    object
    
    Bug: chromium:504716948
    Change-Id: I1246fecac26d0761be006b069658d072183546bf
    Reviewed-on: https://webrtc-review.googlesource.com/c/src/+/469980
    Reviewed-by: Tomas Gunnarsson <tommi@webrtc.org>
    Commit-Queue: Danil Chapovalov <danilchap@webrtc.org>
    Cr-Commit-Position: refs/heads/main@{#47635}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/743306
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/third_party/webrtc/pc/sctp_data_channel.h
+++ chromium/third_party/webrtc/pc/sctp_data_channel.h
@@ -264,7 +264,7 @@ class SctpDataChannel : public DataChannelInterface {
       RTC_RUN_ON(network_thread_);
 
   bool connected_to_transport() const RTC_RUN_ON(network_thread_) {
-    return network_safety_->alive();
+    return connected_to_transport_;
   }
   void MaybeSendOnBufferedAmountChanged() RTC_RUN_ON(network_thread_);
 
@@ -296,9 +296,8 @@ class SctpDataChannel : public DataChannelInterface {
       kHandshakeInit;
   // Did we already start the graceful SCTP closing procedure?
   bool started_closing_procedure_ RTC_GUARDED_BY(network_thread_) = false;
+  bool connected_to_transport_ RTC_GUARDED_BY(network_thread_) = false;
   PacketQueue queued_received_data_ RTC_GUARDED_BY(network_thread_);
-  rtc::scoped_refptr<PendingTaskSafetyFlag> network_safety_ =
-      PendingTaskSafetyFlag::CreateDetachedInactive();
 };
 
 }  // namespace webrtc
