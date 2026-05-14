a9ebaf26d0662239210fc95963f7ccc2613b96e4
Brandon Jones <bajones@chromium.org>
2026-05-14 15:03:56 -0700

    [Backport] CVE-2026-10973: Uninitialized Use in Dawn
    Cherry-pick of patch originally reviewed on
    https://dawn-review.googlesource.com/c/dawn/+/308837:
    ZeroInit workaround on all Mali, not just Android
    
    Forces the VulkanUseZeroInitializeWorkgroupMemoryExtension
    workaround to be enabled on any Mali (ARM) GPU, rather than just
    Android Mali devices.
    
    Bug: 513042859
    Fixes: 513042859
    Change-Id: I1cc3416db50ecb89e9ac1574be3368f43df15b3a
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/308837
    Auto-Submit: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Loko Kung <lokokung@google.com>
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/748329
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/third_party/dawn/src/dawn/native/vulkan/PhysicalDeviceVk.cpp
+++ chromium/third_party/dawn/src/dawn/native/vulkan/PhysicalDeviceVk.cpp
@@ -1000,7 +1000,7 @@ void PhysicalDevice::SetupBackendDeviceToggles(dawn::platform::Platform* platfor
     if (!GetDeviceInfo().HasExt(DeviceExt::ZeroInitializeWorkgroupMemory) ||
         GetDeviceInfo().zeroInitializeWorkgroupMemoryFeatures.shaderZeroInitializeWorkgroupMemory ==
             VK_FALSE ||
-        IsAndroidARM() || gpu_info::IsImgTec(GetVendorId())) {
+        gpu_info::IsARM(GetVendorId()) || gpu_info::IsImgTec(GetVendorId())) {
         deviceToggles->ForceSet(Toggle::VulkanUseZeroInitializeWorkgroupMemoryExtension, false);
     }
     // By default try to initialize workgroup memory with OpConstantNull according to the Vulkan
