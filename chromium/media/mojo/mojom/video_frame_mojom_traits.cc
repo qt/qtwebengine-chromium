39e51a7e14b64162b0f0798ef7177c7f10740189
Hirokazu Honda <hiroh@chromium.org>
2026-05-18 13:25:24 -0700

    [Backport] CVE-2026-10911: Insufficient validation of untrusted input in Media (1/2)
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/7834743:
    media: Validate mappable SharedImage in video frame mojo traits
    
    Add handle verification to VideoFrame Mojo traits for mappable
    SharedImages on Linux and ChromeOS and promote GpuMemoryBufferHandle
    verification in CreateGpuMemoryBufferHandle() from a debug-only check
    to a production check.
    
    Bug: 495819067
    Test: webrtc.RTCPeerConnection.*
    Change-Id: I56b9761b67a4e1ec8bbe6813c9bc920477a70531
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7834743
    Commit-Queue: Hirokazu Honda <hiroh@chromium.org>
    Reviewed-by: Ted (Chromium) Meyer <tmathmeyer@chromium.org>
    Auto-Submit: Hirokazu Honda <hiroh@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1632369}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/750968
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/media/mojo/mojom/video_frame_mojom_traits.cc
+++ chromium/media/mojo/mojom/video_frame_mojom_traits.cc
@@ -411,6 +411,23 @@ bool StructTraits<media::mojom::VideoFrameDataView,
 
     bool is_mappable_si_enabled = shared_image_data.is_mappable_si_enabled();
     if (is_mappable_si_enabled) {
+#if BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_CHROMEOS)
+      // TODO(crbug.com/513289253): Avoid CloneGpuMemoryBufferHandle() only for
+      // validation.
+      const auto video_pixel_format =
+          media::SharedImageFormatToVideoPixelFormat(shared_image->format());
+      if (video_pixel_format) {
+        auto gmb_handle = shared_image->CloneGpuMemoryBufferHandle();
+        if (!gmb_handle.is_null() && gmb_handle.type == gfx::NATIVE_PIXMAP) {
+          if (!media::VerifyGpuMemoryBufferHandle(*video_pixel_format,
+                                                  coded_size, gmb_handle)) {
+            LOG(ERROR)
+                << "Invalid GpuMemoryBufferHandle for mappable SharedImage";
+            return false;
+          }
+        }
+      }
+#endif
       // VideoFrame should have buffer usage if Mappable SharedImage is enabled.
       // NOTE: This isn't exactly correct for software SharedImages can be
       // mappable but do not have buffer usage. But since, such software
