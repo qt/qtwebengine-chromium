07f9efbacbe59d53f5953cf431d7c020a7a647f6
Corentin Wallez <cwallez@chromium.org>
2026-05-20 02:42:52 -0700

    [Backport] CVE-2026-10983: Insufficient validation of untrusted input in Dawn
    Cherry-pick of patch originally reviewed on
    https://dawn-review.googlesource.com/c/dawn/+/309395:
    Dirty the validation aspect on SetIndexBuffer
    
    When the CommandBufferStateTracker was introduced, there was no format
    parameter to SetIndexBuffer so the validation aspect was always valid
    after the first SetIndexBuffer call. However with the introduction of
    the IndexFormat, we now need to dirty the aspect so that on the next
    draw, the IndexFormat is checked for compatibility between the pipeline
    and SetIndexBuffer.
    
    Fixed: 513947609
    Change-Id: I8c23968b44b5d090af5c8a717d4504718dfabe2e
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/309395
    Reviewed-by: Brandon Jones <bajones@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/748308
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/third_party/dawn/src/dawn/native/CommandBufferStateTracker.cpp
+++ chromium/third_party/dawn/src/dawn/native/CommandBufferStateTracker.cpp
@@ -771,6 +771,7 @@ void CommandBufferStateTracker::SetIndexBuffer(BufferBase* buffer,
     mIndexFormat = format;
     mIndexBufferSize = size;
     mIndexBufferOffset = offset;
+    mAspects.reset(VALIDATION_ASPECT_INDEX_BUFFER);
 }
 
 void CommandBufferStateTracker::UnsetVertexBuffer(VertexBufferSlot slot) {
