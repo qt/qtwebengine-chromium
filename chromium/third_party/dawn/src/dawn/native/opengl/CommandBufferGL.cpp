e7333d1262a71c4a2cefd18014a6fa8c9bee4738
Stephen White <senorblanco@chromium.org>
2026-05-21 10:10:38 -0700

    [Backport] CVE-2026-9889: Out of bounds read and write in Dawn
    Cherry-pick of patch originally reviewed on
    https://dawn-review.googlesource.com/c/dawn/+/309876:
    GL: fix issue with ShaderStage::None bindings.
    
    If more than 96 bindings with visibility ShaderStage::None are used,
    they will exceed kGLMaxShaderStorageBufferBindingsReported (aka
    GL_MAX_SHADER_STORAGE_BUFFER_BINDINGS) in the GL backend.
    Dawn validation will not catch this, since it skips such bindings.
    The fix is to skip all of the bindings with visibility ShaderStage::None
    when computing binding indices in pipeline layout construction and
    when applying bind groups in the GL backend.
    
    Bug: 511727159
    Change-Id: Ibc873a5afab4e73c12ad25b7d7cedcf48f35ca6e
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/309876
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Stephen White <senorblanco@chromium.org>
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/739512
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/third_party/dawn/src/dawn/native/opengl/CommandBufferGL.cpp
+++ chromium/third_party/dawn/src/dawn/native/opengl/CommandBufferGL.cpp
@@ -346,6 +346,9 @@ class BindGroupTracker : public BindGroupTrackerBase<false, uint64_t> {
 
         for (BindingIndex bindingIndex : Range(group->GetLayout()->GetBindingCount())) {
             const BindingInfo& bindingInfo = group->GetLayout()->GetBindingInfo(bindingIndex);
+            if (bindingInfo.visibility == wgpu::ShaderStage::None) {
+                continue;
+            }
             DAWN_TRY(MatchVariant(
                 bindingInfo.bindingLayout,
                 [&](const BufferBindingInfo& layout) -> MaybeError {
