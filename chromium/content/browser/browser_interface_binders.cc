fd649db54e853797de3cbd5cf2d28c27d9eb9ad4
Guido Urdaneta <guidou@chromium.org>
2026-05-21 07:20:20 -0700

    [Backport] CVE-2026-14039: Insufficient policy enforcement in GetUserMedia
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/7867635:
    Ensure setPreferredSink can only be called from top-level frames
    
    Fixed: 497358012
    Change-Id: Ic336d5eca007822a18179cbbf05646204c036df3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7867635
    Reviewed-by: Antonio Sartori <antoniosartori@chromium.org>
    Reviewed-by: Tony Herre <toprice@chromium.org>
    Commit-Queue: Guido Urdaneta <guidou@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1634252}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/756350
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/content/browser/browser_interface_binders.cc
+++ chromium/content/browser/browser_interface_binders.cc
@@ -988,10 +988,12 @@ void PopulateFrameBinders(RenderFrameHostImpl* host, mojo::BinderMap* map) {
         BrowserMainLoop::GetInstance()->media_stream_manager();
 
     map->Add<blink::mojom::MediaDevicesDispatcherHost>(
-        base::BindRepeating(&MediaDevicesDispatcherHost::Create,
-                            host->GetMainFrame()->GetGlobalFrameToken(),
-                            host->GetGlobalId(),
-                            base::Unretained(media_stream_manager)),
+        base::BindRepeating(
+            &MediaDevicesDispatcherHost::Create,
+            host->GetMainFrame()->GetGlobalFrameToken(), host->GetGlobalId(),
+            base::Unretained(media_stream_manager),
+            /*is_outermost_main_frame=*/host->GetParentOrOuterDocument() ==
+                nullptr),
         GetIOThreadTaskRunner({}));
 
     map->Add<blink::mojom::MediaStreamDispatcherHost>(
--- chromium/content/browser/renderer_host/media/media_devices_dispatcher_host.h
+++ chromium/content/browser/renderer_host/media/media_devices_dispatcher_host.h
@@ -37,7 +37,8 @@ class CONTENT_EXPORT MediaDevicesDispatcherHost
   MediaDevicesDispatcherHost(
       const GlobalRenderFrameHostToken& main_frame_host_token,
       GlobalRenderFrameHostId render_frame_host_id,
-      MediaStreamManager* media_stream_manager);
+      MediaStreamManager* media_stream_manager,
+      bool is_outermost_main_frame);
 
   MediaDevicesDispatcherHost(const MediaDevicesDispatcherHost&) = delete;
   MediaDevicesDispatcherHost& operator=(const MediaDevicesDispatcherHost&) =
@@ -49,6 +50,7 @@ class CONTENT_EXPORT MediaDevicesDispatcherHost
       const GlobalRenderFrameHostToken& main_frame_host_token,
       GlobalRenderFrameHostId render_frame_host_id,
       MediaStreamManager* media_stream_manager,
+      bool is_outermost_main_frame,
       mojo::PendingReceiver<blink::mojom::MediaDevicesDispatcherHost> receiver);
 
   // blink::mojom::MediaDevicesDispatcherHost implementation.
@@ -202,6 +204,7 @@ class CONTENT_EXPORT MediaDevicesDispatcherHost
 
   // The following const fields can be accessed on any thread.
   const GlobalRenderFrameHostId render_frame_host_id_;
+  const bool is_outermost_main_frame_;
 
   // The following fields can only be accessed on the IO thread.
   const raw_ptr<MediaStreamManager> media_stream_manager_;
