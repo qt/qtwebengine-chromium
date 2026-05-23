7b1e5c26c7e46f366b98b55c723dfb12eebce41b
Ken Russell <kbr@chromium.org>
2026-05-22 18:10:16 -0700

    [backport] CVE-2026-17675: Out of bounds write in ANGLE
    PLS: Reject redefinition of attachments while active.
    
    "glRenderbufferStorage*", "glTexStorage*", "glTexImage*", and
    "glGenerateMipmap" can change the size of the current framebuffer's
    attachments while PLS is active, even if they aren't bound as PLS
    planes, which can cause problems with the active PLS planes since they
    won't be resized. Generate GL_INVALID_OPERATION in these cases.
    
    Update the PLS spec defining this behavior. Add a unit test verifying
    the behavior of all of these API calls while PLS is active, including
    when the objects aren't bound to the current framebuffer.
    
    Co-authored with jetski-cli.
    
    Fixed: chromium:513920258
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7871931
    Task-number: QTBUG-148656
    Change-Id: I8c52c3983a2514e333740a463a865c73c766ab8d
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/761781
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/third_party/angle/src/libANGLE/validationES3.cpp
+++ chromium/third_party/angle/src/libANGLE/validationES3.cpp
@@ -588,10 +588,20 @@ bool ValidateES3TexImageParametersBase(const Context *context,
         return false;
     }
 
-    if (context->getState().isTextureBoundToActivePLS(texture->id()))
+    if (isSubImage)
     {
-        ANGLE_VALIDATION_ERROR(GL_INVALID_OPERATION, kActivePLSBackingTexture);
-        return false;
+        if (context->getState().isTextureBoundToActivePLS(texture->id()))
+        {
+            ANGLE_VALIDATION_ERROR(GL_INVALID_OPERATION, kActivePLSBackingTexture);
+            return false;
+        }
+    }
+    else
+    {
+        if (!ValidateNoActivePLSConflict(context, entryPoint, texture->id()))
+        {
+            return false;
+        }
     }
 
     if (texture->getImmutableFormat() && !isSubImage)
@@ -1428,6 +1438,11 @@ bool ValidateES3TexStorageParametersTexObject(const Context *context,
     if (texture->getImmutableFormat())
     {
         ANGLE_VALIDATION_ERROR(GL_INVALID_OPERATION, kTextureIsImmutable);
+        return false;
+    }
+
+    if (!ValidateNoActivePLSConflict(context, entryPoint, texture->id()))
+    {
         return false;
     }
 
