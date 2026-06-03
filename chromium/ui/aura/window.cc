d4529fbe65905dc342ce82aade4d177226680119
Mitsuru Oshima <oshima@chromium.org>
2026-06-02 17:37:52 -0700

    [backport] Dependency for CVE-2026-79290 (2/2)
    Use ScopedDeleteBlocker in Window::NotifyWindowHierarchyChange
    
    The window should not be deleted in the observer / delegates called in
    NotifyWindowHierarchyChange.
    
    Bug: 516276225
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7874399
    Task-number: QTBUG-149641
    Change-Id: I27314500abd171f1ad8193b654ee6abef49914c6
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/766600
    Reviewed-by: Moss Heim <moss.heim@qt.io>

--- chromium/ui/aura/window.cc
+++ chromium/ui/aura/window.cc
@@ -1262,6 +1262,8 @@ void Window::NotifyAddedToRootWindow() {
 
 void Window::NotifyWindowHierarchyChange(
     const WindowObserver::HierarchyChangeParams& params) {
+  ScopedDeleteBlocker blocker(this);
+
   params.target->NotifyWindowHierarchyChangeDown(params);
   switch (params.phase) {
     case WindowObserver::HierarchyChangeParams::HIERARCHY_CHANGING:
