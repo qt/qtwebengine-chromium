c0d9dea9f6a7443872ec3acbf3eec4739ab03cbb
Michal Klocek <michal.klocek@qt.io>
2026-06-10 13:14:26 +0200

    [fixup][backport] CVE-2026-8559: Integer overflow in Internationalization
    Add missing header for numeric_limits.
    
    Task-number: QTBUG-147334
    Change-Id: If4c4cf1dcf96cd104fe0ff8d3a1ad2af7d803565
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/750686
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/third_party/blink/renderer/platform/text/bidi_paragraph.cc
+++ chromium/third_party/blink/renderer/platform/text/bidi_paragraph.cc
@@ -9,6 +9,8 @@
 #include "third_party/blink/renderer/platform/wtf/text/string_builder.h"
 #include "third_party/blink/renderer/platform/wtf/text/wtf_string.h"
 
+#include <limits>
+
 namespace blink {
 
 bool BidiParagraph::SetParagraph(const String& text,
