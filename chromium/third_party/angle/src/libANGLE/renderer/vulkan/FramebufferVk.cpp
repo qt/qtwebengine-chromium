34d75b77b1e1ae4139c58f95bbdf223814529d62
Shahbaz Youssefi <syoussefi@chromium.org>
2026-06-10 22:27:34 -0400

    [Backport] CVE-2026-17676: Inappropriate implementation in ANGLE
    Cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/angle/angle/+/7921634:
    Vulkan: Fix MSRTT when all attachments are disabled
    
    The render pass was considered single-sampled in that case when the
    render pass starts, which is incorrect.  This was caught by an ASSERT
    when resolve attachments were being added to this
    mistakenly-single-sampled attachment.
    
    Bug: chromium:513920298
    Change-Id: I0886bf0c5e075729188dc53de678effaf6fa352c
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7921634
    Reviewed-by: Charlie Lao <cclao@google.com>
    Commit-Queue: Shahbaz Youssefi <syoussefi@chromium.org>
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/756762
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/third_party/angle/src/libANGLE/renderer/vulkan/FramebufferVk.cpp
+++ chromium/third_party/angle/src/libANGLE/renderer/vulkan/FramebufferVk.cpp
@@ -2815,7 +2815,7 @@ void FramebufferVk::updateRenderPassDesc(ContextVk *contextVk)
     {
         // Update descriptions regarding multisampled-render-to-texture use.
         bool isRenderToTexture = false;
-        for (size_t colorIndexGL : mState.getEnabledDrawBuffers())
+        for (size_t colorIndexGL : mState.getColorAttachmentsMask())
         {
             const gl::FramebufferAttachment *color = mState.getColorAttachment(colorIndexGL);
             ASSERT(color);
@@ -3963,7 +3963,7 @@ GLint FramebufferVk::getSamplesImpl() const
 {
     const gl::FramebufferAttachment *lastAttachment = nullptr;
 
-    for (size_t colorIndexGL : mState.getEnabledDrawBuffers() & mState.getColorAttachmentsMask())
+    for (size_t colorIndexGL : mState.getColorAttachmentsMask())
     {
         const gl::FramebufferAttachment *color = mState.getColorAttachment(colorIndexGL);
         ASSERT(color);
