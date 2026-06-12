56a6413d3bb304f0498385ef9144a2dc3e490547
alexisdavidc <alexisdavidc@google.com>
2026-06-12 13:28:34 -0400

    [backport] CVE-2026-17712: Race in Skia
    Resolved a Data Race on fStream in SkTypeface_Mac
    
    There was a data race in SkTypeface_Mac where `onOpenStream`
    and `onOpenExistingStream` would race to read/write `fStream`. While `onOpenStream` would begin initializing `fStream` on one thread, a separate thread could be calling `onOpenExistingStream` to try to read `fStream` before it was done initializing.
    
    The issue was resolved by applying a mutex on `fStream`.
    
    The cl introducing this bug (https://skia-review.git.corp.google.com/c/skia/+/204720) was focused on caching the typefaces received with a global process wide `gTFCache` to save on performance and memory. The issue arose in that since the SkTypeface_Mac could be accessed across threads, it became thread unsafe.
    
    A test was added to this CL but removed as it was too large and took too long. It helps us keep it in the patch history for reference.
    
    Bug: b/520535595
    Bug: 520535595
    Fixed: 524508854
    Reviewed-on: https://skia-review.googlesource.com/c/skia/+/1264296
    Task-number: QTBUG-148658
    Change-Id: Id28aeed3d67a5a5246d22681f2c7ab0e6c133558
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/763148
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/third_party/skia/src/ports/SkTypeface_mac_ct.cpp
+++ chromium/third_party/skia/src/ports/SkTypeface_mac_ct.cpp
@@ -612,9 +612,9 @@ static SK_SFNT_ULONG get_font_type_tag(CTFontRef ctFont) {
 std::unique_ptr<SkStreamAsset> SkTypeface_Mac::onOpenStream(int* ttcIndex) const {
     *ttcIndex = 0;
 
-    fInitStream([this]{
+    SkAutoSharedMutexExclusive sm(fStreamMutex);
     if (fStream) {
-        return;
+        return fStream->duplicate();
     }
 
     SK_SFNT_ULONG fontType = get_font_type_tag(fFontRef.get());
@@ -712,12 +712,12 @@ std::unique_ptr<SkStreamAsset> SkTypeface_Mac::onOpenStream(int* ttcIndex) const
         ++entry;
     }
     fStream = std::make_unique<SkMemoryStream>(std::move(streamData));
-    });
     return fStream->duplicate();
 }
 
 std::unique_ptr<SkStreamAsset> SkTypeface_Mac::onOpenExistingStream(int* ttcIndex) const {
     *ttcIndex = 0;
+    SkAutoSharedMutexShared sm(fStreamMutex);
     return fStream ? fStream->duplicate() : nullptr;
 }
 
@@ -1229,7 +1229,7 @@ sk_sp<SkTypeface> SkTypeface_Mac::onMakeClone(const SkFontArguments& args) const
     if (!ctVariant) {
         return nullptr;
     }
-
+    SkAutoSharedMutexShared sm(fStreamMutex);
     return SkTypeface_Mac::Make(std::move(ctVariant), ctVariation.opsz,
                                 fStream ? fStream->duplicate() : nullptr);
 }
