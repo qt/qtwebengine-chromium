ed5aaf68c9341f9926c5d9b4e9bf2038931a642f
Moss Heim <moss.heim@qt.io>
2026-06-18 11:45:34 +0200

    [Backport] CVE-2026-11022: Insufficient validation of untrusted input in DevTools (2/2)
    Roll inspector_protocol to d025eb48e8febbe8b846b2675e031295c8ef605e
    
    Related to: https://chromium-review.googlesource.com/c/chromium/src/+/7853419
    
    https://chromium.googlesource.com/deps/inspector_protocol/+log/bdc11c1d..d025eb48e8
    
    $ git log bdc11c1d..d025eb48e8 --date=short --no-merges --format='%ad %ae %s'
    2026-05-18 alexrudenko@chromium.org Add HasKeyInMap
    2026-05-07 dsv@chromium.org Harden sessionId verification and CBOR parsing
    2026-04-23 dsv@chromium.org Add GetString8ValueFromMap utility for shallow parsing
    2025-12-17 victorvianna@google.com Roll src/third_party/mini_chromium/mini_chromium/ c4bcfd045..706fce5b1 (25 commits)
    2025-12-17 victorvianna@google.com Roll src/third_party/gtest/gtest from 16f637fbf4ff to 4fe3307fb2d9
    2025-12-17 victorvianna@google.com Format third_party/gtest/BUILD.gn
    2025-12-11 ellyjones@chromium.org erase(std::remove(...)) -> std::erase(...)
    2025-10-06 davidben@google.com Format some files
    2025-10-01 davidben@chromium.org Fix compilation of generated C++ with C++23
    2025-09-30 tikuta@google.com Make depfile deterministic
    2025-09-12 alexrudenko@chromium.org Introduce a template for protocol concatenation
    2025-08-15 caseq@chromium.org Remove bailout upon explicit timestamp check from code_generator.py
    2025-08-15 caseq@chromium.org Remove unused deps var
    2025-08-15 caseq@chromium.org Fix dependencies processing for PDL includes, take two
    2025-08-14 alexrudenko@chromium.org Fix a KeyError when accessing domain.source
    2025-08-13 caseq@chromium.org Produce deplist when generating protocol sources
    
    Task-number: QTBUG-147443
    Change-Id: Ibb44cb239a120e5d0e7e37a4296a8d51586d7017
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/746093
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/third_party/inspector_protocol/code_generator.py
+++ chromium/third_party/inspector_protocol/code_generator.py
@@ -75,12 +75,18 @@ def read_config():
       "--inspector_protocol_dir", type=unicode, required=True,
       help=("directory with code_generator.py and C++ encoding / binding "
           "libraries, relative to the root of the source tree."))
+    cmdline_parser.add_argument("--depfile", type=unicode, required=False)
+    cmdline_parser.add_argument("--stamp", type=unicode, required=False)
     arg_options = cmdline_parser.parse_args()
     jinja_dir = arg_options.jinja_dir
     output_base = arg_options.output_base
     config_file = arg_options.config
     config_base = os.path.dirname(config_file)
     config_values = arg_options.config_value
+    if bool(arg_options.depfile) != bool(arg_options.stamp):
+      raise Exception("--depfile requires --stamp and vice versa")
+    depfile = arg_options.depfile
+    stamp = arg_options.stamp
     inspector_protocol_dir = arg_options.inspector_protocol_dir.lstrip('/')
   except Exception:
     # Work with python 2 and 3 http://docs.python.org/py3k/howto/pyporting.html
@@ -95,34 +101,35 @@ def read_config():
                                     config_base)
     config_json_file.close()
     defaults = {
-      ".use_snake_file_names": False,
-      ".use_title_case_methods": False,
-      ".use_embedder_types": False,
-      ".imported": False,
-      ".imported.export_macro": "",
-      ".imported.export_header": False,
-      ".imported.header": False,
-      ".imported.package": False,
-      ".imported.options": False,
-      ".protocol.export_macro": "",
-      ".protocol.export_header": False,
-      ".protocol.options": False,
-      ".protocol.file_name_prefix": "",
-      ".exported": False,
-      ".exported.export_macro": "",
-      ".exported.export_header": False,
-      ".lib": False,
-      ".lib.export_macro": "",
-      ".lib.export_header": False,
-      ".crdtp": False,
-      ".crdtp.dir": os.path.join(inspector_protocol_dir, "crdtp"),
-      ".crdtp.namespace": "crdtp",
+        ".use_snake_file_names": False,
+        ".use_title_case_methods": False,
+        ".use_embedder_types": False,
+        ".imported": False,
+        ".imported.export_macro": "",
+        ".imported.export_header": False,
+        ".imported.header": False,
+        ".imported.package": False,
+        ".imported.options": False,
+        ".protocol.export_macro": "",
+        ".protocol.export_header": False,
+        ".protocol.options": False,
+        ".protocol.file_name_prefix": "",
+        ".exported": False,
+        ".exported.export_macro": "",
+        ".exported.export_header": False,
+        ".lib": False,
+        ".lib.export_macro": "",
+        ".lib.export_header": False,
+        ".crdtp": False,
+        ".crdtp.dir": os.path.join(inspector_protocol_dir, "crdtp"),
+        ".crdtp.namespace": "crdtp",
     }
     for key_value in config_values:
       parts = key_value.split("=")
       if len(parts) == 2:
         defaults["." + parts[0]] = parts[1]
-    return (jinja_dir, config_file, init_defaults(config_partial, "", defaults))
+    return (jinja_dir, config_file, init_defaults(config_partial, "",
+                                                  defaults), depfile, stamp)
   except Exception:
     # Work with python 2 and 3 http://docs.python.org/py3k/howto/pyporting.html
     exc = sys.exc_info()[1]
@@ -279,32 +286,32 @@ def create_any_type_definition():
 def create_string_type_definition():
   # pylint: disable=W0622
   return {
-    "return_type": "String",
-    "pass_type": "const String&",
-    "to_pass_type": "%s",
-    "to_raw_type": "%s",
-    "to_rvalue": "%s",
-    "type": "String",
-    "raw_type": "String",
-    "raw_pass_type": "const String&",
-    "raw_return_type": "String",
-    "is_primitive": True
+      "return_type": "String",
+      "pass_type": "const String&",
+      "to_pass_type": "%s",
+      "to_raw_type": "%s",
+      "to_rvalue": "%s",
+      "type": "String",
+      "raw_type": "String",
+      "raw_pass_type": "const String&",
+      "raw_return_type": "String",
+      "is_primitive": True
   }
 
 
 def create_binary_type_definition():
   # pylint: disable=W0622
   return {
-    "return_type": "Binary",
-    "pass_type": "const Binary&",
-    "to_pass_type": "%s",
-    "to_raw_type": "%s",
-    "to_rvalue": "%s",
-    "type": "Binary",
-    "raw_type": "Binary",
-    "raw_pass_type": "const Binary&",
-    "raw_return_type": "Binary",
-    "is_primitive": True
+      "return_type": "Binary",
+      "pass_type": "const Binary&",
+      "to_pass_type": "%s",
+      "to_raw_type": "%s",
+      "to_rvalue": "%s",
+      "type": "Binary",
+      "raw_type": "Binary",
+      "raw_pass_type": "const Binary&",
+      "raw_return_type": "Binary",
+      "is_primitive": True
   }
 
 
@@ -326,17 +333,17 @@ def create_primitive_type_definition(type):
     "boolean": "TypeBoolean",
   }
   return {
-    "return_type": typedefs[type],
-    "pass_type": typedefs[type],
-    "to_pass_type": "%s",
-    "to_raw_type": "%s",
-    "to_rvalue": "%s",
-    "type": typedefs[type],
-    "raw_type": typedefs[type],
-    "raw_pass_type": typedefs[type],
-    "raw_return_type": typedefs[type],
-    "default_value": defaults[type],
-    "is_primitive": True
+      "return_type": typedefs[type],
+      "pass_type": typedefs[type],
+      "to_pass_type": "%s",
+      "to_raw_type": "%s",
+      "to_rvalue": "%s",
+      "type": typedefs[type],
+      "raw_type": typedefs[type],
+      "raw_pass_type": typedefs[type],
+      "raw_return_type": typedefs[type],
+      "default_value": defaults[type],
+      "is_primitive": True
   }
 
 def wrap_array_definition(type):
@@ -360,6 +367,7 @@ class Protocol(object):
   def __init__(self, config):
     self.config = config
     self.json_api = {"domains": []}
+    self.source_set = set()
     self.imported_domains = []
     self.exported_domains = []
     self.generate_domains = self.read_protocol_file(config.protocol.path)
@@ -381,7 +389,8 @@ class Protocol(object):
 
   def read_protocol_file(self, file_name):
     input_file = open(file_name, "r")
-    parsed_json = pdl.loads(input_file.read(), file_name)
+    parsed_json = pdl.loads(input_file.read(), file_name, False,
+                            self.source_set)
     input_file.close()
     version = '%s.%s' % (parsed_json["version"]["major"],
                          parsed_json["version"]["minor"])
@@ -435,10 +444,12 @@ class Protocol(object):
       domain_match = re.match(r'^(.*)[.]', ref)
       if domain_match:
         referenced_domain_name = domain_match.group(1)
-        if referenced_domain_name != domain['domain'] and not referenced_domain_name in dependencies:
-            sys.stderr.write(("Domains [%s] uses type [%s] from domain [%s], but did not declare the dependency\n\n"
-                    ) % (domain["domain"], ref, referenced_domain_name))
-            exit(1)
+        if referenced_domain_name != domain[
+            'domain'] and not referenced_domain_name in dependencies:
+          sys.stderr.write((
+              "Domains [%s] uses type [%s] from domain [%s], but did not declare the dependency\n\n"
+          ) % (domain["domain"], ref, referenced_domain_name))
+          exit(1)
 
   def generate_used_types(self):
     all_refs = set()
@@ -543,10 +554,10 @@ class Protocol(object):
     return self.type_definitions[prop["type"]]
 
   def optional_type(self, prop):
-      type = self.resolve_type(prop)
-      template = ("std::optional<{}>" if type.get('is_primitive', False)
-                  else "std::unique_ptr<{}>")
-      return template.format(type.get("raw_type"))
+    type = self.resolve_type(prop)
+    template = ("std::optional<{}>"
+                if type.get('is_primitive', False) else "std::unique_ptr<{}>")
+    return template.format(type.get("raw_type"))
 
   def generate_command(self, domain, command):
     if not self.config.protocol.options:
@@ -600,9 +611,10 @@ class Protocol(object):
 
 
 def main():
-  jinja_dir, config_file, config = read_config()
+  jinja_dir, config_file, config, deps_filename, stamp_filename = read_config()
 
   protocol = Protocol(config)
+  source_set = protocol.source_set
 
   if not config.exported and len(protocol.exported_domains):
     sys.stderr.write(("Domains [%s] are exported, but config is missing export "
@@ -648,18 +660,24 @@ def main():
     }
 
     if domain["domain"] in protocol.generate_domains:
-      outputs[os.path.join(config.protocol.output, to_file_name(
-          config, file_name + ".h"))] = h_template.render(template_context)
-      outputs[os.path.join(config.protocol.output, to_file_name(
-          config, file_name + ".cpp"))] = cpp_template.render(template_context)
+      output_h = os.path.join(config.protocol.output,
+                              to_file_name(config, file_name + ".h"))
+      outputs[output_h] = h_template.render(template_context)
+      source_set.add(h_template.filename)
+      output_cpp = os.path.join(config.protocol.output,
+                                to_file_name(config, file_name + ".cpp"))
+      outputs[output_cpp] = cpp_template.render(template_context)
+      source_set.add(cpp_template.filename)
       if domain["domain"] in protocol.exported_domains:
-        outputs[os.path.join(config.exported.output, to_file_name(
-            config, file_name + ".h"))] = exported_template.render(
-                template_context)
+        output_export_h = os.path.join(config.exported.output,
+                                       to_file_name(config, file_name + ".h"))
+        outputs[output_export_h] = exported_template.render(template_context)
+        source_set.add(exported_template.filename)
     if domain["domain"] in protocol.imported_domains:
-      outputs[os.path.join(config.protocol.output, to_file_name(
-          config, file_name + ".h"))] = imported_template.render(
-              template_context)
+      output_import_h = os.path.join(config.protocol.output,
+                                     to_file_name(config, file_name + ".h"))
+      outputs[output_import_h] = imported_template.render(template_context)
+      source_set.add(imported_template.filename)
 
   if config.lib:
     template_context = {
@@ -681,19 +699,19 @@ def main():
 
     if not config.use_embedder_types:
       protocol_h_templates += [
-        "Values_h.template",
-        "Object_h.template",
-        "ValueConversions_h.template",
+          "Values_h.template",
+          "Object_h.template",
+          "ValueConversions_h.template",
       ]
       protocol_cpp_templates += [
-        "Protocol_cpp.template",
-        "Values_cpp.template",
-        "Object_cpp.template",
-        "ValueConversions_cpp.template",
+          "Protocol_cpp.template",
+          "Values_cpp.template",
+          "Object_cpp.template",
+          "ValueConversions_cpp.template",
       ]
     else:
       protocol_h_templates += [
-        "Forward_h.template",
+          "Forward_h.template",
       ]
 
     def generate_lib_file(file_name, template_files):
@@ -702,6 +720,7 @@ def main():
         inputs.append(os.path.join(lib_templates_dir, template_file))
         template = jinja_env.get_template("lib/" + template_file)
         parts.append(template.render(template_context))
+        source_set.add(template.filename)
       outputs[file_name] = "\n\n".join(parts)
 
     generate_lib_file(os.path.join(config.lib.output, to_file_name(
@@ -710,20 +729,13 @@ def main():
         config, "Protocol.h")), protocol_h_templates)
 
     if not config.use_embedder_types:
-      generate_lib_file(os.path.join(config.lib.output, to_file_name(
-          config, "Protocol.cpp")), protocol_cpp_templates)
+      generate_lib_file(
+          os.path.join(config.lib.output, to_file_name(config, "Protocol.cpp")),
+          protocol_cpp_templates)
 
-  # Make gyp / make generatos happy, otherwise make rebuilds world.
-  inputs_ts = max(map(os.path.getmtime, inputs))
-  up_to_date = True
-  for output_file in outputs.keys():
-    if (not os.path.exists(output_file)
-        or os.path.getmtime(output_file) < inputs_ts):
-      up_to_date = False
-      break
-  if up_to_date:
-    sys.exit()
-
+  if stamp_filename:
+    with open(stamp_filename, "w"):
+      pass
   for file_name, content in outputs.items():
     # Remove output file first to account for potential case changes.
     try:
@@ -733,6 +745,12 @@ def main():
     out_file = open(file_name, "w")
     out_file.write(content)
     out_file.close()
+
+  if deps_filename:
+    assert stamp_filename
+    with open(deps_filename, "w") as deps_file:
+      deps_file.write("%s: %s\n" %
+                      (stamp_filename, " ".join(sorted(source_set))))
 
 
 if __name__ == "__main__":
