dd4e2b4de7f71f09be057578087903de775ce906
Lei Zhang <thestig@chromium.org>
2026-06-17 17:07:13 -0700

    [backport] CVE-2026-17679: Insufficient validation of untrusted input in Print Preview (1/2)
    Print Preview: Move preview_ui_id field out of PrintParams mojo struct
    
    This field in the printing.mojom.PrintParams struct is only read/written
    by PrintRenderFrameHelper in the renderer process. Since it does not
    need to be transported via IPC to the browser process, remove it and
    make it a member variable in PrintRenderFrameHelper instead.
    
    Refactor PrintRenderFrameHelper::SetPrintPagesParams() to preserve the
    pre-existing behavior, where it is only set for Print Preview using the
    browser-provided value, or set to the default -1 value when printing.
    To make it clear when it's Print Preview vs. printing, split
    SetPrintPagesParams() into SetPrintPagesParamsForPrintPreview() and
    SetPrintPagesParamsForPrinting().
    
    This is a cleanup CL in preparation for changing the field from an int
    to a base::UnguessableToken.
    
    Bug: 516430649
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7952269
    Task-number: QTBUG-148657
    Change-Id: I41fed8f17f5a1b2164df4edea3dcce5eadfa95bb
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/761857
    Reviewed-by: Allan Sandfeld Jensen <allan.jensen@qt.io>

--- chromium/components/printing/renderer/print_render_frame_helper.cc
+++ chromium/components/printing/renderer/print_render_frame_helper.cc
@@ -1368,7 +1368,7 @@ void PrintRenderFrameHelper::PrintWithParams(
                            mojom::SkiaDocumentType::kMSKP
                        ? DebugEvent::kSetPrintSettings1
                        : DebugEvent::kSetPrintSettings2);
-  SetPrintPagesParams(*settings);
+  SetPrintPagesParamsForPrinting(*settings);
   prep_frame_view_ =
       std::make_unique<PrepareFrameAndViewForPrint>(frame, plugin_node);
   prep_frame_view_->EnterPrintMode(*settings->params,
@@ -2145,7 +2145,7 @@ void PrintRenderFrameHelper::Print(blink::WebLocalFrame* frame,
                              mojom::SkiaDocumentType::kMSKP
                          ? DebugEvent::kSetPrintSettings3
                          : DebugEvent::kSetPrintSettings4);
-    SetPrintPagesParams(*print_settings);
+    SetPrintPagesParamsForPrinting(*print_settings);
   }
 
   // Render Pages for printing.
@@ -2223,6 +2223,9 @@ void PrintRenderFrameHelper::DidFinishPrinting(PrintingResult result) {
 void PrintRenderFrameHelper::Reset() {
   prep_frame_view_.reset();
   print_pages_params_.reset();
+#if BUILDFLAG(ENABLE_PRINT_PREVIEW)
+  preview_ui_id_ = -1;
+#endif
   notify_browser_of_print_failure_ = true;
   snapshotter_.reset();
 
@@ -2411,7 +2414,7 @@ bool PrintRenderFrameHelper::InitPrintSettings(blink::WebLocalFrame* frame,
                            mojom::SkiaDocumentType::kMSKP
                        ? DebugEvent::kSetPrintSettings5
                        : DebugEvent::kSetPrintSettings6);
-  SetPrintPagesParams(settings);
+  SetPrintPagesParamsForPrinting(settings);
   return true;
 }
 
@@ -2470,8 +2473,6 @@ bool PrintRenderFrameHelper::UpdatePrintSettings(
     return false;
   }
 
-  settings->params->preview_ui_id = job_settings->FindInt(kPreviewUIID).value();
-
   // Validate expected print preview settings.
   settings->params->is_first_request =
       job_settings->FindBool(kIsFirstRequest).value();
@@ -2487,7 +2488,8 @@ bool PrintRenderFrameHelper::UpdatePrintSettings(
                            mojom::SkiaDocumentType::kMSKP
                        ? DebugEvent::kSetPrintSettings7
                        : DebugEvent::kSetPrintSettings8);
-  SetPrintPagesParams(*settings);
+  SetPrintPagesParamsForPrintPreview(
+      *settings, job_settings->FindInt(kPreviewUIID).value());
   return true;
 }
 #endif  // BUILDFLAG(ENABLE_PRINT_PREVIEW)
@@ -2513,6 +2515,9 @@ mojom::PrintPagesParamsPtr PrintRenderFrameHelper::GetPrintSettingsFromUser(
   GetPrintManagerHost()->DidShowPrintDialog();
 
   print_pages_params_.reset();
+#if BUILDFLAG(ENABLE_PRINT_PREVIEW)
+  preview_ui_id_ = -1;
+#endif
 
   mojom::PrintPagesParamsPtr print_settings;
   GetPrintManagerHost()->ScriptedPrint(std::move(params), &print_settings);
@@ -2754,9 +2759,8 @@ bool PrintRenderFrameHelper::CheckForCancel() {
   const mojom::PrintParams& print_params = *print_pages_params_->params;
   bool cancel = false;
 
-  if (!GetPrintManagerHost()->CheckForCancel(print_params.preview_ui_id,
-                                             print_params.preview_request_id,
-                                             &cancel)) {
+  if (!GetPrintManagerHost()->CheckForCancel(
+          preview_ui_id_, print_params.preview_request_id, &cancel)) {
     cancel = true;
   }
 
@@ -3096,11 +3100,25 @@ void PrintRenderFrameHelper::PrintPreviewContext::CalculatePluginAttributes() {
                                   : DebugEvent::kPrintPreviewIsNotModifiable);
 }
 
-void PrintRenderFrameHelper::SetPrintPagesParams(
+void PrintRenderFrameHelper::SetPrintPagesParamsForPrinting(
     const mojom::PrintPagesParams& settings) {
   CHECK(PrintMsgPrintParamsIsValid(*settings.params));
   print_pages_params_ = settings.Clone();
+#if BUILDFLAG(ENABLE_PRINT_PREVIEW)
+  // Invalid ID since this is not for Print Preview.
+  preview_ui_id_ = -1;
+#endif
 }
+
+#if BUILDFLAG(ENABLE_PRINT_PREVIEW)
+void PrintRenderFrameHelper::SetPrintPagesParamsForPrintPreview(
+    const mojom::PrintPagesParams& settings,
+    int preview_ui_id) {
+  CHECK(PrintMsgPrintParamsIsValid(*settings.params));
+  print_pages_params_ = settings.Clone();
+  preview_ui_id_ = preview_ui_id;
+}
+#endif
 
 void PrintRenderFrameHelper::QuitScriptedPrintPreviewRunLoop() {
   closures_for_mojo_responses_->RunScriptedPrintPreviewQuitClosure();
