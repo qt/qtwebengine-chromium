73b454e13f2763e2bd039999c586c54e2e300f51
Moss Heim <moss.heim@qt.io>
2026-06-18 13:57:22 +0200

    [Backport] CVE-2026-11055: Use after free in ANGLE
    Cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/angle/angle/+/7765258:
    D3D11: Avoid cache self-eviction for uniform buffers.
    
    D3D11 makes copies of buffers to handle different binding
    offsets since they are not natively supported. It limits the cache to
    the buffer's size * 2. glBindBufferRange allows giving sizes larger than
    the buffer, this can cause the D3D11 buffer storage to trim even with
    one element, evicting the newly allocated buffer.
    
    Avoid this by checking if the buffer to evict was the newly inserted
    one. It would always be the last element in the cache because it has the
    most recent LRU value.
    
    Fixed: chromium:498881735
    Change-Id: Ie235f1c3efa6b3a2395bbfdf6b165c4c42d95fdc
    Reviewed-on: https://chromium-review.googlesource.com/c/angle/angle/+/7765258
    Reviewed-by: Shahbaz Youssefi <syoussefi@chromium.org>
    Commit-Queue: Geoff Lang <geofflang@chromium.org>
    Task-number: QTBUG-147442
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/746447
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/third_party/angle/src/libANGLE/renderer/d3d/d3d11/Buffer11.cpp
+++ chromium/third_party/angle/src/libANGLE/renderer/d3d/d3d11/Buffer11.cpp
@@ -884,7 +884,14 @@ angle::Result Buffer11::getConstantBufferRangeStorage(const gl::Context *context
                     return a.second.lruCount < b.second.lruCount;
                 });
 
-            ASSERT(iter->second.storage != newStorage);
+            // Don't remove the newly added storage. This can only happen if it is the last entry
+            // since it has the most recent LRU value.
+            if (iter->second.storage == newStorage)
+            {
+                ASSERT(mConstantBufferRangeStoragesCache.size() == 1);
+                break;
+            }
+
             ASSERT(mConstantBufferStorageAdditionalSize >= iter->second.storage->getSize());
 
             mConstantBufferStorageAdditionalSize -= iter->second.storage->getSize();
@@ -951,7 +958,14 @@ angle::Result Buffer11::getStructuredBufferRangeSRV(const gl::Context *context,
                                              return a.second.lruCount < b.second.lruCount;
                                          });
 
-            ASSERT(iter->second.storage != newStorage);
+            // Don't remove the newly added storage. This can only happen if it is the last entry
+            // since it has the most recent LRU value.
+            if (iter->second.storage == newStorage)
+            {
+                ASSERT(mStructuredBufferRangeStoragesCache.size() == 1);
+                break;
+            }
+
             ASSERT(mStructuredBufferStorageAdditionalSize >= iter->second.storage->getSize());
 
             mStructuredBufferStorageAdditionalSize -= iter->second.storage->getSize();
