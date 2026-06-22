827e8da2906b13bdd469ebbe82f08ef7d66a5c14
Jakob Kummerow <jkummerow@chromium.org>
2026-06-22 10:59:08 +0200

    [backport] CVE-2026-17658: Use after free in V8
    Terminate any pending validation job...
    
    ...when receiving a ContextDisposedNotification.
    
    Bug: 523030583
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7967983
    Task-number: QTBUG-148656
    Change-Id: Iee732c5e62ca63f473548209f285049126a87a65
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/758627
    Reviewed-by: Kaloyan Chehlarski <kaloyan.chehlarski@qt.io>

--- chromium/v8/src/wasm/module-compiler.cc
+++ chromium/v8/src/wasm/module-compiler.cc
@@ -3185,6 +3185,13 @@ AsyncStreamingProcessor::AsyncStreamingProcessor(AsyncCompileJob* job)
       compilation_unit_builder_(nullptr) {}
 
 AsyncStreamingProcessor::~AsyncStreamingProcessor() {
+  // When we receive a ContextDisposedNotification before the stream ends,
+  // we might still have a running validation job. Terminate that before
+  // freeing {validate_functions_job_data_}.
+  if (validate_functions_job_handle_) {
+    validate_functions_job_handle_->Cancel();
+  }
+
   if (owns_cache_entry_) {
     GetWasmEngine()->StreamingCompilationFailed(prefix_hasher_.hash(),
                                                 job_->compile_imports_);
