6ec876a0bedd9808225f81cbfa891e368aaeccee
Anders Hartvoll Ruud <andruud@chromium.org>
2026-06-23 15:31:28 +0200

    [Backport] CVE-2026-11076: Type Confusion in CSS (2/2)
    Cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/7748546:
    Perform rule replacement on immediate parent only
    
    After landing CL:7743265, I realized that our current behavior
    of starting the "rule replacement search" from the top of the stylesheet
    (or root of the detached rule tree) doesn't make sense: The rule we're
    looking for must exist in the immediate parent object of the rule
    being replaced. Therefore, this CL avoids the recursive search
    and instead just looks in the parent rule/sheet.
    
    The lack of unification between style rules (which can have children)
    and grouping rules (which can also have children) continues to make
    this more annoying than it should be, but addressing that is out
    of scope.
    
    This CL also fixes a problem similar to the original (Issue 499784386)
    in CSSScopeRule. For a detached rule tree, we would fail to update
    the child vector of the parent rule.
    
    As a minor bonus, the "position hint" optimization now works on any
    nesting level.
    
    Bug: 499784386
    Change-Id: I0164380dc7caf80bc6f4b06bc22661dcc19faa6b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/7748546
    Commit-Queue: Anders Hartvoll Ruud <andruud@chromium.org>
    Reviewed-by: Steinar H Gunderson <sesse@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1612809}
    Task-number: QTBUG-147441
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/747890
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/third_party/blink/renderer/core/css/style_rule.cc
+++ chromium/third_party/blink/renderer/core/css/style_rule.cc
@@ -21,6 +21,8 @@
 
 #include "third_party/blink/renderer/core/css/style_rule.h"
 
+#include <limits>
+
 #include "base/compiler_specific.h"
 #include "third_party/blink/renderer/core/css/cascade_layer.h"
 #include "third_party/blink/renderer/core/css/css_container_rule.h"
@@ -74,6 +76,7 @@
 #include "third_party/blink/renderer/platform/heap/visitor.h"
 #include "third_party/blink/renderer/platform/instrumentation/use_counter.h"
 #include "third_party/blink/renderer/platform/wtf/size_assertions.h"
+#include "third_party/blink/renderer/platform/wtf/wtf_size_t.h"
 
 namespace blink {
 
@@ -549,11 +552,14 @@ MutableCSSPropertyValueSet& StyleRule::MutableProperties() {
   return *To<MutableCSSPropertyValueSet>(properties_.Get());
 }
 
-void StyleRule::ReplaceRuleIfExists(StyleRuleBase* old_rule,
-                                    StyleRuleBase* new_rule) {
+wtf_size_t StyleRule::ReplaceChildRuleIfExists(StyleRuleBase* old_rule,
+                                               StyleRuleBase* new_rule,
+                                               wtf_size_t position_hint) {
   if (child_rules_) {
-    ReplaceStyleRuleInVector(old_rule, new_rule, *child_rules_);
+    return ReplaceStyleRuleInVector(old_rule, new_rule, position_hint,
+                                    *child_rules_);
   }
+  return std::numeric_limits<wtf_size_t>::max();  // Not found.
 }
 
 void StyleRule::WrapperInsertRule(CSSStyleSheet* parent_sheet,
@@ -816,9 +822,11 @@ StyleRuleGroup::StyleRuleGroup(const StyleRuleGroup& group_rule)
   }
 }
 
-void StyleRuleGroup::ReplaceRuleIfExists(StyleRuleBase* old_rule,
-                                         StyleRuleBase* new_rule) {
-  ReplaceStyleRuleInVector(old_rule, new_rule, child_rules_);
+wtf_size_t StyleRuleGroup::ReplaceChildRuleIfExists(StyleRuleBase* old_rule,
+                                                    StyleRuleBase* new_rule,
+                                                    wtf_size_t position_hint) {
+  return ReplaceStyleRuleInVector(old_rule, new_rule, position_hint,
+                                  child_rules_);
 }
 
 void StyleRuleGroup::WrapperInsertRule(CSSStyleSheet* parent_sheet,
