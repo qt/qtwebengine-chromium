35ecc1a809eace3128650bad80f5b7dd21b637f8
Dave Tapuska <dtapuska@chromium.org>
2026-07-29 08:23:59 -0700

    [Backport] CVE-2026-87460: Use after free in Platform
    Cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/8162668:
    Prevent use-after-free in WidgetBase by adding weak pointer checks.
    
    In WidgetBase::DidBeginMainFrame and WidgetBase::ShowVirtualKeyboardOnElementFocus, synchronous calls to update text input state or show the virtual keyboard can result in the destruction of the WidgetBase instance. This CL adds weak pointer checks to safely return early if the instance is destroyed during these calls.
    
    BUG=540058837,540058837
    
    Change-Id: I9c87677f6ec186faaefda461f2a9f0ef49e19bb3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8162668
    Commit-Queue: Dave Tapuska <dtapuska@chromium.org>
    Reviewed-by: Vladimir Levin <vmpstr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1670274}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/768947
    Reviewed-by: Moss Heim <moss.heim@qt.io>

--- chromium/third_party/blink/renderer/platform/widget/widget_base.cc
+++ chromium/third_party/blink/renderer/platform/widget/widget_base.cc
@@ -683,7 +683,11 @@ void WidgetBase::OnCommitRequested() {
 }
 
 void WidgetBase::DidBeginMainFrame() {
+  base::WeakPtr<WidgetBase> weak_this = weak_ptr_factory_.GetWeakPtr();
   UpdateTextInputState();
+  if (!weak_this) {
+    return;
+  }
   client_->DidBeginMainFrame();
 }
 
@@ -1295,6 +1299,7 @@ void WidgetBase::ClearTextInputState() {
 }
 
 void WidgetBase::ShowVirtualKeyboardOnElementFocus() {
+  base::WeakPtr<WidgetBase> weak_this = weak_ptr_factory_.GetWeakPtr();
 #if BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_IOS_TVOS)
   // On ChromeOS, virtual keyboard is triggered only when users leave the
   // mouse button or the finger and a text input element is focused at that
@@ -1306,6 +1311,9 @@ void WidgetBase::ShowVirtualKeyboardOnElementFocus() {
 #else
   ShowVirtualKeyboard();
 #endif
+  if (!weak_this) {
+    return;
+  }
 
 // TODO(rouslan): Fix ChromeOS and Windows 8 behavior of autofill popup with
 // virtual keyboard.
