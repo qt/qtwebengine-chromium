0d55253a32504677bc916893716687ffb509690c
kylechar <kylechar@chromium.org>
2026-08-26 13:46:54 -0700

    [Backport] CVE-2026-85048: Use after free in Compositing
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/8297301:
    Fix re-entrancy bug in UpdateActivationDependencies()
    
    Surface::UpdateActivationDependencies() first removes all existing
    activation dependencies and clears blocking_allocation_groups_. It then
    does a loop through new activation dependencies that either activates
    dependent surfaces or registers the surface as a blocked embedder. If a
    surface S0 had two activation dependencies, S1 and S2, it could register
    as blocked embedder for S1 group G1 in the first loop iteration if S1
    didn't have an active frame. It would also store G1 in
    new_blocking_allocation_groups for S0. The second loop iteration could
    activate S2, which could in turn active S1, which allows G1 to be
    deleted if S1 is destroyed. The pointer to G1 is stored in
    blocking_allocation_groups_ for S0 at the end of
    UpdateActivationDependencies() when it shouldn't be. S0 won't be
    notified that G1 is being deleted and the pointer can go stale.
    
    The fix here is to split that problematic loop into two loops. The first
    loop updates last pending reference and maybe activates surfaces for the
    SurfaceAllocationGroup. The second loop checks for active surfaces and
    registers as a blocked embedder. Since the surface isn't registered as a
    blocked embedder when other surfaces activate it reduces reentrancy and
    prevents the stale pointer.
    
    A logic change is that UpdateLastPendingReferenceAndMaybeActivate() is
    now called for all activation dependencies, even when two of them have
    the same SurfaceAllocationGroup. Having a surface be blocked on
    activation of multiple surface in the same group, aka with the same
    embed token, doesn't make much sense but it's allowed by the code.
    
    Lastly change the DCHECK into a CHECK in
    OnActivationDependencyResolved(), that ensures dependency being removed
    was actually registered. It should always be the case now.
    
    Bug: 540357382
    Change-Id: I4bb5858c274c7d76f3da03fcabbf8f947ccd7750
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8297301
    Reviewed-by: Saifuddin Hitawala <hitawala@chromium.org>
    Commit-Queue: Kyle Charbonneau <kylechar@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1686642}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/768074
    Reviewed-by: Michal Klocek <michal.klocek@qt.io>

--- chromium/components/viz/service/surfaces/surface.cc
+++ chromium/components/viz/service/surfaces/surface.cc
@@ -360,8 +360,8 @@ bool Surface::RequestCopyOfOutputOnActiveFrameRenderPassId(
 void Surface::OnActivationDependencyResolved(
     const SurfaceId& activation_dependency,
     SurfaceAllocationGroup* group) {
-  DCHECK(activation_dependencies_.count(activation_dependency));
-  activation_dependencies_.erase(activation_dependency);
+  size_t erased = activation_dependencies_.erase(activation_dependency);
+  CHECK_EQ(erased, 1u);
   blocking_allocation_groups_.erase(group);
   if (!activation_dependencies_.empty())
     return;
@@ -695,6 +695,22 @@ void Surface::UpdateActivationDependencies(
     return;
   }
 
+  // Update the last pending reference in each allocation group. This
+  // may trigger fallback or deadline inheritance activations in those
+  // allocation groups. This surface isn't registered as a blocked embedder
+  // while this happens to avoid re-entrancy.
+  for (const SurfaceId& surface_id :
+       current_frame.metadata.activation_dependencies) {
+    SurfaceAllocationGroup* group =
+        surface_manager_->GetOrCreateAllocationGroupForSurfaceId(surface_id);
+    if (group) {
+      group->UpdateLastPendingReferenceAndMaybeActivate(surface_id);
+    }
+  }
+
+  // Now that all surface activations have happened, inspect which
+  // dependencies remain unresolved and register as a blocked embedder only for
+  // those.
   base::flat_set<raw_ptr<SurfaceAllocationGroup, CtnExperimental>>
       new_blocking_allocation_groups;
   std::vector<SurfaceId> new_activation_dependencies;
@@ -704,8 +720,7 @@ void Surface::UpdateActivationDependencies(
         surface_manager_->GetOrCreateAllocationGroupForSurfaceId(surface_id);
     if (base::Contains(new_blocking_allocation_groups, group))
       continue;
-    if (group)
-      group->UpdateLastPendingReferenceAndMaybeActivate(surface_id);
+
     Surface* dependency = surface_manager_->GetSurfaceForId(surface_id);
     if (dependency && dependency->HasActiveFrame()) {
       // Normally every creation of SurfaceAllocationGroup should be followed by
