d28c418ecd055a657eeb0c102933e31b66723c28
Stefan Zager <szager@chromium.org>
2026-08-26 19:15:26 -0700

    [Backport] CVE-2026-91731: Type confusion in Compositing
    Manual cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/chromium/src/+/8301512:
    Prevent overflow of cc::Layer ID's.
    
    Overflowing a 32-bit integer is pathological so just crash.
    
    Fixed: 523554372
    Change-Id: Iadc3ce18060cce112f161679f9491d1cc4b2f64b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/8301512
    Auto-Submit: Stefan Zager <szager@chromium.org>
    Commit-Queue: Stefan Zager <szager@chromium.org>
    Reviewed-by: Philip Rogers <pdr@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1686872}
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/772326
    Reviewed-by: Moss Heim <moss.heim@qt.io>

--- chromium/cc/layers/layer.cc
+++ chromium/cc/layers/layer.cc
@@ -8,6 +8,7 @@
 #include <stdint.h>
 
 #include <algorithm>
+#include <limits>
 #include <string>
 #include <unordered_set>
 #include <utility>
@@ -95,7 +96,9 @@ Layer::LayerTreeInputs::~LayerTreeInputs() = default;
 
 int Layer::GetNextLayerId() {
   // Layer IDs start from 1.
-  return g_next_layer_id.GetNext() + 1;
+  int prev = g_next_layer_id.GetNext();
+  CHECK_LT(prev, std::numeric_limits<int>::max());
+  return prev + 1;
 }
 
 scoped_refptr<Layer> Layer::Create() {
