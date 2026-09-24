aa5387c3f35e77b4b09918728153ef08b474383d
Jakob Kummerow <jkummerow@chromium.org>
2026-09-24 14:25:08 +0200

    [Backport] CVE-2026-95306: Type confusion in V8 (2/4)
    Cherry-pick of patch originally reviewed on
    https://chromium-review.googlesource.com/c/v8/v8/+/7660963:
    [wasm][arm64] Really fix walking WasmLiftoffSetupFrame
    
    The recent fix crrev.com/c/7653477 wasn't entirely correct on arm64:
    it got the offset wrong, and it didn't account for pointer authentication.
    
    No-Tree-Checks: true
    Bug: 490980546
    Change-Id: I6745b7c69e4636b4cdb91fe3757f6fdca4b9cb15
    Reviewed-on: https://chromium-review.googlesource.com/c/v8/v8/+/7660963
    Auto-Submit: Jakob Kummerow <jkummerow@chromium.org>
    Reviewed-by: Matthias Liedtke <mliedtke@chromium.org>
    Commit-Queue: Jakob Kummerow <jkummerow@chromium.org>
    Commit-Queue: Matthias Liedtke <mliedtke@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#105756}
    Task-number: QTBUG-150731
    Reviewed-on: https://codereview.qt-project.org/c/qt/qtwebengine-chromium/+/774198
    Reviewed-by: Michael Brüning <michael.bruning@qt.io>

--- chromium/v8/src/execution/frames.cc
+++ chromium/v8/src/execution/frames.cc
@@ -3864,8 +3864,8 @@ FullObjectSlot WasmLiftoffSetupFrame::wasm_instance_data_slot() const {
 }
 
 Address WasmLiftoffSetupFrame::CallingPC() const {
-  return Memory<Address>(fp() +
-                         WasmLiftoffSetupFrameConstants::kCallingPCOffset);
+  return ReadPC(reinterpret_cast<Address*>(
+      fp() + WasmLiftoffSetupFrameConstants::kCallingPCOffset));
 }
 
 void WasmLiftoffSetupFrame::Iterate(RootVisitor* v) const {
